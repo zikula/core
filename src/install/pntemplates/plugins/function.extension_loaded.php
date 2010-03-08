de4cac93bf074fcd43fa068e283baa147d0a764f
Drak <drak@zikula.org>
2010-03-08 12:54:10 +0000

    Added check for PDO on install.

---
+++ src/install/pntemplates/plugins/function.extension_loaded.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2010 Zikula Foundation
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ */
+
+function smarty_function_extension_loaded($params, &$smarty)
+{
+    if (!isset($params['extension'])) {
+        return false;
+    }
+
+    // check if the file exists
+    $result = extension_loaded($params['extension_loaded']);
+
+    if (isset($params['assign'])) {
+        $smarty->assign($params['assign'], $result);
+    } else {
+        return $result;
+    }
+}
