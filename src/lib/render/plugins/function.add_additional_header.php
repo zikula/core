0816aa4f2a846775f6eea35cbf23871c25209dbb
Robert M. Burkhead <rmburkhead@rmburkhead.com>
2010-03-12 09:52:42 +0000

    Update plugin docs. Refs #1621

--- src/lib/render/plugins/function.add_additional_header.php
+++ src/lib/render/plugins/function.add_additional_header.php
@@ -13,22 +13,25 @@
  */
 
 /**
- * Smarty function to add additional information to the <head> </head>
- * section of a Zikula document
+ * Add additional information to the <head>...</head> section of a Zikula
+ * document
  *
- * Available parameters:
- *   - header:   If set, the value is assigned to the global
- *               $additional_header array.  The value can be a single
- *               string or an array of strings.
+ * Available attributes:
+ *   - header (string|array) If set, the value is assigned to the global
+ *                           <var>$additional_header array</var>.  The value can
+ *                           be a single string or an array of strings.
  *
- * Example
- *   <!--[add_additional_header header='<title>This is the title</title>']-->
+ * Examples:
+ * 
+ * <samp>{add_additional_header header='<title>This is the title</title>'}</samp>
+ * 
  *  OR
- *   <!--[add_additional_header header=$title]-->
+ * 
+ * <samp>{add_additional_header header=$title}</samp>
  *
- * @param        array       $params      All attributes passed to this function from the template
- * @param        object      &$smarty     Reference to the Smarty object
- * @return       string      the value of the last status message posted, or void if no status message exists
+ * @param   array   $params      All attributes passed to this function from the template
+ * @param   Smarty  &$smarty     Reference to the {@link Renderer} object
+ * @return  null    (The value is added to the <head> section of the document.)
  */
 function smarty_function_add_additional_header($args, &$smarty)
 {
