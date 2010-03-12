a49eb95b1a93892bcf693aeefc257137dece5fd4
Robert M. Burkhead <rmburkhead@rmburkhead.com>
2010-03-12 09:51:18 +0000

    Update plugin docs. Refs #1621

--- src/lib/render/plugins/block.case.php
+++ src/lib/render/plugins/block.case.php
@@ -15,30 +15,34 @@
 /**
  * Smarty case block to implement switchs in a template
  *
- * available parameters:
- *  - params.expr  string|numeric to be tested
- *  - content      contents of the block
+ * Available attributes:
+ *  - expr (string|numeric) the value to be tested against the expr provided in
+ *    the {@link smarty_block_switch() switch} tag.
  *
- * Example
- * <!--[switch expr=$var]-->
- *   <!--[case expr='1']-->
+ * Example:
+ * <pre>
+ * {switch expr=$var}
+ *   {case expr='1'}
  *     do some stuff for case $var == '1'
- *   <!--[/case]-->
- *   <!--[case expr='2']-->
+ *   {/case}
+ *   {case expr='2'}
  *     do some stuff for case $var == '2'
- *   <!--[/case]-->
- *   <!--[case]-->
+ *   {/case}
+ *   {case}
  *     default stuff
- *   <!--[/case]-->
- * <!--[/switch]-->
+ *   {/case}
+ * {/switch}
+ * </pre>
  *
  * @author   messju mohr <messju@lammfellpuschen.de>
  * @author   slightly modified and expanded by dasher <dasher@inspiredthinking.co.uk>
  * @see      smarty_block_switch
- * @param    array    $params     All attributes passed to this function from the template
- * @param    string   $content    The content between the block tags
- * @param    object   $smarty     Reference to the Smarty object
- * @return   string   content of the matching case
+ * 
+ * @param   array   $params     All attributes passed to this function from the template
+ * @param   string  $content    The content between the block tags
+ * @param   Smarty  &$smarty    Reference to the {@link Renderer} object
+ * @param   bool    &$repeat    Controls block repetition. See {@link http://www.smarty.net/manual/en/plugins.block.functions.php Smarty - Block Functions}.
+ * @return  string  The content of the matching case
  */
 function smarty_block_case($params, $content, &$smarty, &$repeat)
 {
@@ -86,6 +90,15 @@ function smarty_block_case($params, $content, &$smarty, &$repeat)
 
 }
 
+/**
+ * <i>Internal function, not meant to be called from outside {@link smarty_block_case()}.</i>
+ *
+ * Evaluates and returns the expression by echoing the expression into and
+ * capturing an output buffer.
+ *
+ * @param   string|numeric  $expression the case expression to evaluate
+ * @return  string          The result of capturing the trimmed and echoed expression from the output buffer.
+ */
 function smarty_block_case_eval($expression = '')
 {
     // Evaluates the expression
