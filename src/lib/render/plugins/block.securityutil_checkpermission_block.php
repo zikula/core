4aa7484c9eb6f15c63f02851009dfcacdd99c435
Robert M. Burkhead <rmburkhead@rmburkhead.com>
2010-03-12 09:52:23 +0000

    Update plugin docs. Refs #1621

--- src/lib/render/plugins/block.securityutil_checkpermission_block.php
+++ src/lib/render/plugins/block.securityutil_checkpermission_block.php
@@ -13,22 +13,26 @@
  */
 
 /**
- * Smarty block to implement PN permissions checks in a template
+ * Implement permissions checks in a template
  *
- * available parameters:
- *  - component   the component under test
- *  - instance    the instance under test
- *  - level       the level of access required
+ * Available attributes:
+ *  - component (string) The component to be tested, e.g., 'ModuleName::'
+ *  - instance  (string) The instance to be tested, e.g., 'name::1'
+ *  - level     (int)    The level of access required, e.g., ACCESS_READ
  *
- * Example
- * <!--[securityutil_checkpermission_block component='News::' instance='1::' level=ACCESS_COMMENT]-->
- * do some stuff now we have permission
- * <!--[/securityutil_checkpermission_block]-->
+ * Example:
+ * <pre>
+ * {securityutil_checkpermission_block component='News::' instance='1::' level=ACCESS_COMMENT}
+ *   do some stuff now that we have permission
+ * {/securityutil_checkpermission_block}
+ * </pre>
  *
- * @param    array    $params     All attributes passed to this function from the template
- * @param    string   $content    The content between the block tags
- * @param    object   $smarty     Reference to the Smarty object
- * @return   mixed    the content if permission is held, null if no permissions is held (or on the opening tag), false on an error
+ * @param   array   $params     All attributes passed to this function from the template
+ * @param   string  $content    The content between the block tags
+ * @param   Smarty  &$smarty    Reference to the {@link Renderer} object
+ * @return  mixed   The content of the block, if the user has the specified
+ *                  access level for the component and instance;
+ *                  otherwise null; false on an error
  */
 function smarty_block_securityutil_checkpermission_block($params, $content, &$smarty)
 {
