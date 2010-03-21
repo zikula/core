9e43503ffb6ec2da7f17be336900904d3b068bf2
Robert M. Burkhead <rmburkhead@rmburkhead.com>
2010-03-21 16:24:48 +0000

    Replace $HTTP_SESSION_VARS with $_SESSION; Reference to pnRender now Theme; fix variable references. Fixes #1900

--- src/lib/render/plugins/function.debugenvironment.php
+++ src/lib/render/plugins/function.debugenvironment.php
@@ -27,25 +27,20 @@
  */
 function smarty_function_debugenvironment($params, &$smarty)
 {
-    global $HTTP_SESSION_VARS;
-
-    $allvars = $HTTP_SESSION_VARS;
-    $smarty->assign('_ZSession_keys', array_keys($allvars) );
-    $smarty->assign('_ZSession_vals', array_values($allvars) );
+    $smarty->assign('_ZSession_keys', array_keys($_SESSION) );
+    $smarty->assign('_ZSession_vals', array_values($_SESSION) );
 
     $smarty->assign('_smartyversion', $smarty->_version);
-    $_pnrender = pnModGetInfo(pnModGetIDFromName('pnRender'));
-    $smarty->assign('_pnrenderversion', $_pnrender['version']);
     $_theme = pnModGetInfo(pnModGetIDFromName('Theme'));
     $smarty->assign('_themeversion', $_theme['version']);
 
-    $smarty->assign('_force_compile', (pnModGetVar('pnRender', 'force_compile')) ? __('On') : __('Off'));
-    $smarty->assign('_compile_check', (pnModGetVar('pnRender', 'compile_check')) ? __('On') : __('Off'));
+    $smarty->assign('_force_compile', (pnModGetVar('Theme', 'force_compile')) ? __('On') : __('Off'));
+    $smarty->assign('_compile_check', (pnModGetVar('Theme', 'compile_check')) ? __('On') : __('Off'));
 
     $smarty->assign('_baseurl', pnGetBaseURL());
     $smarty->assign('_baseuri', pnGetBaseURI());
 
-    $smarty->assign('template', $smarty->_plugins['function']['pndebug'][1]);
-    $smarty->assign('_path',    $smarty->get_template_path($smarty->_plugins['function']['ZDebug'][1]));
-    $smarty->assign('_line',    $smarty->_plugins['function']['pndebug'][2]);
+    $smarty->assign('_template', $smarty->_plugins['function']['zdebug'][1]);
+    $smarty->assign('_path',    $smarty->get_template_path($smarty->_plugins['function']['zdebug'][1]));
+    $smarty->assign('_line',    $smarty->_plugins['function']['zdebug'][2]);
 }
