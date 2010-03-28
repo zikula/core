1f2db7ff6dab6f6b9e982b05fc11a7c0a8cf83a1
Drak <drak@zikula.org>
2010-03-28 03:58:39 +0000

    Adding example facad ftp to allow simply unit testing refs #1517.

---
+++ src/lib/FileSystem/Facad/Ftp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2009-2010 Zikula Foundation - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * FileSystem_Facad_Ftp is a facad interface for FTP connections.
+ */
+class FileSystem_Facad_Ftp
+{
+    public static function ftp_put($ftp_stream, $remote_file, $local_file, $mode, $startpos=0)
+    {
+        return ftp_put($ftp_stream, $remote_file, $local_file, $mode, $startpos);
+    }
+}
