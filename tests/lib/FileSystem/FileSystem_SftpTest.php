ade309a0f1f257704bfd1aaf68b69c62b2096451
Drak <drak@zikula.org>
2010-03-28 06:55:44 +0000

    Implement new interfaces refs #1517.

--- tests/lib/FileSystem/FileSystem_SftpTest.php
+++ tests/lib/FileSystem/FileSystem_SftpTest.php
@@ -3,18 +3,18 @@ require_once dirname(__FILE__) . '/../../bootstrap.php';
 require_once dirname(__FILE__) . '/../../../src/lib/FileSystem/Interface.php';
 require_once dirname(__FILE__) . '/../../../src/lib/FileSystem/Error.php';
 require_once dirname(__FILE__) . '/../../../src/lib/FileSystem/Driver.php';
-require_once dirname(__FILE__) . '/../../../src/lib/FileSystem/SFtp.php';
+require_once dirname(__FILE__) . '/../../../src/lib/FileSystem/Sftp.php';
 
 /**
- * FileSystem_SFtp test case.
+ * FileSystem_Sftp test case.
  */
-class FileSystem_SFtpTest extends PHPUnit_Framework_TestCase
+class FileSystem_SftpTest extends PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var FileSystem_SFtp
+     * @var FileSystem_Sftp
      */
-    private $FileSystem_SFtp;
+    private $FileSystem_Sftp;
 
     /**
      * Prepares the environment before running a test.
@@ -23,10 +23,10 @@ class FileSystem_SFtpTest extends PHPUnit_Framework_TestCase
     {
         parent::setUp();
 
-        // TODO Auto-generated FileSystem_SFtpTest::setUp()
+        // TODO Auto-generated FileSystem_SftpTest::setUp()
 
 
-        $this->FileSystem_SFtp = new FileSystem_SFtp(/* parameters */);
+        $this->FileSystem_Sftp = new FileSystem_Sftp(/* parameters */);
 
     }
 
@@ -35,10 +35,10 @@ class FileSystem_SFtpTest extends PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-        // TODO Auto-generated FileSystem_SFtpTest::tearDown()
+        // TODO Auto-generated FileSystem_SftpTest::tearDown()
 
 
-        $this->FileSystem_SFtp = null;
+        $this->FileSystem_Sftp = null;
 
         parent::tearDown();
     }
@@ -52,134 +52,134 @@ class FileSystem_SFtpTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Tests FileSystem_SFtp->connect()
+     * Tests FileSystem_Sftp->connect()
      */
     public function testConnect()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testConnect()
+        // TODO Auto-generated FileSystem_SftpTest->testConnect()
         $this->markTestIncomplete("connect test not implemented");
 
-        $this->FileSystem_SFtp->connect(/* parameters */);
+        $this->FileSystem_Sftp->connect(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->put()
+     * Tests FileSystem_Sftp->put()
      */
     public function testPut()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testPut()
+        // TODO Auto-generated FileSystem_SftpTest->testPut()
         $this->markTestIncomplete("put test not implemented");
 
-        $this->FileSystem_SFtp->put(/* parameters */);
+        $this->FileSystem_Sftp->put(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->fput()
+     * Tests FileSystem_Sftp->fput()
      */
     public function testFput()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testFput()
+        // TODO Auto-generated FileSystem_SftpTest->testFput()
         $this->markTestIncomplete("fput test not implemented");
 
-        $this->FileSystem_SFtp->fput(/* parameters */);
+        $this->FileSystem_Sftp->fput(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->get()
+     * Tests FileSystem_Sftp->get()
      */
     public function testGet()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testGet()
+        // TODO Auto-generated FileSystem_SftpTest->testGet()
         $this->markTestIncomplete("get test not implemented");
 
-        $this->FileSystem_SFtp->get(/* parameters */);
+        $this->FileSystem_Sftp->get(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->fget()
+     * Tests FileSystem_Sftp->fget()
      */
     public function testFget()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testFget()
+        // TODO Auto-generated FileSystem_SftpTest->testFget()
         $this->markTestIncomplete("fget test not implemented");
 
-        $this->FileSystem_SFtp->fget(/* parameters */);
+        $this->FileSystem_Sftp->fget(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->chmod()
+     * Tests FileSystem_Sftp->chmod()
      */
     public function testChmod()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testChmod()
+        // TODO Auto-generated FileSystem_SftpTest->testChmod()
         $this->markTestIncomplete("chmod test not implemented");
 
-        $this->FileSystem_SFtp->chmod(/* parameters */);
+        $this->FileSystem_Sftp->chmod(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->ls()
+     * Tests FileSystem_Sftp->ls()
      */
     public function testLs()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testLs()
+        // TODO Auto-generated FileSystem_SftpTest->testLs()
         $this->markTestIncomplete("ls test not implemented");
 
-        $this->FileSystem_SFtp->ls(/* parameters */);
+        $this->FileSystem_Sftp->ls(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->cd()
+     * Tests FileSystem_Sftp->cd()
      */
     public function testCd()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testCd()
+        // TODO Auto-generated FileSystem_SftpTest->testCd()
         $this->markTestIncomplete("cd test not implemented");
 
-        $this->FileSystem_SFtp->cd(/* parameters */);
+        $this->FileSystem_Sftp->cd(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->mv()
+     * Tests FileSystem_Sftp->mv()
      */
     public function testMv()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testMv()
+        // TODO Auto-generated FileSystem_SftpTest->testMv()
         $this->markTestIncomplete("mv test not implemented");
 
-        $this->FileSystem_SFtp->mv(/* parameters */);
+        $this->FileSystem_Sftp->mv(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->cp()
+     * Tests FileSystem_Sftp->cp()
      */
     public function testCp()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testCp()
+        // TODO Auto-generated FileSystem_SftpTest->testCp()
         $this->markTestIncomplete("cp test not implemented");
 
-        $this->FileSystem_SFtp->cp(/* parameters */);
+        $this->FileSystem_Sftp->cp(/* parameters */);
 
     }
 
     /**
-     * Tests FileSystem_SFtp->rm()
+     * Tests FileSystem_Sftp->rm()
      */
     public function testRm()
     {
-        // TODO Auto-generated FileSystem_SFtpTest->testRm()
+        // TODO Auto-generated FileSystem_SftpTest->testRm()
         $this->markTestIncomplete("rm test not implemented");
 
-        $this->FileSystem_SFtp->rm(/* parameters */);
+        $this->FileSystem_Sftp->rm(/* parameters */);
 
     }
 
