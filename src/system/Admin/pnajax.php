0effd92b3bd6e1695cd63a5ff782ee3c5463c214
Kyle Giovannetti <078220g@acadiau.ca>
2010-04-28 20:52:05 +0000

    Add right click context menu. Add support for deleting admin categories by ajax. refs #1919

--- src/system/Admin/pnajax.php
+++ src/system/Admin/pnajax.php
@@ -1,7 +1,25 @@
 <?php
 /**
+ * Copyright 2009 Zikula Foundation - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2 (or at your option, any later version).
+ * @package Zikula
+ * @link http://www.zikula.org
+ * @version $Id$
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */ 
+
+/**
  * Change the category a module belongs to by ajax.
- * 
+ *
+ * @return AjaxUtil::output Output to the calling ajax request is returned. 
+ *                          alerttext is a string empty if no problems.
+ *                          response is a string -1 on failure moduleid on sucess.
  */
 function Admin_Ajax_changeModuleCategory() {
     if (!SecurityUtil::checkPermission('Admin::', '::', ACCESS_ADMIN)) {
@@ -13,19 +31,23 @@ function Admin_Ajax_changeModuleCategory() {
     if (!$module) {
         //deal with couldnt get category id
         $output["alerttext"] = "Could not get module name for id:$moduleID";
-        AjaxUtil::output($output);
-        return;
+        return AjaxUtil::output($output);
     }
     $module = $module['name'];
     $result = pnModAPIFunc('Admin', 'admin', 'addmodtocategory', array('category' => $newParentCat,
         'module' => $module));
     $output['alerttext'] = '';
-    $output['response'] = ($result) ? "1" : "0";
-    AjaxUtil::output($output);
+    $output['response'] = ($result) ? $moduleID : "-1";
+    return AjaxUtil::output($output);
 }
 
 /**
  * Add a new admin category by ajax.
+ * 
+ * @return AjaxUtil::output Output to the calling ajax request is returned. 
+ *                          alerttext is a string empty if no problems.
+ *                          response is a string 0 on failure new cid on sucess. 
+ *                          url is a formatted url to the new category on success.
  */
 function Admin_Ajax_addCategory() {
     if (!SecurityUtil::checkPermission('Admin::', '::', ACCESS_ADD)) {
@@ -41,9 +63,38 @@ function Admin_Ajax_addCategory() {
     }
     $result = pnModAPIFunc('Admin', 'admin', 'create', array('catname' => $catName,
         'description' => ''));
-    $output['alerttext'] = '';    
+    $output['alerttext'] = '';
     $output['response'] = (!$result) ? "0" : $result;
     $url = pnModURL('adminpanel', 'admin', 'adminpanel', array('acid' => $result));
     $output['url'] = $url;
     AjaxUtil::output($output);
+}
+
+/**
+ * Delete an admin category by ajax.
+ * 
+ * @return AjaxUtil::output Output to the calling ajax request is returned. 
+ *                          alerttext is a string empty if no problems.
+ *                          response is a string -1 on failure deleted cid on sucess.
+ */
+function Admin_Ajax_deleteCategory() {
+    $cid = trim(FormUtil::getPassedValue('cid'));
+    $category = pnModAPIFunc('Admin', 'admin', 'get', array('cid' => $cid));
+    if ($category == false) {
+        $output['alerttext'] = 'Could not find category:'.$cid;
+        $output['response'] = '-1';
+        return AjaxUtil::output($output);
+    }
+
+    if (!SecurityUtil::checkPermission('Admin::Category', "$category[catname]::$cid", ACCESS_DELETE)) {
+        $output['alerttext'] = 'You do not have permission to delete category:'.$cid;
+        $output['response'] = '-1';
+        return AjaxUtil::output($output);
+    }
+    if (pnModAPIFunc('Admin', 'admin', 'delete', array('cid' => $cid))) {
+        // Success
+        $output['alerttext'] = '';
+        $output['response'] = $cid;
+        return AjaxUtil::output($output);
+    }
 }
--- src/system/Admin/pnjavascript/admin_admin_ajax.js
+++ src/system/Admin/pnjavascript/admin_admin_ajax.js
@@ -1,72 +1,153 @@
+/**
+ * Onload function adds droppable locations to all the tabs as well as context
+ * menus.
+ */
+window.onload = function() {
+	context_menu = Array();
+	droppables = Array();
+	var list = document.getElementById('minitabs');
+	if (list.hasChildNodes) {
+		var nodes = list.getElementsByTagName("a");
+		for ( var i = 0; i < nodes.length; i++) {
+			var nid = nodes[i].getAttribute('id');
+			if (nid != null) {
+				addContext(nid);
+				if (nodes[i].className == 'active')
+					continue;
+				var droppable = Droppables.add(nid, {
+					accept : 'draggable',
+					hoverclass : 'ajaxhover',
+					onDrop : function(drag, drop) {
+						moveModule(drag.id, drop.id);
+					}
+				});
+				droppables.push(droppable);
+			}
+		}
+	}
+}
+
+function addContext(nid)
+{
+	context_menu.push(new Control.ContextMenu(nid));
+	context_menu[context_menu.length - 1].addItem( {
+		label : 'Edit',
+		callback : function(nid) {
+			var match = /acid=([0-9]*){1,}/.exec(nid);
+			if (match instanceof Array) {
+				if (match.length == 2) {
+					cid = match[match.length - 1];
+				}
+			}
+			alert(cid);
+			return;
+		},
+	    enabled : false
+	});
+	context_menu[context_menu.length - 1].addItem( {
+		label : 'Delete',
+		callback : function(nid) {
+			var match = /acid=([0-9]*){1,}/.exec(nid);
+			if (match instanceof Array) {
+				if (match.length == 2) {
+					var cid = match[match.length - 1];
+					deleteTab(cid);
+				}
+			}
+			return;
+		}
+	});
+}
+
+function deleteTab(id) {
+	var pars = "module=Admin&type=ajax&func=deleteCategory&cid=" + id;
+	var myAjax = new Ajax.Request("ajax.php", {
+		method : 'get',
+		parameters : pars,
+		onComplete : deleteTabResponse
+	});
+}
+
+function deleteTabResponse(req) {
+	if (req.status != 200) {
+		pnshowajaxerror(req.responseText);
+		return false;
+	}
+	var json = pndejsonize(req.responseText);
+	if (json.alerttext !== '' || json.response == '-1') {
+		alert("Oops something went wrong! " + json.alerttext + "response: "
+				+ json.respnse);
+	} else {
+		var element = document.getElementById("C" + json.response);
+		element.parentNode.removeChild(element);
+	}
+	return false;
+}
+
 function moveModule(id, cid) {
-    var element = document.getElementById(id);
-    element.parentNode.removeChild(element);
-    var id = id.substr(1);
-    var cid = cid.substr(1);
-    var pars = "module=Admin&type=ajax&func=changeModuleCategory&modid=" + id + "&cat=" + cid;
-    var myAjax = new Ajax.Request(
-            "ajax.php",
-            {
-                method: 'get',
-                parameters: pars,
-                onComplete:changeModuleCategoryResponse
-            });
+	var id = id.substr(1);
+	var cid = cid.substr(1);
+	var pars = "module=Admin&type=ajax&func=changeModuleCategory&modid=" + id
+			+ "&cat=" + cid;
+	var myAjax = new Ajax.Request("ajax.php", {
+		method : 'get',
+		parameters : pars,
+		onComplete : changeModuleCategoryResponse
+	});
 }
 
 function changeModuleCategoryResponse(req) {
-    if (req.status != 200) {
-        pnshowajaxerror(req.responseText);
-        return;
-    }
-    var json = pndejsonize(req.responseText);
-    if (json.alerttext !== '') {
-        alert(json.alerttext);
-    }
-    if (json.response !== '1')
-    {
-        alert("Oops something went wrong!");
-    }
-    return;    
+	if (req.status != 200) {
+		pnshowajaxerror(req.responseText);
+		return;
+	}
+	var json = pndejsonize(req.responseText);
+	if (json.alerttext !== '') {
+		alert(json.alerttext);
+	}
+	if (json.response == '-1') {
+		alert("Oops something went wrong!");
+		return;
+	}
+	var element = document.getElementById('A' + json.response);
+	element.parentNode.removeChild(element);
+	return;
 }
 
-function newCategory(cat)
-{
-    var parent = cat.parentNode;
-    old = parent.innerHTML;
-    var innerhtml = document.getElementById('ajaxNewCatHidden').innerHTML;
-    parent.innerHTML = innerhtml;
-    parent.setAttribute("class","newCat");
-    parent.setAttribute("className","newCat");
-    return false;
+function newCategory(cat) {
+	var parent = cat.parentNode;
+	old = parent.innerHTML;
+	var innerhtml = document.getElementById('ajaxNewCatHidden').innerHTML;
+	parent.innerHTML = innerhtml;
+	parent.setAttribute("class", "newCat");
+	parent.setAttribute("className", "newCat");
+	return false;
 }
 
-function addCategory(cat)
-{
-    oldcat = cat;
-    catname = document.getElementById('ajaxNewCatForm').elements['catName'].value;
-    if (catname == '') {
-        pnshowajaxerror('You must enter a name for the new category');
-        cancelCategory(oldcat);
-        return false;
-    }
-    var pars = "module=Admin&type=ajax&func=addCategory&catname=" + catname;
-    var myAjax = new Ajax.Request(
-            "ajax.php",
-            {
-                method: 'get',
-                parameters: pars,
-                onComplete:addCategoryResponse
-            });
-    return false;
+function addCategory(cat) {
+	oldcat = cat;
+	catname = document.getElementById('ajaxNewCatForm').elements['catName'].value;
+	if (catname == '') {
+		pnshowajaxerror('You must enter a name for the new category');
+		cancelCategory(oldcat);
+		return false;
+	}
+	var pars = "module=Admin&type=ajax&func=addCategory&catname=" + catname;
+	var myAjax = new Ajax.Request("ajax.php", {
+		method : 'get',
+		parameters : pars,
+		onComplete : addCategoryResponse
+	});
+	return false;
 }
 
-function cancelCategory(cat)
-{
-    var parent = cat.parentNode.parentNode;
-    parent.innerHTML = old;
-    parent.setAttribute("class","");
-    parent.setAttribute("className","");
-    return false;
+// cancel the addition of a new category, puts widget back to normal.
+function cancelCategory(cat) {
+	var parent = cat.parentNode.parentNode;
+	parent.innerHTML = old;
+	parent.setAttribute("class", "");
+	parent.setAttribute("className", "");
+	return false;
 }
 
 function addCategoryResponse(req) {
@@ -91,6 +172,7 @@ function addCategoryResponse(req) {
         newelement.innerHTML = old;
         newelement.setAttribute('id', 'addcat');
         document.getElementById('minitabs').appendChild(newelement);
+        addContext('C'+json.response);
         Droppables.add('C'+json.response, { 
             accept: 'draggable',
             hoverclass: 'ajaxhover',
@@ -100,38 +182,39 @@ function addCategoryResponse(req) {
     return false;    
 }
 
-/*
-function addDropDown(id) {
-    document.getElementById(id).addEventListener(
-            "mouseover",
-            function() {
-                return showMenu(id);
-            },
-            false
-        );
+function editmode() {
+	if (typeof editors == 'undefined') {
+		editors = new Array();
+	}
+	if (editors.length != 0) {
+		while (editors.length > 0) {
+			editors.pop().dispose();
+		}
+		document.getElementById('editcat').setAttribute("class", "");
+		document.getElementById('editcat').setAttribute("className", "");
+		return false;
+	}
+	var list = document.getElementById('minitabs');
+	if (list.hasChildNodes) {
+		var nodes = list.getElementsByTagName("a");
+		for ( var i = 0; i < nodes.length; i++) {
+			var nid = nodes[i].getAttribute('id');
+			if (nid != null) {
+				if (nid == 'editcat') {
+					nodes[i].setAttribute("class", "editmode");
+					nodes[i].setAttribute("className", "editmode");
+					continue;
+				}
+				editors.push(new Ajax.InPlaceEditor(nid, 'ajaxedit', {
+					submitOnBlur : true,
+					okButton : false,
+					cols : 10,
+					formClassName : "tabedit tabeditHeight",
+					cancelControl : false
+				}));
+				continue;
+			}
+		}
+	}
+	return false;
 }
-
-function showMenu(id) {
-    var scope = Effect.Queues.get(id);
-    scope.each(function(effect){effect.cancel()});
-    var menu = document.getElementById('D'+id);
-    if (!menu) return false;
-    if (!menu.hasClassName('open')) menu.addClassName('open');
-    if (menu.style.display == 'none') {    
-        Effect.BlindDown('D'+id, {duration: 0.2, queue: {position:'end',scope:id}});
-        menu.style.display = "block";
-    }
-    return;
-}
-
-function hideMenu(id) {
-    var scope = Effect.Queues.get(id);
-    scope.each(function(effect){effect.cancel()});
-    var menu = document.getElementById('D'+id);
-    if (!menu) return false;
-    if (menu.style.display !== 'none') {
-        Effect.BlindUp('D'+id, {duration: 0.2, queue: {position:'end',scope:id}});
-        menu.style.display = "none";
-    }
-}
-*/
