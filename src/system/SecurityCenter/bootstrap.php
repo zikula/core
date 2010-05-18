71d54d7c7857ae8e0b822c3a60bdeaa8a47a8c7d
Drak <drak@zikula.org>
2010-05-18 02:20:05 +0000

    Update docs.

--- src/system/SecurityCenter/bootstrap.php
+++ src/system/SecurityCenter/bootstrap.php
@@ -1,3 +1,16 @@
 <?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPv2.1 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
 ZLoader::addAutoloader('HTMLPurifier', dirname(__FILE__) . '/pnincludes/htmlpurifier');
 ZLoader::addAutoloader('IDS', realpath(dirname(__FILE__) . '/pnincludes'));
