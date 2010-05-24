c87b65c764386aaf392ae31dc7637a2872e71c0d
Drak <drak@zikula.org>
2010-05-24 21:48:19 +0000

    Changed headers at Robert's request.

--- src/system/SecurityCenter/DBObject/IntrusionArray.php
+++ src/system/SecurityCenter/DBObject/IntrusionArray.php
@@ -1,31 +1,20 @@
 <?php
-/** ----------------------------------------------------------------------
- *  LICENSE
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License (GPL)
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WIthOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * @license GNU/LGPv2.1 (or at your option, any later version).
+ * @package Zikula
  *
- *  To read the license please visit http://www.gnu.org/copyleft/gpl.html
- *  ----------------------------------------------------------------------
- *  Original Author of  OpenStar Module Generator
- *  Author Contact: r.gasch@chello.nl, robert.gasch@value4business.com
- *  Purpose of file:  object array class implementation
- *  Copyright: Value4Business GmbH
- *  ----------------------------------------------------------------------
- * @package Zikula_System_Modules
- * @subpackage SecurityCenter
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
  */
 
 
 /**
- * PNIntrusionArray
+ * IntrusionArray
  *
  * @package Zikula_System_Modules
  * @subpackage SecurityCenter
