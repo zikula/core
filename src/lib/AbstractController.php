e76bb1d9efc34349a45986fb4014ebec150e4b8f
Drak <drak@zikula.org>
2010-05-24 09:00:26 +0000

    Add headers.

--- src/lib/AbstractController.php
+++ src/lib/AbstractController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2009 Zikula Foundation.
+ * Copyright 2010 Zikula Foundation.
  *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
