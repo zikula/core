a03d0421a9841f7698a1496d17f3b5c57a7f5e4f
Drak <drak@zikula.org>
2010-05-25 13:15:27 +0000

    Browserhack block refs #2087

---
+++ src/lib/render/plugins/block.browserhack.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPv2.1 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+
+function smarty_block_browserhack($params, $content, Renderer &$render)
+{
+    if ($content) {
+        if (!isset($params['condition'])) {
+            $render->trigger_error(__('browserhack block: condition param is required, non specified.'));
+        }
+
+        $condition = $params['condition'];
+        return "<!--[$condition]>$content<![endif]-->";
+    }
+}
