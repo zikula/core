0d982d5efd2e6948903a526f16679331cd88daaf
Drak <drak@zikula.org>
2010-05-30 04:20:51 +0000

    Add blocks base class

---
+++ src/lib/AbstractBlock.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation.
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPv2.1 (or at your option, any later version).
+ * @package EventManager
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Abstract controller for blocks.
+ */
+abstract class AbstractBlock extends AbstractBase
+{
+    public function __call($method, $args)
+    {
+        $event = new Event('block.method_not_found', $this, array('method' => $method, 'args' => $args));
+        EventManagerUtil::notifyUntil($event);
+        if ($event->hasNotified()) {
+            return $event->getData();
+        }
+    }
+}
