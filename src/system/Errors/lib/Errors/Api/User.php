cf1947d3177c9d0213adfecf8e8aeda7e8a20eba
Drak <drak@zikula.org>
2010-05-31 03:19:13 +0000

    Convert Errors to new standards.

---
+++ src/system/Errors/lib/Errors/Api/User.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package Zikula_System_Modules
+ * @subpackage Errors
+ * @license http://www.gnu.org/copyleft/gpl.html
+ */
+
+class Errors_Api_User extends AbstractApi
+{
+    /**
+     * Send E-mail
+     * This function e-mails the site administrator with an error.
+     * @author Brian Lindner
+     * @author Brook Humphrey
+     */
+    function send_email()
+    {
+        /* send error reporting email to admin */
+        $adminmail = System::getVar('adminmail');
+        $notify_from = System::getVar('notify_from');
+        $sitename = System::getVar('sitename');
+        $errortime = date("m/j/Y at g:i a");
+
+        $doc = System::serverGetVar('REDIRECT_URL');
+        $server = System::serverGetVar('HTTP_HOST');
+        $doc = "http://$server$doc";
+        $headers = "ATTN: Fatal Error at $doc (".System::serverGetVar('REDIRECT_STATUS').")\n";
+        $headers .= "From: $sitename Error Tools $adminmail\n";
+        $headers .= "X-Sender: <$notify_from>\n";
+        $headers .= "X-Mailer: PHP Error Tools by WebMedic\n";
+        $headers .= "X-Mailer-Version: ".System::VERSION_ID." ".System::VERSION_NUM."\n";
+        $headers .= "X-Priority: 1\n";
+        $headers .= "Get-Script-At: <http://www.zikula.org>\n";
+        $body = "Webmaster, the following item was not found on your website:\n\n";
+        $body .= " at ".$errortime;
+        $body .= "WEBSITE\n-- ".System::serverGetVar('SERVER_NAME').':'.System::serverGetVar('SERVER_PORT')."\n\n";
+        $body .= "REASON\n-- ".System::serverGetVar('$REDIRECT_ERRORSOR_NOTES')."\n\n";
+        $body .= "PROBLEM URL\n-- $doc\n\n";
+        $body .= "REFERRER\n-- ".System::serverGetVar('HTTP_REFERER')."\n\n";
+        $body .= "REQUEST\n-- Host: ".System::serverGetVar('HTTP_HOST')."\n-- Query String: ".System::serverGetVar('REDIRECT_QUERY_STRING')."\n";
+        $body .= "-- Method: ".System::serverGetVar('$REQUEST_METHOD')."\n\n";
+        $body .= "USER\n-- Host: ".System::serverGetVar('REMOTE_HOST')."\n-- IP: ".System::serverGetVar('REMOTE_ADDR')."\n-- User: ".System::serverGetVar('REMOTE_USER')."\n-- Agent: ".System::serverGetVar('HTTP_USER_AGENT')."\n-- Cookies: ".System::serverGetVar('HTTP_COOKIE')."\n\n";
+        $body .= "Envolution\n-- version: ".System::VERSION_NUM;
+
+        // Send the mail message.
+        System::mail($adminmail, $headers, $body);
+    }
+}
---
+++ src/system/Errors/lib/Errors/User.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * @copyright (c) 2001, Zikula Development Team
+ * @link http://www.zikula.org
+ * @version $Id$
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package Zikula_System_Modules
+ * @subpackage Errors
+ */
+
+class Errors_User extends AbstractController
+{
+    /**
+     * Display an error
+     * This function displays a generic error form
+     * The template used is based on the error type passed
+     * @author Brian Lindner
+     * @author Brook Humphrey
+     * @param string $args['type'] error type ''404' or 'module'
+     * @param string $args['message'] custom error message
+     * @return string HTML string
+     */
+    public function main($args)
+    {
+        $type = FormUtil::getPassedValue('errtype', isset($args['type']) ? $args['type'] : LogUtil::getErrorType(), 'GET');
+
+        // create an output object
+        $pnRender = Renderer::getInstance('Errors', false);
+
+        // perform any error specific tasks
+        $protocol = System::serverGetVar('SERVER_PROTOCOL');
+        switch ($type) {
+            case 301:
+                header("{$protocol} 301 Moved Permanently");
+                break;
+            case 403:
+                header("{$protocol} 403 Access Denied");
+                break;
+            case 404:
+                header("{$protocol} 404 Not Found");
+                break;
+            case 500:
+                header("{$protocol} 500 Internal Server Error");
+            default:
+        }
+
+        // load the stylesheet
+        PageUtil::addVar('stylesheet', 'system/Errors/style/style.css');
+
+        // assign the document info
+        $pnRender->assign('reportlevel', System::getVar('reportlevel'));
+        $pnRender->assign('currenturi', pnGetCurrentURI());
+        $pnRender->assign('localreferer', pnLocalReferer());
+        $pnRender->assign('sitename', System::getVar('sitename'));
+        $pnRender->assign('reportlevel', System::getVar('reportlevel'));
+        $pnRender->assign('funtext', System::getVar('funtext'));
+
+        // assign the list of registered errors
+        $pnRender->assign('messages', LogUtil::getErrorMessages());
+
+        // return the template output
+        if ($pnRender->template_exists($template = "errors_user_{$type}.htm")) {
+            return $pnRender->fetch($template);
+        } else {
+            return $pnRender->fetch('errors_user_main.htm');
+        }
+    }
+
+    /**
+     * display a system error
+     *
+     * @author Mark West
+     */
+    public function system($args)
+    {
+        $pnRender = Renderer::getInstance('Errors', false);
+        $pnRender->assign($args);
+        return $pnRender->fetch('errors_user_system.htm');
+    }
+}
