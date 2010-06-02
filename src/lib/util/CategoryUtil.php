7849acbf31661f3d16dc47184acb9b9a5a5dce0a
Paweł Preneta <jusuff@jabster.pl>
2010-06-02 12:26:09 +0000

    Separate Tree class from smarty plugin (refs #777)

--- src/lib/util/CategoryUtil.php
+++ src/lib/util/CategoryUtil.php
@@ -879,11 +879,11 @@ class CategoryUtil
             $cats[$i]['href'] = $url;
             $cats[$i]['name'] = $name;
         }
-        $pnRender = Renderer::getInstance('Categories', false);
-        $treePath = $pnRender->_get_plugin_filepath('function', 'tree');
-        Loader::loadFile($treePath);
+//        $pnRender = Renderer::getInstance('Categories', false);
+//        $treePath = $pnRender->_get_plugin_filepath('function', 'tree');
+//        Loader::loadFile($treePath);
 
-        $tree = new Tree();
+        $tree = new Zikula_Tree();
         $tree->setOption('id','categoriesTree');
         $tree->setOption('sortable',$sortable);
         $tree->setOption('disabled',array(1));
