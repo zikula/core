7dd55caa036b0e315d973995867e05f04f626be7
Drak <drak@zikula.org>
2010-06-03 10:00:28 +0000

    Added some frontcontroller exceptions.

---
+++ src/lib/Zikula/Exception/NotFound.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation.
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPv2.1 (or at your option, any later version).
+ * @package EventManager
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+class Zikula_Exception_NotFound extends Exception
+{
+    
+}
