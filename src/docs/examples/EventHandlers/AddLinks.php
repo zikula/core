e3e6b02741a3d70721d2e09a075bd2087a962343
Drak <drak@zikula.org>
2010-06-03 04:44:28 +0000

    Added some eventhandler examples.

---
+++ src/docs/examples/EventHandlers/AddLinks.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2.1 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Add a link to SysInfo administration links Event Handler class.
+ */
+class SysInfo_EventHandlers_AddLinks extends CustomEventHandler
+{
+    /**
+     * Array of event names for this handler (usually just one).
+     *
+     * @var array
+     */
+    protected $names = array('module.postexecute');
+
+    /**
+     * Event handler here.
+     *
+     * @param Event $event
+     */
+    public function handler(Event $event)
+    {
+        // check if this is for this handler
+        if (!($event->getSubject() instanceof SysInfo_Api_Admin && $event['modfunc'][1] == 'getlinks')) {
+            return;
+        }
+
+        if (SecurityUtil::checkPermission('SysInfo::', '::', ACCESS_ADMIN)) {
+            $event->data[] = array('url' => ModUtil::url('SysInfo', 'admin', 'somelink'), 'text' => __('Here is another link'));
+        }
+    }
+}
---
+++ src/docs/examples/EventHandlers/Extensions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2.1 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Add a method to the SysInfo module using Method not found Event Handler class.
+ */
+class SysInfo_EventHandlers_Extensions extends CustomEventHandler
+{
+    /**
+     * Array of event names for this handler (usually just one).
+     *
+     * @var array
+     */
+    protected $names = array('controller.method_not_found');
+
+    /**
+     * Event handler here.
+     *
+     * @param Event $event
+     */
+    public function handler(Event $event)
+    {
+        /**
+         * Show version information for installed Zikula modules
+         * @return string HTML output string
+         */
+
+        // check if this is for this handler
+        $subject = $event->getSubject();
+        if (!($event['method'] == 'extensions' && $subject instanceof SysInfo_admin)) {
+            return;
+        }
+
+        if (!SecurityUtil::checkPermission('SysInfo::', '::', ACCESS_ADMIN)) {
+            return LogUtil::registerPermissionError();
+        }
+
+        // Zikula Modules and Themes versions
+        $pnRender = Renderer::getInstance('SysInfo');
+        $pnRender->assign('mods', ModuleUtil::getModules());
+        $pnRender->assign('themes', ThemeUtil::getAllThemes());
+
+        $event->setData($pnRender->fetch('sysinfo_admin_extensions.htm'));
+        $event->setNotified();
+    }
+}
