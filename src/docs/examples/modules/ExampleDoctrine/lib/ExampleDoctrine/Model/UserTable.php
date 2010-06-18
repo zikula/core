749f6b7d1739bc037191f95b0fb182dc00f77e97
Drak <drak@zikula.org>
2010-06-18 12:38:20 +0000

    Added template Table for easy DAO.  Reverted unnecessary shortcut.

---
+++ src/docs/examples/modules/ExampleDoctrine/lib/ExampleDoctrine/Model/UserTable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2.1 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+
+/**
+ * Doctrine_Table class used to implement own special entity methods
+ *
+ * This class is optional, do not include it of you don't need it.
+ */
+class ExampleDoctrine_Model_UserTable extends Doctrine_Table
+{
+
+}
