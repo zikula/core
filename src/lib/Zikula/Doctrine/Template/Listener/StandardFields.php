d1102f11f55baa67aeeba8ba7b119f12653e5f22
Roland Bär <RolandBaer@gmx.ch>
2010-06-22 16:19:22 +0000

    forgot phpdoc fileheader, refs #2207

--- src/lib/Zikula/Doctrine/Template/Listener/StandardFields.php
+++ src/lib/Zikula/Doctrine/Template/Listener/StandardFields.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Copyright 2010 Zikula Foundation.
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv2.1 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Zikula_Doctrine
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
 
 /**
  * This listener takes care for setting the standard fields properly.
