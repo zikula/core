1ca1ee36911fb70a41365e3042175d60757f4da9
Drak <drak@zikula.org>
2010-07-16 21:07:45 +0000

    Allow native ajax handlers in themes refs #2326

---
+++ src/system/Theme/lib/Theme/Controller/Ajax.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+class Theme_Controller_Ajax extends Zikula_Controller
+{
+    public function dispatch()
+    {
+        $event = new Zikula_Event("theme.ajax_request");
+        $this->eventManager->notifyUntil($event);
+        $this->throwNotFoundUnless($event->hasNotified(), $this->__('No event handlers responded.'));
+        return $event->getData();
+    }
+}
