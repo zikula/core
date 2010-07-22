a8787ac5fffcdafe7ea218c930079bf986237fb9
Drak <drak@zikula.org>
2010-07-22 14:12:29 +0000

    Better development defaults #2392: System error A non-critical problem occurred

--- src/config/config.php
+++ src/config/config.php
@@ -98,12 +98,12 @@ $ZConfig['Log']['log_level_files'] = array('DB' => $ZConfig['System']['temp'] . 
 $ZConfig['Log']['log.apache_uname'] = 0;                                    // log username to apache logs: please see documentation.  Please check you country's local law covering the logging of personally identifiable user data before enabling.
 
 $ZConfig['Log']['log.enabled'] = 1;                         // Enable to allow Zikula to handle errors, 0 passes everything to PHP directly.
-$ZConfig['Log']['log.to_display'] = 0;                      // Display errors?
+$ZConfig['Log']['log.to_display'] = 1;                      // Display errors?
 $ZConfig['Log']['log.display_level'] = 4;                   // 0, 1, 2, 3, 4, 5, 6, 7
 $ZConfig['Log']['log.to_file'] = 0;                         // Log to file?
 $ZConfig['Log']['log.file_level'] = 4;                      // 0, 1, 2, 3, 4, 5, 6, 7
 $ZConfig['Log']['log.show_php_errorhandler'] = 0;           // Set 1 if you want to see PHP's output, including XDebug output. This overrides log.display_template.
-$ZConfig['Log']['log.display_template'] = 1;                // overrides log.show_php_error_handler and gains full control of output.
+$ZConfig['Log']['log.display_template'] = 0;                // overrides log.show_php_error_handler and gains full control of output.
 
 // ----------------------------------------------------------------------
 // The following define some data layer settings
