5d5a95ddb7c8855e642358afa0769fad3de4aadf
Drak <drak@zikula.org>
2010-07-26 05:58:38 +0000

    Deprecated logging refs #2422 closes #2421

--- src/lib/legacy/plugins/function.usergetlang.php
+++ src/lib/legacy/plugins/function.usergetlang.php
@@ -31,6 +31,7 @@
  */
 function smarty_function_usergetlang($params,&$smarty)
 {
+    LogUtil::log(__f('Warning! Template plugin {%1$s} is deprecated, please use {%2$s} instead.', array('usergetlang', 'lang')), E_USER_DEPRECATED);
     $assign = isset($params['assign'])  ? $params['assign']  : null;
 
     $result = ZLanguage::getLanguageCodeLegacy();
