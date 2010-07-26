44b5b7d618933af4dce21bdb099c89962273fe84
Drak <drak@zikula.org>
2010-07-26 11:10:35 +0000

    Refactor config.php settings.

--- src/config/config.php
+++ src/config/config.php
@@ -62,14 +62,6 @@ $ZConfig['DBInfo']['external1']['dbcollate'] = 'utf8_general_ci';
 // additional DB can be configured here as above external2, external3 etc...
 
 // ----------------------------------------------------------------------
-// Debugging/Tracing settings
-// ----------------------------------------------------------------------
-$ZConfig['Debug']['debug'] = 0;             //
-$ZConfig['Debug']['debug.pagerendertime'] = 0;    // display page render time, 0 to disable
-$ZConfig['Debug']['debug.toolbar'] = 0;     // 1 to show the debug toolbar (reqires development 1), 0 to disable
-
-
-// ----------------------------------------------------------------------
 // Error Reporting
 // ----------------------------------------------------------------------
 // This level of reporting only affect PHP's own native handlers (if you enable them).
@@ -98,16 +90,21 @@ $ZConfig['Log']['log.apache_uname'] = 0;          // log username to apache logs
 $ZConfig['Log']['log.enabled'] = 1;               // Enable to allow Zikula to handle errors, 0 passes everything to PHP directly.
 $ZConfig['Log']['log.to_display'] = 1;            // Display errors.
 $ZConfig['Log']['log.display_level'] = 5;         // 0 - EMERG, 1 - CRIT, 2 - ALERT, 3 - ERR, 4 - WARN, 5 - NOTICE, 6 - INFO, 7 - DEBUG
-$ZConfig['Log']['log.to_file'] = 0;               // Log to file?
+$ZConfig['Log']['log.to_file'] = 0;               // Log to file 1 yes, 0 no.
 $ZConfig['Log']['log.file_level'] = 7;            // 0 - EMERG, 1 - CRIT, 2 - ALERT, 3 - ERR, 4 - WARN, 5 - NOTICE, 6 - INFO, 7 - DEBUG
 
 $ZConfig['Log']['log.show_php_errorhandler'] = 0; // Allow PHP error handlers to display additionally? Set this to 1 if you want to see PHP's error handler
-                                                  // If you have XDebug installed, setting this will allow XDebug to output. If log.display_template set, PHP's handlers will not show!
+                                                  // If you have XDebug installed, setting this will allow XDebug to output.
+                                                  // If log.display_template set, PHP's handlers will not show!
+
 $ZConfig['Log']['log.display_template'] = 0;      // Overrides PHP's output handler if activated by log.show_php_error_handler and gains full control of output.
                                                   // This setting is generally NOT desirable when developing/debugging.
 
+$ZConfig['Log']['log.to_debug_toolbar'] = 0;      // 1 to show the debug toolbar (reqires development 1), 0 to disable
 $ZConfig['Log']['log.sql.to_display'] = 0;        // Display sql queries.
 $ZConfig['Log']['log.sql.to_file'] = 0;           // Log sql queries to file.
+
+$ZConfig['Log']['debug.display_pagerendertime'] = 0;      // display page render time, 0 to disable
 
 // ----------------------------------------------------------------------
 // The following define some data layer settings
@@ -121,30 +118,24 @@ $ZConfig['System']['Z_CONFIG_USE_TRANSACTIONS'] = 0;           // run request as
 // ----------------------------------------------------------------------
 // Database cache settings
 // ----------------------------------------------------------------------
-$ZConfig['System']['CACHE_ENABLE'] = false;          // false to disable, true to enable
-$ZConfig['System']['CACHE_TYPE'] = 'Apc';           // Memcache, Apc, Array, Db, Xcache (todo: Memcached, File)
+$ZConfig['System']['dbcache.enable'] = 0;             // 0 to disable, 1 to enable
+$ZConfig['System']['dbcache.type'] = 'Apc';           // Memcache, Apc, Array, Db, Xcache (todo: Memcached, File)
 
 // CACHE_SERVERS valid for Memcache/d only.
 // array of arrays: params according to the addServer methods at e.g.
 // http://php.net/manual/memcached.addservers.php or
 // http://php.net/manual/function.memcache-addserver.php
-$ZConfig['System']['CACHE_SERVERS'][] = array('host' => 'localhost', 'port' => '11211', 'weight' => 1);
-$ZConfig['System']['CACHE_COMPRESSION'] = true; // true/false valid for CACHE_TYPE = Memcache/d
+$ZConfig['System']['dbcache.servers'][] = array('host' => 'localhost', 'port' => '11211', 'weight' => 1);
+$ZConfig['System']['dbcache.compression'] = true; // true/false valid for dbcache.type = Memcache/d
 
-// For pure Doctrine Queries only. Effective only when CACHE_ENABLE = true and CACHE_RESULT = true
+// For pure Doctrine Queries only. Effective only when dbcache.enable = true and dbcache.cache_result = 1
 // http://www.doctrine-project.org/projects/orm/1.2/docs/manual/caching/en#query-cache-result-cache:result-cache
-$ZConfig['System']['CACHE_RESULT'] = false;   // true to enable or null to disable.
-$ZConfig['System']['CACHE_RESULT_TTL'] = 30; // seconds, 3600 = 1 hour.
+$ZConfig['System']['dbcache.cache_result'] = 0;      // 1 to enable or 0 to disable.
+$ZConfig['System']['dbcache.cache_result_ttl'] = 30; // seconds, 3600 = 1 hour.
 
 // Initialize multisites array
 $ZConfig['Multisites'] = array();
 $ZConfig['Multisites']['multisites.enabled'] = 0;
-
-// ----------------------------------------------------------------------
-// Initialize runtime variables to sane defaults
-// ----------------------------------------------------------------------
-global $ZRuntime;
-$ZRuntime['cache_time_request'] = 0;
 
 // * NOTE  If you copy this file to personal_config.php, remove the includes below
 if (!strpos(__FILE__, 'personal_config.php')) {
