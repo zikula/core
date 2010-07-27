4e360a49a9b844d604369799ebc26d090a053549
Robert M. Burkhead <rmburkhead@rmburkhead.com>
2010-07-27 00:13:55 +0000

    Coding standards.

--- src/lib/view/plugins/modifier.themetype.php
+++ src/lib/view/plugins/modifier.themetype.php
@@ -1,14 +1,30 @@
 <?php
 /**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
  * Zikula_View modifier to convert theme type into a language string
  *
  * Example
  *
  *   <!--[$mythemetype|themetype]-->
  *
- * @see          modifier.safetext.php::smarty_modifier_safetext
- * @param        array    $string     the contents to transform
- * @return       string   the modified output
+ * @param array $string The contents to transform.
+ *
+ * @see    modifier.safetext.php::smarty_modifier_safetext
+ *
+ * @return string The modified output.
  */
 function smarty_modifier_themetype($string)
 {
