8612b5384e57e0df2bc2f88ae44bbea0a5d4a356
Drak <drak@zikula.org>
2010-07-30 02:20:42 +0000

    Doc block.

--- src/lib/Zikula/Provider/AggregateItem.php
+++ src/lib/Zikula/Provider/AggregateItem.php
@@ -13,16 +13,60 @@
  * information regarding copyright and licensing.
  */
 
-
+/**
+ * Pending content aggregate.
+ */
 class Zikula_Provider_AggregateItem
 {
+    /**
+     * Type of aggregate.
+     *
+     * @var string
+     */
     protected $type;
+
+    /**
+     * Description of aggregate.
+     *
+     * @var string
+     */
     protected $description;
+
+    /**
+     * Number of items in aggregate.
+     *
+     * @var integer
+     */
     protected $number;
+
+    /**
+     *
+     * @var string
+     */
     protected $controller;
+
+    /**
+     *
+     * @var string
+     */
     protected $method;
+
+    /**
+     *
+     * @var array
+     */
     protected $args;
 
+    /**
+     * Constructor.
+     *
+     * @param string  $type        Type of aggregate item.
+     * @param string  $description Description of aggregate.
+     * @param integer $number      Number of items in aggregate.
+     * @param string  $controller  Name of controller (to view action).
+     * @param string  $method      Name of method in controller (to view action).
+     * @param array   $args        Arguments for method.
+     */
     public function __construct($type, $description, $number, $controller, $method, array $args = array())
     {
         $this->type = (string)$type;
@@ -33,31 +77,61 @@ class Zikula_Provider_AggregateItem
         $this->args = $args;
     }
 
+    /**
+     * Get type.
+     *
+     * @return string
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     * Get description.
+     *
+     * @return string
+     */
     public function getDescription()
     {
         return $this->description;
     }
 
+    /**
+     * Get number.
+     *
+     * @return integer
+     */
     public function getNumber()
     {
         return $this->number;
     }
 
+    /**
+     * Get Controller name.
+     *
+     * @return string
+     */
     public function getController()
     {
         return $this->controller;
     }
 
+    /**
+     * Get method (to view action).
+     *
+     * @return string
+     */
     public function getMethod()
     {
         return $this->method;
     }
 
+    /**
+     * Get method call args.
+     *
+     * @return array
+     */
     public function getArgs()
     {
         return $this->args;
