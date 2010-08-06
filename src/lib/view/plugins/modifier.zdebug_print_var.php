2538b7e82d7ca558d1601920e8a2a3f6f6b6f6aa
Drak <drak@zikula.org>
2010-08-06 06:07:03 +0545

    Coding standards.

--- src/lib/view/plugins/modifier.zdebug_print_var.php
+++ src/lib/view/plugins/modifier.zdebug_print_var.php
@@ -1,46 +1,47 @@
 <?php
 /**
- * Smarty plugin
- * @package Smarty
- * @subpackage plugins
+ * Debug modifier.
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
  */
 
-
 /**
- * Smarty debug_print_var modifier plugin
+ * Formats variable contents for display in the console
  *
- * Type:     modifier<br>
- * Name:     debug_print_var<br>
- * Purpose:  formats variable contents for display in the console
- * @link http://smarty.php.net/manual/en/language.modifier.debug.print.var.php
- *          debug_print_var (Smarty online manual)
- * @author   Monte Ohrt <monte at ohrt dot com>
- * @param array|object
- * @param integer
- * @param integer
+ *  * @link http://smarty.php.net/manual/en/language.modifier.debug.print.var.php
+ * 
+ * @param array|object What is being modified.
+ * @param integer      Depth to print arrays.
+ * @param integer      Max length.
+ *
  * @return string
  */
 function smarty_modifier_zdebug_print_var($var, $depth = 0, $length = 40)
 {
     $_replace = array(
-        "\n" => '<i>\n</i>',
-        "\r" => '<i>\r</i>',
-        "\t" => '<i>\t</i>'
+            "\n" => '<i>\n</i>',
+            "\r" => '<i>\r</i>',
+            "\t" => '<i>\t</i>'
     );
 
     if ($var instanceof Doctrine_Record or $var instanceof Doctrine_Collection) {
-		$varname = get_class($var);
-		$var = $var->toArray();
-	}
+        $varname = get_class($var);
+        $var = $var->toArray();
+    }
 
     switch (gettype($var)) {
         case 'array' :
-            $results = '<b>'.(isset($varname) ? "$varname Object" : 'Array').' (' . count($var) . ')</b>';
+            $results = '<b>' . (isset($varname) ? "$varname Object" : 'Array') . ' (' . count($var) . ')</b>';
             foreach ($var as $curr_key => $curr_val) {
                 $results .= '<br>' . str_repeat('&nbsp;', $depth * 2)
-                    . '<b>' . strtr($curr_key, $_replace) . '</b> =&gt; '
-                    . smarty_modifier_debug_print_var($curr_val, ++$depth, $length);
-                    $depth--;
+                        . '<b>' . strtr($curr_key, $_replace) . '</b> =&gt; '
+                        . smarty_modifier_debug_print_var($curr_val, ++$depth, $length);
+                $depth--;
             }
             break;
         case 'object' :
@@ -48,9 +49,9 @@ function smarty_modifier_zdebug_print_var($var, $depth = 0, $length = 40)
             $results = '<b>' . get_class($var) . ' Object (' . count($object_vars) . ')</b>';
             foreach ($object_vars as $curr_key => $curr_val) {
                 $results .= '<br>' . str_repeat('&nbsp;', $depth * 2)
-                    . '<b> -&gt;' . strtr($curr_key, $_replace) . '</b> = '
-                    . smarty_modifier_debug_print_var($curr_val, ++$depth, $length);
-                    $depth--;
+                        . '<b> -&gt;' . strtr($curr_key, $_replace) . '</b> = '
+                        . smarty_modifier_debug_print_var($curr_val, ++$depth, $length);
+                $depth--;
             }
             break;
         case 'boolean' :
@@ -63,25 +64,25 @@ function smarty_modifier_zdebug_print_var($var, $depth = 0, $length = 40)
             } elseif (null === $var) {
                 $results = 'null';
             } else {
-                $results = htmlspecialchars((string) $var);
+                $results = htmlspecialchars((string)$var);
             }
             $results = '<i>' . $results . '</i>';
             break;
         case 'integer' :
         case 'float' :
-            $results = htmlspecialchars((string) $var);
+            $results = htmlspecialchars((string)$var);
             break;
         case 'string' :
             $results = strtr($var, $_replace);
-            if (strlen($var) > $length ) {
+            if (strlen($var) > $length) {
                 $results = substr($var, 0, $length - 3) . '...';
             }
             $results = htmlspecialchars('"' . $results . '"');
             break;
         case 'unknown type' :
         default :
-            $results = strtr((string) $var, $_replace);
-            if (strlen($results) > $length ) {
+            $results = strtr((string)$var, $_replace);
+            if (strlen($results) > $length) {
                 $results = substr($results, 0, $length - 3) . '...';
             }
             $results = htmlspecialchars($results);
@@ -90,6 +91,3 @@ function smarty_modifier_zdebug_print_var($var, $depth = 0, $length = 40)
     return $results;
 }
 
-/* vim: set expandtab: */
-
-?>
