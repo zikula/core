26293df09652dac300f4d89fe2dd6b770a4645d3
Drak <drak@zikula.org>
2010-10-14 09:53:00 +0545

    Add continue and break functionality to loops in templates, closes #2560

---
+++ src/lib/view/plugins/compiler.break.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Zikula_View function provide {break} in templates.
+ *
+ * @param string      $content The content.
+ * @param Zikula_View $view    Reference to the Zikula_View object.
+ *
+ * @return string 'break;'
+ */
+function smarty_compiler_break($content, $view)
+{
+    return 'break;';
+}
---
+++ src/lib/view/plugins/compiler.continue.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Zikula_View function provide {continue} in templates.
+ *
+ * @param string      $content The content.
+ * @param Zikula_View $view    Reference to the Zikula_View object.
+ *
+ * @return string 'continue;'
+ */
+function smarty_compiler_continue($content, $view)
+{
+    return 'continue;';
+}
