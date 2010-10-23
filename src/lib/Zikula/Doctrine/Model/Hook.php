6ff5714b347392fffcf8302a6436bd77d73551aa
Drak <drak@zikula.org>
2010-10-23 16:55:08 +0545

    Adding Hook Entities to Doctrine models.  These need to be converted to Doctrine 1 models refs #2582

---
+++ src/lib/Zikula/Doctrine/Model/Hook.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_Core
+ * @subpackage Entities
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Core\Entities;
+use \Zikula\Framework\Entities\StandardEntity as StandardEntity;
+
+/**
+ * @Entity
+ * @Table(name="hook_registry")
+ */
+class Hook extends StandardEntity
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $hookname; // links to HookAssociation::hookname
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $servicename;
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $handlerclass;
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $handlermethod;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getHookName()
+    {
+        return $this->hookname;
+    }
+
+    public function getServiceName()
+    {
+        return $this->servicename;
+    }
+
+    public function getHandlerClass()
+    {
+        return $this->handlerclass;
+    }
+
+    public function getHandlerMethod()
+    {
+        return $this->handlermethod;
+    }
+
+    public function setHookName($value)
+    {
+        $this->hookname = $value;
+    }
+
+    public function setServiceName($value)
+    {
+        $this->servicename = $value;
+    }
+
+    public function setHookClass($value)
+    {
+        $this->hookclass = $value;
+    }
+
+    public function setHandlerMethod($value)
+    {
+        $this->handlermethod = $value;
+    }
+
+    public function set($hookName, $serviceName, $handlerClass, $handlerMethod)
+    {
+        $this->hookname = $hookName;
+        $this->servicename = $serviceName;
+        $this->handlerclass = $handlerClass;
+        $this->handlermethod = $handlerMethod;
+    }
+}
---
+++ src/lib/Zikula/Doctrine/Model/HookBinding.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_Core
+ * @subpackage Entities
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Core\Entities;
+use \Zikula\Framework\Entities\StandardEntity as StandardEntity;
+
+/**
+ * @Entity
+ * @Table(name="hook_bindings")
+ */
+class HookBinding extends StandardEntity
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $hookname;
+
+    /**
+     * @Column(type="string", length=100)
+     */
+    private $who;
+
+    /**
+     * @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $weight;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getHookName()
+    {
+        return $this->hookname;
+    }
+
+    public function getWho()
+    {
+        return $this->who;
+    }
+
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    public function setHookName($value)
+    {
+        $this->hookname = $value;
+    }
+
+    public function setWho($value)
+    {
+        $this->who = strtolower($value);
+    }
+
+    public function setWeight($value)
+    {
+        $this->weight = $value;
+    }
+
+    public function set($hookname, $who, $weight = 1)
+    {
+        $this->hookname = $hookname;
+        $this->who = $who;
+        $this->weight = $weight;
+    }
+}
