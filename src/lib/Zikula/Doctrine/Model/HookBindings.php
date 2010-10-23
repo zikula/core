cdc03b2dfdb4464b2d30e141957fbf8f34465e9f
Drak <drak@zikula.org>
2010-10-23 17:19:40 +0545

    HookManager Doctrine model schema refs #2582

---
+++ src/lib/Zikula/Doctrine/Model/HookBindings.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Zikula_Doctrine
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * HookBindings Model class.
+ */
+class Zikula_Doctrine_Model_HookBindings extends Doctrine_Record
+{
+    public function setTableDefinition()
+    {
+        $this->setTableName('hook_bindings');
+        $this->hasColumn('id', 'integer', 4, array(
+             'type' => 'integer',
+             'length' => 4,
+             'fixed' => false,
+             'unsigned' => false,
+             'primary' => true,
+             'autoincrement' => true,
+             ));
+        $this->hasColumn('hookname', 'string', 100, array(
+             'type' => 'string',
+             'length' => 100,
+             'fixed' => false,
+             'unsigned' => false,
+             'primary' => false,
+             'notnull' => true,
+             'autoincrement' => false,
+             ));
+        $this->hasColumn('who', 'string', 100, array(
+             'type' => 'string',
+             'length' => 100,
+             'fixed' => false,
+             'unsigned' => false,
+             'primary' => false,
+             'notnull' => true,
+             'autoincrement' => false,
+             ));
+        $this->hasColumn('weight', 'integer', 4, array(
+             'type' => 'integer',
+             'length' => 4,
+             'fixed' => false,
+             'unsigned' => false,
+             'primary' => false,
+             'notnull' => true,
+             'autoincrement' => false,
+             ));
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        
+    }
+}
