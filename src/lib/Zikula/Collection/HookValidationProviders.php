768f8b0920edb85fd91f3956fef2d843f6dd4f0b
Drak <drak@zikula.org>
2010-11-24 12:13:49 +0545

    Add validation responses for validation hooks refs #2627

---
+++ src/lib/Zikula/Collection/HookValidationProviders.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation.
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Collection
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Hook validation collection
+ */
+class Zikula_Collection_HookValidationProviders extends Zikula_Collection_Container
+{
+    /**
+     * Construct a new Zikula_Collection_HookValidationProviders.
+     *
+     * @param string      $name       The name of the collection.
+     * @param ArrayObject $collection The collection (optional).
+     */
+    public function __construct($name='validation', ArrayObject $collection = null)
+    {
+        parent::__construct($name, $collection);
+    }
+
+    /**
+     * Set response.
+     *
+     * @param string                         $name     Name.
+     * @param Zikula_Response_HookValidation $response Validation response.
+     */
+    protected function set($name, Zikula_Provider_HookValidation $response)
+    {
+        $this->collection[$name] = $response;
+    }
+
+    /**
+     * Check if there are any errors in any of the validation responses.
+     *
+     * @return boolean
+     */
+    public function hasErrors()
+    {
+        foreach ($this->collection as $response) {
+            if ($response->hasErrors()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
