beeec0260cc52bce6e42eef10f843b94b7535653
Drak <drak@zikula.org>
2010-11-26 14:29:45 +0545

    More permissive license for code examples.
    Code examples have the purpose of teaching developers how to use Zikula.  We provider such examples as open source without making any requirement for developers to open source any derivative work they make from the examples, as is the case with L/GPL.  This only applies to the specified example files where the MIT license has been applied.

--- src/docs/examples/EventHandlers/AddLinks.php
+++ src/docs/examples/EventHandlers/AddLinks.php
@@ -5,9 +5,8 @@
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
  *
- * @license GNU/LGPLv3 (or at your option, any later version).
- * @package Zikula
- *
+ * @license MIT http://www.opensource.org/licenses/mit-license.php
+ * 
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
--- src/docs/examples/EventHandlers/Extensions.php
+++ src/docs/examples/EventHandlers/Extensions.php
@@ -5,8 +5,7 @@
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
  *
- * @license GNU/LGPLv3 (or at your option, any later version).
- * @package Zikula
+ * @license MIT
  *
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
