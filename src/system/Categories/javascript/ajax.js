8d1510968fb3786f979cfc5f1e5bbd73a1607e19
Herr.Vorragend <carsten@zikula.org>
2011-01-20 12:09:10 +0100

    collapsible meta info area (refs #2656)

--- src/system/Categories/javascript/ajax.js
+++ src/system/Categories/javascript/ajax.js
@@ -1,5 +1,35 @@
 // Copyright Zikula Foundation 2009 - license GNU/LGPLv2.1 (or at your option, any later version).
 
+Event.observe(window, 'load', categories_edit_init);
+
+function categories_edit_init()
+{
+    if ($('categories_meta_collapse')) {
+        categories_meta_init();
+    }
+}
+
+
+function categories_meta_init()
+{
+    $('categories_meta_collapse').observe('click', categories_meta_click);
+    $('categories_meta_collapse').addClassName('z-toggle-link');
+    if ($('categories_meta_details').style.display != "none") {
+        $('categories_meta_collapse').removeClassName('z-toggle-link-open');
+        $('categories_meta_details').hide();
+    }
+}
+
+function categories_meta_click()
+{
+    if ($('categories_meta_details').style.display != "none") {
+        Element.removeClassName.delay(0.9, $('categories_meta_collapse'), 'z-toggle-link-open');
+    } else {
+        $('categories_meta_collapse').addClassName('z-toggle-link-open');
+    }
+    switchdisplaystate('categories_meta_details');
+}
+
 var Category = {};
 
 Category.Attributes = {};
@@ -51,4 +81,3 @@ Category.Attributes.Remove = function(inputElement)
     table.deleteRow(tr.rowIndex);
     return false;
 }
-
