4f74207aaccfcf2039034a940c8f61f7bf10e55f
rmburkhead <rmburkhead@gmail.com>
2011-01-21 22:20:14 -0700

    Added comment regarding 'if' condition that should no longer happen. Refs #2709
    See commit [cd4edce22fc1c123a6fb] and especially commentary on [831775681c1f78d1c134].

--- src/system/Users/lib/Users/Block/Login.php
+++ src/system/Users/lib/Users/Block/Login.php
@@ -88,6 +88,24 @@ class Users_Block_Login extends Zikula_Block
             $authmodule = false;
             $numAuthmodules = count($authmodules);
             if (!$numAuthmodules || ($numAuthmodules <= 0)) {
+                // NOTE: as of commit [cd4edce22fc1c123a6fb4ffc5b60b05b56a472df 22-Jan-2011 02:51:46 UTC
+                // (The 'mods list' should always contain the essential core modules refs #2709)]
+                // this condition should be impossible, as the commit ensures that the Users module will
+                // always respond to ModUtil::getModulesCapableOf('authentication') regardless of its
+                // state. We're leaving the code here for now until we decide how to make logging in
+                // with a uname/pwd optional. At some point someone will want to allow access to a site
+                // using *only* an authmodule other than Users. For example, the site might want to
+                // accept only authentication from OpenID, or might want to accept authentication only
+                // from an internal corporate LDAP server. In those cases, we need to decide what to
+                // do about (1) preventing users from accessing the default uname/pwd method, and (2)
+                // ensuring that the admin user can *always* log in with a Users module uname/pwd in
+                // cases where everything else is broken or unavailable. If we make it possible again
+                // for the Users module to not be on the getModulesCapableOf('authentication') list,
+                // then we'll need this again.
+                //
+                // If we go another way, then this specific 'if' condition can be removed (although
+                // the others are still required).
+
                 // No auth modules?! We know Users is installed, so force that one.
                 $authmodules[] = ModUtil::getInfoFromName('Users');
                 $authmodule = 'Users';
