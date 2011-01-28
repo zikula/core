aca8691f353f42465705a98e0464c7baa14d2d79
Pawel Preneta <jusuff@jabster.pl>
2011-01-29 00:47:16 +0100

    Temporary fix for Category tree sorting problem (refs #2763)

--- src/system/Categories/lib/Categories/Controller/Admin.php
+++ src/system/Categories/lib/Categories/Controller/Admin.php
@@ -43,8 +43,8 @@ class Categories_Controller_Admin extends Zikula_Controller
         // disable attribution for performance
         $GLOBALS['dbtables']['categories_category_db_extra_enable_attribution'] = false;
         $dbtables    = DBUtil::getTables();
-        $columnArray = array ('id', 'name', 'display_name', 'parent_id', 'path', 'ipath', 'status');
-        $cats        = CategoryUtil::getSubCategories ($root_id, true, true, true, true, true, '', '', null, 'sort_value', $columnArray);
+        $columnArray = array ('id', 'name', 'display_name', 'parent_id', 'path', 'ipath', 'status','sort_value');
+        $cats        = CategoryUtil::getSubCategories ($root_id, true, true, true, true, true, '', '', null, '', $columnArray);
         $menuTxt     = CategoryUtil::getCategoryTreeJS ($cats, true, true);
         $GLOBALS['dbtables']['categories_category_db_extra_enable_attribution'] = true;
 
