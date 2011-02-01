ff6e94d1664e16d06e83688e994b5371c06c11a1
Drak <drak@zikula.org>
2011-02-01 14:25:26 +0545

    Exceptions thrown during module dispatch result in no trace information.  This makes debugging a real pest.
    Altered the front controller to pass the exception to the Errors module.  In turn,
    the Errors module will display the trace assuming development mode is on.
    
    You can test this nicely by throwing an exception in SystemListeners::addHooksLink()
    after the security check, then visit the admin panel.

--- src/system/Errors/lib/Errors/Controller/User.php
+++ src/system/Errors/lib/Errors/Controller/User.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright Zikula Foundation 2009 - Zikula Application Framework
  *
@@ -11,9 +12,9 @@
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
-
 class Errors_Controller_User extends Zikula_Controller
 {
+
     /**
      * Display an error
      * This function displays a generic error form
@@ -25,6 +26,8 @@ class Errors_Controller_User extends Zikula_Controller
     public function main($args)
     {
         $type = FormUtil::getPassedValue('errtype', isset($args['type']) ? $args['type'] : LogUtil::getErrorType(), 'GET');
+        $exception = isset($args['exception']) ? $args['exception'] : null;
+        $message = isset($args['message']) ? $args['message'] : '';
 
         // perform any error specific tasks
         $protocol = System::serverGetVar('SERVER_PROTOCOL');
@@ -50,14 +53,22 @@ class Errors_Controller_User extends Zikula_Controller
 
         // assign the document info
         $this->view->assign('reportlevel', System::getVar('reportlevel'))
-                   ->assign('currenturi', System::getCurrentUri())
-                   ->assign('localreferer', System::localReferer())
-                   ->assign('sitename', System::getVar('sitename'))
-                   ->assign('reportlevel', System::getVar('reportlevel'))
-                   ->assign('funtext', System::getVar('funtext'));
+                ->assign('currenturi', System::getCurrentUri())
+                ->assign('localreferer', System::localReferer())
+                ->assign('sitename', System::getVar('sitename'))
+                ->assign('reportlevel', System::getVar('reportlevel'))
+                ->assign('funtext', System::getVar('funtext'));
 
         // assign the list of registered errors
         $this->view->assign('messages', LogUtil::getErrorMessages());
+        $trace = array();
+        if (System::isDevelopmentMode() && $exception instanceof Exception) {
+            $line = $exception->getLine();
+            $file = $exception->getFile();
+            $trace = explode("\n", $exception->getTraceAsString());
+            $trace[0] .= " in $file, line $line";
+        }
+        $this->view->assign('trace', $trace);
 
         // return the template output
         if ($this->view->template_exists($template = "errors_user_{$type}.tpl")) {
@@ -76,4 +87,5 @@ class Errors_Controller_User extends Zikula_Controller
         $this->view->assign($args);
         return $this->view->fetch('errors_user_system.tpl');
     }
+
 }
--- src/system/Errors/templates/errors_user_main.tpl
+++ src/system/Errors/templates/errors_user_main.tpl
@@ -5,5 +5,13 @@
         <li>{$message|safehtml}</li>
         {/foreach}
     </ul>
+    {if isset($trace)}
+    <ul>
+        <h2>{gt text="Exception Trace"}</h2>
+        {foreach from=$trace item=t}
+            <li>{$t|safehtml}</li>
+        {/foreach}
+    </ul>
+    {/if}
     <p><a href="javascript:history.back(-1)">{gt text="Go back to previous page"}</a></p>
 </div>
