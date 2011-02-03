6aad62c37346f0e822cc5a229fbeec188fe08390
Drak <drak@zikula.org>
2011-02-03 18:30:29 +0545

    Ideas

---
+++ src/lib/Zikula/Helper - Copy (3).php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Zikula_Core
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Helper class.
+ */
+abstract class Zikula_Helper
+{
+    /**
+     * ServiceManager.
+     *
+     * @var Zikula_ServiceManager
+     */
+    protected $serviceManager;
+
+    /**
+     * EventManager.
+     *
+     * @var Zikula_EventManager
+     */
+    protected $eventManager;
+
+    /**
+     * This object's reflection.
+     *
+     * @var ReflectionObject
+     */
+    protected $reflection;
+
+    /**
+     * Setup of this class.
+     * 
+     * @return void
+     */
+    protected function inject(Zikula_ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+        $this->eventManager = $this->serviceManager->getService('zikula.eventmanager');
+    }
+
+    /**
+     * Get reflection of this object.
+     *
+     * @return ReflectionObject
+     */
+    public function getReflection()
+    {
+        if (!$this->reflection) {
+            $this->reflection = new ReflectionObject($this);
+        }
+        return $this->reflection;
+    }
+    
+    /**
+     * Get eventManager.
+     *
+     * @return Zikula_EventManager
+     */
+    public function getEventManager()
+    {
+        return $this->eventManager;
+    }
+
+    /**
+     * Get servicemanager.
+     *
+     * @return Zikula_ServiceManager
+     */
+    public function getServiceManager()
+    {
+        return $this->serviceManager;
+    }
+}
