4faf720a147a2d7adba760379ad5a893528a3df9
Drak <drak@zikula.org>
2011-02-04 18:15:24 +0545

    Docblocks

--- src/system/Permissions/javascript/permissions.js
+++ src/system/Permissions/javascript/permissions.js
@@ -1,4 +1,4 @@
-// Copyright Zikula Foundation 2009 - license GNU/LGPLv2.1 (or at your option, any later version).
+// Copyright Zikula Foundation 2009 - license GNU/LGPLv3 (or at your option, any later version).
 
 /**
  * Inits the ajax stuff: show ajax buttons, remove non ajax buttons etc.
