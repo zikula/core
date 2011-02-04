2456743d972f949809df33e3e3b0d9817c6174b3
Drak <drak@zikula.org>
2011-02-04 18:15:24 +0545

    Docblocks

--- src/system/Admin/javascript/admin_admin_ajax.js
+++ src/system/Admin/javascript/admin_admin_ajax.js
@@ -1,4 +1,4 @@
-// Copyright Zikula Foundation 2009 - license GNU/LGPLv2.1 (or at your option, any later version).
+// Copyright Zikula Foundation 2009 - license GNU/LGPLv3 (or at your option, any later version).
 
 /**
  * Onload function adds droppable locations to all the tabs as well as context
