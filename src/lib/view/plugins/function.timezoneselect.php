7741dcc3f31a025be2a17e0f62eb289eacc6b52f
Drak <drak@zikula.org>
2011-02-04 18:10:40 +0545

    Add timezone plugin to template plugins.

---
+++ src/lib/view/plugins/function.timezoneselect.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Template plugin to display timezone list.
+ *
+ * <!--[timezoneselect selected="Timezone"]-->
+ *
+ * @see    function.timezoneselect.php::smarty_function_timezoneselect().
+ *
+ * @param  array       $params   All attributes passed to this function from the template.
+ * @param  Zikula_View $view     The Zikula_View.
+ *
+ * @return string The results of the module function.
+ */
+function smarty_function_timezoneselect($params, $view)
+{
+    require_once $view->_get_plugin_filepath('function', 'html_options');
+
+    $timezones = DateUtil::getTimezones();
+    if (!isset($params['selected']) || empty($params['selected']) || !isset($timezones[$params['selected']])) {
+        $params['selected'] = System::getVar('timezone_offset');
+    }
+
+    return smarty_function_html_options(array('options' => $timezones, 'selected' => $params['selected'], 'print_result' => false), $view);
+}
