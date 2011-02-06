ec8a4c77e0ee9b3b1982a3622922ce89e54e2b49
Drak <drak@zikula.org>
2011-02-06 10:27:08 +0545

    Update tests.

--- tests/lib/Tests/Zikula/FileSystem/FileSystem_LocalTest.php
+++ tests/lib/Tests/Zikula/FileSystem/FileSystem_LocalTest.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/../../../../bootstrap.php';
 
 // exclude the following file from code coverage reports.
-PHP_CodeCoverage_Filter::getInstance()->addFileToBlacklist(dirname(__FILE__). '/../../../src/lib/Zikula/FileSystem/Facade/Local.php');
+PHP_CodeCoverage_Filter::getInstance()->addFileToBlacklist(dirname(__FILE__). '/../../../../../src/lib/Zikula/FileSystem/Facade/Local.php');
 
 /**
  * Zikula_FileSystem_Local test case.
