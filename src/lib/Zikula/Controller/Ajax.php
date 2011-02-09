e46f4fa535f1832fd8a6ef222aae850121a9212a
Drak <drak@zikula.org>
2011-02-09 15:18:14 +0545

    Add base class for Ajax controllers refs #859
    Introduce a simple method to check CSFR token.

---
+++ src/lib/Zikula/Controller/Ajax.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2010 Zikula Foundation.
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Zikula_Controller
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Abstract AJAX controller.
+ */
+abstract class Zikula_Controller_Ajax extends Zikula_Controller
+{
+    /**
+     * Check the CSFR token.
+     *
+     * @throws Zikula_Response_Ajax_Forbidden If the CSFR token fails.
+     *
+     * @return void
+     */
+    public function checkCsfrToken()
+    {
+        $token = isset($_SERVER['X_ZIKULA_AJAX_TOKEN']) ? $_SERVER['X_ZIKULA_AJAX_TOKEN'] : '';
+        // we might have to account for session regeneration here - drak
+        if (!$token == session_id()) {
+            throw new Zikula_Response_Ajax_Forbidden(__('Ajax security checks failed.'));
+        }
+    }
+}
