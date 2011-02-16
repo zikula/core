4347841df12b7d0590d74d8228d1e71e8285dc27
Pawel Preneta <jusuff@jabster.pl>
2011-02-16 01:51:51 +0100

    Editing categories in tree view (refs #2656)

--- src/system/Categories/javascript/categories_admin_view.js
+++ src/system/Categories/javascript/categories_admin_view.js
@@ -1,7 +1,9 @@
 // Copyright Zikula Foundation 2009 - license GNU/LGPLv3 (or at your option, any later version).
 
+Zikula.define('Categories');
+
 Event.observe(window, 'load', function() {
-    Zikula.TreeSortable.trees.categoriesTree.config.onSave = CategoriesSave;
+    Zikula.TreeSortable.trees.categoriesTree.config.onSave = Zikula.Categories.Resequence;
     $('catExpand').observe('click',function(e){
         e.preventDefault();
         Zikula.TreeSortable.trees.categoriesTree.expandAll();
@@ -10,32 +12,246 @@ Event.observe(window, 'load', function() {
         e.preventDefault();
         Zikula.TreeSortable.trees.categoriesTree.collapseAll();
     });
+    Zikula.Categories.AttachMenu();
 });
 
-function CategoriesSave(node,params,data) {
+Zikula.Categories.AttachMenu = function () {
+    Zikula.Categories.ContextMenu = new Control.ContextMenu(Zikula.TreeSortable.trees.categoriesTree.tree, {
+        animation: false,
+        beforeOpen: function(event) {
+            Zikula.Categories.ContextMenu.lastClick = event;
+            if(!event.findElement('a')) {
+                throw $break;
+            }
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Edit',
+        callback: function(node){
+            Zikula.Categories.MenuAction(node,'edit');
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Delete',
+        callback: function(node){
+            Zikula.UI.Confirm('Do you really want to delete this category?','Confirmation prompt',function(res){
+                if(res) {
+                    Zikula.Categories.MenuAction(node,'delete');
+                }
+            });
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Copy',
+        callback: function(node){
+            Zikula.Categories.MenuAction(node,'copy');
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Activate',
+        condition: function() {
+            return Zikula.Categories.ContextMenu.lastClick.findElement('a').hasClassName(Zikula.TreeSortable.trees.categoriesTree.config.nodeUnactive);
+        },
+        callback: function(node){
+            Zikula.Categories.MenuAction(node,'activate');
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Deactivate',
+        condition: function() {
+            return !Zikula.Categories.ContextMenu.lastClick.findElement('a').hasClassName(Zikula.TreeSortable.trees.categoriesTree.config.nodeUnactive);
+        },
+        callback: function(node){
+            Zikula.Categories.MenuAction(node,'deactivate');
+        }
+    });
+    Zikula.Categories.ContextMenu.addItem({
+        label: 'Add',
+        callback: function(node){
+            Zikula.Categories.MenuAction(node,'add');
+        }
+    });
+}
+
+Zikula.Categories.Indicator = function() {
+    return $('ajax_indicator') ? $('ajax_indicator') : new Element('img',{id: 'ajax_indicator', src: 'images/ajax/indicator_circle.gif'});
+}
+
+Zikula.Categories.MenuAction = function(node, action){
+    if (!['edit', 'delete', 'copy', 'activate', 'deactivate', 'add'].include(action)) {
+        return false;
+    }
+    node.insert({after: Zikula.Categories.Indicator()});
+    var url = "ajax.php?module=Categories&func=",
+        pars = {
+            cid: Zikula.TreeSortable.trees.categoriesTree.getNodeId(node.up('li'))
+        };
+    switch(action) {
+        case 'edit':
+            pars.mode = 'edit';
+            break;
+        case 'copy':
+            pars.parent = Zikula.TreeSortable.trees.categoriesTree.getNodeId(node.up('li').up('li'));
+            break;
+        case 'add':
+            pars.mode = 'new';
+            pars.parent = Zikula.TreeSortable.trees.categoriesTree.getNodeId(node.up('li').up('li'));
+            action = 'edit'
+            break;
+    }
+    url = url + action;
+
+    var request = new Zikula.Ajax.Request(
+        url, {
+            method: 'post',
+            parameters: pars,
+            authid: 'categoriesauthid',
+            onComplete: Zikula.Categories.MenuActionCallback
+        });
+    return true;
+}
+
+Zikula.Categories.MenuActionCallback = function(req) {
+    if (!req.isSuccess()) {
+    	Zikula.showajaxerror(req.getMessage());
+        return false;
+    }
+    var data = req.getData(),
+        node = $(Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.cid);
+    switch(data.action) {
+        case 'delete':
+            Droppables.remove(node);
+            node.select('li').each(function(subnode) {
+                Droppables.remove(subnode);
+            });
+            Effect.SwitchOff(node,{
+                afterFinish:function(){node.remove()}
+            });
+            Zikula.TreeSortable.trees.categoriesTree.drawNodes();
+            break;
+        case 'activate':
+            node.down('a').removeClassName(Zikula.TreeSortable.trees.categoriesTree.config.nodeUnactive);
+            break;
+        case 'deactivate':
+            node.down('a').addClassName(Zikula.TreeSortable.trees.categoriesTree.config.nodeUnactive);
+            break;
+        case 'copy':
+            var newParent = $(Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.parent).down('ul'),
+                newNode = Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.copycid;
+            newParent.insert(data.node);
+            newNode = $(newNode);
+            Zikula.Categories.ReinitTreeNode(newNode);
+            break;
+        case 'edit':
+            $(document.body).insert(data.result);
+            Zikula.Categories.Form = new Zikula.UI.FormDialog($('categories_ajax_form_container'),function(res){
+                if(!res) {
+                    return false;
+                }
+                var pars = $('categories_ajax_form').serialize(true);
+                pars.mode = 'edit';
+                new Zikula.Ajax.Request('ajax.php?module=Categories&func=save',{
+                    method: 'post',
+                    parameters: pars,
+                    authid: 'categoriesauthid',
+                    onComplete: function(req){
+                        var data = req.getData(),
+                            node = $(Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.cid);
+                            node.replace(data.node);
+                        Zikula.Categories.ReinitTreeNode(node);
+                        Zikula.Categories.Form.destroy();
+                    }
+                })
+            },{title: $('categories_ajax_form_container').title, width: 700});
+            Zikula.Categories.Form.open();
+            break;
+        case 'add':
+            $(document.body).insert(data.result);
+            Zikula.Categories.Form = new Zikula.UI.FormDialog($('categories_ajax_form_container'),function(res){
+                if(!res) {
+                    return false;
+                }
+                var pars = $('categories_ajax_form').serialize(true);
+                pars.mode = 'new';
+                new Zikula.Ajax.Request('ajax.php?module=Categories&func=save',{
+                    method: 'post',
+                    parameters: pars,
+                    authid: 'categoriesauthid',
+                    onComplete: function(req){
+                        var data = req.getData(),
+                            newParent = $(Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.parent).down('ul');
+                        newParent.insert(data.node);
+                        var node = $(Zikula.TreeSortable.trees.categoriesTree.config.nodePrefix + data.cid);
+                        Zikula.Categories.ReinitTreeNode(node);
+                        Zikula.Categories.Form.destroy();
+                    }
+                })
+            },{title: $('categories_ajax_form_container').title, width: 700});
+            Zikula.Categories.Form.open();
+            break;
+    }
+    return true;
+}
+
+Zikula.Categories.ReinitTreeNode = function(node) {
+    Zikula.TreeSortable.trees.categoriesTree.initNode(node);
+    var subNodes = node.select('li');
+    if (subNodes) {
+        subNodes.each(Zikula.TreeSortable.trees.categoriesTree.initNode.bind(Zikula.TreeSortable.trees.categoriesTree));
+    }
+    Zikula.TreeSortable.trees.categoriesTree.expandAll(node);
+    Zikula.TreeSortable.trees.categoriesTree.drawNodes();
+    Zikula.UI.Tooltips(node.select('a'));
+}
+
+Zikula.Categories.Resequence = function(node,params,data) {
     // do not allow inserts on root level
     if (node.up('li') == undefined) {
         return false;
     }
     var pars = {
             'data': data
-    }
+        };
+    node.insert({bottom: Zikula.Categories.Indicator()});
+
     var request = new Zikula.Ajax.Request(
         "ajax.php?module=Categories&func=resequence",
         {
             method: 'post',
             parameters: pars,
             authid: 'categoriesauthid',
-            onComplete: CategoriesSaveResponse
+            onComplete: Zikula.Categories.ResequenceCallback
         });
     return request.success();
 }
 
-function CategoriesSaveResponse(req)
+Zikula.Categories.ResequenceCallback = function(req)
 {
     if (!req.isSuccess()) {
     	Zikula.showajaxerror(req.getMessage());
         return Zikula.TreeSortable.categoriesTree.revertInsertion();
     }
     return true;
+}
+
+
+function categories_meta_init()
+{
+    $('categories_meta_collapse').observe('click', categories_meta_click);
+    $('categories_meta_collapse').addClassName('z-toggle-link');
+    if ($('categories_meta_details').style.display != "none") {
+        $('categories_meta_collapse').removeClassName('z-toggle-link-open');
+        $('categories_meta_details').hide();
+    }
+}
+
+function categories_meta_click(event)
+{
+    event.preventDefault();
+    if ($('categories_meta_details').style.display != "none") {
+        Element.removeClassName.delay(0.9, $('categories_meta_collapse'), 'z-toggle-link-open');
+    } else {
+        $('categories_meta_collapse').addClassName('z-toggle-link-open');
+    }
+    switchdisplaystate('categories_meta_details');
 }
--- src/system/Categories/lib/Categories/Controller/Ajax.php
+++ src/system/Categories/lib/Categories/Controller/Ajax.php
@@ -15,18 +15,14 @@
 
 class Categories_Controller_Ajax extends Zikula_Controller_Ajax
 {
+
     /**
      * Resequence categories
      */
     public function resequence() {
-        if (!SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT)) {
-            LogUtil::registerPermissionError(null,true);
-            throw new Zikula_Exception_Forbidden();
-        }
-        if (!SecurityUtil::confirmAuthKey()) {
-            LogUtil::registerAuthidError();
-            throw new Zikula_Exception_Fatal();
-        }
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT));
+
         $data = json_decode(FormUtil::getPassedValue('data', null, 'post'), true);
         $cats = CategoryUtil::getSubCategories(1, true, true, true, true, true, '', 'id');
 
@@ -40,8 +36,249 @@ class Categories_Controller_Ajax extends Zikula_Controller_Ajax
             }
         }
 
-        $output['response'] = true;
+        $result = array(
+            'response' => true
+        );
         return new Zikula_Response_Ajax($output);
+    }
+
+    public function edit() {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT));
+
+        $cid = FormUtil::getPassedValue('cid', 0);
+        $root_id = FormUtil::getPassedValue('dr', 1);
+        $mode = FormUtil::getPassedValue('mode', 'new');
+        $parent = FormUtil::getPassedValue('parent', null, 'post');
+        $editCat  = '';
+
+        $languages = ZLanguage::getInstalledLanguages();
+
+        // indicates that we're editing
+        if ($mode == 'edit')
+        {
+            if (!SecurityUtil::checkPermission('Categories::category', "::", ACCESS_ADMIN)) {
+                return LogUtil::registerPermissionError();
+            }
+
+            if (!$cid) {
+                return LogUtil::registerError($this->__('Error! Cannot determine valid \'cid\' for edit mode in \'Categories_admin_edit\'.'));
+            }
+
+            $category = new Categories_DBObject_Category();
+            $editCat = $category->select($cid);
+            if ($editCat == false) {
+                return LogUtil::registerError($this->__('Sorry! No such item found.'), 404);
+            }
+        }
+        else
+        {
+            // new category creation
+            if (!SecurityUtil::checkPermission('Categories::category', '::', ACCESS_ADD)) {
+                return LogUtil::registerPermissionError();
+            }
+
+            // since we inherit the domain settings from the parent, we get
+            // the inherited (and merged) object from session
+            if (isset($_SESSION['newCategory']) && $_SESSION['newCategory']) {
+                $editCat = $_SESSION['newCategory'];
+                unset ($_SESSION['newCategory']);
+                $category = new Categories_DBObject_Category(); // need this for validation info
+            }
+            // if we're back from validation get the object from input
+            elseif (FormUtil::getValidationErrors()) {
+                $category = new Categories_DBObject_Category('V'); // need this for validation info
+                $editCat  = $category->get();
+            }
+            // someone just pressen 'new' -> populate defaults
+            else {
+                $category = new Categories_DBObject_Category(); // need this for validation info
+                $editCat['sort_value'] = '0';
+                $editCat['parent_id'] = $parent;
+            }
+        }
+
+        $attributes = isset($editCat['__ATTRIBUTES__']) ? $editCat['__ATTRIBUTES__'] : array();
+
+        $this->view->setCaching(false);
+
+        $this->view->assign('mode', $mode)
+                   ->assign('category', $editCat)
+                   ->assign('attributes', $attributes)
+                   ->assign('languages', $languages)
+                   ->assign('validation', $category->_objValidation);
+
+        if ($mode == 'edit') {
+            $this->view->assign('haveSubcategories', CategoryUtil::haveDirectSubcategories ($cid))
+                       ->assign('haveLeafSubcategories', CategoryUtil::haveDirectSubcategories ($cid, false, true));
+        }
+
+        $result = array(
+            'action' => $mode == 'new' ? 'add' : 'edit',
+            'result' => $this->view->fetch('categories_adminajax_edit.tpl')
+        );
+        return new Zikula_Response_Ajax($result);
+    }
+
+    public function copy() {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_ADD));
+
+        $cid = FormUtil::getPassedValue('cid', null, 'post');
+        $parent = FormUtil::getPassedValue('parent', null, 'post');
+
+        $cat = new Categories_DBObject_Category(DBObject::GET_FROM_DB, $cid);
+        $cat->copy($parent);
+
+        // need to find id of new category
+        // co get categories with path like source category
+        $cats = CategoryUtil::getCategoriesByPath($cat->getDataField('path'), 'id', 'path');
+
+        // find the one with path equal soruce cat and highest id - it will be new root cat
+        foreach($cats as $c) {
+            if ($c['path'] == $cat->getDataField('path')) {
+                $newRoot = $c['id'];
+            }
+        }
+
+        $categories = CategoryUtil::getSubCategories($newRoot, true, true, true, true, true);
+        $options = array(
+            'nullParent' => $cat->getDataField('parent_id'),
+            'withWraper' => false,
+        );
+        $node = CategoryUtil::getCategoryTreeJS((array)$categories, true, true, $options);
+
+        $result = array(
+            'action' => 'copy',
+            'cid' => $cid,
+            'copycid' => $newRoot,
+            'parent' => $parent,
+            'node' => $node,
+            'result' => true
+        );
+        return new Zikula_Response_Ajax($result);
+    }
+
+    public function delete() {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_DELETE));
+
+        $cid = FormUtil::getPassedValue('cid', null, 'post');
+        $cat = new Categories_DBObject_Category(DBObject::GET_FROM_DB, $cid);
+        $cat->delete(true);
+
+        $result = array(
+            'action' => 'delete',
+            'cid' => $cid,
+            'result' => true
+        );
+        return new Zikula_Response_Ajax($result);
+    }
+
+    public function activate() {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT));
+
+        $cid = FormUtil::getPassedValue('cid', null, 'post');
+        $cat = new Categories_DBObject_Category(DBObject::GET_FROM_DB, $cid);
+        $cat->setDataField('status', 'A');
+        $cat->update();
+
+        $result = array(
+            'action' => 'activate',
+            'cid' => $cid,
+            'result' => true
+        );
+        return new Zikula_Response_Ajax($result);
+    }
+
+    public function deactivate() {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT));
+
+        $cid = FormUtil::getPassedValue('cid', null, 'post');
+        $cat = new Categories_DBObject_Category(DBObject::GET_FROM_DB, $cid);
+        $cat->setDataField('status', 'I');
+        $cat->update();
+
+        $result = array(
+            'action' => 'deactivate',
+            'cid' => $cid,
+            'result' => true
+        );
+        return new Zikula_Response_Ajax($result);
+    }
+
+    public function save()
+    {
+        $this->checkAjaxToken();
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission('Categories::', '::', ACCESS_EDIT));
+
+        $mode = FormUtil::getPassedValue('mode', 'new');
+
+        $args = array();
+        $result = array();
+
+        $cat = new Categories_DBObject_Category();
+        $cat->getDataFromInput ();
+
+        if (!$cat->validate('admin')) {
+            // add validation info
+            $result['validation'] = 'error';
+            return new Zikula_Response_Ajax($result);
+        }
+
+        $attributes = array();
+        $values = FormUtil::getPassedValue('attribute_value', 'POST');
+        foreach (FormUtil::getPassedValue('attribute_name', 'POST') as $index => $name)
+        {
+            if (!empty($name)) {
+                $attributes[$name] = $values[$index];
+            }
+        }
+
+        $cat->setDataField('__ATTRIBUTES__', $attributes);
+
+        if ($mode == 'edit') {
+            // retrieve old category from DB
+            $category = FormUtil::getPassedValue ('category', null, 'POST');
+            $oldCat = new Categories_DBObject_Category(DBObject::GET_FROM_DB, $category['id']);
+
+            // update new category data
+            $cat->update();
+
+            // since a name change will change the object path, we must rebuild it here
+            if ($oldCat->getDataField('name') != $cat->getDataField('name')) {
+                CategoryUtil::rebuildPaths ('path', 'name', $cat->getDataField('id'));
+            }
+        } else {
+            $cat->insert();
+            // update new category data
+            $cat->update();
+        }
+
+        $categories = CategoryUtil::getSubCategories($cat->getDataField('id'), true, true, true, true, true);
+        $options = array(
+            'nullParent' => $cat->getDataField('parent_id'),
+            'withWraper' => false,
+        );
+        $node = CategoryUtil::getCategoryTreeJS((array)$categories, true, true, $options);
+
+        if ($mode == 'edit') {
+            $msg = __f('Done! Saved the %s category.', $oldCat->getDataField('name'));
+        } else {
+            $msg = __f('Done! Inserted the %s category.', $cat->getDataField('name'));
+        }
+        LogUtil::registerStatus($msg);
+
+        $result = array(
+            'action' => $mode == 'edit' ? 'edit' : 'add',
+            'cid' => $cat->getDataField('id'),
+            'parent' => $cat->getDataField('parent_id'),
+            'node' => $node,
+            'result' => true
+        );
+        return new Zikula_Response_Ajax($result);
     }
 
 }
