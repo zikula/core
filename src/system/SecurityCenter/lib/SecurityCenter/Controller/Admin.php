ef5882bb84fa6808efcba62e1fd69ba34beea6e2
rmburkhead <rmburkhead@gmail.com>
2011-02-19 02:10:56 -0700

    Fix calls to checkCsrfToken that still contain URLs Fixes #2854

--- src/system/SecurityCenter/lib/SecurityCenter/Controller/Admin.php
+++ src/system/SecurityCenter/lib/SecurityCenter/Controller/Admin.php
@@ -105,7 +105,7 @@ class SecurityCenter_Controller_Admin extends Zikula_Controller
             return LogUtil::registerPermissionError();
         }
 
-        $this->checkCsrfToken(ModUtil::url('SecurityCenter', 'admin', 'modifyconfig'));
+        $this->checkCsrfToken();
 
         $validates = true;
 
@@ -460,7 +460,7 @@ class SecurityCenter_Controller_Admin extends Zikula_Controller
             return LogUtil::registerPermissionError();
         }
 
-        $this->checkCsrfToken(ModUtil::url('SecurityCenter', 'admin', 'purifierconfig'));
+        $this->checkCsrfToken();
 
         // Load HTMLPurifier Classes
         $purifier = SecurityCenter_Util::getpurifier();
@@ -752,7 +752,7 @@ class SecurityCenter_Controller_Admin extends Zikula_Controller
         $redirect_url = ModUtil::url('SecurityCenter', 'admin', 'viewidslog');
 
         // Confirm authorisation code
-        $this->checkCsrfToken($redirect_url);
+        $this->checkCsrfToken();
 
         // delete all entries
         if (ModUtil::apiFunc('SecurityCenter', 'admin', 'purgeidslog')) {
@@ -801,7 +801,7 @@ class SecurityCenter_Controller_Admin extends Zikula_Controller
             return LogUtil::registerPermissionError();
         }
 
-        $this->checkCsrfToken(ModUtil::url('SecurityCenter', 'admin', 'allowedhtml'));
+        $this->checkCsrfToken();
 
         // update the allowed html settings
         $allowedhtml = array();
