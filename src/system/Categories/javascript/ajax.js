696a2910b18e6bd7d1b209b5c0f596785307fa9c
Pawel Preneta <jusuff@jabster.pl>
2011-02-21 23:42:38 +0100

    Refactor categories admin edit javascript (refs #2656)

--- src/system/Categories/javascript/ajax.js
+++ src/system/Categories/javascript/ajax.js
@@ -1,83 +1,79 @@
 // Copyright Zikula Foundation 2009 - license GNU/LGPLv3 (or at your option, any later version).
 
-Event.observe(window, 'load', categories_edit_init);
-
-function categories_edit_init()
-{
+$(document).observe('dom:loaded', function() {
     if ($('categories_meta_collapse')) {
-        categories_meta_init();
+        Zikula.Categories.Meta.Init();
     }
-}
+    if ($('category_attributes_add')) {
+        Zikula.Categories.Attributes.Init();
+    }
+});
 
+Zikula.define('Categories.Meta');
 
-function categories_meta_init()
-{
-    $('categories_meta_collapse').observe('click', categories_meta_click);
+Zikula.Categories.Meta.Init = function() {
+    $('categories_meta_collapse').observe('click', Zikula.Categories.Meta.Click);
     $('categories_meta_collapse').addClassName('z-toggle-link');
-    if ($('categories_meta_details').style.display != "none") {
+    if ($('categories_meta_details').visible()) {
         $('categories_meta_collapse').removeClassName('z-toggle-link-open');
         $('categories_meta_details').hide();
     }
-}
+};
 
-function categories_meta_click()
+Zikula.Categories.Meta.Click = function(event)
 {
-    if ($('categories_meta_details').style.display != "none") {
+    event.preventDefault();
+    if ($('categories_meta_details').visible()) {
         Element.removeClassName.delay(0.9, $('categories_meta_collapse'), 'z-toggle-link-open');
     } else {
         $('categories_meta_collapse').addClassName('z-toggle-link-open');
     }
-    switchdisplaystate('categories_meta_details');
-}
+    Zikula.switchdisplaystate('categories_meta_details');
+};
 
-var Category = {};
+Zikula.define('Categories.Attributes');
 
-Category.Attributes = {};
+Zikula.Categories.Attributes.Init = function () {
+    $('category_attributes_add').observe('click', Zikula.Categories.Attributes.Add);
+    $$('.category_attributes_remove').invoke('observe','click', Zikula.Categories.Attributes.Remove);
+};
 
-Category.Attributes.Add = function(inputElement)
-{
-    if ($('new_attribute_name').value == '' || $('new_attribute_value').value == '')
-    return false;
+Zikula.Categories.Attributes.Add = function(event) {
+    event.preventDefault();
+    var inputElement = event.element();
+    if ($('new_attribute_name').getValue().empty() || $('new_attribute_value').getValue().empty()) {
+        return false;
+    }
 
-    var td = inputElement.parentNode;
-    var tr = td.parentNode;
-    var table = tr.parentNode.parentNode;
+    var td = inputElement.up('td'),
+        tr = td.up('tr'),
+        table = tr.up('table'),
+        newRow = table.insertRow(tr.rowIndex+1);
 
-    var newRow = table.insertRow(tr.rowIndex+1);
-
-    var newTd1 = newRow.insertCell(0);
-    var newInput1 = document.createElement("input");
-    newInput1.name = "attribute_name[]";
-    newInput1.value = $('new_attribute_name').value;
-    $('new_attribute_name').value = '';
+    var newTd1 = newRow.insertCell(0),
+        newInput1 = new Element('input',{name: 'attribute_name[]', value: $('new_attribute_name').getValue()});
+    $('new_attribute_name').clear();
     newTd1.appendChild(newInput1);
 
-    var newTd2 = newRow.insertCell(1);
-    var newInput2 = document.createElement("input");
-    newInput2.name = "attribute_value[]";
-    newInput2.value = $('new_attribute_value').value;
-    newInput2.size = $('new_attribute_value').size;
-    $('new_attribute_value').value = '';
+    var newTd2 = newRow.insertCell(1),
+        newInput2 = new Element('input',{name: 'attribute_value[]', value: $('new_attribute_value').getValue()});
+    $('new_attribute_value').clear();
     newTd2.appendChild(newInput2);
 
-    var newTd3 = newRow.insertCell(2);
-    var newInput3 = document.createElement("input");
-    newInput3.type = 'image';
-    newInput3.src = 'images/icons/extrasmall/edit_remove.png';
-    newInput3.onclick = function() { return Category.Attributes.Remove(newInput3); }
+    var newTd3 = newRow.insertCell(2),
+        newInput3 = new Element('input',{type: 'image', src: 'images/icons/extrasmall/edit_remove.png'}).observe('click',Zikula.Categories.Attributes.Remove);
     newTd3.appendChild(newInput3);
 
     $('new_attribute_name').focus();
 
-    return false;
-}
+    return true;
+};
 
-
-Category.Attributes.Remove = function(inputElement)
-{
-    var td = inputElement.parentNode;
-    var tr = td.parentNode;
-    var table = tr.parentNode.parentNode;
+Zikula.Categories.Attributes.Remove = function(event) {
+    event.preventDefault();
+    var inputElement = event.element(),
+        tr = inputElement.up('tr'),
+        table = tr.up('table');
     table.deleteRow(tr.rowIndex);
-    return false;
-}
+    return true;
+};
