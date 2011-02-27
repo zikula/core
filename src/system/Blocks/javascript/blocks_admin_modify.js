144d0414bdf4b6f572c9c4ff66fe8278edf07254
Thanasis Fotis <tfotis@gmail.com>
2011-02-27 15:11:42 +0200

    improve usability and functionality of advanced block filtering, closes #2875
    if you happen to use filtering in one of your blocks, you will have to edit them and select the new filtering rules.

--- src/system/Blocks/javascript/blocks_admin_modify.js
+++ src/system/Blocks/javascript/blocks_admin_modify.js
@@ -5,9 +5,13 @@ Event.observe(window, 'load', blocks_modify_init);
 function blocks_modify_init()
 {
     $('blocks_advanced_placement_onclick').observe('click', blocks_advanced_placement_onclick);
-    $('block_placement_advanced').hide();
     $('blocks_advanced_placement_onclick').removeClassName('z-hide');
     $('blocks_advanced_placement_onclick').addClassName('z-show');
+
+    if (total_existing_filters == 0)
+    {
+        $('block_placement_advanced').hide();
+    }
 }
 
 function blocks_advanced_placement_onclick()
