d42492d68a316f2fc5a23472a760d6d0aa8dd346
Pawel Preneta <jusuff@jabster.pl>
2011-02-27 13:52:38 +0100

    Replace non-existing openwindow.js with Zikula.UI.Window (refs #2870)

--- src/system/Admin/templates/plugins/function.adminonlinemanual.php
+++ src/system/Admin/templates/plugins/function.adminonlinemanual.php
@@ -29,10 +29,12 @@ function smarty_function_adminonlinemanual($params, $smarty)
     $modinfo = ModUtil::getInfoFromName(ModUtil::getName());
     $modpath = ($modinfo['type'] == ModUtil::TYPE_SYSTEM) ? 'system' : 'modules';
     $file = DataUtil::formatForOS("$modpath/$modinfo[directory]/lang/$lang/manual.html");
+    fdump($file,is_readable($file));
     $man_link = '';
     if (is_readable($file)) {
-        PageUtil::setVar('javascript', 'javascript/openwindow.js');
-        $man_link = '<div style="margin-top: 20px; text-align:center">[ <a href="javascript:openwindow(\'' . $file . '\')">'.__('Online manual').'</a> ]</div>'."\n";
+        PageUtil::addVar('javascript', 'zikula.ui');
+        $man_link = '<div style="margin-top: 20px; text-align:center">[ <a id="online_manual" href="'. $file . '">'.__('Online manual').'</a> ]</div>'."\n";
+        $man_link .= '<script type="text/javascript">var online_manual = new Zikula.UI.Window($(\'online_manual\'),{resizable: true})</script>'."\n";
     }
     return $man_link;
 }
