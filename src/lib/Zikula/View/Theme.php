552dd3d2320b5d270dea0cdc3b3b5e1e11f17cac
Herr.Vorragend <carsten@zikula.org>
2011-02-28 09:56:55 +0100

    Removed all references for wrapper divs. Closes #2869

--- src/lib/Zikula/View/Theme.php
+++ src/lib/Zikula/View/Theme.php
@@ -644,9 +644,7 @@ class Zikula_View_Theme extends Zikula_View
         $maincontent = $this->eventManager->notify($event)->getData();
 
         // add the module wrapper
-        if (!$this->system) {
-            $maincontent = '<div id="z-maincontent" class="z-module-' . DataUtil::formatForDisplay(strtolower($this->toplevelmodule)) . '">' . $maincontent . '</div>';
-        }
+        $maincontent = '<div id="z-maincontent" class="z-module-' . DataUtil::formatForDisplay(strtolower($this->toplevelmodule)) . '">' . $maincontent . '</div>';
 
         // Assign the main content area to the template engine
         $this->assign_by_ref('maincontent', $maincontent);
