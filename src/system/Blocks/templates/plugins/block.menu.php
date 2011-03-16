f8ab75a4fb9bab043254e12c12950794ca7a9e09
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-03-15 23:55:03 -0500

    Coding standards

--- src/system/Blocks/templates/plugins/block.menu.php
+++ src/system/Blocks/templates/plugins/block.menu.php
@@ -33,22 +33,25 @@ function smarty_block_menu($params, $content, $smarty, &$repeat)
         $smarty->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('smarty_block_menu', 'item')));
         return false;
     }
+
     // find this block in smarty tag stack
-    foreach($smarty->_tag_stack as $key => $tag_stack) {
-        if($tag_stack[0] == 'menu') {
+    foreach ($smarty->_tag_stack as $key => $tag_stack) {
+        if ($tag_stack[0] == 'menu') {
             $menuTagStackKey = $key;
         }
     }
     if (is_null($content)) {
         $smarty->_tag_stack[$menuTagStackKey][1]['_content'] = array();
     }
+
     $from = $params['from'];
     $item = $params['item'];
-    $key = isset($params['key']) ? $params['key'] : null;
+    $key   = isset($params['key']) ? $params['key'] : null;
     $index = isset($params['_index']) ? $params['_index'] : 0;
     $total = count($from);
     $repeat = $index < $total;
     $iterator = new ArrayIterator($from);
+
     try {
         $iterator->seek($index);
         $currentKey = $iterator->key();
@@ -62,7 +65,7 @@ function smarty_block_menu($params, $content, $smarty, &$repeat)
         $lastKey = null;
     }
 
-    if(isset($params['name'])) {
+    if (isset($params['name'])) {
         $menuProps = array(
             'index' => $index,
             'iteration' => $index+1,
@@ -71,25 +74,26 @@ function smarty_block_menu($params, $content, $smarty, &$repeat)
             'last' =>  $index+1 >= $total
         );
     }
-    if($repeat || (empty($from) && is_null($content))) {
+
+    if ($repeat || (empty($from) && is_null($content))) {
         $smarty->assign($item, isset($from[$currentKey]) ? $from[$currentKey] : null);
         $smarty->assign('index', $index);
         $smarty->assign('total', $total);
-        if(isset($menuProps)) {
+        if (isset($menuProps)) {
             $smarty->assign($params['name'], $menuProps);
         }
-        if(isset($key)) {
+        if (isset($key)) {
             $smarty->assign($key,$currentKey);
         }
         $smarty->_tag_stack[$menuTagStackKey][1]['_index'] = $index+1;
-        if(!is_null($content)) {
+        if (!is_null($content)) {
             $smarty->_tag_stack[$menuTagStackKey][1]['_content'][$lastKey] = $content;
         }
-        if(empty($from) && is_null($content)) {
+        if (empty($from) && is_null($content)) {
             $repeat = true;
         }
     } else {
-        if(empty($from)) {
+        if (empty($from)) {
             $params['_content'] = $content;
             $result = _smarty_block_menu_parseheader($params);
         } else {
@@ -102,27 +106,35 @@ function smarty_block_menu($params, $content, $smarty, &$repeat)
             return $result;
         }
     }
+
     return;
 }
 
 function _smarty_block_menu_parsemenu($params)
 {
-    if(isset($params['multilang'])) {
+    if (isset($params['multilang'])) {
         $tmp = current($params['from']);
         $reflang = key($tmp);
     }
+
     $tree = array();
-    $map = array();
-    foreach($params['from'] as $i => $item) {
-        if(isset($reflang)) {
+    $map  = array();
+
+    foreach ($params['from'] as $i => $item)
+    {
+        if (isset($reflang)) {
             $item = $item[$reflang];
         }
+
         $item['content'] = $params['_content'][$i];
-        if(!isset($item['id'])) {
+
+        if (!isset($item['id'])) {
             $item['id'] = 'dummy_'.$i;
         }
+
         $_node = array('item' => $item, 'nodes' => array());
-        if(!isset($item['parentid']) || $item['parentid'] === null) {
+
+        if (!isset($item['parentid']) || $item['parentid'] === null) {
             $tree[$item['id']] = $_node;
             $path = null;
         } else {
@@ -130,7 +142,9 @@ function _smarty_block_menu_parsemenu($params)
             $path[] = $item['parentid'];
             $handle =& $tree;
             while (list($key, $value) = each($path)) {
-                if($value === null) continue;
+                if ($value === null) {
+                    continue;
+                }
                 $handle =& $handle[$value]['nodes'];
             }
             $handle[$item['id']] = $_node;
@@ -139,23 +153,23 @@ function _smarty_block_menu_parsemenu($params)
     }
 
     $listClass = isset($params['class']) ? $params['class'] : null;
-    $listId = isset($params['id']) ? $params['id'] : null;
-    $listTag = isset($params['tag']) ? $params['tag'] : 'ul';
+    $listId    = isset($params['id']) ? $params['id'] : null;
+    $listTag   = isset($params['tag']) ? $params['tag'] : 'ul';
 
     return _smarty_block_menu_parsemenu_html($tree,$listTag,$listClass,$listId);
 }
 
 function _smarty_block_menu_parsemenu_html($tree,$listTag,$listClass=null,$listId=null)
 {
-    $html = '<'.$listTag;
+    $html  = '<'.$listTag;
     $html .= !empty($listId) ? ' id="'.$listId.'"' : '';
     $html .= !empty($listClass) ? ' class="'.$listClass.'"' : '';
     $html .= '>';
 
     foreach ($tree as $tab) {
-        if(!empty($tab['nodes'])) {
+        if (!empty($tab['nodes'])) {
             $subhtml = _smarty_block_menu_parsemenu_html($tab['nodes'],$listTag);
-            $html .= preg_replace('/<\/li>\s*$/Di', $subhtml.'</li>', $tab['item']['content'],1);
+            $html   .= preg_replace('/<\/li>\s*$/Di', $subhtml.'</li>', $tab['item']['content'],1);
         } else {
             $html .= $tab['item']['content'];
         }
@@ -165,12 +179,14 @@ function _smarty_block_menu_parsemenu_html($tree,$listTag,$listClass=null,$listI
 
     return $html;
 }
+
 function _smarty_block_menu_parseheader($params)
 {
     $listClass = isset($params['class']) ? $params['class'] : null;
-    $listId = isset($params['id']) ? $params['id'] : null;
-    $listTag = isset($params['tag']) ? $params['tag'] : 'ul';
-    $html = '<'.$listTag;
+    $listId    = isset($params['id']) ? $params['id'] : null;
+    $listTag   = isset($params['tag']) ? $params['tag'] : 'ul';
+
+    $html  = '<'.$listTag;
     $html .= !empty($listId) ? ' id="'.$listId.'"' : '';
     $html .= !empty($listClass) ? ' class="'.$listClass.'"' : '';
     $html .= '>';
