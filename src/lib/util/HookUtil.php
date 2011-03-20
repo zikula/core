be5174826e76f60258728a8133e29e3ecdf6461a
Drak <drak@zikula.org>
2011-03-20 17:10:14 +0545

    Corrected class name.

--- src/lib/util/HookUtil.php
+++ src/lib/util/HookUtil.php
@@ -349,10 +349,9 @@ class HookUtil
 
         $serviceManager = ServiceUtil::getManager();
         $eventManager = EventUtil::getManager();
-        $sm = new Zikula_ServiceManager_Service('zikula.servicemanager');
         foreach ($handlers as $key => $handler) {
             if ($handler['serviceid'] && !$serviceManager->hasService($handler['serviceid'])) {
-                $definition = new Zikula_ServiceManager_Definition($handler['classname'], array($sm));
+                $definition = new Zikula_ServiceManager_Definition($handler['classname'], array(new Zikula_ServiceManager_Reference('zikula.servicemanager')));
                 $serviceManager->registerDefinition($handler['serviceid'], $definition);
                 $callable = self::resolveCallable($handler);
             } else {
