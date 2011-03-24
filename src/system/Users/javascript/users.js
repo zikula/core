baa6783da6c5bf7e5c1b00d80cfd0555df4ef64c
Pawel Preneta <jusuff@jabster.pl>
2011-03-24 18:44:27 +0100

    Refactor autocompleter in Users module to use csrf protection (refs #2824)

--- src/system/Users/javascript/users.js
+++ src/system/Users/javascript/users.js
@@ -18,18 +18,17 @@ function liveusersearch()
     $('deleteuser').observe('click', function() { 
         window.location.href = Zikula.Config.entrypoint + "?module=users&type=admin&func=deleteusers&uname=" + $F('username');
     });
-    new Ajax.Autocompleter('username', 'username_choices', Zikula.Config.baseURL + 'ajax.php?module=users&func=getusers',
-        {
-            paramName: 'fragment',
-            minChars: 3,
-            afterUpdateElement: function(data){
-                $('modifyuser').observe('click', function() {
-                    window.location.href = Zikula.Config.entrypoint + "?module=users&type=admin&func=modify&userid=" + $($(data).value).value;
-                });
-                $('deleteuser').observe('click', function() {
-                    window.location.href=Zikula.Config.entrypoint + "?module=users&type=admin&func=deleteusers&userid=" + $($(data).value).value;
-                });
-            }
+    var options = Zikula.Ajax.Request.defaultOptions({
+        paramName: 'fragment',
+        minChars: 3,
+        afterUpdateElement: function(data){
+            $('modifyuser').observe('click', function() {
+                window.location.href = Zikula.Config.entrypoint + "?module=users&type=admin&func=modify&userid=" + $($(data).value).value;
+            });
+            $('deleteuser').observe('click', function() {
+                window.location.href=Zikula.Config.entrypoint + "?module=users&type=admin&func=deleteusers&userid=" + $($(data).value).value;
+            });
         }
-    );
+    });
+    new Ajax.Autocompleter('username', 'username_choices', Zikula.Config.baseURL + 'ajax.php?module=users&func=getusers', options);
 }
