445cf2e66594f19ed0bbc368bd2c3587cb13e855
Drak <drak@zikula.org>
2011-03-26 07:20:01 +0545

    E_STRICT issues refs #2915

--- src/lib/Zikula/Controller/AbstractPlugin.php
+++ src/lib/Zikula/Controller/AbstractPlugin.php
@@ -84,13 +84,19 @@ abstract class Zikula_Controller_AbstractPlugin extends Zikula_AbstractControlle
      *
      * @return Zikula_Controller_Plugin
      */
-    protected function setView(Zikula_View_Plugin $view = null)
+    protected function setView(Zikula_View $view = null)
     {
+        // please note the docblock param signature is deliberately different to the method signature - drak
         if (is_null($view)) {
             if ($this->plugin->getPluginType() == Zikula_AbstractPlugin::TYPE_MODULE) {
                 $view = Zikula_View_Plugin::getModulePluginInstance($this->moduleName, $this->pluginName);
             } else {
                 $view = Zikula_View_Plugin::getSystemPluginInstance($this->pluginName);
+            }
+        } else {
+            if (!$view instanceof Zikula_View_Plugin) {
+                $name = is_object($view) ? get_class($view) : '$view';
+                throw new InvalidArgumentException(sprintf('%s must be an instance of Zikula_View_Plugin', $name));
             }
         }
 
