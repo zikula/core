cc1f1f3f8250189543180c2050019d8a961114dd
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-04-05 03:43:41 -0500

    formatpermalink Smarty modifier added to the core

---
+++ src/lib/viewplugins/modifier.formatpermalink.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2011 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Format permalink.
+ *
+ * Example:
+ *   {$MyString|formatpermalink}
+ *
+ * @param string $string         The contents to transform.
+ *
+ * @return string The modified output.
+ */
+function smarty_modifier_formatpermalink($string)
+{
+    return DataUtil::formatPermalink($string);
+}
