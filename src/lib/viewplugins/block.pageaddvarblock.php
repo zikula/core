4a16066ab2410d33c0c938bbc22c1575f6f81300
rmburkhead <rmburkhead@gmail.com>
2011-04-13 14:14:00 -0700

    New template block function: pageaddvarblock. Allows inline scripts, inline styles, etc. to be placed in head ('rawtext') or 'footer' (just before </body>).
    This template block function works similarly to {pageaddvar}, however only the 'rawtext' and 'footer' page variables are allowed. 'rawtext' is the default if no page var name is specified. This block function will place the contents of the block in either the head section of the page (if name is 'rawtext' or is not specified), or in the area just prior to the closing body tag (if name is 'footer'). It can be used to (among other things) place inline script blocks and inline style blocks specified in the template in a more appropriate place within the page, rather than leaving them in the flow of the HTML markup.
    
    For example, if the script within an inline script block will run when the DOM is loaded, it does not need to be in the flow of the HTML markup. Instead, it should be placed in the head section of the page. Surrounding the inline script block with {pageaddvarblock}...{/pageaddvarblock} (or explicitly, {pageaddvarblock name='rawtext'}...{/pageaddvarblock}) will place it there as if it had been placed there by PageUtil::adVar('rawtext', $content). (In fact that is exactly what happens within the block function.) Likewise, inline style blocks should be placed in the head section. Script blocks that cannot be placed in the head section, but that do not emit anything to the document's markup or modify any form controls in immediate mode, can be moved to the end of the document's markup in order to (very slightly, almost insignificantly) improve the document's rendering. Surround these blocks with {pageaddvarblock name='footer'}...{/pageaddvarblock}.

---
+++ src/lib/viewplugins/block.pageaddvarblock.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2011 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Zikula_View function to add the contents of a block to either the rawtext (header) or footer multicontent page variable
+ *
+ * This function adds the content of the block to either the end of the <head> portion of the page (using 'rawtext') or to 
+ * a position just prior to the closing </body> tag, using 'footer'.
+ *
+ * Available parameters:
+ *   - name:     The name of the page variable to set, either 'rawtext' or 'footer'; default is 'rawtext'
+ *
+ * Examples:
+ * 
+ *  This inline stylesheet will appear in the page's <head> section just before the closing </head>:
+ * <code>
+ *   {pageaddvarblock name='rawtext'}
+ *   <style type="text/css">
+ *       p { font-size: 1.5em; }
+ *   </style>
+ *   {/pageaddvarblock}
+ * </code>
+ * 
+ *  This inline script will appear in the page's <body> section just before the closing </body>:
+ * <code>
+ *   {pageaddvarblock name='footer'}
+ *   <script language="javascript" type="text/javascript">
+ *       alert ('The closing </body> tag is coming.');
+ *   </style>
+ *   {/pageaddvarblock}
+ * </code>
+ * 
+ * @param array       $params  All attributes passed to this function from the template.
+ * @param string      $content The content of the block.
+ * @param Zikula_View $view    Reference to the Zikula_View object.
+ *
+ * @return string
+ */
+function smarty_block_pageaddvarblock($params, $content, Zikula_View $view)
+{
+    if ($content) {
+        $varname = isset($params['name']) ? $params['name'] : 'rawtext';
+        
+        if (($varname != 'rawtext') && ($varname != 'footer')) {
+            throw new Zikula_Exception_Fatal(__f('Invalid page variable name: \'%1$s\'.', array($varname)));
+        }
+     
+        PageUtil::addVar($varname, $content);
+    }
+}
