14e0ec7c4cdabd17463aa8ffa0f7800f144b9180
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-04-16 13:32:50 -0500

    Blocks_MenutreeTree sync to the latest Zikula.Tree code. Closes #2998

--- src/system/Blocks/lib/Blocks/MenutreeTree.php
+++ src/system/Blocks/lib/Blocks/MenutreeTree.php
@@ -24,13 +24,13 @@ class  Blocks_MenutreeTree extends Zikula_Tree
      */
     public function __construct(array $config=array())
     {
-        $config = array_merge($config,array(
-            'langs'         => array('en'),
-            'flat'          => false,
-            'parseURL'      => false,
-            'sortable'      => false,
-            'dynamicClass'  => 'z-tree-dynamic',
-            'imagesDir'     => 'system/Blocks/images/menutree/',
+        $config = array_merge($config, array(
+            'langs'        => array('en'),
+            'flat'         => false,
+            'parseURL'     => false,
+            'sortable'     => false,
+            'dynamicClass' => 'z-tree-dynamic',
+            'imagesDir'    => 'system/Blocks/images/menutree/',
         ));
         parent::__construct($config);
     }
@@ -46,7 +46,7 @@ class  Blocks_MenutreeTree extends Zikula_Tree
         $map = array();
 
         $langs = $this->config['langs'];
-        $reflang =  $langs[0];
+        $reflang = $langs[0];
 
         foreach ($this->data as $a) {
             $item = array();
@@ -59,22 +59,23 @@ class  Blocks_MenutreeTree extends Zikula_Tree
                         $_item = current($a);
                     }
                     $_item['state'] = 0;
-                    $_item['lang'] = $lang;
+                    $_item['lang']  = $lang;
                 } else {
                     $_item = $a[$lang];
                 }
+
                 $item[$lang] = array(
-                    'id' => isset($_item['id']) ? $_item['id'] : null,
+                    'id'        => isset($_item['id']) ? $_item['id'] : null,
                     'parent_id' => isset($_item['parent']) ? $_item['parent'] : $this->config['nullParent'],
-                    'name' => isset($_item['name']) ? $_item['name'] : null,
-                    'title' => isset($_item['title']) ? $_item['title'] : null,
-                    'icon' => isset($_item['icon']) ? $_item['icon'] : null,
-                    'class' => isset($_item['className']) ? $_item['className'] : null,
-                    'active' => isset($_item['state']) ? $_item['state'] : true,
-                    'expanded' => isset($_item['expanded']) ? $_item['expanded'] : null,
-                    'href' => isset($_item['href']) ? $_item['href'] : '',
-                    'lang' => isset($_item['lang']) ? $_item['lang'] : '',
-                    'dynamic' => strpos($_item['href'],'{ext:') === 0,
+                    'name'      => isset($_item['name']) ? $_item['name'] : null,
+                    'title'     => isset($_item['title']) ? $_item['title'] : null,
+                    'icon'      => isset($_item['icon']) ? $_item['icon'] : null,
+                    'class'     => isset($_item['className']) ? $_item['className'] : null,
+                    'active'    => isset($_item['state']) ? $_item['state'] : true,
+                    'expanded'  => isset($_item['expanded']) ? $_item['expanded'] : null,
+                    'href'      => isset($_item['href']) ? $_item['href'] : '',
+                    'lang'      => isset($_item['lang']) ? $_item['lang'] : '',
+                    'dynamic'   => strpos($_item['href'],'{ext:') === 0,
                 );
                 if ($this->config['parseURL']) {
                     $item[$lang]['href'] = ModUtil::apiFunc('Blocks', 'user', 'encodebracketurl', $item[$lang]['href']);
@@ -91,11 +92,13 @@ class  Blocks_MenutreeTree extends Zikula_Tree
                 $this->tree[$a[$reflang]['id']] = $_node;
                 $path = null;
             } else {
-                $path = $map[$a[$reflang]['parent']];
+                $path   = $map[$a[$reflang]['parent']];
                 $path[] = $a[$reflang]['parent'];
                 $handle =& $this->tree;
                 while (list($key, $value) = each($path)) {
-                    if ($value === 0) continue;
+                    if ($value === 0) {
+                        continue;
+                    }
                     $handle =& $handle[$value]['nodes'];
                 }
                 $handle[$a[$reflang]['id']] = $_node;
@@ -116,6 +119,7 @@ class  Blocks_MenutreeTree extends Zikula_Tree
     {
         return $this->tree;
     }
+
     /**
      * Get HTML output
      *
@@ -132,15 +136,19 @@ class  Blocks_MenutreeTree extends Zikula_Tree
         $initScript = "
         <script type=\"text/javascript\">
             document.observe('dom:loaded', function() {
-                {$jsClass}.add('{$this->config['id']}','{$this->getConfigForScript()}');
+                {$jsClass}.add('{$this->config['id']}', '{$this->getConfigForScript()}');
             });
         </script>";
         PageUtil::addVar('header', $initScript);
+
+        $tree = $this->_toHTML($this->tree, 1, $this->config['id']);
+
         $wraperClass = !empty($this->config['wraperClass']) ? 'class="'.$this->config['wraperClass'].'"' : '';
-        $tree = $this->_toHTML($this->tree,$this->config['id']);
-        $this->html = "<div {$wraperClass}>{$tree}</div>";
+        $this->html  = "\n<div {$wraperClass}>\n{$tree}</div>";
+
         return $this->html;
     }
+
     /**
      * Parse single tree node to HTML
      *
@@ -152,42 +160,46 @@ class  Blocks_MenutreeTree extends Zikula_Tree
      *
      * @return string Node HTML code
      */
-    protected function _nodeToHTML($id,$tab, $size, $i,$nodeSub=null)
+    protected function _nodeToHTML($id, $tab, $size, $i, $nodeSub = null, $indentLevel = 1)
     {
         $langs = $this->config['langs'];
-        $reflang =  $langs[0];
+        $reflang = $langs[0];
         $isDynamic = false;
 
-        $links = array();
-        $item = $tab['item'];
-        $toggle = '<img class="'.$this->config['toggler'].'" alt="" src="'.$this->config['imagesDir'].$this->config['minus'].'" />';
+        $item   = $tab['item'];
+        $indent = str_repeat(' ', ($indentLevel+1)*4);
+
+        $toggle = $indent.'<img class="'.$this->config['toggler'].'" alt="" src="'.$this->config['imagesDir'].$this->config['minus'].'" />';
 
         $iconImage = !empty($item[$reflang]['icon']) ? $item[$reflang]['icon'] : $this->config['item'];
-        $iconImage = !empty($tab['nodes']) ?  $this->config['parentOpen'] : $this->config['item'];
-        $icon = '<img class="'.$this->config['icon'].'" alt="" src="'.$this->config['imagesDir'].$iconImage.'" />';
+        $iconImage = !empty($tab['nodes']) ?  $this->config['parentOpen'] : $iconImage;
+        $icon      = $indent.'<img class="'.$this->config['icon'].'" alt="" src="'.$this->config['imagesDir'].$iconImage.'" />';
 
-        foreach($item as $lang => $translated) {
+        $links = array();
+        foreach ($item as $lang => $translated) {
             $isDynamic = $isDynamic || $translated['dynamic'];
-            $class = $translated['active'] == 1 ? $translated['class'] : $this->config['nodeUnactive'].' '.$translated['class'];
-            $linkClass = !empty($class) ? ' class="'.$class.'"' : '';
-            $linkLang = 'lang="'.$translated['lang'].'"';
-            $linkHref = 'href="'.DataUtil::formatForDisplay($translated['href']).'"';
+            $linkClass = $translated['active'] == 1 ? $translated['class'] : $this->config['nodeUnactive'].' '.$translated['class'];
+            $linkClass = !empty($linkClass) ? ' class="'.$linkClass.'"' : '';
+            $linkLang  = ' lang="'.$translated['lang'].'"';
+            $linkHref  = ' href="'.DataUtil::formatForDisplay($translated['href']).'"';
             $linkTitle = !empty($translated['title']) ? ' title="'.$translated['title'].'"' : '';
 
-            $links[] = "<a {$linkHref} {$linkLang} {$linkTitle} {$linkClass}>{$translated['name']}</a>";
+            $links[] = "<a{$linkHref}{$linkLang}{$linkTitle}{$linkClass}>{$translated['name']}</a>";
         }
+        $links = $indent.implode("\n{$indent}", $links);
 
-        $liId = !empty($this->config['nodePrefix']) ? ' id="'.$this->config['nodePrefix'].$id.'"' : '';
-        $links = implode('',$links);
+        $liId    = !empty($this->config['nodePrefix']) ? ' id="'.$this->config['nodePrefix'].$id.'"' : '';
         $liClass = array();
         $liClass[] = $isDynamic ?  $this->config['dynamicClass'] : '';
         $liClass[] = $size == 1 ? $this->config['nodeSingle'] : '';
         $liClass[] = ($i == 1 && $size > 1) ? $this->config['nodeFirst'] : '';
         $liClass[] = ($i == $size && $size > 1) ? $this->config['nodeLast'] : '';
         $liClass[] = !empty($tab['nodes']) ? $this->config['nodeParent'] : $this->config['nodeLeaf'];
-        $liClass = trim(implode(' ', $liClass));
-        $liClass ='class="'.$liClass.'"';
+        $liClass = trim(implode(' ', array_filter($liClass)));
+        $liClass = ' class="'.$liClass.'"';
 
-        return "<li {$liId} {$liClass}>{$toggle}{$icon}{$links}{$nodeSub}</li>";
+        $indent = str_repeat(' ', $indentLevel*4);
+
+        return "{$indent}<li{$liId}{$liClass}>\n{$toggle}\n{$icon}\n{$links}\n{$nodeSub}{$indent}</li>";
     }
 }
