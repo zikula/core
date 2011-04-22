4a0f49ca709a93c7c5f1df6c1f3d8e3c3c2a94ae
Craig Heydenburg <craigh@mac.com>
2011-04-22 06:45:02 -0400

    add missing setVar in updateconfig. fixes #3010

--- src/system/Theme/lib/Theme/Controller/Admin.php
+++ src/system/Theme/lib/Theme/Controller/Admin.php
@@ -1052,6 +1052,7 @@ class Theme_Controller_Admin extends Zikula_AbstractController
 
         $render_lifetime = (int)FormUtil::getPassedValue('render_lifetime', isset($args['render_lifetime']) ? $args['render_lifetime'] : 3600, 'POST');
         if ($render_lifetime < -1) $render_lifetime = 3600;
+        $this->setVar('render_lifetime', $render_lifetime);
 
         $render_expose_template = (bool)FormUtil::getPassedValue('render_expose_template', isset($args['render_expose_template']) ? $args['render_expose_template'] : false, 'POST');
         $this->setVar('render_expose_template', $render_expose_template);
