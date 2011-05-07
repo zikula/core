4beae44ea9f1545b2982fd2a5667dd15bb1aa7a4
rmburkhead <rmburkhead@gmail.com>
2011-05-07 12:03:59 -0700

    Add validation of user-entered authentication information for the Users module. Closes #3031
    The user-entered credential validation for the Users module is fairly simplistic. It checks only that the required items are present and not empty.

--- src/system/Users/lib/Users/Controller/Authentication.php
+++ src/system/Users/lib/Users/Controller/Authentication.php
@@ -155,4 +155,86 @@ class Users_Controller_Authentication extends Zikula_Controller_AbstractAuthenti
                     ->fetch($templateName);
         }
     }
+    
+    /**
+     * Performs initial user-interface level validation on the user name and password received by the user from the login process.
+     * 
+     * Parameters passed in the $args array:
+     * -------------------------------------
+     * - array $args['authenticationMethod'] The authentication method (selected either by the user or by the system) for which
+     *                                          the credentials in $authenticationInfo were entered by the user. For the Users 
+     *                                          module, the 'modname' element should contain 'Users' and the 'method' element
+     *                                          should contain either 'uname' or 'email'.
+     * - array $args['authenticationInfo']   The user's credentials, as supplied by him on a log-in form on the log-in screen,
+     *                                          log-in block, or some other equivalent control. For the Users module, it should
+     *                                          contain the elements 'login_id' and 'pass'.
+     * 
+     * @param array $args The parameters for this function.
+     * 
+     * @return boolean True if the authentication information (the user's credentials) pass initial user-interface level validation;
+     *                  otherwise false and an error status message is set.
+     * 
+     * @throws Zikula_Exception_Fatal Thrown if no authentication module name or method is specified, or if the module name or method
+     *                                  is invalid for this module.
+     */
+    public function validateAuthenticationInformation(array $args)
+    {
+        $validates = false;
+        
+        $authenticationMethod = isset($args['authenticationMethod']) ? $args['authenticationMethod'] : array();
+        $authenticationInfo   = isset($args['authenticationInfo']) ? $args['authenticationInfo'] : array();
+        
+        if (!is_array($authenticationMethod) || empty($authenticationMethod) || !isset($authenticationMethod['modname'])) {
+            throw new Zikula_Exception_Fatal($this->__('The authentication module name was not specified during an attempt to validate user authentication information.'));
+        } elseif ($authenticationMethod['modname'] != 'Users') {
+            throw new Zikula_Exception_Fatal($this->__f('Attempt to validate authentication information with incorrect authentication module. Credentials should be validated with the \'%1$s\' module instead.', array($authenticationMethod['modname'])));
+        }
+        
+        if (!isset($authenticationMethod['method'])) {
+            throw new Zikula_Exception_Fatal($this->__('The authentication method name was not specified during an attempt to validate user authentication information.'));
+        } elseif (($authenticationMethod['method'] != 'uname') && ($authenticationMethod['method'] != 'email')) {
+            throw new Zikula_Exception_Fatal($this->__f('Unknown authentication method (\'%1$s\') while attempting to validate user authentication information in the Users module.', array($authenticationMethod['method'])));
+        }
+        
+        if (!is_array($authenticationInfo) || empty($authenticationInfo) || !isset($authenticationInfo['login_id']) 
+                || !is_string($authenticationInfo['login_id'])
+                ) {
+            // This is an internal error that the user cannot recover from, and should not happen (it is an exceptional situation).
+            if ($authenticationMethod['method'] == 'uname') {
+                throw new Zikula_Exception_Fatal($this->__('A user name was not specified, or the user name provided was invalid.'));
+            } else {
+                throw new Zikula_Exception_Fatal($this->__('An e-mail address was not specified, or the e-mail address provided was invalid.'));
+            }
+        }
+
+        if (!isset($authenticationInfo['pass']) || !is_string($authenticationInfo['pass'])) {
+            // This is an internal error that the user cannot recover from, and should not happen (it is an exceptional situation).
+            throw new Zikula_Exception_Fatal($this->__('A password was not specified, or the password provided was invalid.'));
+        }
+
+        // No need to be too fancy or too specific here. If the login id (the uname or email) is not empty, then that's sufficient.
+        // If we are too specific here, then we are giving a potential hacker too much information about how the authentication process
+        // works and what is expected. Just validate it enough so that a lookup can be performed.
+        if (!empty($authenticationInfo['login_id'])) {
+            if (!empty($authenticationInfo['pass'])) {
+                $validates = true;
+            } else {
+                $this->registerError($this->__('Please provide a password.'));
+            }
+        } elseif (empty($authenticationInfo['pass'])) {
+            if ($authenticationMethod['method'] == 'uname') {
+                $this->registerError($this->__('Please provide a user name and password.'));
+            } else {
+                $this->registerError($this->__('Please provide an e-mail address and password.'));
+            }
+        } else {
+            if ($authenticationMethod['method'] == 'uname') {
+                $this->registerError($this->__('Please provide a user name.'));
+            } else {
+                $this->registerError($this->__('Please provide an e-mail address.'));
+            }
+        }
+        
+        return $validates;
+    }
 }
