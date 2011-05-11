9cf98ca49d604e900a0902521e7678941f6a1591
Guite <info@guite.de>
2011-05-11 13:43:57 +0200

    bypass short urls if we have a language change without any parameters; otherwise the language code will be used as a module causing an error. A proper fix has to wait until we have a flexible routing functionality available.

--- src/system/Blocks/lib/Blocks/Block/Lang.php
+++ src/system/Blocks/lib/Blocks/Block/Lang.php
@@ -140,7 +140,8 @@ class Blocks_Block_Lang extends Zikula_Controller_AbstractBlock
         foreach ($languages as $code) {
             $thisurl = ModUtil::url($module, $type, $func, $get, null, null, $forcefqdn, !$shorturls, $code);
             if ($thisurl == '') {
-                $thisurl = ($shorturls ? $code : "$homepage?lang=$code");
+                //$thisurl = ($shorturls ? $code : "$homepage?lang=$code");
+                $thisurl = "$homepage?lang=$code";
             }
             $codeFS = ZLanguage::transformFS($code);
 
