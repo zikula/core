zikula/core

commit 82263ce2b931f63707db29eb52160cb796139a78

Theme Page Configuration form improved. Refs #3034
.ini file not filled with empty values.
lowercase block types for general configuration.
reintroduced modulewrapper and blockwrapper values.
improved tooltips and texts along the form

src/lib/Zikula/View/Theme.php
@@ -307,7 +307,7 @@ class Zikula_View_Theme extends Zikula_View
         ob_end_clean();
 
         // add the module wrapper
-        if (!$this->themeinfo['system']) {
+        if (!$this->themeinfo['system'] && (bool)$this->themeconfig['modulewrapper']) {
             $maincontent = '<div id="z-maincontent" class="'.($this->homepage ? 'z-homepage ' : '').'z-module-' . DataUtil::formatForDisplay(strtolower($this->toplevelmodule)) . '">' . $maincontent . '</div>';
         }
 
@@ -336,9 +336,9 @@ class Zikula_View_Theme extends Zikula_View
         // assign the block information
         $this->assign($block);
 
-        $bid = $block['bid'];
-        $bkey = $block['bkey'];
-        $position = $block['position'];
+        $bid      = $block['bid'];
+        $bkey     = strtolower($block['bkey']);
+        $position = strtolower($block['position']);
 
         // fix block positions - for now....
         if ($position == 'l') {
@@ -356,7 +356,7 @@ class Zikula_View_Theme extends Zikula_View
         $this->_plugins['outputfilter'] = array();
         // HACK: Save/restore cache settings
         $caching = $this->caching;
-        $this->caching = false;
+        $this->caching = Zikula_View::CACHE_DISABLED;
 
         $return = '';
         // determine the correct template and construct the output
@@ -366,10 +366,10 @@ class Zikula_View_Theme extends Zikula_View
         } elseif (isset($this->themeconfig['blocktypes'][$bkey]) && !empty($this->themeconfig['blocktypes'][$bkey])) {
             $return .= $this->fetch($this->themeconfig['blocktypes'][$bkey]);
 
-        } else if (isset($this->themeconfig['blockpositions'][$position]) && !empty($this->themeconfig['blockpositions'][$position])) {
+        } elseif (isset($this->themeconfig['blockpositions'][$position]) && !empty($this->themeconfig['blockpositions'][$position])) {
             $return .= $this->fetch($this->themeconfig['blockpositions'][$position]);
 
-        } else if (isset($this->themeconfig['block']) && !empty($this->themeconfig['block'])) {
+        } elseif (!empty($this->themeconfig['block'])) {
             $return .= $this->fetch($this->themeconfig['block']);
 
         } else {
@@ -384,7 +384,9 @@ class Zikula_View_Theme extends Zikula_View
         // HACK: Save/restore cache settings
         $this->caching = $caching;
 
-        $return = '<div class="z-block z-blockposition-' . DataUtil::formatForDisplay($block['position']) . ' z-bkey-' . DataUtil::formatForDisplay(strtolower($block['bkey'])) . ' z-bid-' . DataUtil::formatForDisplay($block['bid']) . '">' . "\n" . $return . "</div>\n";
+        if ((bool)$this->themeconfig['blockwrapper']) {
+            $return = '<div class="z-block z-blockposition-' . DataUtil::formatForDisplay($block['position']) . ' z-bkey-' . DataUtil::formatForDisplay(strtolower($block['bkey'])) . ' z-bid-' . DataUtil::formatForDisplay($block['bid']) . '">' . "\n" . $return . "</div>\n";
+        }
 
         return $return;
     }
@@ -676,29 +678,35 @@ class Zikula_View_Theme extends Zikula_View
         }
 
         // register any filters
-        if (isset($this->themeconfig['filters']) && !empty($this->themeconfig['filters'])) {
+        if (!empty($this->themeconfig['filters'])) {
+            // check for output filters
             if (isset($this->themeconfig['filters']['outputfilters']) && !empty($this->themeconfig['filters']['outputfilters'])) {
-                $this->themeconfig['filters']['outputfilters'] = explode(',', $this->themeconfig['filters']['outputfilters']);
-                foreach ($this->themeconfig['filters']['outputfilters'] as $filter) {
+                $filters = $this->themeconfig['filters']['outputfilters'];
+                $filters = !is_array($filters) ? explode(',', $filters) : $filters;
+                foreach ($filters as $filter) {
                     $this->load_filter('output', $filter);
                 }
             }
+            // check for pre filters
             if (isset($this->themeconfig['filters']['prefilters']) && !empty($this->themeconfig['filters']['prefilters'])) {
-                $this->themeconfig['filters']['prefilters'] = explode(',', $this->themeconfig['filters']['prefilters']);
-                foreach ($this->themeconfig['filters']['prefilters'] as $filter) {
+                $filters = $this->themeconfig['filters']['prefilters'];
+                $filters = !is_array($filters) ? explode(',', $filters) : $filters;
+                foreach ($filters as $filter) {
                     $this->load_filter('pre', $filter);
                 }
             }
+            // check for post filters
             if (isset($this->themeconfig['filters']['postfilters']) && !empty($this->themeconfig['filters']['postfilters'])) {
-                $this->themeconfig['filters']['postfilters'] = explode(',', $this->themeconfig['filters']['postfilters']);
-                foreach ($this->themeconfig['filters']['postfilters'] as $filter) {
+                $filters = $this->themeconfig['filters']['postfilters'];
+                $filters = !is_array($filters) ? explode(',', $filters) : $filters;
+                foreach ($filters as $filter) {
                     $this->load_filter('post', $filter);
                 }
             }
         }
 
         // load the palette if set
-        if (isset($this->themeconfig['palette'])) {
+        if (!empty($this->themeconfig['palette'])) {
             $inifile = $this->themepath . '/templates/config/themepalettes.ini';
             $this->load_vars($inifile, $this->themeconfig['palette'], 'palette');
         }

src/system/Theme/lib/Theme/Controller/Admin.php
@@ -468,26 +468,27 @@ class Theme_Controller_Admin extends Zikula_AbstractController
 
         // get all block positions
         $blockpositions = ModUtil::apiFunc('Blocks', 'user', 'getallpositions');
-        $positions = array();
-        foreach ($blockpositions as $blockposition) {
+        foreach ($blockpositions as $name => $blockposition) {
             // check the page configuration
             if (!isset($pageconfiguration['blockpositions'][$blockposition['name']])) {
-                $pageconfiguration['blockpositions'][$blockposition['name']] = '';
+                $pageconfiguration['blockpositions'][$name] = '';
             }
-            $positions[$blockposition['name']] = $blockposition['name'];
+            $blockpositions[$name] = $blockposition['description'];
         }
-        $this->view->assign('blockpositions', $positions);
+        $this->view->assign('blockpositions', $blockpositions);
 
         // call the block API to get a list of all available blocks
-        $this->view->assign('allblocks', $allblocks = BlockUtil::loadAll());
+        $allblocks = BlockUtil::loadAll();
         foreach ($allblocks as $key => $blocks) {
-            foreach ($blocks as $block) {
+            foreach ($blocks as $k => $block) {
+                $allblocks[$key][$k]['bkey'] = $bkey = strtolower($block['bkey']);
                 // check the page configuration
-                if (!isset($pageconfiguration['blocktypes'][$block['bkey']])) {
-                    $pageconfiguration['blocktypes'][$block['bkey']] = '';
+                if (!isset($pageconfiguration['blocktypes'][$bkey])) {
+                    $pageconfiguration['blocktypes'][$bkey] = '';
                 }
             }
         }
+        $this->view->assign('allblocks', $allblocks);
 
         // call the block API to get a list of all defined block instances
         $this->view->assign('blocks', $blocks = ModUtil::apiFunc('Blocks', 'user', 'getall'));
@@ -500,21 +501,23 @@ class Theme_Controller_Admin extends Zikula_AbstractController
 
         // palette default
         if (!isset($pageconfiguration['palette'])) {
-            $pageconfiguration['palette'] = null;
+            $pageconfiguration['palette'] = '';
         }
 
         // block  default
         if (!isset($pageconfiguration['block'])) {
-            $pageconfiguration['block'] = null;
+            $pageconfiguration['block'] = '';
         }
 
-        // filter defaults
-        if (!isset($pageconfiguration['filters'])) {
-            $pageconfiguration['filters'] = array(
-                    'outputfilters' => null,
-                    'prefilters' => null,
-                    'postfilters' => null
-            );
+        // filters defaults
+        if (!isset($pageconfiguration['filters']['outputfilters'])) {
+            $pageconfiguration['filters']['outputfilters'] = '';
+        }
+        if (!isset($pageconfiguration['filters']['prefilters'])) {
+            $pageconfiguration['filters']['prefilters'] = '';
+        }
+        if (!isset($pageconfiguration['filters']['postfilters'])) {
+            $pageconfiguration['filters']['postfilters'] = '';
         }
 
         // assign the page configuration array
@@ -533,17 +536,22 @@ class Theme_Controller_Admin extends Zikula_AbstractController
         $this->checkCsrfToken();
 
         // get our input
-        $themename = FormUtil::getPassedValue('themename', isset($args['themename']) ? $args['themename'] : null, 'POST');
-        $filename = FormUtil::getPassedValue('filename', isset($args['filename']) ? $args['filename'] : null, 'POST');
-        $pagetemplate = FormUtil::getPassedValue('pagetemplate', isset($args['pagetemplate']) ? $args['pagetemplate'] : null, 'POST');
-        $pagepalette = FormUtil::getPassedValue('pagepalette', isset($args['pagepalette']) ? $args['pagepalette'] : null, 'POST');
-        $blockpositiontemplates = FormUtil::getPassedValue('blockpositiontemplates', isset($args['blockpositiontemplates']) ? $args['blockpositiontemplates'] : null, 'POST');
-        $blocktypetemplates = FormUtil::getPassedValue('blocktypetemplates', isset($args['blocktypetemplates']) ? $args['blocktypetemplates'] : null, 'POST');
+        $themename     = FormUtil::getPassedValue('themename', isset($args['themename']) ? $args['themename'] : null, 'POST');
+        $filename      = FormUtil::getPassedValue('filename', isset($args['filename']) ? $args['filename'] : null, 'POST');
+        $pagetemplate  = FormUtil::getPassedValue('pagetemplate', isset($args['pagetemplate']) ? $args['pagetemplate'] : '', 'POST');
+        $blocktemplate = FormUtil::getPassedValue('blocktemplate', isset($args['blocktemplate']) ? $args['blocktemplate'] : '', 'POST');
+        $pagepalette   = FormUtil::getPassedValue('pagepalette', isset($args['pagepalette']) ? $args['pagepalette'] : '', 'POST');
+        $modulewrapper = FormUtil::getPassedValue('modulewrapper', isset($args['modulewrapper']) ? $args['modulewrapper'] : 1, 'POST');
+        $blockwrapper  = FormUtil::getPassedValue('blockwrapper', isset($args['blockwrapper']) ? $args['blockwrapper'] : 1, 'POST');
+
         $blockinstancetemplates = FormUtil::getPassedValue('blockinstancetemplates', isset($args['blockinstancetemplates']) ? $args['blockinstancetemplates'] : null, 'POST');
+        $blocktypetemplates     = FormUtil::getPassedValue('blocktypetemplates', isset($args['blocktypetemplates']) ? $args['blocktypetemplates'] : null, 'POST');
+        $blockpositiontemplates = FormUtil::getPassedValue('blockpositiontemplates', isset($args['blockpositiontemplates']) ? $args['blockpositiontemplates'] : null, 'POST');
+
         $filters = FormUtil::getPassedValue('filters', isset($args['filters']) ? $args['filters'] : null, 'POST');
 
         // check our input
-        if (!isset($themename) || empty($themename)) {
+        if (empty($themename) || empty($pagetemplate)) {
             return LogUtil::registerArgsError(ModUtil::url('Theme', 'admin', 'view'));
         }
 
@@ -558,11 +566,14 @@ class Theme_Controller_Admin extends Zikula_AbstractController
         }
 
         // form the new page configuration
-        $pageconfiguration['page'] = $pagetemplate;
-        $pageconfiguration['palette'] = $pagepalette;
-        $pageconfiguration['blocktypes'] = $blocktypetemplates;
-        $pageconfiguration['blockpositions'] = $blockpositiontemplates;
-        $pageconfiguration['blockinstances'] = $blockinstancetemplates;
+        $pageconfiguration['page']           = $pagetemplate;
+        $pageconfiguration['block']          = $blocktemplate;
+        $pageconfiguration['palette']        = $pagepalette;
+        $pageconfiguration['modulewrapper']  = $modulewrapper;
+        $pageconfiguration['blockwrapper']   = $blockwrapper;
+        $pageconfiguration['blockinstances'] = array_filter($blockinstancetemplates);
+        $pageconfiguration['blocktypes']     = array_filter($blocktypetemplates);
+        $pageconfiguration['blockpositions'] = array_filter($blockpositiontemplates);
 
         // check if the filters exists. We do this now and not when using them to increase performance
         $filters['outputfilters'] = $this->_checkfilters('outputfilter', $filters['outputfilters']);

src/system/Theme/style/style.css
@@ -27,7 +27,7 @@ img#theme_credits_preview {
 }
 
 div#theme_undefinedblockpositions {
-    width: 85%;
+    /*width: 85%;*/
 }
 
 div#theme_undefinedblockpositions p {

src/system/Theme/templates/theme_admin_modifypageconfigtemplates.tpl
@@ -1,19 +1,25 @@
+{pageaddvar name='javascript' value='zikula.ui'}
+
 {include file='theme_admin_menu.tpl'}
+
 <div class="z-admincontainer">
     {include file="theme_admin_modifymenu.tpl"}
     <div class="z-adminpageicon">{icon type="edit" size="large"}</div>
+
     <h2>{gt text="Edit page configuration"} - {$filename|safetext}</h2>
+
     <form class="z-form" action="{modurl modname="Theme" type="admin" func="updatepageconfigtemplates"}" method="post" enctype="application/x-www-form-urlencoded">
         <div>
             <input type="hidden" name="csrftoken" value="{insert name='csrftoken'}" />
             <input type="hidden" name="themename" value="{$themename|safetext}" />
             <input type="hidden" name="filename" value="{$filename|safetext}" />
+
             <fieldset>
                 <legend>{gt text="Page settings"}</legend>
+
                 <div class="z-formrow">
                     <label for="theme_pagetemplate">{gt text="Page template"}</label>
                     <select id="theme_pagetemplate" name="pagetemplate">
-                        <option value="">&nbsp;</option>
                         {html_options values=$moduletemplates output=$moduletemplates selected=$pageconfiguration.page}
                     </select>
                 </div>
@@ -32,9 +38,42 @@
                     </select>
                 </div>
             </fieldset>
+
+            <fieldset>
+                <legend>{gt text="Wrappers settings"}</legend>
+
+                <div class="z-formrow">
+                    <label for="theme_modulewrapper">{gt text="Enable main content wrapper"}</label>
+                    <div id="theme_modulewrapper">
+                        <input id="theme_modulewrapper_yes" type="radio" name="modulewrapper" value="1" {if $pageconfiguration.modulewrapper eq 1}checked="checked"{/if} />
+                        <label for="theme_modulewrapper_yes">{gt text="Yes"}</label>
+                        <input id="theme_modulewrapper_no" type="radio" name="modulewrapper" value="0" {if $pageconfiguration.modulewrapper eq 0}checked="checked"{/if} />
+                        <label for="theme_modulewrapper_no">{gt text="No"}</label>
+                    </div>
+                    <div class="z-informationmsg z-formnote">
+                        {gt text='This will add a wrapper for the main content, like:'}
+                        <br />&lt;div id="z-maincontent" class="z-module-$module"&gt;$maincontent&lt;/div&gt;
+                    </div>
+                </div>
+                <div class="z-formrow">
+                    <label for="theme_blockwrapper">{gt text="Enable block wrappers"}</label>
+                    <div id="theme_blockwrapper">
+                        <input id="theme_blockwrapper_yes" type="radio" name="blockwrapper" value="1" {if $pageconfiguration.blockwrapper eq 1}checked="checked"{/if} />
+                        <label for="theme_blockwrapper_yes">{gt text="Yes"}</label>
+                        <input id="theme_blockwrapper_no" type="radio" name="blockwrapper" value="0" {if $pageconfiguration.blockwrapper eq 0}checked="checked"{/if} />
+                        <label for="theme_blockwrapper_no">{gt text="No"}</label>
+                    </div>
+                    <div class="z-informationmsg z-formnote">
+                        {gt text='This will add a wrapper for each block content, like:'}
+                        <br />&lt;div class="z-block z-blockposition-$position z-bkey-$bkey z-bid-$bid"&gt;$blockcontent&lt;/div&gt;
+                    </div>
+                </div>
+            </fieldset>
+
             <fieldset>
                 <legend>{gt text="Filter settings"}</legend>
-                <p class="z-informationmsg">{gt text='Comma separated list of filters, eg. myfilter (for outputfilters) represents outputfilter.myfilter.php file, see <a href="http://www.smarty.net/manual/en/advanced.features.php">the Smarty documentation</a> for more information about filters.'}</p>
+
+                <p class="z-informationmsg z-formnote">{gt text='Comma separated list of filters, eg. myfilter (for outputfilters) represents outputfilter.myfilter.php file, see <a href="http://www.smarty.net/manual/en/advanced.features.php">the documentation</a> for more information about filters.'}</p>
                 <div class="z-formrow">
                     <label for="theme_filters_outputfilters">{gt text="Output filters"}</label>
                     <input type="text" id="theme_filters_outputfilters" name="filters[outputfilters]" size="40" maxlength="255" value="{$pageconfiguration.filters.outputfilters|safetext}" />
@@ -48,64 +87,78 @@
                     <input type="text" id="theme_filters_postfilters" name="filters[postfilters]" size="40" maxlength="255" value="{$pageconfiguration.filters.postfilters|safetext}" />
                 </div>
             </fieldset>
+
+            <div class="z-informationmsg">{gt text='The theme engine will consider the more specific setup; in that order, a block instance template, is used over a block type template, and a block position one.'}</div>
+
             <fieldset>
                 <legend>{gt text="Block position templates"}</legend>
-                {foreach from=$blockpositions item=position}
+
+                {foreach from=$blockpositions key='position' item='description'}
                 <div class="z-formrow">
-                    <label for="theme_blocktemplate_{$position|safetext}">{$position}</label>
+                    <label for="theme_blocktemplate_{$position|safetext}" title="{$description|safetext}">{$position}</label>
                     <select id="theme_blocktemplate_{$position|safetext}" name="blockpositiontemplates[{$position|safetext}]">
                         <option value="">{gt text="Default template"}</option>
                         {html_options values=$blocktemplates output=$blocktemplates selected=$pageconfiguration.blockpositions.$position}
                     </select>
                 </div>
                 {/foreach}
-                {capture assign=undefinedblockpositions}
+                {capture assign='undefinedblockpositions'}
                 {strip}
                 <ul>
-                    {assign var=undefinedblockposition value=false}
-                    {foreach name=blockpositions from=$pageconfiguration.blockpositions key=position item=template}
-                    {if $blockpositions.$position eq false}
-                    {assign var=undefinedblockposition value=true}
-                    <li{if $smarty.foreach.blockpositions.last} class="last"{/if}><a href="{modurl modname="Blocks" type="admin" func="newposition"}">{$position}</a></li>
-                    {/if}
+                    {assign var='undefinedblockposition' value=false}
+                    {foreach name='blockpositions' from=$pageconfiguration.blockpositions key='position' item='template'}
+                        {if !isset($blockpositions.$position)}
+                            {assign var='undefinedblockposition' value=true}
+                            <li><a href="{modurl modname="Blocks" type="admin" func="newposition" name=$position|safetext}">{$position|safetext}</a></li>
+                        {/if}
                     {/foreach}
                 </ul>
                 {/strip}
                 {/capture}
                 {if $undefinedblockposition eq true}
-                <div class="z-warningmsg" id="theme_undefinedblockpositions"><p>{gt text="The following block positions are used in this page configuration, but they have not been defined within the Blocks module;</p>%s<p>Click on a block position to go create that position." tag1=$undefinedblockpositions}</p></div>
+                <div class="z-warningmsg z-formnote" id="theme_undefinedblockpositions">{gt text="<p>The following block positions are used in this page configuration, but they have not been defined within the Blocks module;</p>%s<p>Click on a block position to go create that position.</p>" tag1=$undefinedblockpositions}</div>
                 {/if}
             </fieldset>
+
             <fieldset>
                 <legend>{gt text="Block type templates"}</legend>
-                {foreach from=$allblocks item=moduleblocks}
-                {foreach from=$moduleblocks item=block}
-                <div class="z-formrow">
-                    <label for="theme_blocktemplate_{$block.bkey|safetext}">{$block.module|safetext}/{$block.text_type_long|safetext}</label>
-                    {assign var=bkey value=$block.bkey}
-                    <select id="theme_blocktemplate_{$block.bkey|safetext}" name="blocktypetemplates[{$block.bkey|safetext}]">
-                        <option value="">{gt text="Default template"}</option>
-                        {html_options values=$blocktemplates output=$blocktemplates selected=$pageconfiguration.blocktypes.$bkey}
-                    </select>
-                </div>
+
+                {foreach from=$allblocks item='moduleblocks'}
+                {foreach from=$moduleblocks item='block' name='modblocks'}
+                {if $smarty.foreach.modblocks.first}
+                <fieldset>
+                    <legend>{$block.module|safetext}</legend>
+                    {/if}
+                    <div class="z-formrow">
+                        <label for="theme_blocktemplate_{$block.bkey|safetext}" title="{$block.text_type_long|safetext}">{$block.text_type|safetext}</label>
+                        {assign var=bkey value=$block.bkey}
+                        <select id="theme_blocktemplate_{$block.bkey|safetext}" name="blocktypetemplates[{$block.bkey|safetext}]">
+                            <option value="">{gt text="Default template"}</option>
+                            {html_options values=$blocktemplates output=$blocktemplates selected=$pageconfiguration.blocktypes.$bkey}
+                        </select>
+                    </div>
+                   {if $smarty.foreach.modblocks.last}
+                </fieldset>
+                {/if}
                 {/foreach}
                 {/foreach}
             </fieldset>
+
             <fieldset>
                 <legend>{gt text="Block instance templates"}</legend>
-                {foreach from=$blocks item=block}
+                {foreach from=$blocks item='block'}
                 <div class="z-formrow">
-                    <label for="theme_blocktemplate_{$block.bid|safetext}">
+                    <label for="theme_blocktemplate_{$block.bid|safetext}" title="{$block.description|safetext}">
                         {if $block.title neq ''}
-                        {$block.title|safetext}
+                            {$block.title|safetext}
                         {else}
-                        {gt text='Untitled block of type %1$s, ID %2$s' tag1=$block.bkey tag2=$block.bid"}
+                            {gt text='Untitled block of type %1$s' tag1=$block.bkey}
                         {/if}
+                        <span class="z-sub">({$block.bid})</span>
                     </label>
-                    {assign var=bid value=$block.bid}
                     <select id="theme_blocktemplate_{$block.bid|safetext}" name="blockinstancetemplates[{$block.bid|safetext}]">
                         <option value="">{gt text="Default template"}</option>
-                        {html_options values=$blocktemplates output=$blocktemplates selected=$pageconfiguration.blockinstances.$bid}
+                        {html_options values=$blocktemplates output=$blocktemplates selected=$pageconfiguration.blockinstances[$block.bid]}
                     </select>
                 </div>
                 {/foreach}
@@ -117,3 +170,7 @@
         </div>
     </form>
 </div>
+
+<script type="text/javascript">
+    Zikula.UI.Tooltips($$('label'));
+</script>

commit 06ce00606f407cae6f841a8f58557f04e8cd15ea

Fix administration area which was broken in commit 82263ce2b931f63707db29eb52160cb796139a78

src/lib/Zikula/View/Theme.php
@@ -304,7 +304,7 @@ class Zikula_View_Theme extends Zikula_View
         ob_end_clean();
 
         // add the module wrapper
-        if (!$this->themeinfo['system'] && (bool)$this->themeconfig['modulewrapper']) {
+        if (!$this->themeinfo['system']) {
             $maincontent = '<div id="z-maincontent" class="'.($this->homepage ? 'z-homepage ' : '').'z-module-' . DataUtil::formatForDisplay(strtolower($this->toplevelmodule)) . '">' . $maincontent . '</div>';
         }
 