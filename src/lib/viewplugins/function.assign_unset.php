4b0b831139423b931ea1b1c8795ee55e308704e2
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-05-18 00:27:37 -0500

    Added useful plugin to pop elements from array template vars

---
+++ src/lib/viewplugins/function.assign_unset.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_View
+ * @subpackage Template_Plugins
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Assign a value caching its parameters if cache is enabled.
+ *
+ * Available attributes:
+ *  - array  (string) Name of the template array variable to process
+ *  - field  (string) Name of the array field to assign then unset
+ *  - assign (string) Name of the assign variable to setup (optional)
+ *
+ * Example:
+ *
+ *  Having an $objarray in our template, we want to process its fields in different
+ *  sections of the template, so we get the needed fields separately on the desired positions,
+ *  clearing the array in the process.
+ *
+ *  For instance, the $hooks array resulted of notify the 'display_view' hooks, can be
+ *  processed individually using this plugin:
+ *
+ *  <samp>{assign_unset array='hooks' field='EZComments'}</samp>
+ *
+ *  And display later the remaining ones with a foreach.
+ *
+ * @param array       $params All attributes passed to this function from the template.
+ * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
+ *
+ * @return Void
+ */
+function smarty_function_assign_unset($params, Zikula_View $view)
+{
+    if (!isset($params['array'])) {
+        $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('assign_cache', 'var')));
+        return false;
+    }
+
+    if (!isset($params['field'])) {
+        $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('assign_cache', 'value')));
+        return false;
+    }
+
+    $value = null;
+
+    $array = $view->getTplVar($params['array']);
+
+    if ($array && isset($array[$params['field']])) {
+        $value = $array[$params['field']];
+
+        unset($array[$params['field']]);
+
+        $view->assign($params['array'], $array);
+    }
+
+    if (isset($params['assign']) && $params['assign']) {
+        $view->assign($params['assign'], $value);
+    } else {
+        return $value;
+    }
+}
