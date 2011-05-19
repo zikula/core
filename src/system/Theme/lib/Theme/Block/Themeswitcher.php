7e813cc4b3992ed474a56e92d41e0ae1c21fea3f
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-05-18 20:59:16 -0500

    Clears the Theme cache when a Block is updated. Refs #3030
    Pending review of Interactive Installer Controllers with theme cache enabled.

--- src/system/Theme/lib/Theme/Block/Themeswitcher.php
+++ src/system/Theme/lib/Theme/Block/Themeswitcher.php
@@ -144,6 +144,9 @@ class Theme_Block_Themeswitcher extends Zikula_Controller_AbstractBlock
         // clear the block cache
         $this->view->clear_cache('theme_block_themeswitcher.tpl');
 
+        // and clear the theme cache
+        Zikula_View_Theme::getInstance()->clear_cache();
+
         return $blockinfo;
     }
 }
