8a63ee152b0d7bdb53228bd2a86f2ecacbcf6203
Drak <drak@zikula.org>
2011-06-02 13:34:28 +0545

    Make PHP version check robust.
    The PHP version checks are done well after the core has begin initialisation.
    If there are any PHP 5.3 syntax used before the checks, the checks will never
    be reached since PHP will throw a syntax error.  The check has to happen here.

--- src/lib/bootstrap.php
+++ src/lib/bootstrap.php
@@ -11,6 +11,18 @@
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
  */
+
+// Check PHP version
+// Please note this version check must occur here and not in the internals of Zikula
+// because of syntax changes in PHP 5.3, the version checks may never be reached since
+// PHP will just bomb out with syntax errors.  There is no translation available at
+// this point either.
+$x = explode('.', str_replace('-', '.', phpversion()));
+$phpVersion = "$x[0].$x[1].$x[2]";
+if (version_compare($phpVersion, '5.3.2', '>=') == false) {
+    die("Zikula requires PHP version 5.3.2 or greater. Your server is using version $phpVersion.");
+}
+
 include 'lib/ZLoader.php';
 ZLoader::register();
 
