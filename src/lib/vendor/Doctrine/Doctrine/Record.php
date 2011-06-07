1e7af091e9d18acfddef14210ed827ae0759b128
Mateo Tibaquirá Palacios <mateo@zikula.org>
2011-06-06 19:37:56 -0500

    Patch for Doctrine_Record->unlinkInDb field names on unlink queries
    Their jira is down ATM to report this.
    I've made it work unlinking Clip relations.
    Clean patch is explicit on https://gist.github.com/1011438

--- src/lib/vendor/Doctrine/Doctrine/Record.php
+++ src/lib/vendor/Doctrine/Doctrine/Record.php
@@ -2478,18 +2478,18 @@ abstract class Doctrine_Record extends Doctrine_Record_Abstract implements Count
             $q = $rel->getAssociationTable()
                 ->createQuery()
                 ->delete()
-                ->where($rel->getLocal() . ' = ?', array_values($this->identifier()));
+                ->where($rel->getLocalFieldName() . ' = ?', array_values($this->identifier()));
 
             if (count($ids) > 0) {
-                $q->whereIn($rel->getForeign(), $ids);
+                $q->whereIn($rel->getForeignFieldName(), $ids);
             }
 
             $q->execute();
         } else if ($rel instanceof Doctrine_Relation_ForeignKey) {
             $q = $rel->getTable()->createQuery()
                 ->update()
-                ->set($rel->getForeign(), '?', array(null))
-                ->addWhere($rel->getForeign() . ' = ?', array_values($this->identifier()));
+                ->set($rel->getForeignFieldName(), '?', array(null))
+                ->addWhere($rel->getForeignFieldName() . ' = ?', array_values($this->identifier()));
 
             if (count($ids) > 0) {
                 $q->whereIn($rel->getTable()->getIdentifier(), $ids);
