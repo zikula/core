2e623ecaea4afd224c32159365853ed86e623888
Drak <drak@zikula.org>
2011-06-10 09:57:32 +0545

    Sync to latest DoctrineExtensions.

--- src/plugins/DoctrineExtensions/lib/Gedmo/Mapping/ExtensionMetadataFactory.php
+++ src/plugins/DoctrineExtensions/lib/Gedmo/Mapping/ExtensionMetadataFactory.php
@@ -143,7 +143,6 @@ final class ExtensionMetadataFactory
             // create driver instance
             $driverClassName = $this->extensionNamespace . '\Mapping\Driver\\' . $driverName;
             if (!class_exists($driverClassName)) {
-                // @TODO: implement XML driver also
                 $driverClassName = $this->extensionNamespace . '\Mapping\Driver\Annotation';
                 if (!class_exists($driverClassName)) {
                     throw new \Gedmo\Exception\RuntimeException("Failed to fallback to annotation driver: ({$driverClassName}), extension driver was not found.");
@@ -153,6 +152,7 @@ final class ExtensionMetadataFactory
             $driver->setOriginalDriver($omDriver);
             if ($driver instanceof FileDriver) {
                 $driver->setPaths($omDriver->getPaths());
+                $driver->setExtension($omDriver->getFileExtension());
             }
             if ($driver instanceof AnnotationDriverInterface) {
                 $driver->setAnnotationReader($this->annotationReader);
