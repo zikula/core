26317c0a9290ba77dd2e0138b001d9347fcde506
Drak <drak@zikula.org>
2011-07-05 06:42:17 +0545

    Upgrade to Doctrine 2.1.0 closes #3110

--- src/plugins/Doctrine/lib/vendor/Doctrine/ORM/Mapping/Driver/AnnotationDriver.php
+++ src/plugins/Doctrine/lib/vendor/Doctrine/ORM/Mapping/Driver/AnnotationDriver.php
@@ -21,10 +21,9 @@ namespace Doctrine\ORM\Mapping\Driver;
 
 use Doctrine\Common\Cache\ArrayCache,
     Doctrine\Common\Annotations\AnnotationReader,
+    Doctrine\Common\Annotations\AnnotationRegistry,
     Doctrine\ORM\Mapping\ClassMetadataInfo,
     Doctrine\ORM\Mapping\MappingException;
-
-require __DIR__ . '/DoctrineAnnotations.php';
 
 /**
  * The AnnotationDriver reads the mapping metadata from docblock annotations.
@@ -42,7 +41,7 @@ class AnnotationDriver implements Driver
      *
      * @var AnnotationReader
      */
-    private $_reader;
+    protected $_reader;
 
     /**
      * The paths where to look for mapping files.
