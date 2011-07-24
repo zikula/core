b28e4669970d74ea31246d7de87c2a28b5fb9ad2
Drak <drak@zikula.org>
2011-07-24 11:34:08 +0545

    Correct error checking of {assign_concat} plugin.

--- src/lib/viewplugins/function.assign_concat.php
+++ src/lib/viewplugins/function.assign_concat.php
@@ -43,7 +43,7 @@
  */
 function smarty_function_assign_concat($params, Zikula_View $view)
 {
-    if (!isset($params['name']) || $params['name']) {
+    if (!isset($params['name']) || !$params['name']) {
         $view->trigger_error(__f('Invalid %1$s passed to %2$s.', array('name', 'assign_concat')));
         return false;
     }
