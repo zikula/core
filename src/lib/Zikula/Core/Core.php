a1c4ec76ab1f067c4d50ddaeb54dfcdde8e8d9a8
Drak <drak@zikula.org>
2011-11-30 14:16:17 +0545

    Remove compat layer

--- src/lib/Zikula/Core/Core.php
+++ src/lib/Zikula/Core/Core.php
@@ -379,10 +379,6 @@ class Core
 
         // Initialise and load configuration
         if ($stage & self::STAGE_CONFIG) {
-            if (\System::isLegacyMode()) {
-                require_once 'lib/legacy/Compat.php';
-            }
-
             // error reporting
             if (!\System::isInstalling()) {
                 // this is here because it depends on the config.php loading.
