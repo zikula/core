a859cc80c9f2363d8833951747fb2df599d4e0bc
Drak <drak@zikula.org>
2011-12-02 12:23:28 +0545

    Regenerate POT and bump version number for 1.3.2

--- src/lib/Zikula/Core.php
+++ src/lib/Zikula/Core.php
@@ -37,7 +37,7 @@ class Zikula_Core
     /**
      * The core Zikula version number.
      */
-    const VERSION_NUM = '1.3.1-dev';
+    const VERSION_NUM = '1.3.2';
 
     /**
      * The version ID.
