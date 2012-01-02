ba6f37b8bdae18ae10d4b960a67a5f8edad0247f
Drak <drak@zikula.org>
2012-01-02 10:01:41 +0545

    Refactor Ajax responses.

---
+++ src/lib/legacy/Zikula/Response/Ajax.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2010 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Response
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+/**
+ * Ajax class.
+ */
+class Zikula_Response_Ajax extends Zikula\Framework\Response\Ajax\AbstractMediatorBaseResponse
+{
+}
