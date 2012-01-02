31b173dce90f73050505960aef5625f6f89c705f
Drak <drak@zikula.org>
2012-01-02 10:01:41 +0545

    Refactor Ajax responses.

---
+++ src/lib/Zikula/Framework/Response/Ajax/BadDataResponse.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2010 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Response
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Framework\Response\Ajax;
+
+/**
+ * Ajax class.
+ */
+class BadDataResponse extends AbstractErrorResponse
+{
+    /**
+     * Response code.
+     *
+     * @var integer
+     */
+    protected $statusCode = 400;
+}
