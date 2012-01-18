8ab465a50a4508da5b093c9679a8025998049a41
Drak <drak@zikula.org>
2012-01-18 21:14:06 +0545

    Add support for plain responses which prevent the response being rendered in the theme.

---
+++ src/lib/Zikula/Framework/Response/PlainResponse.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2010 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Response
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Framework\Response;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Plain response will override theme.
+ */
+class PlainResponse extends Response
+{
+
+}
