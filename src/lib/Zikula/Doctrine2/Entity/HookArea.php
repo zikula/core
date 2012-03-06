6d0307f914e27a51eb6bb596f0aa492e3ae76b00
Craig Heydenburg <craigh@mac.com>
2012-03-06 13:49:21 -0500

    code formatting

--- src/lib/Zikula/Doctrine2/Entity/HookArea.php
+++ src/lib/Zikula/Doctrine2/Entity/HookArea.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ * Copyright Zikula Foundation 2012 - Zikula Application Framework
  *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
  *
  * @license GNU/LGPLv3 (or at your option, any later version).
- * @package Zikula_Form
+ * @package Zikula
+ * @subpackage Hooks
  *
  * Please see the NOTICE file distributed with this source code for further
  * information regarding copyright and licensing.
@@ -59,62 +60,109 @@ class Zikula_Doctrine2_Entity_HookArea {
      */    
     private $areaname;
     
-    /* getters and setters */
+    /**
+     * Get Row Id
+     * @return integer 
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Set Row Id
+     * @param integer $id 
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
+    /**
+     * Get Hook Area Owner
+     * @return string 
+     */
     public function getOwner()
     {
         return $this->owner;
     }
 
+    /**
+     * Set Hook Area Owner
+     * @param string $owner 
+     */
     public function setOwner($owner)
     {
         $this->owner = $owner;
     }
 
+    /**
+     * Get Hook Area SubOwner
+     * @return string
+     */
     public function getSubowner()
     {
         return $this->subowner;
     }
 
+    /**
+     * Set Hook Area SubOwner
+     * @param string $subowner 
+     */
     public function setSubowner($subowner)
     {
         $this->subowner = $subowner;
     }
 
+    /**
+     * Get Hook Area type
+     * @return string 
+     */
     public function getAreatype()
     {
         return $this->areatype;
     }
 
+    /**
+     * Set Hook Area type
+     * @param string $areatype 
+     */
     public function setAreatype($areatype)
     {
         $this->areatype = $areatype;
     }
 
+    /**
+     * Get Hook Area category
+     * @return string
+     */
     public function getCategory()
     {
         return $this->category;
     }
 
+    /**
+     * Set Hook Area cetgory
+     * @param type $category 
+     */
     public function setCategory($category)
     {
         $this->category = $category;
     }
 
+    /**
+     * Get Hook AreaName
+     * @return string 
+     */
     public function getAreaname()
     {
         return $this->areaname;
     }
 
+    /**
+     * Set Hook AreaName
+     * @param string $areaname 
+     */
     public function setAreaname($areaname)
     {
         $this->areaname = $areaname;
