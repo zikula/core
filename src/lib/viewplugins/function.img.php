b89ad2f613f0da25c3ffd08503c20737b71629a7
Mateo TibaPalacios <nestormateo@gmail.com>
2012-03-14 19:52:10 -0500

    Update and full revision of the {img} core plugin

--- src/lib/viewplugins/function.img.php
+++ src/lib/viewplugins/function.img.php
@@ -23,6 +23,8 @@
  * Available parameters:
  *   - src            The file name of the image
  *   - modname        The well-known name of a module (default: the current module)
+ *   - modplugin      The name of the plugin in the passed module
+ *   - sysplugin      The name of the system plugin
  *   - width, height  If set, they will be passed. If none is set, they are obtained from the image
  *   - alt            If not set, an empty string is being assigned
  *   - title          If set it will be passed as a title attribute
@@ -30,19 +32,25 @@
  *   - optional       If set then the plugin will not return an error if an image is not found
  *   - default        If set then a default image is used should the requested image not be found (Note: full path required)
  *   - set            If modname is 'core' then the set parameter is set to define the directory in /images/
- *   - nostoponerror  If set and error ocurs (image not found or src is no image), do not trigger_error, but return false and fill pnimg_error instead
+ *   - nostoponerror  If set and error ocurs (image not found or src is no image), do not trigger_error, but return false
  *   - retval         If set indicated the field to return instead the array of values (src, width, etc.)
  *   - fqurl          If set the image path is absolute, if not relative
  *   - all remaining parameters are passed to the image tag
  *
- * Example: {img src="heading.png" }
- * Output:  <img src="modules/Example/images/en/heading.png" alt="" width="261" height="69"  />
+ * Example: {img src='heading.png'}
+ * Output:  <img src="modules/Example/images/en/heading.png" alt="" width="261" height="69" />
  *
- * Example: {img src="heading.png" width="100" border="1" alt="foobar" }
- * Output:  <img src="modules/Example/images/en/heading.png" width="100" border="1" alt="foobar"  />
+ * Example: {img src='heading.png' width='100' border='1' __alt='foobar'}
+ * Output:  <img src="modules/Example/images/en/heading.png" width="100" border="1" alt="foobar" />
  *
- * Example {img src=xhtml11.png modname=core set=powered}
- * <img src="/Theme/images/powered/xhtml11.png" alt="" width="88" height="31"  />
+ * Example: {img src='xhtml11.png' modname='core' set='powered'}
+ * Output:  <img src="themes/Theme/images/powered/xhtml11.png" alt="" width="88" height="31" />
+ *
+ * Example: {img src='iconX.png' modname='ModName' modplugin='Plug1' set='icons'}
+ * Output:  <img src="modules/ModName/plugins/Plug1/images/icons/iconX.png" alt="" width="16" height="16" />
+ *
+ * Example: {img src='iconY.png' sysplugin='Plug2' set='icons/small'}
+ * Output:  <img src="plugins/Plug2/images/icons/small/iconY.png" alt="" width="16" height="16" />
  *
  * If the parameter assign is set, the results are assigned as an array. The components of
  * this array are the same as the attributes of the img tag; additionally an entry 'imgtag' is
@@ -66,89 +74,111 @@
  */
 function smarty_function_img($params, Zikula_View $view)
 {
-    $nostoponerror = (isset($params['nostoponerror'])) ? true : false;
+    $nostoponerror = isset($params['nostoponerror']) && $params['nostoponerror'] ? true : false;
 
-    if (!isset($params['src'])) {
-        $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('img', 'src')));
-        if ($nostoponerror == true) {
+    if (!isset($params['src']) || !$params['src']) {
+        if (!$nostoponerror) {
+            $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('img', 'src')));
             return;
         } else {
             return false;
         }
     }
 
-    // default for the module
-    $modname = isset($params['modname']) ? $params['modname'] : $view->toplevelmodule;
+    // process the image location
+    $modname   = isset($params['modname']) ? $params['modname'] : $view->toplevelmodule;
+    $modplugin = isset($params['modplugin']) ? $params['modplugin'] : null;
+    $sysplugin = isset($params['sysplugin']) ? $params['sysplugin'] : null;
 
-    // if the module name is 'core' then we require an image set
+    // process the image set
+    $set   = isset($params['set']) ? $params['set'] : null;
+    $osset = DataUtil::formatForOS($set);
+
+    // if the module name is 'core'
     if ($modname == 'core') {
-        if (!isset($params['set'])) {
-            $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('img', 'set')));
-            if ($nostoponerror == true) {
+        if (!$osset) {
+            if (!$nostoponerror) {
+                $view->trigger_error(__f('Error! in %1$s: the %2$s parameter must be specified.', array('img', 'set')));
                 return;
             } else {
                 return false;
             }
         }
-        $osset = DataUtil::formatForOS($params['set']);
+
         if (System::isLegacyMode() && (strpos($osset, 'icons/') !== false || strpos($osset, 'global/') !== false) && strpos($params['src'], '.gif')) {
             LogUtil::log(__f('Core image %s does not exist, please use the png format (called from %s).', array($params['src'], $view->getTemplatePath())), E_DEPRECATED);
             $params['src'] = str_replace('.gif', '.png', $params['src']);
         }
     }
 
-    // default for the optional flag
-    $optional = isset($params['optional']) ? $params['optional'] : true;
-
     // always provide an alt attribute.
     // if none is set, assign an empty one.
     $params['alt'] = isset($params['alt']) ? $params['alt'] : '';
 
-    if (!isset($params['title'])) {
-        $params['title'] = '';
-    }
-
     // prevent overwriting surrounding titles (#477)
-    if (empty($params['title'])) {
+    if (isset($params['title']) && empty($params['title'])) {
         unset($params['title']);
     }
 
     // language
     $lang =  ZLanguage::transformFS(ZLanguage::getLanguageCode());
 
-    // theme directory
-    $theme         = DataUtil::formatForOS(UserUtil::getTheme());
-    $osmodname     = DataUtil::formatForOS($modname);
-    $themelangpath = "themes/$theme/templates/modules/$osmodname/images/$lang";
-    $themepath     = "themes/$theme/templates/modules/$osmodname/images";
-    $corethemepath = "themes/$theme/images";
+    if ($sysplugin) {
+        $osplugdir    = DataUtil::formatForOS($sysplugin);
+        $pluglangpath = "plugins/$osplugdir/images/$lang";
+        $plugpath     = "plugins/$osplugdir/images";
 
-    // module directory
-    $modinfo       = ModUtil::getInfoFromName($modname);
-    $osmoddir      = DataUtil::formatForOS($modinfo['directory']);
-    $moduleDir     = ($modinfo['type'] == ModUtil::TYPE_SYSTEM ? 'system' : 'modules');
-    if ($modname == 'core') {
-        $modpath        = "images/$osset";
+        // form the array of paths
+        $paths = array($pluglangpath, $plugpath);
+
     } else {
-        $modlangpath    = "$moduleDir/$osmoddir/images/$lang";
-        $modpath        = "$moduleDir/$osmoddir/images";
-        $modlangpathOld = "$moduleDir/$osmoddir/pnimages/$lang";
-        $modpathOld     = "$moduleDir/$osmoddir/pnimages";
+        // module directory
+        $modinfo   = ModUtil::getInfoFromName($modname);
+        $osmoddir  = DataUtil::formatForOS($modinfo['directory']);
+        $moduleDir = ($modinfo['type'] == ModUtil::TYPE_SYSTEM ? 'system' : 'modules');
+
+        if ($modplugin) {
+            $osmodplugdir    = DataUtil::formatForOS($modplugin);
+            $modpluglangpath = "$moduleDir/$osmoddir/plugins/$osmodplugdir/images/$lang";
+            $modplugpath     = "$moduleDir/$osmoddir/plugins/$osmodplugdir/images";
+
+            // form the array of paths
+            $paths = array($modpluglangpath, $modplugpath);
+
+        } else {
+            // theme directory
+            $ostheme       = DataUtil::formatForOS(UserUtil::getTheme());
+            $osmodname     = DataUtil::formatForOS($modname);
+            $themelangpath = "themes/$ostheme/templates/modules/$osmodname/images/$lang";
+            $themepath     = "themes/$ostheme/templates/modules/$osmodname/images";
+            $corethemepath = "themes/$ostheme/images";
+
+            if ($modname == 'core') {
+                $modpath        = "images";
+            } else {
+                $modlangpath    = "$moduleDir/$osmoddir/images/$lang";
+                $modpath        = "$moduleDir/$osmoddir/images";
+                $modlangpathOld = "$moduleDir/$osmoddir/pnimages/$lang";
+                $modpathOld     = "$moduleDir/$osmoddir/pnimages";
+            }
+
+            // form the array of paths
+            if ($modname == 'core') {
+                $paths = array($themepath, $corethemepath, $modpath);
+            } else {
+                $paths = array($themelangpath, $themepath, $corethemepath, $modlangpath, $modpath, $modlangpathOld, $modpathOld);
+            }
+        }
     }
+
     $ossrc = DataUtil::formatForOS($params['src']);
-
-    // form the array of paths
-    if ($modname == 'core') {
-        $paths = array($themepath, $corethemepath, $modpath);
-    } else {
-        $paths = array($themelangpath, $themepath, $corethemepath, $modlangpath, $modpath, $modlangpathOld, $modpathOld);
-    }
 
     // search for the image
     $imgsrc = '';
     foreach ($paths as $path) {
-        if (is_readable("$path/$ossrc")) {
-            $imgsrc = "$path/$ossrc";
+        $fullpath = $path . ($osset ? "/$osset/" : '/') . $ossrc;
+        if (is_readable($fullpath)) {
+            $imgsrc = $fullpath;
             break;
         }
     }
@@ -157,10 +187,13 @@ function smarty_function_img($params, Zikula_View $view)
         $imgsrc = $params['default'];
     }
 
+    // default for the optional flag
+    $optional = isset($params['optional']) ? $params['optional'] : true;
+
     if ($imgsrc == '') {
         if ($optional) {
-            $view->trigger_error(__f("%s: Image '%s' not found", array('img', DataUtil::formatForDisplay($params['src']))));
-            if ($nostoponerror == true) {
+            if (!$nostoponerror) {
+                $view->trigger_error(__f("%s: Image '%s' not found", array('img', DataUtil::formatForDisplay(($set ? "$set/" : '') . $params['src']))));
                 return;
             } else {
                 return false;
@@ -174,8 +207,8 @@ function smarty_function_img($params, Zikula_View $view)
     // This way it is easy to scale the image to a certain dimension.
     if (!isset($params['width']) && !isset($params['height'])) {
         if (!($_image_data = @getimagesize($imgsrc))) {
-            $view->trigger_error(__f("%s: Image '%s' is not a valid image file", array('pnimg', DataUtil::formatForDisplay($params['src']))));
-            if ($nostoponerror == true) {
+            if (!$nostoponerror) {
+                $view->trigger_error(__f("%s: Image '%s' is not a valid image file", array('img', DataUtil::formatForDisplay(($set ? "$set/" : '') . $params['src']))));
                 return;
             } else {
                 return false;
@@ -189,14 +222,11 @@ function smarty_function_img($params, Zikula_View $view)
     $params['src'] = $basepath . '/' . $imgsrc;
 
     $retval = isset($params['retval']) ? $params['retval'] : null;
+    $assign = isset($params['assign']) ? $params['assign'] : null;
 
     unset($params['modname']);
-    $assign = null;
-    if (isset($params['assign'])) {
-        $assign = $params['assign'];
-        unset($params['assign']);
-    }
     unset($params['retval']);
+    unset($params['assign']);
     if (isset($params['altml'])) {
         // legacy
         unset($params['altml']);
@@ -219,9 +249,11 @@ function smarty_function_img($params, Zikula_View $view)
 
     if (!empty($retval) && isset($params[$retval])) {
         return $params[$retval];
+
     } else if (!empty($assign)) {
         $params['imgtag'] = $imgtag;
         $view->assign($assign, $params);
+
     } else {
         return $imgtag;
     }
