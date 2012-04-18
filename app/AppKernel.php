fef5dd8e15017d7271464aa665c92d821cff4e64
Drak <drak@zikula.org>
2012-04-18 16:21:34 +0545

    Activate frameworkbundle and switch away several services to Symfony2.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,16 +9,16 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-//            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Zikula\Bundle\CoreBundle\CoreBundle(),
 //            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-//            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-//            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-//            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-//            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-//            new JMS\AopBundle\JMSAopBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
 //            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
 //            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Zikula\Bundle\ModuleBundle\ZikulaModuleBundle(),
@@ -26,9 +26,9 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-//            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-//            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-//            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
