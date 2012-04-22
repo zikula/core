2c21d05c98ae4d026b05fa9e1f4c32657ba485c9
Drak <drak@zikula.org>
2012-04-22 16:56:03 +0545

    Update composer.json

--- src/Zikula/Component/HookDispatcher/Tests/bootstrap.php
+++ src/Zikula/Component/HookDispatcher/Tests/bootstrap.php
@@ -1,20 +1,14 @@
 <?php
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
+} else {
+    die('The test environment requires composer to install vendors, have you run composer?');
 }
 
 if (!class_exists('Symfony\Component\EventDispatcher\Event')) {
-    die('The test environment requires Symfony2 Event Dispatcher Component');
+    die('The test environment requires Symfony2 Event Dispatcher Component, have you run composer?');
 }
 
 if (!class_exists('Symfony\Component\DependencyInjection\Container')) {
-    die('The test environment requires Symfony2 Dependency Injection Component');
+    die('The test environment requires Symfony2 Dependency Injection Component, have you run composer?');
 }
-
-spl_autoload_register(function ($class) {
-    if (0 === strpos(ltrim($class, '/'), 'Zikula\Component\HookDispatcher')) {
-        if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Zikula\Component\HookDispatcher')).'.php')) {
-            require_once $file;
-        }
-    }
-});
