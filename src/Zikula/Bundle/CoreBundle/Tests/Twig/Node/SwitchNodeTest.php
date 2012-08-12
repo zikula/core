47149bda7706d0d0f7e64313f8b0c167d78e0d3b
Drak <drak@zikula.org>
2012-08-12 20:37:04 +0100

    Added some stub Tests.

---
+++ src/Zikula/Bundle/CoreBundle/Tests/Twig/Node/SwitchNodeTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Zikula\Bundle\CoreBundle\Tests\Twig\Node;
+
+use Zikula\Bundle\CoreBundle\Twig\Node\SwitchNode;
+
+class SwitchNodeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SwitchNode
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        //$this->object = new SwitchNode;
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers Zikula\Bundle\CoreBundle\Twig\Node\SwitchNode::compile
+     * @todo   Implement testCompile().
+     */
+    public function testCompile()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+}
---
+++ src/Zikula/Bundle/CoreBundle/Tests/Twig/TokenParser/SwitchTokenParserTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Zikula\Bundle\CoreBundle\Tests\Twig\TokenParser;
+
+use Zikula\Bundle\CoreBundle\Twig\TokenParser\SwitchTokenParser;
+
+class SwitchTokenParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SwitchTokenParser
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        //$this->object = new SwitchTokenParser;
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers Zikula\Bundle\CoreBundle\Twig\TokenParser\SwitchTokenParser::parse
+     * @todo   Implement testParse().
+     */
+    public function testParse()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers Zikula\Bundle\CoreBundle\Twig\TokenParser\SwitchTokenParser::decideCaseFork
+     * @todo   Implement testDecideCaseFork().
+     */
+    public function testDecideCaseFork()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @covers Zikula\Bundle\CoreBundle\Twig\TokenParser\SwitchTokenParser::getTag
+     * @todo   Implement testGetTag().
+     */
+    public function testGetTag()
+    {
+        // Remove the following lines when you implement this test.
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+    }
+}
