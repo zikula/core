b5d0dd22a96077fd130aa05c844633c166a186c3
Pawel Preneta <jusuff@jabster.pl>
2012-08-14 08:57:54 +0200

    SwitchNode - default might be null

--- src/Zikula/Bundle/CoreBundle/Twig/Node/SwitchNode.php
+++ src/Zikula/Bundle/CoreBundle/Twig/Node/SwitchNode.php
@@ -4,7 +4,7 @@ namespace Zikula\Bundle\CoreBundle\Twig\Node;
 
 class SwitchNode extends \Twig_Node
 {
-    public function __construct(\Twig_NodeInterface $cases, \Twig_NodeInterface $default, \Twig_Node_Expression $expression, $lineno, $tag = null)
+    public function __construct(\Twig_NodeInterface $cases, \Twig_NodeInterface $default = null, \Twig_Node_Expression $expression, $lineno, $tag = null)
     {
         $nodes = array(
             'cases' => $cases,
