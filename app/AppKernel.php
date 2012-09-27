3fec403d068d605fcb67caaf6e8bcffea293bac0
Drak <drak@zikula.org>
2012-09-27 14:25:10 +0100

    Update dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,7 +48,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new JMS\CommandBundle\JMSCommandBundle();
         }
 
         return $bundles;
