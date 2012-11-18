a64413f97a8059af4b724467792100b8e66945df
Pawel Preneta <jusuff@jabster.pl>
2012-11-18 12:49:37 +0100

    Zikula Form - automatically set proper form enctype when upload input is used (closes #572)

--- src/lib/viewplugins/formplugins/block.form.php
+++ src/lib/viewplugins/formplugins/block.form.php
@@ -26,12 +26,24 @@ function smarty_block_form($params, $content, $view)
 {
     if ($content) {
         PageUtil::addVar('stylesheet', 'system/Theme/style/form/style.css');
-        $encodingHtml = (array_key_exists('enctype', $params) ? " enctype=\"$params[enctype]\"" : '');
         $action = htmlspecialchars(System::getCurrentUri());
         $classString = '';
         if (isset($params['cssClass'])) {
-            $classString = "class=\"$params[cssClass]\" ";
+            $classString = "class=\"{$params['cssClass']}\" ";
         }
+
+        $enctype = array_key_exists('enctype', $params) ? $params['enctype'] : null;
+        // if enctype is not set directly, check whenever upload plugins were used;
+        // if so - set proper enctype for file upload
+        if (is_null($enctype)) {
+            $uploadPlugins = array_filter($view->plugins, function($plugin) {
+                return $plugin instanceof Zikula_Form_Plugin_UploadInput;
+            });
+            if (!empty($uploadPlugins)) {
+                $enctype = 'multipart/form-data';
+            }
+        }
+        $encodingHtml = !is_null($enctype) ? " enctype=\"{$enctype}\"" : '';
 
         $view->postRender();
 
