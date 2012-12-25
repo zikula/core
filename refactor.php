e7ae91dffa7607b38494392f2c12f38ebd5ed8cc
Drak <drak@zikula.org>
2012-12-25 21:58:11 +0000

    Fix include path

--- refactor.php
+++ refactor.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 
-require 'vendor/autoload.php';
+require __DIR__.'/src/vendor/autoload.php';
 
 class ControllerActionCommand extends Command
 {
