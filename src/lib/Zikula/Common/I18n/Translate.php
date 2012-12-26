3604cd3fc92c1bbdc2370d69293cc0b032d9af2b
Drak <drak@zikula.org>
2012-12-26 21:17:37 +0000

    Backport Zikula Common from 2.0

---
+++ src/lib/Zikula/Common/I18n/Translate.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula
+ * @subpackage Zikula_Translate
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Common\I18n;
+
+/**
+ * Zikula Translate helper.
+ */
+class Translate implements TranslatableInterface
+{
+    /**
+     * Translation domain.
+     *
+     * @var string
+     */
+    protected $domain;
+
+    /**
+     * Constructor.
+     *
+     * @param string $domain Gettext domain.
+     */
+    private function __construct($domain=null)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * Set the translation domain.
+     *
+     * @param string $domain Gettext domain.
+     *
+     * @return void
+     */
+    public function setDomain($domain=null)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * Get translation domain.
+     *
+     * @return string $this->domain
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * singular translation for modules.
+     *
+     * @param string $msg Message.
+     *
+     * @return string
+     */
+    public function __($msg)
+    {
+        return __($this->domain, $msg);
+    }
+
+    /**
+     * Plural translations for modules.
+     *
+     * @param string  $m1 Singular.
+     * @param string  $m2 Plural.
+     * @param integer $n  Count.
+     *
+     * @return string
+     */
+    public function _n($m1, $m2, $n)
+    {
+        return _n($this->domain, $m1, $m2, $n);
+    }
+
+    /**
+     * Format translations for modules.
+     *
+     * @param string       $msg   Message.
+     * @param string|array $param Format parameters.
+     *
+     * @return string
+     */
+    public function __f($msg, $param)
+    {
+        return __f($msg, $param, $this->domain);
+    }
+
+    /**
+     * Format pural translations for modules.
+     *
+     * @param string       $m1    Singular.
+     * @param string       $m2    Plural.
+     * @param integer      $n     Count.
+     * @param string|array $param Format parameters.
+     *
+     * @return string
+     */
+    public function _fn($m1, $m2, $n, $param)
+    {
+        return _fn($m1, $m2, $n, $param, $this->domain);
+    }
+
+}
