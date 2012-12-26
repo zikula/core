dff9fb50081a4a5ccdc115403805d4d354d892a6
Drak <drak@zikula.org>
2012-12-26 16:34:39 +0000

    Backport Doctrine Entities from 2.0

---
+++ src/lib/Zikula/Core/Doctrine/Entity/EntityAttribute.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_Form
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\Core\Doctrine\Entity;
+
+use Zikula\Core\Doctrine\EntityAccess;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Base class of many-to-many assocation between any entity and attribute.
+ *
+ * @ORM\MappedSuperclass
+ */
+abstract class EntityAttribute extends EntityAccess
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="array")
+     * @var string
+     */
+    private $value;
+
+    public function __construct($name, $value, $entity)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->setEntity($entity);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+
+    abstract public function getEntity();
+
+    abstract public function setEntity($entity);
+}
+
