9c0261caf6c84efdd5258d524861cf7f0d9187f0
Drak <drak@zikula.org>
2013-01-01 21:28:36 +0000

    Correct redirect for uninstalled site.

--- src/lib/Zikula/Core.php
+++ src/lib/Zikula/Core.php
@@ -428,7 +428,7 @@ class Zikula_Core
 
         // Check that Zikula is installed before continuing
         if (System::getVar('installed') == 0 && !System::isInstalling()) {
-            $response = new RedirectResponse($request->getBaseUrl().'/install.php?notinstalled', 302);
+            $response = new RedirectResponse($request->getBasePath().'/install.php?notinstalled', 302);
             $response->send();
             System::shutDown();
         }
