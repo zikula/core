67eb27704a5c7ed9567869bf3747029e966a74c9
Drak <drak@zikula.org>
2013-01-03 15:56:12 +0000

    Abstact module plugin

---
+++ src/lib/Zikula/Bundle/CoreBundle/AbstractModulePlugin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zikula\Bundle\ModuleBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+abstract class AbstractModulePlugin extends Bundle
+{
+}
