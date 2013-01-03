517d908c651c8e62dea907089d091dadb7a977e6
Drak <drak@zikula.org>
2013-01-03 15:54:57 +0000

    Add base classes for module and theme bundles

---
+++ src/lib/Zikula/Bundle/CoreBundle/AbstractModule.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Zikula\Bundle\ModuleBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+abstract class AbstractModule extends Bundle
+{
+    private $serviceIds = array();
+
+//    abstract public function getVersion();
+
+//    /**
+//     * @return ModuleInstallerInterface
+//     */
+//    abstract public function createInstaller();
+
+    public function build(ContainerBuilder $container)
+    {
+        // modules have to use DI Extensions
+    }
+
+//    public function getContainerExtension()
+//    {
+//        $ex = parent::getContainerExtension();
+//
+//        if ($ex != null) {
+//            $ex = new DependencyInjection\SandboxContainerExtension($ex, $this->serviceIds);
+//        }
+//
+//        return $ex;
+//    }
+
+    public function getServiceIds()
+    {
+        return $this->serviceIds;
+    }
+}
