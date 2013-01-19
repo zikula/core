07119f2633736191d320d424c8f2b898a41a6615
Drak <drak@zikula.org>
2013-01-19 21:33:35 +0000

    Refactor Search module to namespace

--- src/system/Search/EventHandlers.php
+++ src/system/Search/EventHandlers.php
@@ -1,59 +1,19 @@
 <?php
 /**
  * Copyright Zikula Foundation 2009 - Zikula Application Framework
- *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
  *
  * @license GNU/LGPLv3 (or at your option, any later version).
  * @package Search
- *
- * Please see the NOTICE file distributed with this source code for further
- * information regarding copyright and licensing.
+ *          Please see the NOTICE file distributed with this source code for further
+ *          information regarding copyright and licensing.
  */
 
 /**
- * EventHandlers class.
+ * BC class
  */
-class Search_EventHandlers
+class Search_EventHandler extends Search\Listener\ModuleListener
 {
-    /**
-     * Handle module install event "installer.module.installed".
-     * Receives $modinfo as $args
-     *
-     * @param Zikula_Event $event
-     *
-     * @return void
-     */
-    public static function moduleInstall(Zikula_Event $event)
-    {
-        $mod = $event['name'];
 
-        // determine search capability
-        if (ModUtil::apiFunc($mod, 'search', 'info')) {
-
-            // get all search blocks
-            $blocks = BlockUtil::getBlocksInfo();
-
-            foreach ($blocks as $block) {
-
-                $block = $block->toArray();
-
-                if ($block['bkey'] != 'Search') {
-                    continue;
-                }
-
-                $content = BlockUtil::varsFromContent($block['content']);
-
-                if (!isset($content['active'])) {
-                    $content['active'] = array();
-                }
-                $content['active'][$mod] = 1;
-
-                $block['content'] = BlockUtil::varsToContent($content);
-                ModUtil::apiFunc('Blocks', 'admin', 'update', $block);
-            }
-        }
-    }
 }
-
---
+++ src/system/Search/SearchModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Search;
+
+use Zikula\Bundle\CoreBundle\AbstractModule;
+
+class SearchModule extends AbstractModule
+{
+}
