8ad0606a2db92e90b210d3b0f0b7298597cef53c
Drak <drak@zikula.org>
2013-01-26 22:41:26 +0000

    Consolidate old attributes and metadata entities.

--- src/lib/Zikula/Doctrine2/Entity/EntityAttribute.php
+++ src/lib/Zikula/Doctrine2/Entity/EntityAttribute.php
@@ -13,6 +13,7 @@
  */
 
 use Doctrine\ORM\Mapping as ORM;
+use Zikula\Core\Doctrine\Entity\AbstractEntityAttribute;
 
 /**
  * Base class of many-to-many association between any entity and attribute.
@@ -21,70 +22,8 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @deprecated since 1.3.6
  */
-abstract class Zikula_Doctrine2_Entity_EntityAttribute extends Zikula_EntityAccess
+abstract class Zikula_Doctrine2_Entity_EntityAttribute extends AbstractEntityAttribute
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue
-     * @var integer
-     */
-    private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @ORM\Column(type="array")
-     * @var string
-     */
-    private $value;
-
-    public function __construct($name,
-                                $value,
-                                $entity)
-    {
-        $this->name = $name;
-        $this->value = $value;
-        $this->setEntity($entity);
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function getValue()
-    {
-        return $this->value;
-    }
-
-    public function setValue($value)
-    {
-        $this->value = $value;
-    }
-
-
-    abstract public function getEntity();
-
-    abstract public function setEntity($entity);
 }
 
