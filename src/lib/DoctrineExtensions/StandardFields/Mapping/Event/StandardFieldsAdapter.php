9a9485b1cf4f3c42fb504ea88455f8ba46acaff6
Drak <drak@zikula.org>
2013-02-15 19:01:09 +0000

    Coding standards.

--- src/lib/DoctrineExtensions/StandardFields/Mapping/Event/StandardFieldsAdapter.php
+++ src/lib/DoctrineExtensions/StandardFields/Mapping/Event/StandardFieldsAdapter.php
@@ -15,7 +15,8 @@ interface StandardFieldsAdapter extends AdapterInterface
      * Get the user id value
      *
      * @param ClassMetadata $meta
-     * @param string $field
+     * @param string        $field
+     *
      * @return mixed
      */
     function getUserIdValue(ClassMetadata $meta, $field);
