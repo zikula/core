95ab6da256e53998d681dc81c036460da1e75d80
Drak <drak@zikula.org>
2013-02-23 20:25:33 +0000

    Wire in new classes to kernel.

--- src/app/ZikulaKernel.php
+++ src/app/ZikulaKernel.php
@@ -9,15 +9,19 @@ class ZikulaKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-//            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            //new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-//            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Zikula\Bundle\CoreBundle\CoreBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
+
+        $this->registerCoreModules($bundles);
+
+        $bundles[] = new CustomBundle\CustomBundle();
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
@@ -33,5 +37,23 @@ class ZikulaKernel extends Kernel
         if (is_readable(__DIR__.'/config/personal_parameters.yml')) {
             $loader->load(__DIR__.'/config/personal_parameters.yml');
         }
+    }
+
+    private function registerCoreModules(array &$bundles)
+    {
+        $bundles[] = new Blocks\BlocksModule();
+        $bundles[] = new Categories\CategoriesModule();
+        $bundles[] = new Errors\ErrorsModule();
+        $bundles[] = new Extensions\ExtensionsModule();
+        $bundles[] = new Groups\GroupsModule();
+        $bundles[] = new Mailer\MailerModule();
+        $bundles[] = new PageLock\PageLockModule();
+        $bundles[] = new Permissions\PermissionsModule();
+        $bundles[] = new Search\SearchModule();
+        $bundles[] = new SecurityCenter\SecurityCenterModule();
+        $bundles[] = new Settings\SettingsModule();
+        $bundles[] = new Theme\ThemeModule();
+        $bundles[] = new Users\UsersModule();
+        $bundles[] = new Andreas08\Andreas08Theme();
     }
 }
---
+++ src/themes/Andreas08/Andreas08Theme.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Andreas08;
+
+use Zikula\Bundle\CoreBundle\AbstractTheme;
+
+class Andreas08Theme extends AbstractTheme
+{
+}
