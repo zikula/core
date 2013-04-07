bf1d86bfab0de0a5e5e21c84f23402dda5f46c2e
Drak <drak@zikula.org>
2013-04-07 22:34:06 +0100

    Clear cache on composer install/update
    Prepare asset installation command.

--- src/lib/Zikula/Bundle/CoreBundle/HttpKernel/ZikulaKernel.php
+++ src/lib/Zikula/Bundle/CoreBundle/HttpKernel/ZikulaKernel.php
@@ -8,6 +8,7 @@ use Zikula\Bridge\DependencyInjection\PhpDumper;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Config\ConfigCache;
+use Zikula\Core\AbstractBundle;
 use Zikula\Core\AbstractModule;
 use Zikula\Core\AbstractTheme;
 use Composer\Autoload\ClassLoader;
@@ -23,7 +24,17 @@ abstract class ZikulaKernel extends Kernel
     /**
      * @var array
      */
+    private $modules = array();
+
+    /**
+     * @var array
+     */
     private $moduleMap = array();
+
+    /**
+     * @var array
+     */
+    private $themes = array();
 
     /**
      * @var array
@@ -52,6 +63,14 @@ abstract class ZikulaKernel extends Kernel
         }
 
         parent::boot();
+
+        foreach ($this->bundles as $bundle) {
+            if ($bundle instanceof AbstractModule) {
+                $this->modules[] = $bundle;
+            } elseif ($bundle instanceof AbstractTheme) {
+                $this->themes[] = $bundle;
+            }
+        }
 
         foreach ($this->bundleMap as $name => $bundles) {
             if ($bundles[0] instanceof AbstractModule) {
@@ -92,6 +111,11 @@ abstract class ZikulaKernel extends Kernel
         return $this->moduleMap[$moduleName];
     }
 
+    public function getModules()
+    {
+        return $this->modules;
+    }
+
     /**
      * Get named theme bundle.
      *
@@ -115,6 +139,23 @@ abstract class ZikulaKernel extends Kernel
         return $this->themeMap[$themeName];
     }
 
+    public function getThemes()
+    {
+        return $this->themes;
+    }
+
+    public function getJustBundles()
+    {
+        $bundles = array();
+        foreach ($this->bundles as $bundle) {
+            if (!$bundle instanceof AbstractBundle) {
+                $bundles[] = $bundle;
+            }
+        }
+
+        return $bundles;
+    }
+
     public function setAutoloader(ClassLoader $autoloader)
     {
         $this->autoloader = $autoloader;
@@ -130,6 +171,69 @@ abstract class ZikulaKernel extends Kernel
         $dir = is_readable($dir = $this->rootDir.'/config/custom_parameters.yml') ? $dir : $this->rootDir.'/config/parameters.yml';
 
         return Yaml::parse(file_get_contents($dir));
+    }
+
+    /**
+     * Initializes the data structures related to the bundle management.
+     *
+     *  - the bundles property maps a bundle name to the bundle instance,
+     *  - the bundleMap property maps a bundle name to the bundle inheritance hierarchy (most derived bundle first).
+     *
+     * @throws \LogicException if two bundles share a common name
+     * @throws \LogicException if a bundle tries to extend a non-registered bundle
+     * @throws \LogicException if a bundle tries to extend itself
+     * @throws \LogicException if two bundles extend the same ancestor
+     */
+    protected function initializeBundles()
+    {
+        // init bundles
+        $this->bundles = array();
+        $topMostBundles = array();
+        $directChildren = array();
+
+        foreach ($this->registerBundles() as $bundle) {
+            $name = $bundle->getName();
+            if (isset($this->bundles[$name])) {
+                throw new \LogicException(sprintf('Trying to register two bundles with the same name "%s"', $name));
+            }
+            $this->bundles[$name] = $bundle;
+
+            if ($parentName = $bundle->getParent()) {
+                if (isset($directChildren[$parentName])) {
+                    throw new \LogicException(sprintf('Bundle "%s" is directly extended by two bundles "%s" and "%s".', $parentName, $name, $directChildren[$parentName]));
+                }
+                if ($parentName == $name) {
+                    throw new \LogicException(sprintf('Bundle "%s" can not extend itself.', $name));
+                }
+                $directChildren[$parentName] = $name;
+            } else {
+                $topMostBundles[$name] = $bundle;
+            }
+        }
+
+        // look for orphans
+        if (count($diff = array_values(array_diff(array_keys($directChildren), array_keys($this->bundles))))) {
+            throw new \LogicException(sprintf('Bundle "%s" extends bundle "%s", which is not registered.', $directChildren[$diff[0]], $diff[0]));
+        }
+
+        // inheritance
+        $this->bundleMap = array();
+        foreach ($topMostBundles as $name => $bundle) {
+            $bundleMap = array($bundle);
+            $hierarchy = array($name);
+
+            while (isset($directChildren[$name])) {
+                $name = $directChildren[$name];
+                array_unshift($bundleMap, $this->bundles[$name]);
+                $hierarchy[] = $name;
+            }
+
+            foreach ($hierarchy as $bundle) {
+                $this->bundleMap[$bundle] = $bundleMap;
+                array_pop($bundleMap);
+            }
+        }
+
     }
 
     /**
