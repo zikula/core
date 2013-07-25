3af7a1f6fe43aff6a814d4992c4d397193374f7a
Drak <drak@zikula.org>
2013-07-25 22:28:58 +0100

    Update whitelists
    Source http://www.whatwg.org/specs/web-apps/current-work/#whitelisted-scheme

--- src/lib/legacy/viewplugins/outputfilter.shorturls.php
+++ src/lib/legacy/viewplugins/outputfilter.shorturls.php
@@ -33,7 +33,7 @@ function smarty_outputfilter_shorturls($source, $view)
     $baseurl = System::getBaseUrl();
 
     $prefix = '[(<[^>]*?)[\'"](?:'.$baseurl.'|'.$baseurl.')?(?:[./]{0,2})'; // Match local URLs in HTML tags, removes / and ./
-    $in = array('[<([^>]+)\s(src|href|background|action)\s*=\s*((["\'])?)(?!http)(?!skype)(?!xmpp)(?!icq)(?!mailto)(?!tel)(?!javascript:)(?![/"\'\s#]+)]Ui');
+    $in = array('[<([^>]+)\s(src|href|background|action)\s*=\s*((["\'])?)(?!http)(?!skype:)(?!xmpp:)(?!icq:)(?!mailto:)(?!tel:)(?!javascript:)(?!bitcoin:)(?!geo:)(?!im:)(?!irc:)(?!ircs:)(?!sms:)(?!ssh:)(?!urn:)(?!wtai:)(?!smsto:)(?!sms:)(?!sip:)(?!magnet:)(?!webcal:)(?![/"\'\s#]+)]Ui');
     $out = array('<$1 $2=$3'.$baseurl);
 
     // perform the replacement
