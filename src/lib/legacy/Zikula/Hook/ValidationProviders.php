8b73af0dae5979b08fdc86b8f6837c2108ad6f61
Craig Heydenburg <craigh@mac.com>
2013-08-25 08:29:18 -0400

    invert inheritance of ValidationProviders refs #993

--- src/lib/legacy/Zikula/Hook/ValidationProviders.php
+++ src/lib/legacy/Zikula/Hook/ValidationProviders.php
@@ -16,48 +16,6 @@
 /**
  * Hook validation collection
  */
-class Zikula_Hook_ValidationProviders extends Zikula_Collection_Container
+class Zikula_Hook_ValidationProviders extends Zikula\Core\Hook\ValidationProviders
 {
-    /**
-     * Constructor.
-     *
-     * @param string      $name       The name of the collection.
-     * @param ArrayObject $collection The collection (optional).
-     */
-    public function __construct($name='validation', ArrayObject $collection = null)
-    {
-        parent::__construct($name, $collection);
-    }
-
-    /**
-     * Set response.
-     *
-     * @param string                         $name     Name.
-     * @param Zikula_Hook_ValidationResponse $response Validation response.
-     *
-     * @return void
-     */
-    public function set($name, $response)
-    {
-        if (!$response instanceof Zikula_Hook_ValidationResponse) {
-            throw new InvalidArgumentException('$response must be an instance of Zikula_Hook_ValidationResponse');
-        }
-        $this->collection[$name] = $response;
-    }
-
-    /**
-     * Check if there are any errors in any of the validation responses.
-     *
-     * @return boolean
-     */
-    public function hasErrors()
-    {
-        foreach ($this->collection as $response) {
-            if ($response->hasErrors()) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
