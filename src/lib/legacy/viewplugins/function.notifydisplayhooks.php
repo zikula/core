a0f7f0e09a2a4a9fb5b3cd5dc78c7c4a9b8c58cb
Craig Heydenburg <craigh@mac.com>
2013-08-26 08:29:20 -0400

    use BC classes for now. refs #993

--- src/lib/legacy/viewplugins/function.notifydisplayhooks.php
+++ src/lib/legacy/viewplugins/function.notifydisplayhooks.php
@@ -50,7 +50,7 @@ function smarty_function_notifydisplayhooks($params, Zikula_View $view)
     $assign  = isset($params['assign']) ? $params['assign'] : false;
 
     // create event and notify
-    $hook = new \Zikula\Core\Hook\DisplayHook($id, $urlObject);
+    $hook = new Zikula_DisplayHook($eventname, $id, $urlObject); // @todo Zikula_DisplayHook maintains BC. IN 1.4.0 change to \Zikula\Core\Hook\DisplayHook($id, $urlObject);
     $view->getContainer()->get('hook_dispatcher')->dispatch($eventname, $hook);
     $responses = $hook->getResponses();
 
