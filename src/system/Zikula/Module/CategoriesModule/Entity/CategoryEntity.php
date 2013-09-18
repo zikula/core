50e9838fa130b02c5048c3c632377025c9f45016
Craig Heydenburg <craigh@mac.com>
2013-09-18 15:33:01 -0400

    Revert "properties cannot be private for inheritance to work."
    This reverts commit 1e1bdb1d99fa84fa73d665eba666fc9140b21739.

--- src/system/Zikula/Module/CategoriesModule/Entity/CategoryEntity.php
+++ src/system/Zikula/Module/CategoriesModule/Entity/CategoryEntity.php
@@ -37,80 +37,80 @@ class CategoryEntity extends EntityAccess
      * @ORM\Column(type="integer")
      * @var integer
      */
-    protected $id;
+    private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity="CategoryEntity", inversedBy="children")
      * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
      * @var CategoryEntity
      */
-    protected $parent;
+    private $parent;
 
     /**
      * @ORM\OneToMany(targetEntity="CategoryEntity", mappedBy="parent")
      * @var CategoryEntity
      */
-    protected $children;
+    private $children;
 
     /**
      * @ORM\Column(type="boolean", name="is_locked")
      * @var boolean
      */
-    protected $is_locked;
+    private $is_locked;
 
     /**
      * @ORM\Column(type="boolean", name="is_leaf")
      * @var boolean
      */
-    protected $is_leaf;
+    private $is_leaf;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @var string
      */
-    protected $name;
+    private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @var string
      */
-    protected $value;
+    private $value;
 
     /**
      * @ORM\Column(type="integer", name="sort_value")
      * @var integer
      */
-    protected $sort_value;
+    private $sort_value;
 
     /**
      * @ORM\Column(type="array", name="display_name")
      * @var array
      */
-    protected $display_name;
+    private $display_name;
 
     /**
      * @ORM\Column(type="array", name="display_desc")
      * @var array
      */
-    protected $display_desc;
+    private $display_desc;
 
     /**
      * @ORM\Column(type="text")
      * @var string
      */
-    protected $path;
+    private $path;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @var string
      */
-    protected $ipath;
+    private $ipath;
 
     /**
      * @ORM\Column(type="string", length=1)
      * @var string
      */
-    protected $status;
+    private $status;
 
     /**
      * @ORM\OneToMany(targetEntity="CategoryAttributeEntity",
@@ -119,40 +119,40 @@ class CategoryEntity extends EntityAccess
      *                orphanRemoval=true,
      *                indexBy="name")
      */
-    protected $attributes;
+    private $attributes;
 
     /**
      * @Gedmo\Blameable(on="create")
      * @ORM\ManyToOne(targetEntity="Zikula\Module\UsersModule\Entity\UserEntity")
      * @ORM\JoinColumn(name="cr_uid", referencedColumnName="uid")
      */
-    protected $cr_uid;
+    private $cr_uid;
 
     /**
      * @Gedmo\Blameable(on="update")
      * @ORM\ManyToOne(targetEntity="Zikula\Module\UsersModule\Entity\UserEntity")
      * @ORM\JoinColumn(name="lu_uid", referencedColumnName="uid")
      */
-    protected $lu_uid;
+    private $lu_uid;
 
     /**
      * @ORM\Column(type="datetime")
      * @Gedmo\Timestampable(on="create")
      */
-    protected $cr_date;
+    private $cr_date;
 
     /**
      * @ORM\Column(type="datetime")
      * @Gedmo\Timestampable(on="update")
      */
-    protected $lu_date;
+    private $lu_date;
 
     /**
      * maintain BC (same as status)
      * @ORM\Column(type="string", length=1)
      * @var string
      */
-    protected $obj_status = 'A';
+    private $obj_status = 'A';
 
     /**
      * constructor
