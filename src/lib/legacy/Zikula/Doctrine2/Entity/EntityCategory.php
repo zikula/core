eb1c211e06ac697b601520c57ca9270e4b4417a7
Craig Heydenburg <craigh@mac.com>
2013-09-18 15:29:05 -0400

    revert legacy Doctrine2 classes. Entities apparently do not inherit in the same sense as "normal" classes. So, legacy entities cannot be used as "alias" classes for the new ones. I think reverting these and marking them as deprecated is sufficient for moving forward and they can be removed in Core 1.4.0 and this allows full BC.

--- src/lib/legacy/Zikula/Doctrine2/Entity/EntityCategory.php
+++ src/lib/legacy/Zikula/Doctrine2/Entity/EntityCategory.php
@@ -13,7 +13,6 @@
  */
 
 use Doctrine\ORM\Mapping as ORM;
-use Zikula\Core\Doctrine\Entity\AbstractEntityCategory;
 
 /**
  * Base class of many-to-many association between any entity and Category.
@@ -22,8 +21,69 @@ use Zikula\Core\Doctrine\Entity\AbstractEntityCategory;
  *
  * @deprecated since 1.3.6
  */
-abstract class Zikula_Doctrine2_Entity_EntityCategory extends AbstractEntityCategory
+abstract class Zikula_Doctrine2_Entity_EntityCategory extends Zikula_EntityAccess
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="integer", name="registryId")
+     * @var integer
+     */
+    private $categoryRegistryId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Zikula_Doctrine2_Entity_Category")
+     * @ORM\JoinColumn(name="categoryId", referencedColumnName="id")
+     * @var Zikula_Doctrine2_Entity_Category
+     */
+    private $category;
+
+    public function __construct($registryId,
+                                Zikula_Doctrine2_Entity_Category $category,
+                                $entity)
+    {
+        $this->categoryRegistryId = $registryId;
+        $this->category = $category;
+        $this->setEntity($entity);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getCategoryRegistryId()
+    {
+        return $this->categoryRegistryId;
+    }
+
+    public function setCategoryRegistryId($categoryRegistryId)
+    {
+        $this->categoryRegistryId = $categoryRegistryId;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory(Zikula_Doctrine2_Entity_Category $category)
+    {
+        $this->category = $category;
+    }
+
+    abstract public function getEntity();
+
+    abstract public function setEntity($entity);
 }
-
