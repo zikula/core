c4c5bebee55ed6974c32850d5864a4bf185d259d
Craig Heydenburg <craigh@mac.com>
2013-09-23 15:56:20 -0400

    reduce BlameListener to most basic

--- src/lib/Zikula/Bundle/CoreBundle/EventListener/BlameListener.php
+++ src/lib/Zikula/Bundle/CoreBundle/EventListener/BlameListener.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use UserUtil;
 use ServiceUtil;
-use System;
 
 use Gedmo\Blameable\BlameableListener;
 
@@ -20,40 +19,27 @@ use Gedmo\Blameable\BlameableListener;
  */
 class BlameListener implements EventSubscriberInterface
 {
-    /**
-     * @var SecurityContextInterface
-     */
-    private $securityContext;
-
-    /**
-     * @var BlameableListener
-     */
-    private $blameableListener;
-
     public function __construct(BlameableListener $blameableListener, SecurityContextInterface $securityContext = null)
     {
-        $this->blameableListener = $blameableListener;
-//        $this->securityContext = $securityContext;
+        $em = ServiceUtil::get('doctrine.entitymanager');
+        $user = $em->getRepository('Zikula\Module\UsersModule\Entity\UserEntity')->findOneBy(array('uid' => UserUtil::getVar('uid')));
+        $blameableListener->setUserValue($user);
     }
 
     /**
-     * Set the username from the security context by listening on core.request
+     * required method as result of implementation
      *
      * @param GetResponseEvent $event
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $em = ServiceUtil::get('doctrine.entitymanager');
-        if (System::isInstalling()) {
-            // on system install the user is not created yet...
-            $user = null;
-        } else {
-            $uid = UserUtil::getVar('uid');
-            $user = $em->getRepository('Zikula\Module\UsersModule\Entity\UserEntity')->findOneBy(array('uid' => $uid));
-        }
-        $this->blameableListener->setUserValue($user);
     }
 
+    /**
+     * required implementation of abstract parent class
+     *
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return array(
