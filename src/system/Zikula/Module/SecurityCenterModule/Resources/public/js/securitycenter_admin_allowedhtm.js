d8291d9ecd106c162d2391617024ad8bb0d2e112
Phaidon <dev@wuertz.org>
2013-09-26 17:08:21 +0200

    securitycenter_admin_allowedhtm.js: prototype to jquery migration.

--- src/system/Zikula/Module/SecurityCenterModule/Resources/public/js/securitycenter_admin_allowedhtm.js
+++ src/system/Zikula/Module/SecurityCenterModule/Resources/public/js/securitycenter_admin_allowedhtm.js
@@ -1,25 +1,20 @@
-// Copyright Zikula Foundation 2010 - license GNU/LGPLv2.1 (or at your option, any later version).
- 
-document.observe('dom:loaded',securitycenter_allowedhtml_init);
+// Copyright Zikula Foundation 2013 - license GNU/LGPLv2.1 (or at your option, any later version).
 
-function securitycenter_allowedhtml_init()
-{
-    $('toggle_notallowed').observe('click', function(e){
-        Zikula.toggleInput('.notallowed_radio', true);
+( function($) {
+    $(document).ready(function() {
+        $('#toggle_notallowed').click( function() {
+             $('.notallowed_radio').prop('checked', true);
+        });
+        $('#toggle_allowed').click( function() {
+             $('.allowed_radio').prop('checked', true);
+        });
+        $('#toggle_allowedwith').click( function() {
+             $('.allowedwith_radio').prop('checked', true);
+        });
+        
+        $('.notallowed_radio, .allowed_radio, .allowedwith_radio').change( function() {
+            $('#toggle_notallowed, #toggle_allowed, #toggle_allowedwith').prop('checked', true);
+        });
+        
     });
-    $('toggle_allowed').observe('click', function(e){
-        Zikula.toggleInput('.allowed_radio', true);
-    });
-    $('toggle_allowedwith').observe('click', function(e){
-        Zikula.toggleInput('.allowedwith_radio', true);
-    });
-
-    $$('.notallowed_radio, .allowed_radio, .allowedwith_radio').invoke('observe','click',securitycenter_allowedhtml_check);
-}
-
-function securitycenter_allowedhtml_check(event) {
-    var type = event.element().className,
-        status = $$('.' + type).pluck('checked').all();
-    Zikula.toggleInput('#toggle_notallowed, #toggle_allowed, #toggle_allowedwith',false);
-    $('toggle_' + type.replace('_radio','')).checked = status;
-}
+})(jQuery);
