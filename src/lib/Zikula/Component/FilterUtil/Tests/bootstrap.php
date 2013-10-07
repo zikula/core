8bb5d59338ffec25190a784392e736e84c1c3d68
Drak <drak@zikula.org>
2013-10-07 08:44:40 +0100

    Add bits to make a component library

---
+++ src/lib/Zikula/Component/FilterUtil/Tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+} else {
+    die('The test environment requires composer to install vendors, have you run composer?');
+}
+
+if (!class_exists('Symfony\Component\HttpFoundation\Request')) {
+    die('The test environment requires Symfony2 HttpFoundation Component, have you run composer?');
+}
+
+if (!class_exists('Doctrine\ORM\EntityManager')) {
+    die('The test environment requires Doctrine ORM, have you run composer?');
+}
