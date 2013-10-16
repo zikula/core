f163522207cddcf77f7bc6b9c085767762f2524e
rgasch <rgasch@gmail.com>
2013-10-16 15:53:11 +0200

    Ensure that value numeric values are inserted into DB

--- src/lib/legacy/dbobject/DBUtil.php
+++ src/lib/legacy/dbobject/DBUtil.php
@@ -344,6 +344,11 @@ class DBUtil
         }
 
         if (isset($numericFields[$fieldType])) {
+            if ($fieldType[0] == 'I') {
+                $value = (int)$value;
+            } else {
+                $value = (float)$value;
+            }
             return DataUtil::formatForStore($value);
         }
 
