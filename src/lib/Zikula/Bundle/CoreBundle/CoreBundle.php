237fb04ebf95fa678df098e9f119335b53aa130b
Drak <drak@zikula.org>
2013-10-19 16:04:10 +0100

    Add temporary fix for DoctrineBundle
    Thanks to @matthiasnoback

--- src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
+++ src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
@@ -2,6 +2,7 @@
 
 namespace Zikula\Bundle\CoreBundle;
 
+use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\FixDoctrineDbalConnectionDefinitionPass;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\RegisterCoreListenersPass;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\RegisterKernelListenersPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -14,8 +15,10 @@ class CoreBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+
         $container->addCompilerPass(new RegisterKernelListenersPass(), PassConfig::TYPE_AFTER_REMOVING);
         $container->addCompilerPass(new RegisterCoreListenersPass(), PassConfig::TYPE_AFTER_REMOVING);
+        $container->addCompilerPass(new FixDoctrineDbalConnectionDefinitionPass());
 
         // todo - see if we can do this only on module install/upgrade - drak
         $container->addCompilerPass(new ValidateServiceDefinitionsPass(), PassConfig::TYPE_AFTER_REMOVING);
