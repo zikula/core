636a1a3a01bbff2252d61e44da97edf5020de2d7
Drak <drak@zikula.org>
2013-10-20 16:39:04 +0100

    Revert "Add temporary fix for DoctrineBundle"
    This reverts commit 237fb04ebf95fa678df098e9f119335b53aa130b.
    
    Rreason: fixed upstream.

--- src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
+++ src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
@@ -2,7 +2,6 @@
 
 namespace Zikula\Bundle\CoreBundle;
 
-use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\FixDoctrineDbalConnectionDefinitionPass;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\RegisterCoreListenersPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
@@ -14,9 +13,7 @@ class CoreBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-
         $container->addCompilerPass(new RegisterCoreListenersPass(), PassConfig::TYPE_AFTER_REMOVING);
-        $container->addCompilerPass(new FixDoctrineDbalConnectionDefinitionPass());
 
         // todo - see if we can do this only on module install/upgrade - drak
         $container->addCompilerPass(new ValidateServiceDefinitionsPass(), PassConfig::TYPE_AFTER_REMOVING);
