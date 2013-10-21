858f5bbaf638893b2cc62dcb1882bcc8f95f7e2f
Craig Heydenburg <craigh@mac.com>
2013-10-21 16:31:28 -0400

    use provided method for fetching Version class instance instead.

--- src/system/Zikula/Module/ExtensionsModule/Listener/HookUiListener.php
+++ src/system/Zikula/Module/ExtensionsModule/Listener/HookUiListener.php
@@ -173,12 +173,7 @@ class HookUiListener
                     $sbaProviderModule = HookUtil::getOwnerByArea($areaname);
 
                     // create an instance of the provider's version
-                    $sbaProviderModuleVersionOld = $sbaProviderModule.'_Version';
-                    $module = \ModUtil::getModule($sbaProviderModule);
-                    $sbaProviderModuleVersion = $module->getVersionClass();
-                    $sbaProviderModuleVersion = class_exists($sbaProviderModuleVersion) ? $sbaProviderModuleVersion : $sbaProviderModuleVersionOld;
-
-                    $sbaProviderModuleVersionObj = new $sbaProviderModuleVersion;
+                    $sbaProviderModuleVersionObj = Util::getVersionMeta($sbaProviderModule);
 
                     // get the bundle title
                     $currentSortingTitles[$areaname] = $view->__($sbaProviderModuleVersionObj->getHookProviderBundle($areaname)->getTitle());
