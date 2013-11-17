zikula/core

commit 969fd5934b63f2d98b84ebd19ea218f99e7c521e

removed unnecessary formatforstore - doctrine takes care of this

src/system/Zikula/Module/SearchModule/Api/UserApi.php
@@ -258,9 +258,7 @@ class UserApi extends \Zikula_AbstractApi
      */
     public function log($args)
     {
-        $searchterms = DataUtil::formatForStore($args['q']);
-
-        $obj = $this->entityManager->getRepository('Zikula\Module\SearchModule\Entity\SearchStatEntity')->findOneBy(array('search' => $searchterms));
+        $obj = $this->entityManager->getRepository('Zikula\Module\SearchModule\Entity\SearchStatEntity')->findOneBy(array('search' => $args['q']));
 
         if (!$obj) {
             $obj = new SearchStatEntity();
@@ -469,5 +467,4 @@ class UserApi extends \Zikula_AbstractApi
 
         return $links;
     }
-
 }

commit df2f3eb8a44f415de03ce9e0e06b349405514a43

Fixed minor bug introduced in 969fd5934b63f2d98b84ebd19ea218f99e7c521e.

src/system/Zikula/Module/SearchModule/Api/UserApi.php
@@ -281,7 +281,7 @@ class UserApi extends \Zikula_AbstractApi
 
         $obj['count'] = isset($obj['count']) ? $obj['count'] + 1 : 1;
         $obj['date'] = new \DateTime('now', new \DateTimeZone('UTC'));
-        $obj['search'] = $searchterms;
+        $obj['search'] = $args['q'];
 
         $this->entityManager->flush();
 