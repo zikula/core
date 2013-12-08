61df44170be4291703ad8e1f94cf0ff5bed2e67e
Shefik <shefik@allknightaccess.com>
2013-12-08 18:10:39 -0500

    The check for unique emails should be true by default.

--- src/system/Zikula/Module/UsersModule/Api/RegistrationApi.php
+++ src/system/Zikula/Module/UsersModule/Api/RegistrationApi.php
@@ -985,7 +985,7 @@ class RegistrationApi extends \Zikula_AbstractApi
             throw new AccessDeniedException();
         }
 
-        $uniqueEmails = $this->getVar('reg_uniemail', false);
+        $uniqueEmails = $this->getVar('reg_uniemail', 1);
         // Checks the following:
         // - none of the three possible IDs is set
         // - uid is set along with either uname or email
