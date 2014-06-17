57fbf85d94b6f234903fd973b824c1befa14d88a
Craig Heydenburg <craigh@mac.com>
2014-06-17 11:49:23 -0400

    determine if extensions are inactive and do not autoload inactive extensions

--- src/lib/Zikula/Bundle/CoreBundle/Bundle/Bootstrap.php
+++ src/lib/Zikula/Bundle/CoreBundle/Bundle/Bootstrap.php
@@ -9,6 +9,11 @@ use Zikula\Core\AbstractBundle;
   
 class Bootstrap
 {
+    /**
+     * @var array the active/inactive state of each extension
+     */
+    private $extensionState = array();
+
     public function getConnection($kernel)
     {
         // get bundles from persistence
@@ -35,42 +40,88 @@ class Bootstrap
     {
         $conn = $this->getConnection($kernel);
         $conn->connect();
-        $res = $conn->executeQuery('SELECT bundlename, bundleclass, autoload, bundlestate FROM bundles');
+        $res = $conn->executeQuery('SELECT bundlename, bundleclass, autoload, bundlestate, bundletype FROM bundles');
         foreach ($res->fetchAll(\PDO::FETCH_NUM) as $row) {
-            list($name, $class, $autoload, $state) = $row;
-            $autoload = unserialize($autoload);
-            if (isset($autoload['psr-0'])) {
-                foreach($autoload['psr-0'] as $prefix => $path) {
-                    $kernel->getAutoloader()->add($prefix, $path);
-                }
-            }
-            if (isset($autoload['psr-4'])) {
-                foreach($autoload['psr-4'] as $prefix => $path) {
-                    $kernel->getAutoloader()->addPsr4($prefix, $path);
-                }
-            }
-            if (isset($autoload['classmap'])) {
-                $kernel->getAutoloader()->addClassMap($autoload['classmaps']);
-            }
-            if (isset($autoload['files'])) {
-                foreach($autoload['files'] as $path) {
-                    include $path;
-                }
-            }
-
-            if (class_exists($class)) {
-                $bundle = new $class;
+            list($name, $class, $autoload, $state, $type) = $row;
+            $extensionIsInactive = $this->extensionIsInactive($conn, $class, $type);
+            if (!$extensionIsInactive) {
                 try {
-                    $bundle->setState($state);
-                    $bundles[] = $bundle;
-                } catch (\InvalidArgumentException $e) {
-                    // continue
+                    $autoload = unserialize($autoload);
+                    if (isset($autoload['psr-0'])) {
+                        foreach($autoload['psr-0'] as $prefix => $path) {
+                            $kernel->getAutoloader()->add($prefix, $path);
+                        }
+                    }
+                    if (isset($autoload['psr-4'])) {
+                        foreach($autoload['psr-4'] as $prefix => $path) {
+                            $kernel->getAutoloader()->addPsr4($prefix, $path);
+                        }
+                    }
+                    if (isset($autoload['classmap'])) {
+                        $kernel->getAutoloader()->addClassMap($autoload['classmaps']);
+                    }
+                    if (isset($autoload['files'])) {
+                        foreach($autoload['files'] as $path) {
+                            include $path;
+                        }
+                    }
+
+                    if (class_exists($class)) {
+                        $bundle = new $class;
+                        try {
+                            $bundle->setState($state);
+                            $bundles[] = $bundle;
+                        } catch (\InvalidArgumentException $e) {
+                            // continue
+                        }
+                    }
+                } catch (\Exception $e) {
+                    // unable to autoload $prefix / $path
+                    // todo - should we catch class not loadable here or not? If so how to handle it?
+                    // see https://github.com/zikula/core/issues/1424
                 }
             }
-
-            // todo - should we catch class not loadable here or not? If so how to handle it?
-            // see https://github.com/zikula/core/issues/1424
         }
         $conn->close();
+    }
+
+    /**
+     * determine if the extension is inactive
+     *
+     * @param \Doctrine\DBAL\Connection $conn
+     * @param string $class
+     * @param string $type
+     *
+     * @return bool
+     */
+    private function extensionIsInactive($conn, $class, $type) {
+        $extensionNameArray = explode('\\', $class);
+        $extensionName = array_pop($extensionNameArray);
+        if (isset($this->extensionState[$extensionName])) {
+            // used cached value
+            $state = $this->extensionState[$extensionName];
+        } else {
+            // load all values into class var for lookup
+            $sql = "SELECT m.name, m.state FROM modules as m";
+            $rows = $conn->executeQuery($sql);
+            foreach ($rows as $row) {
+                $this->extensionState[$row['name']] = (int)$row['state'];
+            }
+            $sql = "SELECT t.name, t.state FROM themes as t";
+            $rows = $conn->executeQuery($sql);
+            foreach ($rows as $row) {
+                $this->extensionState[$row['name']] = (int)$row['state'];
+            }
+            $state = $this->extensionState[$extensionName];
+        }
+        switch($type) {
+            case 'T':
+                // themes in an inactive state should not be autoloaded
+                return ($state == \ThemeUtil::STATE_INACTIVE);
+                break;
+            default:
+                // modules in any state other than ACTIVE or UNINITIALIZED are inactive and should not be autoloaded
+                return (($state !== \ModUtil::STATE_ACTIVE) && ($state !== \ModUtil::STATE_UNINITIALISED));
+        }
     }
 }
