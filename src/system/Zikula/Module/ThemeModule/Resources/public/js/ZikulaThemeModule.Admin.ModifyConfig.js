00c6e1d894f83bb0181c2baa6d7c4e816c953ac0
Guite <info@guite.de>
2014-09-09 16:05:28 +0200

    minor js cs fixes

--- src/system/Zikula/Module/ThemeModule/Resources/public/js/ZikulaThemeModule.Admin.ModifyConfig.js
+++ src/system/Zikula/Module/ThemeModule/Resources/public/js/ZikulaThemeModule.Admin.ModifyConfig.js
@@ -1,14 +1,14 @@
-// Copyright Zikula Foundation 2009 - license GNU/LGPLv3 (or at your option, any later version).
+// Copyright Zikula Foundation 2014 - license GNU/LGPLv3 (or at your option, any later version).
 
 ( function($) {
     $(document).ready(function() {
-        if ($('#alt_theme_name').val() == "") {
+        if ($('#alt_theme_name').val() == '') {
             // Not set
             $('#alt_theme_domain').parent().parent().hide();
         }
 
-        $("#alt_theme_name").change(function() {
-            if ($('#alt_theme_name').val() == "") {
+        $('#alt_theme_name').change(function() {
+            if ($('#alt_theme_name').val() == '') {
                 $('#alt_theme_domain').parent().parent().fadeOut();
             } else {
                 $('#alt_theme_domain').parent().parent().fadeIn();
--- src/system/Zikula/Module/ThemeModule/Resources/public/js/themeswitcher.js
+++ src/system/Zikula/Module/ThemeModule/Resources/public/js/themeswitcher.js
@@ -1,8 +1,16 @@
-jQuery( document ).ready(function() {
-    jQuery('#newtheme').change( function() {
-        var $selectedTheme = jQuery(this).find(':selected')
-        jQuery('#preview').attr('src', $selectedTheme.data('previewimage'));
-        jQuery('#preview').attr('title', $selectedTheme.attr('title'));
-        jQuery('#preview').attr('alt', $selectedTheme.attr('title'));
+// Copyright Zikula Foundation 2014 - license GNU/LGPLv3 (or at your option, any later version).
+
+( function($) {
+    $(document).ready(function() {
+        $('#newtheme').change( function() {
+            var selectedTheme;
+
+            selectedTheme = $(this).find(':selected')
+            $('#preview').attr({
+                src: selectedTheme.data('previewimage'),
+                title: selectedTheme.attr('title'),
+                alt: selectedTheme.attr('title')
+            });
+        });
     });
-});
+})(jQuery);
