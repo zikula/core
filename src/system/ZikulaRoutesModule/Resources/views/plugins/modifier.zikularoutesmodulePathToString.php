eb3368a51579db848db388ec70136463f3c32129
cmfcmf <cmfcmf.flach@gmail.com>
2014-09-22 18:31:22 +0200

    Display defaults and requirements for route path variables.

--- src/system/ZikulaRoutesModule/Resources/views/plugins/modifier.zikularoutesmodulePathToString.php
+++ src/system/ZikulaRoutesModule/Resources/views/plugins/modifier.zikularoutesmodulePathToString.php
@@ -28,5 +28,26 @@ function smarty_modifier_zikularoutesmodulePathToString($path, \Zikula\RoutesMod
         return "<abbr title=\"" . \DataUtil::formatForDisplay($matches[0]) . "\">" . \DataUtil::formatForDisplay($container->getParameter($matches[1])) . "</abbr>";
     }, $path);
 
+    $defaults = $route->getDefaults();
+    $requirements = $route->getRequirements();
+    $dom = ZLanguage::getModuleDomain('ZikulaRoutesModule');
+    $path = preg_replace_callback('#{(.*?)}#', function ($matches) use ($container, $defaults, $requirements, $dom) {
+        $title = "";
+        if (isset($defaults[$matches[1]])) {
+            $title .= __f('Default: %s', array(\DataUtil::formatForDisplay($defaults[$matches[1]])), $dom);
+        }
+        if (isset($requirements[$matches[1]])) {
+            if ($title != "") {
+                $title .= " | ";
+            }
+            $title .= __f('Requirement: %s', array(\DataUtil::formatForDisplay($requirements[$matches[1]])), $dom);
+        }
+        if ($title == "") {
+            return $matches[0];
+        }
+
+        return "<abbr title=\"$title\">" . $matches[0] . "</abbr>";
+    }, $path);
+
     return "$prefix<strong>$path</strong>";
 }
