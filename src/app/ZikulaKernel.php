19ef96e4179ee4ad0b608b631cccde0e23371b12
Guite <info@guite.de>
2014-12-06 13:29:55 +0100

    updated to Symfony 2.6, includes some minor additions and simplifications

--- src/app/ZikulaKernel.php
+++ src/app/ZikulaKernel.php
@@ -31,6 +31,7 @@ class ZikulaKernel extends Kernel
         $bundles[] = new CustomBundle\CustomBundle();
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
