d2c1b477a0a861d520c5bce25b4cba711092af5c
Guite <info@guite.de>
2014-12-08 18:12:21 +0100

    proposal: added admin permissions to console commands

--- src/lib/Zikula/Bundle/CoreBundle/Console/Application.php
+++ src/lib/Zikula/Bundle/CoreBundle/Console/Application.php
@@ -18,5 +18,45 @@ class Application extends BaseApplication
 
         $this->setName('Zikula');
         $this->setVersion(\Zikula_Core::VERSION_NUM.' - '.$kernel->getName().'/'.$kernel->getEnvironment().($kernel->isDebug() ? '/debug' : ''));
+
+        // ensure that we have admin access
+        try {
+            $this->loginAsAdministrator()
+        } catch (\Exception $e) {
+            die(__('Sorry, an exception occured:') . ' ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Grants admin access for console commmands (#1908).
+     * This avoids subsequent permission problems from any components used.
+     */
+    protected function loginAsAdministrator()
+    {
+        $adminId = 2;
+
+        // no need to do anything if there is already an admin login
+        if (UserUtil::isLoggedIn()) {
+            if (UserUtil::getVar('uid') == $adminId) {
+                return;
+            }
+
+            if (SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
+                return;
+            }
+        }
+
+        // login / impersonate now
+        UserUtil::setUserByUid($adminId);
+
+        // check if it worked
+        if (!UserUtil::isLoggedIn()) {
+            throw new AccessDeniedException(__('Error! Auto login failed.'));
+        }
+
+        // check if permissions have become available
+        if (!SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
+            throw new AccessDeniedException(__('Error! Insufficient permissions after auto login.'));
+        }
     }
 }
