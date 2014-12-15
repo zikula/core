ef0b76fa0666d8b580f3fd08b511f9c80150fb71
Axel Guckelsberger <info@guite.de>
2014-12-15 12:56:54 +0100

    Updated year of copyright

--- src/lib/Zikula/Bundle/CoreBundle/EventListener/OutputCompressionListener.php
+++ src/lib/Zikula/Bundle/CoreBundle/EventListener/OutputCompressionListener.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ * Copyright Zikula Foundation 2014 - Zikula Application Framework
  *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
