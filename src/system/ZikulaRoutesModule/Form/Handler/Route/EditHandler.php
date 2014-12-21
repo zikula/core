f404eaa1f3e731a03c6577a09c60eb0bc62d72dc
Christian Flach <cmfcmf.flach@gmail.com>
2014-12-21 16:01:39 +0100

    Added simple url conflict checks, refs #1878

--- src/system/ZikulaRoutesModule/Form/Handler/Route/EditHandler.php
+++ src/system/ZikulaRoutesModule/Form/Handler/Route/EditHandler.php
@@ -13,10 +13,11 @@
 namespace Zikula\RoutesModule\Form\Handler\Route;
 
 use ModUtil;
+use Symfony\Component\Routing\RouteCollection;
 use Zikula\RoutesModule\Entity\RouteEntity;
 use Zikula\RoutesModule\Form\Handler\Route\Base\EditHandler as BaseEditHandler;
-use Zikula_Form_View;
 use Zikula\RoutesModule\Routing\Util as RoutingUtil;
+use Zikula_Form_View;
 
 /**
  * This handler class handles the page events of the Form called by the zikulaRoutesModule_admin_edit() function.
@@ -52,16 +53,17 @@ class EditHandler extends BaseEditHandler
         return $result;
     }
 
-    public function handleCommand(Zikula_Form_View $view, &$args)
+    public function applyAction(array $args = array())
     {
-        $return = parent::handleCommand($view, $args);
+        $return = parent::applyAction($args);
+
+        $this->checkConflicts($this->entityRef);
 
         $cacheClearer = $this->view->getContainer()->get('zikula.cache_clearer');
         $cacheClearer->clear("symfony.routing");
 
         return $return;
     }
-
 
     protected function writeRelationDataToEntity(Zikula_Form_View $view, $entity, $entityData)
     {
@@ -138,5 +140,52 @@ class EditHandler extends BaseEditHandler
         $bundlePrefix = isset($options['zkNoBundlePrefix']) ? !$options['zkNoBundlePrefix'] : true;
 
         return array($i18n, $bundlePrefix);
+    }
+
+    private function checkConflicts(RouteEntity $routeEntity)
+    {
+        $newPath = $routeEntity->getPathWithBundlePrefix();
+
+        $router = $this->view->getContainer()->get('router');
+        /** @var RouteCollection $routeCollection */
+        $routeCollection = $router->getRouteCollection();
+
+        $errors = array();
+        foreach ($routeCollection->all() as $route) {
+            $path = $route->getPath();
+            if ($path === '/{url}') {
+                continue;
+            }
+
+            if ($path === $newPath) {
+                $errors[] = array(
+                    'type' => 'SAME',
+                    'path' => $path
+                );
+                continue;
+            }
+
+            $pathRegExp = preg_quote(preg_replace("/{(.+)}/", "____DUMMY____", $path), '/');
+            $pathRegExp = "#^" . str_replace('____DUMMY____', '(.+)', $pathRegExp) . "$#";
+
+            $matches = array();
+            preg_match($pathRegExp, $newPath, $matches);
+            if (count($matches)) {
+                $errors[] = array(
+                    'type' => 'SIMILAR',
+                    'path' => $path
+                );
+            }
+        }
+
+        foreach ($errors as $error) {
+            if ($error['type'] == 'SAME') {
+                $message = $this->__('It looks like you created or updated a route with a path which already exists. This is an error in most cases.');
+            } else {
+                $message = $this->__f('The path of the route you created or updated looks similar to the following already existing path: %s Are you sure you haven\'t just introduced a conflict?', $error['path']);
+            }
+
+            \LogUtil::registerError($message);
+        }
     }
 }
