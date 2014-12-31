b3022bc6376eb8ec02a5d246e3f76c3c7030a8e1
Craig Heydenburg <craigh@mac.com>
2014-12-31 15:18:26 -0500

    fix ConfigUtil

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Util/ConfigUtil.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Util/ConfigUtil.php
@@ -18,6 +18,7 @@ use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Zikula\Bundle\CoreBundle\HttpKernel\ZikulaKernel;
 use Zikula\Core\Exception\FatalErrorException;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ConfigUtil
 {
@@ -33,22 +34,31 @@ class ConfigUtil
 
     /**
      * Write the legacy Config file
+     * @param array $params
      * @deprecated since 1.4.0
      */
-    public function writeLegacyConfig()
+    public function writeLegacyConfig($params = null)
     {
-        /**
-         * @TODO rewrite this to copy the old config and write to `personal_config.php` instead
-         */
-        if (is_writable($this->kernel->getRootDir() . '/config/config.php')) {
-            $file = file_get_contents($this->kernel->getRootDir() . '/config/config.php');
-            $file = $this->replaceKeys('dbname', $this->params['database_name'], $file);
-            $file = $this->replaceKeys('dbdriver', substr($this->params['database_driver'], 4), $file);
-            $file = $this->replaceKeys('dbtabletype', $this->params['dbtabletype'], $file);
-            $file = $this->replaceKeys('user', $this->params['database_user'], $file);
-            $file = $this->replaceKeys('password', $this->params['database_password'], $file);
-            $file = $this->replaceKeys('host', $this->params['database_host'], $file);
-            file_put_contents($this->kernel->getRootDir() . '/config/config.php', $file);
+        $personalConfigPath = realpath($this->kernel->getRootDir() . '/../config/personal_config.php');
+        $configPath = realpath($this->kernel->getRootDir() . '/../config/config.php');
+        if (!file_exists($personalConfigPath)) {
+            $fs = new Filesystem();
+            if ($fs->exists($configPath)) {
+                // initialize file from a copy of original
+                $fs->copy($configPath, $this->kernel->getRootDir() . '/../config/personal_config.php');
+            }
+            $personalConfigPath = realpath($this->kernel->getRootDir() . '/../config/personal_config.php');
+        }
+        $params = !empty($params) ? $params : $this->params;
+        if (is_writable($personalConfigPath)) {
+            $file = file_get_contents($personalConfigPath);
+            $file = $this->replaceKeys('dbname', $params['database_name'], $file);
+            $file = $this->replaceKeys('dbdriver', substr($params['database_driver'], 4), $file);
+            $file = $this->replaceKeys('dbtabletype', $params['dbtabletype'], $file);
+            $file = $this->replaceKeys('user', $params['database_user'], $file);
+            $file = $this->replaceKeys('password', $params['database_password'], $file);
+            $file = $this->replaceKeys('host', $params['database_host'], $file);
+            file_put_contents($personalConfigPath, $file);
         } else {
             throw new AccessDeniedException('config.php');
         }
