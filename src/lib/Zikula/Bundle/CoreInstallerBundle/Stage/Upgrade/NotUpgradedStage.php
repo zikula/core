1280ec9f2f245597e139128bde8995c82b53e328
Craig Heydenburg <craigh@mac.com>
2015-01-04 08:04:19 -0500

    correct not upgraded stage to show template

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Stage/Upgrade/NotUpgradedStage.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Stage/Upgrade/NotUpgradedStage.php
@@ -35,7 +35,7 @@ class NotUpgradedStage implements StageInterface
         if (version_compare(ZIKULACORE_CURRENT_INSTALLED_VERSION, UpgraderController::ZIKULACORE_MINIMUM_UPGRADE_VERSION, '<')) {
             throw new AbortStageException(__f('The current installed version of Zikula is reporting (%1$s). You must upgrade to version (%2$s) before you can use this upgrade.', array(ZIKULACORE_CURRENT_INSTALLED_VERSION, UpgraderController::ZIKULACORE_MINIMUM_UPGRADE_VERSION)));
         }
-        return false;
+        return true;
     }
 
     public function getTemplateParams()
