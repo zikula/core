dcb69aeacd7605bcb17ae3f20d1405c38ce910c5
Craig Heydenburg <craigh@mac.com>
2015-01-07 07:41:05 -0500

    add functional doc viewer controller and adjust templates, etc.

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/DocController.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/DocController.php
@@ -20,7 +20,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
-use Zikula\Bundle\CoreInstallerBundle\Util\ControllerUtil;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Michelf\MarkdownExtra;
 
 /**
  * Class DocController
@@ -28,24 +29,28 @@ use Zikula\Bundle\CoreInstallerBundle\Util\ControllerUtil;
  */
 class DocController
 {
+    private $kernel;
     private $router;
     private $templatingService;
-    private $util;
-    private $lang;
+    private $parser;
+    private $locale;
+    private $basePath;
 
     /**
      * Constructor.
      *
+     * @param KernelInterface $kernel
      * @param RouterInterface $router The route generator
      * @param EngineInterface $templatingService
-     * @param $util
+     * @param MarkdownExtra $parser
      */
-    public function __construct(RouterInterface $router, EngineInterface $templatingService, ControllerUtil $util)
+    public function __construct(KernelInterface $kernel, RouterInterface $router, EngineInterface $templatingService, MarkdownExtra $parser)
     {
+        $this->kernel = $kernel;
         $this->router = $router;
         $this->templatingService = $templatingService;
-        $this->util = $util;
-        $this->lang = \ZLanguage::getLanguageCode();
+        $this->parser = $parser;
+        $this->locale = \ZLanguage::getLanguageCode();
     }
 
     /**
@@ -53,9 +58,38 @@ class DocController
      * @param string $name
      * @return Response
      */
-    public function displayAction(Request $request, $name = "INSTALL")
+    public function displayAction(Request $request, $name = "INSTALL-1.4.0.md")
     {
-        // display e.g. docs/$this->lang/{$name}.md
-        return new Response();
+        // @TODO this is temporary method of restricting the user input
+        if (!in_array($name, array("INSTALL-1.4.0.md", "UPGRADE-1.4.0.md", "CHANGELOG.md", "README.md"))) {
+            $name = "INSTALL-1.4.0.md";
+        }
+        $this->setBasePath();
+
+        if (file_exists($this->basePath . "/$name")) {
+            $content = file_get_contents($this->basePath . "/$name");
+        } else {
+            $content = __f('The file you requested (%s) could not be found.', "$name");
+        }
+        $content = $this->parser->defaultTransform($content);
+        $templateParams = array(
+            'lang' => $this->locale,
+            'charset' => \ZLanguage::getEncoding(),
+            'content' => $content,
+        );
+        return $this->templatingService->renderResponse('ZikulaCoreInstallerBundle::doc.html.twig', $templateParams);
+    }
+
+    /**
+     * set the base path for doc files, computing whether this is a Github clone or CI build.
+     */
+    public function setBasePath()
+    {
+        if (file_exists(realpath($this->kernel->getRootDir() . '/../../composer.json'))) {
+            // installation is clone of github repo and files are not moved
+            $this->basePath = realpath($this->kernel->getRootDir() . '/../..');
+        } else {
+            $this->basePath = realpath($this->kernel->getRootDir() . '/../docs/' . $this->locale);
+        }
     }
 }
