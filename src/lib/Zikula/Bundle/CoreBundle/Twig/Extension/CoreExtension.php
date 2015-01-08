e8b60851081f9986b10cc1f6483a161eb76aa7fb
Craig Heydenburg <craigh@mac.com>
2015-01-08 14:04:09 -0500

    add Twig extension for array_unset

--- src/lib/Zikula/Bundle/CoreBundle/Twig/Extension/CoreExtension.php
+++ src/lib/Zikula/Bundle/CoreBundle/Twig/Extension/CoreExtension.php
@@ -39,6 +39,7 @@ class CoreExtension extends \Twig_Extension
             'blockinfo' => new \Twig_Function_Method($this, 'getBlockInfo'),
             'zasset' => new \Twig_Function_Method($this, 'getAssetPath'),
             'showflashes' => new \Twig_Function_Method($this, 'showFlashes', array('is_safe' => array('html'))),
+            'array_unset' => new \Twig_Function_Method($this, 'arrayUnset'),
         );
     }
 
@@ -173,6 +174,21 @@ class CoreExtension extends \Twig_Extension
         }
 
         return $result;
+    }
+
+    /**
+     * Delete a key of an array
+     *
+     * @param array  $array Source array
+     * @param string $key   The key to remove
+     *
+     * @return array
+     */
+    public function arrayUnset($array, $key)
+    {
+        unset($array[$key]);
+
+        return $array;
     }
 
     /**
