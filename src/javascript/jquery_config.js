d5e098369c38a7476edc89b12822d8867a0ac6ed
Craig Heydenburg <craigh@mac.com>
2015-02-09 10:11:51 -0500

    implement fix from http://softec.lu/site/DevelopersCorner/BootstrapPrototypeConflict refs #2238

--- src/javascript/jquery_config.js
+++ src/javascript/jquery_config.js
@@ -1,4 +1,20 @@
 jQuery.noConflict();
+if (Prototype.BrowserFeatures.ElementExtensions) {
+    var disablePrototypeJS = function (method, pluginsToDisable) {
+            var handler = function (event) {
+                event.target[method] = undefined;
+                setTimeout(function () {
+                    delete event.target[method];
+                }, 0);
+            };
+            pluginsToDisable.each(function (plugin) {
+                jQuery(window).on(method + '.bs.' + plugin, handler);
+            });
+        },
+        pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
+    disablePrototypeJS('show', pluginsToDisable);
+    disablePrototypeJS('hide', pluginsToDisable);
+}
 // setup ajax for jQuery
 (function($) {
     var defaultOptions = {
