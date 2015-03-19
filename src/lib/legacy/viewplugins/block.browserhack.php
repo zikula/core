bbea8050c66f482c493fd9c6867dbdd5912a460b
Shefik <shefik@allknightaccess.com>
2015-03-19 01:36:00 -0400

    Removing older IE support.

--- src/lib/legacy/viewplugins/block.browserhack.php
+++ src/lib/legacy/viewplugins/block.browserhack.php
@@ -17,8 +17,8 @@
  * Browserhack block.
  *
  * Sample:
- * {browserhack condition="if lte IE 7"}something goes here{/browserhack}
- * {browserhack condition="if lte IE 7" assign="var"}something goes here{/browserhack}
+ * {browserhack condition="if lte IE 8"}something goes here{/browserhack}
+ * {browserhack condition="if lte IE 8" assign="var"}something goes here{/browserhack}
  *
  * @param array       $params  Array with keys 'condition' whatever goes in the browserhack,
  *                             and 'assign' to assign rather than display.
@@ -31,7 +31,7 @@ function smarty_block_browserhack($params, $content, Zikula_View $view)
 {
     if ($content) {
         if (!isset($params['condition'])) {
-            $view->trigger_error(__('browserhack block: condition param is required, non specified.'));
+            $view->trigger_error(__('browserhack block: condition param is required, none specified.'));
         }
 
         $condition = $params['condition'];
