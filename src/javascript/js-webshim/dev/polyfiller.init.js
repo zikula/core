cbbae7829ba548494bff252c7b773880f7ebec58
Shefik <shefik@allknightaccess.com>
2015-03-29 06:45:19 -0400

    Adding support for "minLength" in Firefox.

--- src/javascript/js-webshim/dev/polyfiller.init.js
+++ src/javascript/js-webshim/dev/polyfiller.init.js
@@ -1,4 +1,5 @@
 (function($) {
+    $.webshim.bugs.bustedValidity = !('minLength' in document.createElement('input'));
     $.webshims.setOptions('basePath', Zikula.Config.baseURL+'javascript/js-webshim/dev/shims/'); 
     $.webshims.activeLang(Zikula.Config.lang);
     $.webshims.polyfill(Zikula.Config.polyfillFeatures);
