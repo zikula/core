dc100ca25ae40d91e5bbfffe30683123bb967013
Christian Flach <cmfcmf.flach@gmail.com>
2015-04-02 19:19:04 +0200

    Make sure to replace Windows backslashes by actual slashes, fixes #2391.

--- src/lib/util/PageUtil.php
+++ src/lib/util/PageUtil.php
@@ -418,7 +418,7 @@ class PageUtil
 
         $root = realpath($kernel->getRootDir() . "/../");
         $fullPath = $kernel->locateResource($path);
-        $path = System::getBaseUri() . substr($fullPath, strlen($root));
+        $path = System::getBaseUri() . str_replace(DIRECTORY_SEPARATOR, '/', substr($fullPath, strlen($root)));
 
         return $path;
     }
