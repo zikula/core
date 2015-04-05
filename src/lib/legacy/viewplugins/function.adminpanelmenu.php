12b8338b176b17ad3a05c34d8aaf013e7a54dbea
Craig Heydenburg <craigh@mac.com>
2015-04-05 18:20:39 -0400

    add template plugin and sass/css files for customizations to library

--- src/lib/legacy/viewplugins/function.adminpanelmenu.php
+++ src/lib/legacy/viewplugins/function.adminpanelmenu.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ * Copyright Zikula Foundation 2015 - Zikula Application Framework
  *
  * This work is contributed to the Zikula Foundation under one or more
  * Contributor Agreements and licensed to You under the following license:
@@ -14,22 +14,97 @@
  */
 
 /**
- * Insert a CSRF protection nonce.
+ * Inserts a hidden admin panel controlled by permissions.
  *
- * Available parameters:
- *   - assign: Assign rather the output.
+ * Inserts required javascript and css files for a hidden admin panel that is triggered by a rendered link.
+ * Builds and renders an unordered list of admin-capable modules and their adminLinks using the
+ * jQuery.mmenu library <@see http://mmenu.frebsite.nl>
  *
- * Example:
- * <input type="hidden" name="csrftoken" value="{insert name='csrftoken'}" />
+ * This plugin currently has NO configuration options.
+ *
+ * Examples:
+ *
+ * <samp>{adminpanelmenu}</samp>
  *
  * @param array       $params All attributes passed to this function from the template.
- * @param Zikula_View $view   Reference to the Zikula_View object.
+ * @param Zikula_View $view   Reference to the {@link Zikula_View} object.
  *
  * @return string
  */
-function smarty_insert_adminpanelmenu($params, $view)
+function smarty_function_adminpanelmenu($params, Zikula_View $view)
 {
-    $modules = ModUtil::getModulesCapableOf('admin');
-    echo "<pre style='text-align:left'>"; var_dump($modules); echo "</pre>";
+    if (!SecurityUtil::checkPermission('ZikulaAdminModule::', "::", ACCESS_EDIT)) {
+        return ''; // Since no permission, return empty
+    }
 
+    // add required scritps and stylesheets to page
+    PageUtil::addVar('javascript', '@ZikulaAdminModule/Resources/public/js/jQuery.mmenu-master/dist/js/jquery.mmenu.min.all.js');
+    PageUtil::addVar('stylesheet', '@ZikulaAdminModule/Resources/public/js/jQuery.mmenu-master/dist/css/jquery.mmenu.all.css');
+    // add override for panel width created from .scss file
+    PageUtil::addVar('stylesheet', '@ZikulaAdminModule/Resources/public/css/mmenu-hiddenpanel-customwidth.css');
+
+    $modules = ModUtil::getModulesCapableOf('admin');
+
+    // create unordered list of admin-capable module links
+    $htmlContent = '<nav id="zikula-admin-hiddenpanel-menu">';
+    $htmlContent .= '<div class="text-left">';
+    $htmlContent .= '<h1><img src="images/logo.gif" height="32px"> ' . __('Administration') . '</h1>';
+    $htmlContent .= '<ul>';
+    foreach ($modules as $module) {
+        if (SecurityUtil::checkPermission("module[name]::", '::', ACCESS_EDIT)) {
+            // first-level list - list modules with general 'index' list
+            $img = ModUtil::getModuleImagePath($module['name']);
+            $url = ModUtil::url($module['name'], 'admin', 'index');
+            $moduleSelected = empty($moduleSelected) && strpos($view->getRequest()->getUri(), $module['url']) ? " class='Selected'" : "";
+            $htmlContent .= "<li{$moduleSelected}><a href='$url'><img src='$img' height='18px' /> " . $module['displayname'] . "</a>";
+            $htmlContent .= "<ul class='text-left'>";
+            $links = ModUtil::apiFunc($module['name'], 'admin', 'getLinks');
+            // create second-level list from module adminLinks
+            foreach ($links as $link) {
+                if (isset($link['icon'])) {
+                    $img = "<i class='fa fa-$link[icon]'></i>";
+                } elseif (isset($link['class'])) {
+                    $img = "<span class='$link[class]'></span>";
+                } else {
+                    $img = '';
+                }
+                $linkSelected = empty($linkSelected) && strpos($view->getRequest()->getUri(), $link['url']) ? " class='Selected'" : "";
+                $htmlContent .= "<li{$linkSelected}><a href='$link[url]'>$img $link[text]</a>";
+                // create third-level list from adminLinks subLinks
+                if (isset($link['links'])) {
+                    $htmlContent .= "<ul class='text-left'>";
+                    foreach ($link['links'] as $sublink) {
+                        $htmlContent .= "<li><a href='$sublink[url]'>$sublink[text]</a></li>";
+                    }
+                    $htmlContent .= '</ul>';
+                }
+                $htmlContent .= "</li>";
+            }
+            $htmlContent .= "</ul>";
+            $htmlContent .= "</li>";
+        }
+    }
+    $htmlContent .= "</ul>";
+    $htmlContent .= "</div>";
+    $htmlContent .= "</nav>";
+    $htmlContent .= '
+            <script type="text/javascript">
+                jQuery(document).ready(function( $ ){
+                    $("#zikula-admin-hiddenpanel-menu").mmenu({
+                        extensions: ["hiddenpanel-customwidth"],
+                        "header": {
+                           "title": "Zikula Administration",
+                           "add": true,
+                           "update": true
+                        },
+                        "searchfield": true
+                    });
+                });
+            </script>';
+
+    // the the html content before </body>
+    PageUtil::addVar('footer', $htmlContent);
+
+    // display the control link
+    return '<a href="#zikula-admin-hiddenpanel-menu"><i class="fa fa-bars"></i></a>';
 }
