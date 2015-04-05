2499ef6423281fc9b80671332704548d52b7690d
Shefik <shefik@allknightaccess.com>
2015-04-04 23:56:13 -0400

    Correcting error "Failed to load resource...412 (Precondition Failed)"

--- src/javascript/js-webshim/minified/polyfiller.init.js
+++ src/javascript/js-webshim/minified/polyfiller.init.js
@@ -1,6 +1,7 @@
 (function($) {
     $.webshim.bugs.bustedValidity = !('minLength' in document.createElement('input'));
     $.webshims.setOptions('basePath', Zikula.Config.baseURL+'javascript/js-webshim/minified/shims/');
+    $.webshims.setOptions('debug', true);
     $.webshims.activeLang(Zikula.Config.lang);
     $.webshims.polyfill(Zikula.Config.polyfillFeatures);
 })(jQuery);
