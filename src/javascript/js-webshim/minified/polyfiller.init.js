1d2e101c94673edb05038cadf7b884af4ca1da5d
Craig Heydenburg <craigh@mac.com>
2015-04-06 08:36:11 -0400

    provide better fix for "Failed to load resource...412 (Precondition Failed)" refs #2402, #2388, and https://github.com/aFarkas/webshim/issues/503

--- src/javascript/js-webshim/minified/polyfiller.init.js
+++ src/javascript/js-webshim/minified/polyfiller.init.js
@@ -1,7 +1,9 @@
 (function($) {
     $.webshim.bugs.bustedValidity = !('minLength' in document.createElement('input'));
     $.webshims.setOptions('basePath', Zikula.Config.baseURL+'javascript/js-webshim/minified/shims/');
-    $.webshims.setOptions('debug', true); // This must be set to TRUE, to avoid "HTTP Error 412 - Precondition failed" in Safari.
+    $.webshims.setOptions('ajax', {
+        method: 'GET'
+    });
     $.webshims.activeLang(Zikula.Config.lang);
     $.webshims.polyfill(Zikula.Config.polyfillFeatures);
 })(jQuery);
