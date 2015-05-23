dd16dee7403decd5d172b44d96ed8449280427cb
Kaik <kapitanogor@googlemail>
2015-05-23 15:23:25 +0200

    Add translator section to rootNode

--- src/lib/Zikula/Bundle/CoreBundle/DependencyInjection/Configuration.php
+++ src/lib/Zikula/Bundle/CoreBundle/DependencyInjection/Configuration.php
@@ -33,7 +33,10 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-
+        $rootNode = $treeBuilder->root('framework');
+               
+        $this->addTranslatorSection($rootNode);
+              
         return $treeBuilder;
     }
        
