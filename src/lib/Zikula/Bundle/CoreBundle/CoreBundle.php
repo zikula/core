7fd9497b86a6d346e003b36b15788925526fe229
Axel Guckelsberger <info@guite.de>
2015-06-25 07:39:01 +0200

    Removed temporary compiler pass
    fixes #2038

--- src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
+++ src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
@@ -2,7 +2,6 @@
 
 namespace Zikula\Bundle\CoreBundle;
 
-use Matthias\SymfonyServiceDefinitionValidator\Compiler\Compatibility\FixSymfonyValidatorDefinitionPass;
 use Matthias\SymfonyServiceDefinitionValidator\Compiler\ValidateServiceDefinitionsPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -18,9 +17,6 @@ class CoreBundle extends Bundle
         $container->addCompilerPass(new DoctrinePass(), PassConfig::TYPE_OPTIMIZE);
 
         $container->addCompilerPass(new RegisterCoreListenersPass(), PassConfig::TYPE_AFTER_REMOVING);
-
-        // see https://github.com/symfony/symfony/issues/11909
-        $container->addCompilerPass(new FixSymfonyValidatorDefinitionPass());
 
         // todo - see if we can do this only on module install/upgrade - drak
         $container->addCompilerPass(new ValidateServiceDefinitionsPass(), PassConfig::TYPE_AFTER_REMOVING);
