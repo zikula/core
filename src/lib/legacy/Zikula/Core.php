9e461397728de2bdb62a7c80c92a5810374651e2
Christian Flach <cmfcmf@users.noreply.github.com>
2015-07-14 11:16:14 +0200

    Allow viewing docs even in upgrade, closes #2530

--- src/lib/legacy/Zikula/Core.php
+++ src/lib/legacy/Zikula/Core.php
@@ -473,13 +473,14 @@ class Zikula_Core
         // all these routes are hard-coded in xml files
         $uriContainsInstall = strpos($request->getRequestUri(), '/install') !== false;
         $uriContainsUpgrade = strpos($request->getRequestUri(), '/upgrade') !== false;
+        $uriContainsDoc = strpos($request->getRequestUri(), '/installdoc') !== false;
         $uriContainsWdt = strpos($request->getRequestUri(), '/_wdt') !== false;
         $uriContainsProfiler = strpos($request->getRequestUri(), '/_profiler') !== false;
         $uriContainsRouter = strpos($request->getRequestUri(), '/js/routing?callback=fos.Router.setData') !== false;
         $doNotRedirect = $uriContainsProfiler || $uriContainsWdt || $uriContainsRouter || $request->isXmlHttpRequest();
 
         // check if Zikula Core is not installed
-        if (!$installed && !$uriContainsInstall && !$doNotRedirect) {
+        if (!$installed && !$uriContainsDoc && !$uriContainsInstall && !$doNotRedirect) {
             $this->container->get('router')->getContext()->setBaseUrl($request->getBasePath()); // compensate for sub-directory installs
             $url = $this->container->get('router')->generate('install');
             $response = new RedirectResponse($url);
@@ -487,7 +488,7 @@ class Zikula_Core
             System::shutDown();
         }
         // check if Zikula Core requires upgrade
-        if ($requiresUpgrade && !$uriContainsUpgrade && !$doNotRedirect) {
+        if ($requiresUpgrade && !$uriContainsDoc && !$uriContainsUpgrade && !$doNotRedirect) {
             $this->container->get('router')->getContext()->setBaseUrl($request->getBasePath()); // compensate for sub-directory installs
             $url = $this->container->get('router')->generate('upgrade');
             $response = new RedirectResponse($url);
