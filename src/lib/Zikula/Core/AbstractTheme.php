2d10adcf3a66110d6d41e5aa56e86bea196c016a
Craig Heydenburg <craigh@mac.com>
2015-07-30 13:45:16 -0400

    move rendering of response from Engine to AbstractTheme ping @cmfcmf

--- src/lib/Zikula/Core/AbstractTheme.php
+++ src/lib/Zikula/Core/AbstractTheme.php
@@ -2,8 +2,14 @@
 
 namespace Zikula\Core;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Yaml\Yaml;
+
 abstract class AbstractTheme extends AbstractBundle
 {
+    private $config;
+    private $isTwigBased = false;
+
     public function getNameType()
     {
         return 'Theme';
@@ -12,5 +18,54 @@ abstract class AbstractTheme extends AbstractBundle
     public function getServiceIds()
     {
         return array();
+    }
+
+    /**
+     * load the theme configuration from the config/theme.yml file
+     */
+    public function __construct()
+    {
+        $configPath = $this->getConfigPath() . '/theme.yml';
+        if (file_exists($configPath)) {
+            $this->config = Yaml::parse($configPath);
+            $this->isTwigBased = true;
+        }
+    }
+
+    /**
+     * generate a response wrapped in the theme
+     * @param Response $response
+     * @return Response
+     */
+    public function generateThemedResponse(Response $response)
+    {
+        // @todo determine proper template? and location
+        // @todo NOTE: 'pagetype' is temporary var in the template
+
+        $template = $this->config['master']['page'];
+
+        return $this->getContainer()->get('templating')->renderResponse($this->name . ':' . $template, array('maincontent' => $response->getContent(), 'pagetype' => 'admin'));
+    }
+
+    /**
+     * convert the block content to a theme-wrapped Response
+     * @param $block
+     * @return mixed
+     */
+    public function generateThemedBlock($block)
+    {
+        $template = $this->config['master']['block']['positions'][$block['position']];
+
+        return $this->getContainer()->get('templating')->render($this->name . ':' . $template, $block); // @todo renderView? renderResponse?
+    }
+
+    /**
+     * Is theme twig (Core-2.0) based?
+     * @deprecated
+     * @return bool
+     */
+    public function isTwigBased()
+    {
+        return $this->isTwigBased;
     }
 }
--- src/lib/Zikula/Core/Theme/Engine.php
+++ src/lib/Zikula/Core/Theme/Engine.php
@@ -14,36 +14,19 @@
 
 namespace Zikula\Core\Theme;
 
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Yaml\Yaml;
 use Zikula\Core\Event\GenericEvent;
 use Zikula\Core\Response\AdminResponse;
 
 class Engine
 {
-    /**
-     * @var EngineInterface
-     */
-    private $templatingService;
     private $themeName = '';
     /**
      * @var \Zikula\Core\AbstractTheme
      */
     private $themeBundle = null;
-    /**
-     * @var array
-     */
-    private $themeConfig = array();
-    /**
-     * indicator flag whether theme is twig-based (default false to force setting)
-     * remove all checks for this in Core-2.0
-     * @deprecated
-     * @var bool
-     */
-    private $themeIsTwigBased = false;
     /**
      * flag indicating whether the theme has been overridden by Response type
      * @var bool
@@ -51,24 +34,19 @@ class Engine
     private $themeIsOverridden = false;
     private $requestAttributes;
 
-    function __construct(EngineInterface $templatingService, RequestStack $requestStack)
+    function __construct(RequestStack $requestStack)
     {
-        $this->templatingService = $templatingService;
         $request = $requestStack->getCurrentRequest(); // not available during installation
         if (is_object($request)) {
             $this->requestAttributes = $request->attributes->all();
             $this->themeName = $this->getCurrentTheme($requestStack->getCurrentRequest());
-            // @todo Note usage of Util classes (ThemeUtil) This must be corrected.
+            // @todo Note usage of ThemeUtil class must be removed.
             $this->themeBundle = \ThemeUtil::getTheme($this->themeName);
-            if (null !== $this->themeBundle) {
-                $this->initTheme();
-            }
         }
     }
 
     /**
      * wrap the response in the theme.
-     * use response content as maincontent
      *
      * @param Response $response
      * @return Response|bool (false if theme is not twigBased)
@@ -78,15 +56,11 @@ class Engine
         $this->overrideThemeIfRequired($response);
 
         // @todo remove twigBased check in 2.0
-        if (!$this->themeIsTwigBased) {
+        if (!$this->themeBundle->isTwigBased()) {
             return false;
         }
 
-        // @todo determine proper template? and location
-        // @todo NOTE: 'pagetype' is temporary var in the template
-        $template = $this->themeConfig['master']['page'];
-
-        return $this->templatingService->renderResponse($this->themeName . ':' . $template, array('maincontent' => $response->getContent(), 'pagetype' => 'admin'));
+        return $this->themeBundle->generateThemedResponse($response);
     }
 
     /**
@@ -98,13 +72,11 @@ class Engine
     public function wrapBlockInTheme(array $block)
     {
         // @todo remove twigBased check in 2.0
-        if (!$this->themeIsTwigBased) {
+        if (!$this->themeBundle->isTwigBased()) {
             return false;
         }
 
-        $template = $this->themeConfig['master']['block']['positions'][$block['position']];
-
-        return $this->templatingService->render($this->themeName . ':' . $template, $block); // @todo renderView?
+        return $this->themeBundle->generateThemedBlock($block);
     }
 
     /**
@@ -124,36 +96,6 @@ class Engine
     public function themeIsOverridden()
     {
         return $this->themeIsOverridden;
-    }
-
-    /**
-     * load the theme config.
-     * @todo this could be used to accomplish other tasks. If this is NOT required, then eliminate the method and
-     *       perform loadThemeConfig() directly in the constructor
-     * set themeIsTwigBased (to be removed in 2.0)
-     */
-    private function initTheme()
-    {
-        // @todo refactor at 2.0 to assume all themes are Core-2.0 type bundles (w/o Version class)
-        $versionClass = $this->themeBundle->getVersionClass();
-        if (class_exists($versionClass)) {
-            $this->themeIsTwigBased = false;
-        } else {
-            // theme is Core-2.0 type. init and load config
-            $this->themeIsTwigBased = true;
-            $this->loadThemeConfig();
-        }
-    }
-
-    /**
-     * load the theme configuration from the config/theme.yml file
-     */
-    private function loadThemeConfig()
-    {
-        $configPath = $this->themeBundle->getConfigPath() . '/theme.yml';
-        if (file_exists($configPath)) {
-            $this->themeConfig = Yaml::parse($configPath);
-        }
     }
 
     /**
@@ -181,9 +123,11 @@ class Engine
                 }
             }
         }
+        // check other Response types here...
 
         if ($this->themeIsOverridden) {
-            $this->initTheme();
+            // load new bundle into Engine
+            $this->themeBundle = \ThemeUtil::getTheme($this->themeName);
         }
     }
 
