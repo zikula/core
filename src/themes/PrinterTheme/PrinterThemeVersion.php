c3c8cc7524498201dafb5965d8ba7c3bbbeeb401
Craig Heydenburg <craigh@mac.com>
2015-08-16 20:46:00 -0400

    convert PrinterTheme to psr-4

--- src/themes/PrinterTheme/PrinterThemeVersion.php
+++ src/themes/PrinterTheme/PrinterThemeVersion.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zikula\Theme\PrinterTheme;
+namespace Zikula\PrinterTheme;
 
 class PrinterThemeVersion extends \Zikula_AbstractThemeVersion
 {
--- src/themes/PrinterTheme/ZikulaPrinterTheme.php
+++ src/themes/PrinterTheme/ZikulaPrinterTheme.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zikula\Theme\PrinterTheme;
+namespace Zikula\PrinterTheme;
 
 use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreTheme;
 
