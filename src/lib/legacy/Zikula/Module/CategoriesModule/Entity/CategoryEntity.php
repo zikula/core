3e8c47008f0ccebc1e66503d6bca90e518833639
Craig Heydenburg <craigh@mac.com>
2015-08-19 13:30:37 -0400

    Add two more BC stub classes needed due to refactoring to psr-4

---
+++ src/lib/legacy/Zikula/Module/CategoriesModule/Entity/CategoryEntity.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Zikula\Module\CategoriesModule\Entity;
+
+/**
+ * @deprecated remove at Core-2.0
+ * @see Zikula\CategoriesModule\Entity\CategoryEntity
+ *
+ * This class is necessary because of the refactoring to psr-4
+ * This class maintains the 1.4.x BC API
+
+ * Class CategoryEntity
+ * @package Zikula\Module\CategoriesModule\Entity
+ */
+class CategoryEntity extends \Zikula\CategoriesModule\Entity\CategoryEntity
+{
+}
