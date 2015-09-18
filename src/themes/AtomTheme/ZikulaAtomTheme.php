445c4fd50a142131fca6f20e7c686d618144f5b2
Craig Heydenburg <craigh@mac.com>
2015-09-18 17:13:03 -0400

    refactor AtomTheme to twig/Core-2.0

--- src/themes/AtomTheme/ZikulaAtomTheme.php
+++ src/themes/AtomTheme/ZikulaAtomTheme.php
@@ -2,8 +2,22 @@
 
 namespace Zikula\AtomTheme;
 
+use Symfony\Component\HttpFoundation\Response;
 use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreTheme;
 
 class ZikulaAtomTheme extends AbstractCoreTheme
 {
+    /**
+     * Override parent method in order to add Content-type header to Response
+     * @param string $realm
+     * @param Response $response
+     * @return mixed
+     */
+    public function generateThemedResponse($realm, Response $response)
+    {
+        $newResponse = new Response();
+        $newResponse->headers->add(array("Content-type" => "application/atom+xml"));
+
+        return $this->getContainer()->get('templating')->renderResponse('ZikulaAtomTheme::master.html.twig', array('maincontent' => $response->getContent()), $newResponse);
+    }
 }
