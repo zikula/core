b0408cc6c9166b23700727cb77aa8897fce6640a
Craig Heydenburg <craigh@mac.com>
2015-09-18 17:38:13 -0400

    refactor RssTheme to twig/Core-2.0

---
+++ src/themes/RssTheme/DependencyInjection/ZikulaRssExtension.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2015 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license MIT
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\RssTheme\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Implementation class for service definition loader using the DependencyInjection extension.
+ */
+class ZikulaRssExtension extends Extension
+{
+    /**
+     * Loads service definition file containing persistent event handlers.
+     * Responds to the app.config configuration parameter.
+     *
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.yml');
+    }
+}
