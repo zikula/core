ee7ca7e42c1908f979f2a7240b8ade7be76c00b9
Craig Heydenburg <craigh@mac.com>
2015-09-29 15:19:35 -0400

    add more defaults assets to new method.

--- src/lib/Zikula/Bundle/CoreBundle/EventListener/Theme/DefaultPageAssetSetterListener.php
+++ src/lib/Zikula/Bundle/CoreBundle/EventListener/Theme/DefaultPageAssetSetterListener.php
@@ -17,24 +17,30 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
 use Zikula\Core\Theme\AssetBag;
 
 class DefaultPageAssetSetterListener implements EventSubscriberInterface
 {
     private $cssAssetBag;
     private $jsAssetBag;
+    private $router;
     private $params;
 
-    public function __construct(AssetBag $jsAssetBag, AssetBag $cssAssetBag)
+    public function __construct(AssetBag $jsAssetBag, AssetBag $cssAssetBag, RouterInterface $router)
     {
         $this->jsAssetBag = $jsAssetBag;
         $this->cssAssetBag = $cssAssetBag;
+        $this->router = $router;
     }
 
     public function setParameters(ContainerInterface $container)
     {
         $this->params = [
             'env' => $container->getParameter('env'),
+            'zikula.javascript.bootstrap.min.path' => $container->getParameter('zikula.javascript.bootstrap.min.path'),
+            'zikula.stylesheet.bootstrap-font-awesome.path' => $container->getParameter('zikula.stylesheet.bootstrap-font-awesome.path'),
+            'zikula.stylesheet.fontawesome.min.path' => $container->getParameter('zikula.stylesheet.fontawesome.min.path'),
         ];
     }
 
@@ -48,26 +54,23 @@ class DefaultPageAssetSetterListener implements EventSubscriberInterface
             return;
         }
         $basePath = $event->getRequest()->getBasePath();
-        $jquery = $this->params['env'] != 'dev' ? 'jquery.min.js' : 'jquery.js';
 
         // add default javascripts to jsAssetBag
+        $this->addJquery($basePath);
         $this->jsAssetBag->add(
             [
-                $basePath . "/web/jquery/$jquery" => 0,
-                $basePath . '/web/bootstrap/js/bootstrap.min.js' => 1,
+                $basePath . '/' . $this->params['zikula.javascript.bootstrap.min.path'] => 1,
                 $basePath . '/javascript/helpers/bootstrap-zikula.js' => 2,
                 $basePath . '/web/html5shiv/dist/html5shiv.js' => 3,
 //            $basePath . '/javascript/helpers/Zikula.js', // @todo legacy remove at Core 2.0
-        // the following currently added by JCSSUtil in Engine::filter()
-//            $basePath . '/web/bundles/fosjsrouting/js/router.js',
-//            $basePath . '/web/js/fos_js_routes.js',
             ]
         );
+        $this->addFosJsRouting($basePath);
 
         // add default stylesheets to cssAssetBag
+        $this->addBootstrapCss($basePath);
         $this->cssAssetBag->add(
             [
-                $basePath . '/web/bootstrap-font-awesome.css' => 0,
                 $basePath . '/style/core.css' => 1,
             ]
         );
@@ -80,5 +83,43 @@ class DefaultPageAssetSetterListener implements EventSubscriberInterface
                 array('setDefaultPageAssets', 201),
             ),
         );
+    }
+
+    private function addJquery($basePath)
+    {
+        $jquery = $this->params['env'] != 'dev' ? 'jquery.min.js' : 'jquery.js';
+        $this->jsAssetBag->add([$basePath . "/web/jquery/$jquery" => 0]);
+    }
+
+    private function addFosJsRouting($basePath)
+    {
+        if ($this->params['env'] != 'dev' && file_exists(realpath('web/js/fos_js_routes.js'))) {
+            $this->jsAssetBag->add([
+                $basePath . '/web/bundles/fosjsrouting/js/router.js' => 4,
+                $basePath . '/web/js/fos_js_routes.js' => 5
+            ]);
+        } else {
+            $routeScript = $this->router->generate('fos_js_routing_js', array('callback' => 'fos.Router.setData'));
+            $this->jsAssetBag->add([
+                $basePath . '/web/bundles/fosjsrouting/js/router.js' => 4,
+                $routeScript => 5
+            ]);
+        }
+    }
+
+    private function addBootstrapCss($basePath)
+    {
+        $overrideBootstrapPath = \ThemeUtil::getVar('bootstrapPath', ''); // allows for theme override of bootstrap css path
+        if (empty($overrideBootstrapPath)) {
+            $bootstrapFontAwesomePath = $this->params['zikula.stylesheet.bootstrap-font-awesome.path'];
+            $this->cssAssetBag->add(["$basePath/$bootstrapFontAwesomePath" => 0]);
+        }
+        if (!empty($overrideBootstrapPath)) {
+            $fontAwesomePath = $this->params['zikula.stylesheet.fontawesome.min.path'];
+            $this->cssAssetBag->add([
+                "$basePath/$overrideBootstrapPath" => 0,
+                "$basePath/$fontAwesomePath" => 1,
+            ]);
+        }
     }
 }
