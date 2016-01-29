b7608365e46f0c0669c5785a5b5968145f649562
Craig Heydenburg <craigh@mac.com>
2016-01-29 17:52:48 -0500

    remove unneeded methods from AbstractBlockHandler

--- src/lib/Zikula/Core/AbstractBlockHandler.php
+++ src/lib/Zikula/Core/AbstractBlockHandler.php
@@ -155,29 +155,6 @@ abstract class AbstractBlockHandler implements BlockHandlerInterface, ContainerA
     }
 
     /**
-     * Creates and returns a Form instance from the type of the form.
-     * @param string|FormTypeInterface $type The built type of the form
-     * @param mixed $data The initial data for the form
-     * @param array $options Options for the form
-     * @return Form
-     */
-    public function createForm($type, $data = null, array $options = array())
-    {
-        return $this->container->get('form.factory')->create($type, $data, $options);
-    }
-
-    /**
-     * Creates and returns a form builder instance.
-     * @param mixed $data The initial data for the form
-     * @param array $options Options for the form
-     * @return FormBuilder
-     */
-    public function createFormBuilder($data = null, array $options = array())
-    {
-        return $this->container->get('form.factory')->createBuilder('Symfony\Component\Form\Extension\Core\Type\FormType', $data, $options);
-    }
-
-    /**
      * Convenience shortcut to check if user has requested permissions.
      * @param null $component
      * @param null $instance
