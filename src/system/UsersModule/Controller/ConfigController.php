ef04a49e795fcd02a7079d32c4a5ea5b1d920598
Craig Heydenburg <craigh@mac.com>
2016-04-13 17:52:10 -0400

    being Config form

---
+++ src/system/UsersModule/Controller/ConfigController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2016 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\UsersModule\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Zikula\Core\Controller\AbstractController;
+use Zikula\ThemeModule\Engine\Annotation\Theme;
+use Zikula\UsersModule\Constant as UsersConstant;
+
+/**
+ * @Route("/newadmin")
+ */
+class ConfigController extends AbstractController
+{
+    /**
+     * @Route("/config")
+     * @Theme("admin")
+     * @Template
+     * @param Request $request
+     * @return array
+     */
+    public function configAction(Request $request)
+    {
+        if (!$this->hasPermission('ZikulaUsersModule::', '::', ACCESS_ADMIN)) {
+            throw new AccessDeniedException();
+        }
+
+        $form = $this->createForm('Zikula\UsersModule\Form\Type\ConfigType',
+            $this->getVars(), ['translator' => $this->get('translator.default')]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            if ($form->get('save')->isClicked()) {
+                $data = $form->getData();
+                $this->setVars($data);
+                $this->addFlash('status', $this->__('Done! Configuration updated.'));
+            }
+            if ($form->get('cancel')->isClicked()) {
+                $this->addFlash('status', $this->__('Operation cancelled.'));
+            }
+        }
+
+        return [
+            'form' => $form->createView(),
+            'UC' => new UsersConstant()
+        ];
+    }
+}
