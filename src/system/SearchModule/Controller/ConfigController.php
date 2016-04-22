4bbf7e15b63feccbd99f2ca78ff70609ef0054c7
Guite <info@guite.de>
2016-04-22 15:22:32 +0200

    fixed styleci issues

--- src/system/SearchModule/Controller/ConfigController.php
+++ src/system/SearchModule/Controller/ConfigController.php
@@ -11,13 +11,9 @@
 namespace Zikula\SearchModule\Controller;
 
 use ModUtil;
-use SecurityUtil;
-use Zikula_View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Zikula\Core\Controller\AbstractController;
 use Zikula\SearchModule\AbstractSearchable;
