8b0706354b8f871c340a72591169b11ec3f2181c
Guite <info@guite.de>
2016-04-23 20:03:45 +0200

    minor amendments

--- src/system/PageLockModule/DependencyInjection/ZikulaPageLockExtension.php
+++ src/system/PageLockModule/DependencyInjection/ZikulaPageLockExtension.php
@@ -10,10 +10,10 @@
 
 namespace Zikula\PageLockModule\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Config\FileLocator;
 
 class ZikulaPageLockExtension extends Extension
 {
