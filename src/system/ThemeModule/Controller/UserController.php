10e0e4e9f84d61a41086b153f6a11b8b3665dc70
Guite <info@guite.de>
2016-04-25 12:18:26 +0200

    removed unused import

--- src/system/ThemeModule/Controller/UserController.php
+++ src/system/ThemeModule/Controller/UserController.php
@@ -22,7 +22,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Zikula\Core\Controller\AbstractController;
 use Zikula\ExtensionsModule\Api\VariableApi;
