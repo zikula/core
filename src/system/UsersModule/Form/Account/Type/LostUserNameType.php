9e6b46a67042827aa3730a3b7eef601231559dbf
Craig Heydenburg <craigh@mac.com>
2016-05-11 16:36:45 -0400

    Refactor Lost Username action.

---
+++ src/system/UsersModule/Form/Account/Type/LostUserNameType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\UsersModule\Form\Account\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LostUserNameType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType', [
+                'label' => $options['translator']->__('Email Address'),
+                'input_group' => ['left' => '<i class="fa fa-at"></i>'],
+            ])
+            ->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', [
+                'label' => $options['translator']->__('Submit'),
+                'icon' => 'fa-check',
+                'attr' => ['class' => 'btn btn-success']
+            ])
+        ;
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'zikulausersmodule_account_lostusername';
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'translator' => null,
+        ]);
+    }
+}
