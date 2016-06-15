946148ee5a5a13517392d279a46ffa8efec913f5
Axel Guckelsberger <info@guite.de>
2016-06-15 18:31:26 +0200

    removed unused import

--- src/system/PermissionsModule/Controller/AdminController.php
+++ src/system/PermissionsModule/Controller/AdminController.php
@@ -19,7 +19,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Zikula\Core\Controller\AbstractController;
 use Zikula\ThemeModule\Engine\Annotation\Theme;
