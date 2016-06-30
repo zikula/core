bdd87086f002d12d06734ca67aa125f9c344b8ce
Craig Heydenburg <craigh@mac.com>
2016-06-29 22:02:20 -0400

    Applied fixes from StyleCI

--- src/system/ZAuthModule/Form/Type/LostPasswordType.php
+++ src/system/ZAuthModule/Form/Type/LostPasswordType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Callback;
-use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Zikula\ZAuthModule\Validator\Constraints\ValidPassword;
 
--- src/system/ZAuthModule/Form/Type/VerifyRegistrationType.php
+++ src/system/ZAuthModule/Form/Type/VerifyRegistrationType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\Constraints\Type;
 use Zikula\ZAuthModule\Validator\Constraints\ValidPassword;
 use Zikula\ZAuthModule\Validator\Constraints\ValidPasswordReminder;
