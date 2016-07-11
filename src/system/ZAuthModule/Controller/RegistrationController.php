f837cf544d22657bbc65fbab700b56bd6a8483dd
Craig Heydenburg <craigh@mac.com>
2016-07-11 16:04:44 -0400

    remove old header.

--- src/system/ZAuthModule/Controller/RegistrationController.php
+++ src/system/ZAuthModule/Controller/RegistrationController.php
@@ -9,18 +9,6 @@
  * file that was distributed with this source code.
  */
 
-/**
- * Copyright Zikula Foundation 2016 - Zikula Application Framework
- *
- * This work is contributed to the Zikula Foundation under one or more
- * Contributor Agreements and licensed to You under the following license:
- *
- * @license GNU/LGPLv3 (or at your option, any later version).
- *
- * Please see the NOTICE file distributed with this source code for further
- * information regarding copyright and licensing.
- */
-
 namespace Zikula\ZAuthModule\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
