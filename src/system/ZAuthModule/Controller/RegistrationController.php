85d6b18c9026c2534d96ee2495ad35af11de00c2
Craig Heydenburg <craigh@mac.com>
2016-07-11 16:03:32 -0400

    Applied fixes from StyleCI

--- src/system/ZAuthModule/Controller/RegistrationController.php
+++ src/system/ZAuthModule/Controller/RegistrationController.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * Copyright Zikula Foundation 2016 - Zikula Application Framework
  *
