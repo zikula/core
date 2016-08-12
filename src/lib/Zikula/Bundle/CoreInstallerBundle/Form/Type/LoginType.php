cd04311de78033851ba7e59894b19c3363cf4c77
Craig Heydenburg <craigh@mac.com>
2016-08-11 21:42:57 -0400

    try to create setting for base_url

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Form/Type/LoginType.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Form/Type/LoginType.php
@@ -14,7 +14,6 @@ namespace Zikula\Bundle\CoreInstallerBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Zikula\Bundle\CoreInstallerBundle\Validator\Constraints\AuthenticateAdminLogin;
 
