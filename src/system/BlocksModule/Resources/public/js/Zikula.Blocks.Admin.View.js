97a414e97afaece4202668a745a73ac30bbbcea7
Craig Heydenburg <craigh@mac.com>
2016-08-21 14:58:36 -0400

    fix & improve block preview. fixes #2672 (#3018)

--- src/system/BlocksModule/Resources/public/js/Zikula.Blocks.Admin.View.js
+++ src/system/BlocksModule/Resources/public/js/Zikula.Blocks.Admin.View.js
@@ -2,9 +2,14 @@
 
 ( function($) {
     $(document).ready(function() {
-        $('#zikulablocksmodule-block-view-modal').on('show.bs.modal', function (e) {
-            var link = $(e.relatedTarget);
-            $(this).find('.modal-body').load(link.attr('href'));
-        });
+        $('#zikulablocksmodule-block-view-modal')
+            .on('show.bs.modal', function (e) {
+                var link = $(e.relatedTarget);
+                $(this).find('.modal-body').load(link.attr('href'));
+            })
+            .on('hidden.bs.modal', function () {
+                $(this).find('.modal-body').html('<div class="text-center"><i class="fa fa-spin fa-3x fa-spinner"></i></div>"');
+            })
+        ;
     })
 })(jQuery);
