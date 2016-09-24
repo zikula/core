7003de129a72c729221bbc978e4705d15dad6cd4
Craig Heydenburg <craigh@mac.com>
2016-09-24 17:47:54 -0400

    typo

--- src/modules/MenuModule/Form/Type/HiddenMenuItemType.php
+++ src/modules/MenuModule/Form/Type/HiddenMenuItemType.php
@@ -56,7 +56,7 @@ class HiddenMenuItemType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'invalid_message' => $this->translator-->__('The selected item does not exist'),
+            'invalid_message' => $this->translator->__('The selected item does not exist'),
         ]);
     }
 
