8a45954727473278ff93c1ddc74d9b05bbb247fc
Craig Heydenburg <craigh@mac.com>
2016-10-11 17:46:24 -0400

    remove deprecated use of Zikula_View in DisplayHook

--- src/lib/Zikula/Bundle/HookBundle/Hook/DisplayHookResponse.php
+++ src/lib/Zikula/Bundle/HookBundle/Hook/DisplayHookResponse.php
@@ -11,8 +11,6 @@
 
 namespace Zikula\Bundle\HookBundle\Hook;
 
-use Zikula_View;
-
 /**
  * Zikula display hook response class.
  *
@@ -34,17 +32,10 @@ class DisplayHookResponse
      * Constructor.
      *
      * @param string $area Name of this response
-     * @param string|Zikula_View $content Response content (or Zikula View instance @deprecated)
-     * @param string $template Template, in the context of the Zikula_View. @deprecated argument
-     *   The third argument in the method will be removed in Core-2.0
+     * @param string $content Response content
      */
-    public function __construct($area, $content, $template = null)
+    public function __construct($area, $content)
     {
-        if (is_object($content) && ($content instanceof Zikula_View) && !empty($template)) {
-            // This is a BC layer to allow old construction methods to work.
-            // remove this if condition in Core-2.0
-            $content = $content->fetch($template);
-        }
         $this->content = $content;
         $this->area = $area;
     }
