b8f7e641c805a484fa28268537a8e01f99a9bd28
Craig Heydenburg <craigh@mac.com>
2016-10-11 17:49:32 -0400

    remove deprecated methods from ExtensionsApi

--- src/system/ExtensionsModule/Api/ExtensionApi.php
+++ src/system/ExtensionsModule/Api/ExtensionApi.php
@@ -11,8 +11,6 @@
 
 namespace Zikula\ExtensionsModule\Api;
 
-use Symfony\Component\HttpKernel\KernelInterface;
-
 class ExtensionApi
 {
     const STATE_UNINITIALISED = 1;
@@ -23,48 +21,4 @@ class ExtensionApi
     const STATE_NOTALLOWED = 6;
     const STATE_INVALID = -1;
     const INCOMPATIBLE_CORE_SHIFT = 20;
-
-    /**
-     * @var KernelInterface
-     */
-    private $kernel;
-
-    /**
-     * ExtensionVar constructor.
-     * @param KernelInterface $kernel
-     */
-    public function __construct(KernelInterface $kernel)
-    {
-        $this->kernel = $kernel;
-    }
-
-    /**
-     * Check if the module is loaded into the kernel or not. Returns null if not.
-     * @param $name
-     * @deprecated remove at Core-2.0 replace with $kernel->getModule($name) when all modules are Bundles
-     * @return null|\Zikula\Core\AbstractBundle
-     */
-    public function getModuleInstanceOrNull($name)
-    {
-        $moduleInstance = null;
-        try {
-            $moduleInstance = $this->kernel->getModule($name);
-        } catch (\Exception $e) {
-            // silent fail
-            // @todo remove this try/catch block at Core-2.0
-        }
-
-        return $moduleInstance;
-    }
-
-    /**
-     * Checks if name is is the list of core modules.
-     * @deprecated remove at Core-2.0 use Kernel method directly
-     * @param $moduleName
-     * @return bool
-     */
-    public function isCoreModule($moduleName)
-    {
-        return \ZikulaKernel::isCoreModule($moduleName);
-    }
 }
