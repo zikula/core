78d687d663ee1e6f9b8c4811eb9e5a21f64eb059
Craig Heydenburg <craigh@mac.com>
2016-10-11 17:56:44 -0400

    eliminate use of deprecated event on link collection

--- src/lib/Zikula/Bundle/HookBundle/Listener/HooksListener.php
+++ src/lib/Zikula/Bundle/HookBundle/Listener/HooksListener.php
@@ -43,41 +43,6 @@ class HooksListener implements EventSubscriberInterface
     {
         return [
             'zikula.link_collector' => 'linkCollectorResponder',
-            'module_dispatch.postexecute' => 'addHooksLink' // deprecated event
-        ];
-    }
-
-    /**
-     * Dynamically add Hooks link to administration.
-     *
-     * Listens for 'module_dispatch.postexecute' events.
-     * @deprecated remove at Core-2.0
-     * move logic to linkCollectorResponder
-     *
-     * @param \Zikula_Event $event The event handler
-     * @return void
-     */
-    public function addHooksLink(\Zikula_Event $event)
-    {
-        // check if this is for this handler
-        if (!($event['modfunc'][1] == 'getLinks' && $event['type'] == 'admin' && $event['api'] == true)) {
-            return;
-        }
-
-        if (!$this->permissionsApi->hasPermission($event['modname'] . '::Hooks', '::', ACCESS_ADMIN)) {
-            return;
-        }
-
-        // return if module is not subscriber or provider capable
-        if (!$this->capabilityApi->isCapable($event['modname'], CapabilityApiInterface::HOOK_SUBSCRIBER)
-            && !$this->capabilityApi->isCapable($event['modname'], CapabilityApiInterface::HOOK_PROVIDER)) {
-            return;
-        }
-
-        $event->data[] = [
-            'url' => $this->router->generate('zikula_hook_hook_edit', ['moduleName' => $event['modname']]),
-            'text' => __('Hooks'),
-            'icon' => 'paperclip'
         ];
     }
 
@@ -93,6 +58,18 @@ class HooksListener implements EventSubscriberInterface
         $event->setArgument('modname', $event->getSubject());
         $event->setArgument('modfunc', [1 => 'getLinks']);
         $event->setArgument('api', true);
-        $this->addHooksLink($event);
+        if (!$this->permissionsApi->hasPermission($event['modname'] . '::Hooks', '::', ACCESS_ADMIN)) {
+            return;
+        }
+        // return if module is not subscriber or provider capable
+        if (!$this->capabilityApi->isCapable($event['modname'], CapabilityApiInterface::HOOK_SUBSCRIBER)
+            && !$this->capabilityApi->isCapable($event['modname'], CapabilityApiInterface::HOOK_PROVIDER)) {
+            return;
+        }
+        $event->data[] = [
+            'url' => $this->router->generate('zikula_hook_hook_edit', ['moduleName' => $event['modname']]),
+            'text' => __('Hooks'),
+            'icon' => 'paperclip'
+        ];
     }
 }
