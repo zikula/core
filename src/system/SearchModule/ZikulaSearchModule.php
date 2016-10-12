72b0d5599a285226f587eadfe02278835ad475ec
Craig Heydenburg <craigh@mac.com>
2016-10-12 18:18:14 -0400

    remove legacy namespace/module class 'aliases'

--- src/system/SearchModule/ZikulaSearchModule.php
+++ src/system/SearchModule/ZikulaSearchModule.php
@@ -9,51 +9,13 @@
  * file that was distributed with this source code.
  */
 
+namespace Zikula\SearchModule;
+
+use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreModule;
+
 /**
  * Base module definition for the search module.
  */
-
-namespace Zikula\SearchModule {
-
-    use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreModule;
-
-    /**
-     * Base module definition for the search module.
-     */
-    class ZikulaSearchModule extends AbstractCoreModule
-    {
-    }
-}
-
-/**
- * Classes for modules prior to 1.4.0
- */
-
-namespace {
-    /**
-     * user api functions for the search module.
-     *
-     * @deprecated since 1.4.0 use \Zikula\SearchModule\Api\UserApi instead
-     */
-    class Search_Api_User extends \Zikula\SearchModule\Api\UserApi
-    {
-    }
-}
-
-namespace Zikula\Module\SearchModule {
-
-    use Zikula\SearchModule\AbstractSearchable as AbstractSearchableActual;
-
-    /**
-     * @deprecated remove at Core-2.0
-     * @see Zikula\SearchModule\AbstractSearchable
-     *
-     * This class is necessary because of the refactoring of the SearchModule to psr-4
-     * This class maintains the 1.4.x BC API
-     *
-     * Class AbstractSearchable
-     */
-    abstract class AbstractSearchable extends AbstractSearchableActual
-    {
-    }
+class ZikulaSearchModule extends AbstractCoreModule
+{
 }
