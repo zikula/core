d3505bf252b586d05562302c37d8b172fe76a5c3
Craig Heydenburg <craigh@mac.com>
2016-10-31 17:42:28 -0400

    add KnpMenu-based solution for admin menu in Bootstrap theme as example.

--- src/docs/Core-2.0/Menu/NavBarMenu.md
+++ src/docs/Core-2.0/Menu/NavBarMenu.md
@@ -22,14 +22,14 @@ Drop Menu
 
 This menu is slightly less intuitive. You must create a structure like so:
 
-Menu Root ("MyDropMenu")
-   |
-   |
-   ––– Displayed "Parent" (first child) ("Click Here")
-           |
-           -- Child 1 (link)
-           -- Child 2 (link)
-           -- etc...
+    Menu Root ("MyDropMenu")
+       |
+       |
+       ––– Displayed "Parent" (first child) ("Click Here")
+               |
+               -- Child 1 (link)
+               -- Child 2 (link)
+               -- etc...
 
 As above for the standard NavBar, you must set the `childrenAttributes` as 
 `{"class":"nav navbar-nav"}` on the Menu root.
---
+++ src/themes/BootstrapTheme/Menu/AdminMenu.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\BootstrapTheme\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Zikula\Common\Translator\TranslatorTrait;
+
+class AdminMenu implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+    use TranslatorTrait;
+
+    public function adminMenu(FactoryInterface $factory, array $options)
+    {
+        $this->setTranslator($this->container->get('translator.default'));
+        $permApi = $this->container->get('zikula_permissions_module.api.permission');
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild($this->__('Home'), ['route' => 'home']);
+        if ($permApi->hasPermission('ZikulaSettingsModule::', '::', ACCESS_ADMIN)) {
+            $menu->addChild($this->__('Settings'), ['route' => 'zikulasettingsmodule_settings_main']);
+        }
+        if ($permApi->hasPermission('ZikulaExtensionsModule::', '::', ACCESS_ADMIN)) {
+            $menu->addChild($this->__('Extensions'), ['route' => 'zikulasettingsmodule_settings_main']);
+        }
+        if ($permApi->hasPermission('ZikulaBlocksModule::', '::', ACCESS_EDIT)) {
+            $menu->addChild($this->__('Blocks'), ['route' => 'zikulablocksmodule_admin_view']);
+        }
+        if ($permApi->hasPermission('ZikulaUsersModule::', '::', ACCESS_MODERATE)) {
+            $menu->addChild($this->__('Users'), ['route' => 'zikulausersmodule_useradministration_list']);
+        }
+        if ($permApi->hasPermission('ZikulaGroupsModule::', '::', ACCESS_EDIT)) {
+            $menu->addChild($this->__('Groups'), ['route' => 'zikulagroupsmodule_admin_index']);
+        }
+        if ($permApi->hasPermission('ZikulaPermissionsModule::', '::', ACCESS_ADMIN)) {
+            $menu->addChild($this->__('Permissions'), ['route' => 'zikulapermissionsmodule_admin_index']);
+        }
+        if ($permApi->hasPermission('ZikulaThemeModule::', '::', ACCESS_EDIT)) {
+            $menu->addChild($this->__('Themes'), ['route' => 'zikulathememodule_theme_view']);
+        }
+
+        return $menu;
+    }
+
+    public function setTranslator($translator)
+    {
+        $this->translator = $translator;
+    }
+}
