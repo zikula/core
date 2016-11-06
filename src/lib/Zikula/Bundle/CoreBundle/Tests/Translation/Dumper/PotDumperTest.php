0aaad0a286aeae0ac0806b09ce022caf99400216
Craig Heydenburg <craigh@mac.com>
2016-11-06 20:13:15 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/lib/Zikula/Bundle/CoreBundle/Tests/Translation/Dumper/PotDumperTest.php
+++ src/lib/Zikula/Bundle/CoreBundle/Tests/Translation/Dumper/PotDumperTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Zikula\Bundle\CoreBundle\Tests\Translation\Dumper;
 
 use JMS\TranslationBundle\Exception\InvalidArgumentException;
