3967b519381d5d4d7cb680d78a4654bb4f7ff3ae
Guite <info@guite.de>
2016-11-11 18:07:41 +0100

    centralise bs modal fix for mmenu, fixed #3098

--- src/lib/legacy/viewplugins/function.adminpanelmenu.php
+++ src/lib/legacy/viewplugins/function.adminpanelmenu.php
@@ -33,11 +33,13 @@ function smarty_function_adminpanelmenu($params, Zikula_View $view)
         return ''; // Since no permission, return empty
     }
 
-    // add required scritps and stylesheets to page
+    // add required scripts and stylesheets to page
     PageUtil::addVar('javascript', '@ZikulaAdminModule/Resources/public/js/jQuery.mmenu-5.6.3/dist/js/jquery.mmenu.all.min.js');
     PageUtil::addVar('stylesheet', '@ZikulaAdminModule/Resources/public/js/jQuery.mmenu-5.6.3/dist/css/jquery.mmenu.all.css');
     // add override for panel width created from .scss file
     PageUtil::addVar('stylesheet', '@ZikulaAdminModule/Resources/public/css/mmenu-hiddenpanel-customwidth.css');
+    // fix for collision between mmenu and bootstrap modals
+    PageUtil::addVar('javascript', '@ZikulaAdminModule/Resources/public/js/ZikulaAdminModule.Modal.Fix.js');
 
     $router = $view->getContainer()->get('router');
     $modules = ModUtil::getModulesCapableOf('admin');
@@ -54,65 +56,68 @@ function smarty_function_adminpanelmenu($params, Zikula_View $view)
     $htmlContent .= '<h1><img src="images/logo.gif" alt="Logo" style="height: 32px"> ' . __('Administration') . '</h1>';
     $htmlContent .= '<ul>';
     foreach ($modules as $module) {
-        if (SecurityUtil::checkPermission("$module[name]::", '::', ACCESS_EDIT)) {
-            // first-level list - list modules with general 'index' link
-            $img = ModUtil::getModuleImagePath($module['name']);
-            $url = isset($module['capabilities']['admin']['url'])
-                ? $module['capabilities']['admin']['url']
-                : $router->generate($module['capabilities']['admin']['route']);
-            $moduleSelected = empty($moduleSelected) && strpos($view->getRequest()->getUri(), $module['url']) ? " class='Selected'" : "";
-            $htmlContent .= "<li{$moduleSelected}><a href=\"" . DataUtil::formatForDisplay($url) . "\"><img src=\"$img\" alt=\"\" style=\"height: 18px\" /> " . $module['displayname'] . "</a>";
-
-            $links = $view->getContainer()->get('zikula.link_container_collector')->getLinks($module['name'], 'admin');
-            if (empty($links)) {
-                $links = (array)ModUtil::apiFunc($module['name'], 'admin', 'getLinks');
-            }
-
-            if ((count($links) > 0) && ($links[0] != false)) {
-                // create second-level list from module adminLinks
-                $htmlContent .= '<ul class="text-left">';
-                foreach ($links as $link) {
-                    if (isset($link['icon'])) {
-                        $img = '<i class="fa fa-' . $link['icon'] . '"></i>';
-                    } elseif (isset($link['class'])) {
-                        $img = '<span class="' . $link['class'] . '"></span>';
-                    } else {
-                        $img = '';
-                    }
-                    $linkSelected = empty($linkSelected) && strpos($view->getRequest()->getUri(), $link['url']) ? " class='Selected'" : "";
-                    $htmlContent .= "<li{$linkSelected}><a href=\"" . DataUtil::formatForDisplay($link['url']) . "\">$img " . $link['text'] . '</a>';
-                    // create third-level list from adminLinks subLinks
-                    if (isset($link['links']) && count($link['links']) > 0) {
-                        $htmlContent .= '<ul class="text-left">';
-                        foreach ($link['links'] as $sublink) {
-                            $htmlContent .= '<li><a href="' . DataUtil::formatForDisplay($sublink['url']) . '">' . $sublink['text'] . '</a></li>';
-                        }
-                        $htmlContent .= '</ul>';
-                    }
-                    $htmlContent .= '</li>';
-                }
-                $htmlContent .= '</ul>';
-            }
-            $htmlContent .= '</li>';
+        if (!SecurityUtil::checkPermission("$module[name]::", '::', ACCESS_EDIT)) {
+            continue;
         }
+
+        // first-level list - list modules with general 'index' link
+        $img = ModUtil::getModuleImagePath($module['name']);
+        $url = isset($module['capabilities']['admin']['url'])
+            ? $module['capabilities']['admin']['url']
+            : $router->generate($module['capabilities']['admin']['route']);
+        $moduleSelected = empty($moduleSelected) && strpos($view->getRequest()->getUri(), $module['url']) ? " class='Selected'" : "";
+        $htmlContent .= "<li{$moduleSelected}><a href=\"" . DataUtil::formatForDisplay($url) . "\"><img src=\"$img\" alt=\"\" style=\"height: 18px\" /> " . $module['displayname'] . "</a>";
+
+        $links = $view->getContainer()->get('zikula.link_container_collector')->getLinks($module['name'], 'admin');
+        if (empty($links)) {
+            $links = (array)ModUtil::apiFunc($module['name'], 'admin', 'getLinks');
+        }
+
+        if (count($links) > 0 && $links[0] != false) {
+            // create second-level list from module adminLinks
+            $htmlContent .= '<ul class="text-left">';
+            foreach ($links as $link) {
+                if (isset($link['icon'])) {
+                    $img = '<i class="fa fa-' . $link['icon'] . '"></i>';
+                } elseif (isset($link['class'])) {
+                    $img = '<span class="' . $link['class'] . '"></span>';
+                } else {
+                    $img = '';
+                }
+                $linkSelected = empty($linkSelected) && strpos($view->getRequest()->getUri(), $link['url']) ? " class='Selected'" : "";
+                $htmlContent .= "<li{$linkSelected}><a href=\"" . DataUtil::formatForDisplay($link['url']) . "\">$img " . $link['text'] . '</a>';
+                // create third-level list from adminLinks subLinks
+                if (isset($link['links']) && count($link['links']) > 0) {
+                    $htmlContent .= '<ul class="text-left">';
+                    foreach ($link['links'] as $sublink) {
+                        $htmlContent .= '<li><a href="' . DataUtil::formatForDisplay($sublink['url']) . '">' . $sublink['text'] . '</a></li>';
+                    }
+                    $htmlContent .= '</ul>';
+                }
+                $htmlContent .= '</li>';
+            }
+            $htmlContent .= '</ul>';
+        }
+        $htmlContent .= '</li>';
     }
     $htmlContent .= '</ul>';
     $htmlContent .= '</div>';
     $htmlContent .= '</nav>';
     $htmlContent .= '
-            <script type="text/javascript">
-                jQuery(document).ready(function( $ ){
-                    $("#zikula-admin-hiddenpanel-menu").mmenu({
-                        extensions: ["hiddenpanel-customwidth"],
-                        "header": {
-                           "title": "' . __('Zikula Administration') . '",
-                           "add": true,
-                           "update": true
-                        },
-                        "searchfield": true
-                    });
+        <script type="text/javascript">
+            jQuery(document).ready(function( $ ) {
+                $("#zikula-admin-hiddenpanel-menu").mmenu({
+                    extensions: ["hiddenpanel-customwidth"],
+                    "header": {
+                        "title": "' . __('Zikula Administration') . '",
+                        "add": true,
+                        "update": true
+                    },
+                    "searchfield": true
                 });
-            </script>';
+            });
+        </script>
+    ';
 
     // the the html content before </body>
     PageUtil::addVar('footer', $htmlContent);
