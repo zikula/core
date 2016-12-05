538fac2542ba28d6682a004841c607da89ac2c87
Guite <info@guite.de>
2016-12-05 18:07:16 +0100

    converted services from xml to yaml

--- src/system/MailerModule/DependencyInjection/ZikulaMailerExtension.php
+++ src/system/MailerModule/DependencyInjection/ZikulaMailerExtension.php
@@ -13,7 +13,7 @@ namespace Zikula\MailerModule\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ZikulaMailerExtension extends Extension
@@ -26,8 +26,8 @@ class ZikulaMailerExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
+        $loader = new YamlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
 
-        $loader->load('services.xml');
+        $loader->load('services.yml');
     }
 }
--- src/system/ThemeModule/DependencyInjection/ZikulaThemeExtension.php
+++ src/system/ThemeModule/DependencyInjection/ZikulaThemeExtension.php
@@ -13,7 +13,7 @@ namespace Zikula\ThemeModule\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class ZikulaThemeExtension extends Extension
@@ -26,12 +26,12 @@ class ZikulaThemeExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
+        $loader = new YamlFileLoader($container, new FileLocator(realpath(__DIR__.'/../Resources/config')));
 
-        $loader->load('services.xml');
-        $loader->load('theme_engine.xml');
-        $loader->load('theme_engine_event_listeners.xml');
-        $loader->load('deprecated_aliases.xml');
-        $loader->load('legacy.xml');
+        $loader->load('services.yml');
+        $loader->load('theme_engine.yml');
+        $loader->load('theme_engine_event_listeners.yml');
+        $loader->load('deprecated_aliases.yml');
+        $loader->load('legacy.yml');
     }
 }
