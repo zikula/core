5f70eb9fa2d9c1b1e1f42308c5b3d5d27f302f68
Guite <info@guite.de>
2016-12-12 07:56:02 +0100

    more robust behaviour for modules without a proper category assignment

--- src/system/AdminModule/Controller/AdminInterfaceController.php
+++ src/system/AdminModule/Controller/AdminInterfaceController.php
@@ -246,6 +246,7 @@ class AdminInterfaceController extends AbstractController
 
         $masterRequest = $this->get('request_stack')->getMasterRequest();
         $currentRequest = $this->get('request_stack')->getCurrentRequest();
+
         // get caller info
         $caller = [];
         $caller['_zkModule'] = $masterRequest->attributes->get('_zkModule');
@@ -253,15 +254,22 @@ class AdminInterfaceController extends AbstractController
         $caller['_zkFunc'] = $masterRequest->attributes->get('_zkFunc');
         $caller['path'] = $masterRequest->getPathInfo();
         $caller['info'] = !empty($caller['_zkModule']) ? $this->get('zikula_extensions_module.extension_repository')->get($caller['_zkModule']) : [];
+
         // category we are in
         $requestedCid = $masterRequest->attributes->get('acid');
+        $defaultCid = empty($requestedCid) ? $this->getVar('startcategory') : $requestedCid;
         if ($caller['_zkModule'] == 'ZikulaAdminModule' || empty($caller['_zkModule'])) {
-            $cid = empty($requestedCid) ? $this->getVar('startcategory') : $requestedCid;
+            $cid = $defaultCid;
         } else {
             $moduleRelation = $this->get('doctrine')->getRepository('ZikulaAdminModule:AdminModuleEntity')->findOneBy(['mid' => $caller['info']['id']]);
-            $cid = $moduleRelation->getCid();
+            if (null !== $moduleRelation) {
+                $cid = $moduleRelation->getCid();
+            } else {
+                $cid = $defaultCid;
+            }
         }
         $caller['category'] = $this->get('doctrine')->getRepository('ZikulaAdminModule:AdminCategoryEntity')->find($cid);
+
         // mode requested
         $mode = $currentRequest->attributes->has('mode') ? $currentRequest->attributes->get('mode') : 'categories';
         $mode = in_array($mode, ['categories', 'modules']) ? $mode : 'categories';
