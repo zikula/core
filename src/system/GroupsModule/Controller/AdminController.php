03bedec0a7612033df7e5ea258a588e02b22ecfc
Craig Heydenburg <craigh@mac.com>
2016-12-18 17:12:15 -0500

    ci

--- src/system/GroupsModule/Controller/AdminController.php
+++ src/system/GroupsModule/Controller/AdminController.php
@@ -16,7 +16,6 @@ use UserUtil;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
