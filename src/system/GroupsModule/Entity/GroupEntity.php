ec4abf723c651ed2ece50b2cb2b9b49f2b5b7fbf
craigh <craigh@mac.com>
2016-12-24 17:10:45 -0500

    deprecate certain groupentity properties as unused. refs #1530

--- src/system/GroupsModule/Entity/GroupEntity.php
+++ src/system/GroupsModule/Entity/GroupEntity.php
@@ -56,6 +56,7 @@ class GroupEntity extends EntityAccess
 
     /**
      * group prefix
+     * @deprecated unused
      * @ORM\Column(type="string", length=25)
      */
     private $prefix;
@@ -69,7 +70,7 @@ class GroupEntity extends EntityAccess
 
     /**
      * number of members of the group
-     *
+     * @deprecated unused
      * @ORM\Column(type="integer")
      */
     private $nbuser;
@@ -83,14 +84,14 @@ class GroupEntity extends EntityAccess
 
     /**
      * link for the group
-     *
+     * @deprecated unused
      * @ORM\Column(type="integer")
      */
     private $link;
 
     /**
      * master uid for the group
-     *
+     * @deprecated unused
      * @ORM\Column(type="integer")
      */
     private $uidmaster;
