4343a671ea397b43c6a0c8eaa82ee147adac146e
craigh <craigh@mac.com>
2016-12-24 16:13:57 -0500

    ci

--- src/system/GroupsModule/Form/Type/ManageApplicationType.php
+++ src/system/GroupsModule/Form/Type/ManageApplicationType.php
@@ -15,7 +15,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
--- src/system/GroupsModule/Form/Type/MembershipApplicationType.php
+++ src/system/GroupsModule/Form/Type/MembershipApplicationType.php
@@ -14,7 +14,6 @@ namespace Zikula\GroupsModule\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
