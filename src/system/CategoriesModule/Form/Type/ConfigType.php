520caad0ab9a69cb2a4542f762e786d78e560dcb
Craig Heydenburg <craigh@mac.com>
2016-12-28 12:55:31 -0500

    ci

--- src/system/CategoriesModule/Form/Type/ConfigType.php
+++ src/system/CategoriesModule/Form/Type/ConfigType.php
@@ -13,7 +13,6 @@ namespace Zikula\CategoriesModule\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
