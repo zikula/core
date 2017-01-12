zikula/core

commit 8f4ef5e8921cf284f3a0cf1761deb9aa12b2de13

remove use of System class in AdminModule

src/system/AdminModule/Controller/AdminController.php
@@ -236,13 +236,11 @@ class AdminController extends AbstractController
      *
      * Display main admin panel for a category
      *
+     * @param Request $request
      * @param integer $acid
-     *
      * @return Response symfony response object
-     *
-     * @throws AccessDeniedException Thrown if the user doesn't have edit permissions to the module
      */
-    public function adminpanelAction($acid = null)
+    public function adminpanelAction(Request $request, $acid = null)
     {
         if (!$this->hasPermission('::', '::', ACCESS_EDIT)) {
             // suppress admin display - return to index.
@@ -311,7 +309,7 @@ class AdminController extends AbstractController
         // get admin capable modules
         $adminModules = $this->get('zikula_extensions_module.api.capability')->getExtensionsCapableOf('admin');
         $adminLinks = [];
-        $baseUrl = System::getBaseUrl();
+        $baseUrl = $request->getBaseUrl();
         foreach ($adminModules as $adminModule) {
             if (!$this->hasPermission($adminModule['name'] . '::', 'ANY', ACCESS_EDIT)) {
                 continue;
@@ -380,11 +378,11 @@ class AdminController extends AbstractController
      *
      * Main category menu.
      *
+     * @param Request $request
      * @param integer $acid
-     *
      * @return Response symfony response object
      */
-    public function categorymenuAction($acid = null)
+    public function categorymenuAction(Request $request, $acid = null)
     {
         $acid = empty($acid) ? $this->getVar('startcategory') : $acid;
 
@@ -406,7 +404,7 @@ class AdminController extends AbstractController
         // get admin capable modules
         $adminModules = $this->get('zikula_extensions_module.api.capability')->getExtensionsCapableOf('admin');
         $adminLinks = [];
-        $baseUrl = System::getBaseUrl();
+        $baseUrl = $request->getBaseUrl();
         foreach ($adminModules as $adminModule) {
             if (!$this->hasPermission($adminModule['name'] . '::', '::', ACCESS_EDIT)) {
                 continue;

src/system/AdminModule/Controller/AdminInterfaceController.php
@@ -14,7 +14,7 @@ namespace Zikula\AdminModule\Controller;
 use DataUtil;
 use ModUtil;
 use StringUtil;
-use System;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -162,9 +162,10 @@ class AdminInterfaceController extends AbstractController
      *
      * Add security analyzer
      *
+     * @param Request $request
      * @return Response symfony response object
      */
-    public function securityanalyzerAction()
+    public function securityanalyzerAction(Request $request)
     {
         if (!$this->hasPermission('ZikulaAdminModule::', '::', ACCESS_ADMIN)) {
             throw new AccessDeniedException();
@@ -175,7 +176,7 @@ class AdminInterfaceController extends AbstractController
         $appDir = $this->get('kernel')->getRootDir();
         if ($appDir) {
             // check if we have an absolute path which is possibly not within the document root
-            $docRoot = System::serverGetVar('DOCUMENT_ROOT');
+            $docRoot = $request->server->get('DOCUMENT_ROOT');
             if (StringUtil::left($appDir, 1) == '/' && false === strpos($appDir, $docRoot)) {
                 // temp dir is outside the webroot, no .htaccess file needed
                 $app_htaccess = true;

commit 408c8adbece1d5f8021a3c6d0c30049dd29cc612

correct issue with admin panel display caused by 8f4ef5e8921cf284f3a0cf1761deb9aa12b2de13

src/system/AdminModule/Controller/AdminController.php
@@ -268,7 +268,7 @@ class AdminController extends AbstractController
 
         $templateParameters = [
             // Add category menu to output
-            'menu' => $this->categorymenuAction($acid)->getContent()
+            'menu' => $this->categorymenuAction($request, $acid)->getContent()
         ];
 
         // Check to see if we have access to the requested category.