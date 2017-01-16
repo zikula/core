64d26f695d92da226142a3bc54187525e53b6b78
gfr <gabriel@freinbichler.net>
2017-01-16 22:29:06 +0100

    Add translation domain detection to ZikulaJsFileExtractor

--- src/lib/Zikula/Bundle/CoreBundle/Translation/ZikulaJsFileExtractor.php
+++ src/lib/Zikula/Bundle/CoreBundle/Translation/ZikulaJsFileExtractor.php
@@ -15,10 +15,12 @@ use JMS\TranslationBundle\Model\FileSource;
 use JMS\TranslationBundle\Model\Message;
 use JMS\TranslationBundle\Model\MessageCatalogue;
 use JMS\TranslationBundle\Translation\Extractor\FileVisitorInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Zikula\Bundle\CoreBundle\Bundle\Scanner;
+
 
 class ZikulaJsFileExtractor implements FileVisitorInterface
 {
-    const JAVASCRIPT_DOMAIN = 'zikula_javascript'; // @todo figure out way to compute the bundle's translation domain?
     const SINGULAR_CAPTURE_REGEX = '\s?[\'"]([^"\'),]+)[\'"]\s?';
     const PLURAL_CAPTURE_REGEX = '\s?[\'"]([^"\'),]+)[\'"]\s?,\s?[\'"]([^"\'),]+)[\'"]';
     const REGEX_DELIMITER = '/';
@@ -33,13 +35,64 @@ class ZikulaJsFileExtractor implements FileVisitorInterface
         '_n',
         '_fn'
     ];
+    
+    /**
+     * @var array
+     */
+    private $bundles;
+    
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+    
+    /**
+     * @var array cache of domain names by composerPath
+     */
+    private static $domainCache;
+    
+    /**
+     * ZikulaJsFileExtractor constructor.
+     * @param KernelInterface $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+        $bundles = $kernel->getBundles();
+        foreach ($bundles as $bundle) {
+            $this->bundles[$bundle->getNamespace()] = $bundle->getName();
+        }
+    }
 
     public function visitFile(\SplFileInfo $file, MessageCatalogue $catalogue)
     {
         if ('.js' !== substr($file, -3)) {
             return;
         }
-
+        
+        $composerPath = str_replace($file->getRelativePathname(), '', $file->getPathname());
+        if (isset(self::$domainCache[$composerPath])) {
+            $domain = self::$domainCache[$composerPath];
+        } else {
+            $scanner = new Scanner();
+            $scanner->scan([$composerPath], 1);
+            $metaData = $scanner->getModulesMetaData(true);
+            $domains = array_keys($metaData);
+            if (isset($domains[0])) {
+                if ($this->kernel->isBundle($domains[0])) {
+                    $bundle = $this->kernel->getBundle($domains[0]);
+                    $domain = ($bundle instanceof AbstractBundle) ? $bundle->getTranslationDomain() : strtolower($bundle->getName());
+                } else {
+                    $domain = strtolower($domains[0]);
+                }
+                $domain = $domain . "_javascript";
+                // cache result of file lookup
+                self::$domainCache[$composerPath] = $domain;
+            } else {
+                $domain = "zikula_javascript";
+            }
+        }
+        
         // singular type
         $argumentsRegex = self::REGEX_DELIMITER
             .'\.(?:' . implode('|', $this->singularFunctions) . ')\('
@@ -47,7 +100,7 @@ class ZikulaJsFileExtractor implements FileVisitorInterface
             .self::REGEX_DELIMITER;
         preg_match_all($argumentsRegex, file_get_contents($file), $matches);
         foreach ($matches[1] as $string) {
-            $message = new Message($string, self::JAVASCRIPT_DOMAIN);
+            $message = new Message($string, $domain);
             $message->addSource(new FileSource((string) $file));
             $catalogue->add($message);
         }
@@ -59,7 +112,7 @@ class ZikulaJsFileExtractor implements FileVisitorInterface
         preg_match_all($argumentsRegex, file_get_contents($file), $matches);
         foreach ($matches[1] as $key => $singluar) {
             $fullString = $singluar . '|' . $matches[2][$key];
-            $message = new Message($fullString, self::JAVASCRIPT_DOMAIN);
+            $message = new Message($fullString, $domain);
             $message->addSource(new FileSource((string) $file));
             $catalogue->add($message);
         }
@@ -73,3 +126,4 @@ class ZikulaJsFileExtractor implements FileVisitorInterface
     {
     }
 }
+
