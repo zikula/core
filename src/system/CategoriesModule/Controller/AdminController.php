ec99b1c91ce1232484cdb00c888d851b059ab779
Craig Heydenburg <craigh@mac.com>
2017-02-25 07:14:38 -0500

    ci

--- src/system/CategoriesModule/Controller/AdminController.php
+++ src/system/CategoriesModule/Controller/AdminController.php
@@ -14,20 +14,18 @@ namespace Zikula\CategoriesModule\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Zikula\CategoriesModule\Entity\CategoryEntity;
 use Zikula\Core\Controller\AbstractController;
 
 /**
  * @Route("/admin")
  * @deprecated
  *
- * Administrative controllers for the categories module.
+ * Administrative controllers for the categories module
  */
 class AdminController extends AbstractController
 {
