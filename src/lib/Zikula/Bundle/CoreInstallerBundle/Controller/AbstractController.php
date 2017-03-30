ae36270f0470497fdcbaf6611be5a05d1d17ba3f
Craig Heydenburg <craigh@mac.com>
2017-03-30 00:50:27 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/AbstractController.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/AbstractController.php
@@ -11,7 +11,6 @@
 
 namespace Zikula\Bundle\CoreInstallerBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\FormFactory;
