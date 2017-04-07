1107dd3a0fc8ed88b7e44db725e054d71f984163
Craig Heydenburg <craigh@mac.com>
2017-04-07 17:38:48 -0400

    check object before using

--- src/system/ZAuthModule/AuthenticationMethod/AbstractNativeAuthenticationMethod.php
+++ src/system/ZAuthModule/AuthenticationMethod/AbstractNativeAuthenticationMethod.php
@@ -136,8 +136,10 @@ abstract class AbstractNativeAuthenticationMethod implements NonReEntrantAuthent
     private function getMapping($field, $value)
     {
         $mapping = $this->mappingRepository->findOneBy([$field => $value]);
-        if (($field == 'email' && ZAuthConstant::AUTHENTICATION_METHOD_UNAME == $mapping->getMethod())
-            || ($field == 'uname' && ZAuthConstant::AUTHENTICATION_METHOD_EMAIL == $mapping->getMethod())) {
+        if (isset($mapping) && (
+            ($field == 'email' && ZAuthConstant::AUTHENTICATION_METHOD_UNAME == $mapping->getMethod())
+            || ($field == 'uname' && ZAuthConstant::AUTHENTICATION_METHOD_EMAIL == $mapping->getMethod()))
+        ) {
             // mapping exists but method is set to opposite. allow either if possible.
             $mapping->setMethod(ZAuthConstant::AUTHENTICATION_METHOD_EITHER);
             $errors = $this->validator->validate($mapping);
