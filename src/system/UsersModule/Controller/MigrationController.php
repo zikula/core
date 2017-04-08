83762ab39e0d6f3d8f77f68e9f14ab49450107d3
Craig Heydenburg <craigh@mac.com>
2017-04-08 17:56:01 -0400

    add method for migrating users manually fixes #3278 (#3535)
    * add method for migrating users manually fixes #3278
    
    * ci and changelog

---
+++ src/system/UsersModule/Controller/MigrationController.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\UsersModule\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Zikula\Core\Controller\AbstractController;
+use Zikula\Core\Response\Ajax\AjaxResponse;
+use Zikula\ThemeModule\Engine\Annotation\Theme;
+use Zikula\UsersModule\Constant as UsersConstant;
+
+/**
+ * @deprecated remove at Core-2.0
+ * @Route("/migration")
+ */
+class MigrationController extends AbstractController
+{
+    const BATCH_LIMIT = 25;
+
+    /**
+     * @deprecated
+     * @Route("/migrate", options={"expose"=true, "i18n"=false})
+     * @Theme("admin")
+     * @Template
+     * @param Request $request
+     * @return array|AjaxResponse
+     */
+    public function migrateAction(Request $request)
+    {
+        if (!$this->hasPermission('ZikulaUsersModule::', '::', ACCESS_ADMIN)) {
+            throw new AccessDeniedException();
+        }
+        $userRepo = $this->get('zikula_users_module.user_repository');
+        if (!$request->isXmlHttpRequest()) {
+            $count = $userRepo->count(['pass' => ['operator' => '!=', 'operand' => '']]);
+            if ($count == 0) {
+                throw new \LogicException($this->__('All users have been migrated already.'));
+            }
+            // set up
+            $request->getSession()->set('user_migration_count', $count);
+            $request->getSession()->set('user_migration_complete', 0);
+            $request->getSession()->set('user_migration_lastuid', 0);
+            $request->getSession()->set('user_migration_maxuid', $userRepo->getMaxUnMigratedUid());
+        } else {
+            $userEntities = $userRepo->getUnMigratedUsers($request->getSession()->get('user_migration_lastuid'), self::BATCH_LIMIT);
+            foreach ($userEntities as $userEntity) {
+                $mapping = $this->get('zikula_zauth_module.helper.migration_helper')->createMappingFromUser($userEntity);
+                if ($mapping) {
+                    $this->get('doctrine')->getManager()->persist($mapping);
+                    $userEntity->setPass('');
+                    $userEntity->setAttribute(UsersConstant::AUTHENTICATION_METHOD_ATTRIBUTE_KEY, $mapping->getMethod());
+                    $request->getSession()->set('user_migration_complete', $request->getSession()->get('user_migration_complete') + 1);
+                }
+                $request->getSession()->set('user_migration_lastuid', $userEntity->getUid());
+            }
+            $this->get('doctrine')->getManager()->flush();
+            if ($request->getSession()->get('user_migration_lastuid') == $request->getSession()->get('user_migration_maxuid')) {
+                $count = $request->getSession()->get('user_migration_count');
+                $complete = $request->getSession()->get('user_migration_complete');
+                if ($count == $complete) {
+                    $this->addFlash('success', $this->__('All users migrated.'));
+                } else {
+                    $this->addFlash('warning', $this->__f('%complete of %count users migrated. Failed to migrate all due to validation errors. See the log for more information.',
+                        ['%complete' => $complete, '%count' => $count]));
+                }
+                $percentComplete = 100;
+                // clean up
+                $request->getSession()->remove('user_migration_count');
+                $request->getSession()->remove('user_migration_complete');
+                $request->getSession()->remove('user_migration_lastuid');
+                $request->getSession()->remove('user_migration_maxuid');
+            } else {
+                $percentComplete = ceil(100 * $request->getSession()->get('user_migration_complete') / $request->getSession()->get('user_migration_count'));
+            }
+
+            return new AjaxResponse([
+                'percentcomplete' => $percentComplete,
+            ]);
+        }
+
+        return ['count' => $count];
+    }
+}
---
+++ src/system/ZAuthModule/Helper/MigrationHelper.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\ZAuthModule\Helper;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Zikula\UsersModule\Constant as UsersConstant;
+use Zikula\UsersModule\Entity\RepositoryInterface\UserRepositoryInterface;
+use Zikula\UsersModule\Entity\UserEntity;
+use Zikula\ZAuthModule\Entity\AuthenticationMappingEntity;
+use Zikula\ZAuthModule\ZAuthConstant;
+
+/**
+ * @deprecated remove at Core-2.0
+ * Class MigrationHelper
+ */
+class MigrationHelper
+{
+    /**
+     * @var UserRepositoryInterface
+     */
+    private $userRepository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * MigrationHelper constructor.
+     * @param UserRepositoryInterface $userRepository
+     * @param EntityManagerInterface $entityManager
+     * @param ValidatorInterface $validator
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        UserRepositoryInterface $userRepository,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+        LoggerInterface $logger
+    ) {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @deprecated
+     * @param UserEntity $userEntity
+     * @param string $method
+     * @return AuthenticationMappingEntity|null
+     * @throws \Exception
+     */
+    public function createMappingFromUser(UserEntity $userEntity, $method = ZAuthConstant::AUTHENTICATION_METHOD_EITHER)
+    {
+        $mapping = new AuthenticationMappingEntity();
+        $mapping->setUid($userEntity->getUid());
+        $mapping->setUname($userEntity->getUname());
+        $mapping->setEmail($userEntity->getEmail());
+        $mapping->setVerifiedEmail(true);
+        $mapping->setPass($userEntity->getPass()); // previously salted and hashed
+        $mapping->setMethod($method);
+        $errors = $this->validator->validate($mapping);
+        if ($errors->count() > 0) {
+            foreach ($errors as $error) {
+                $this->logger->addError('Unable to migrate user (' . $userEntity->getUname() . '/' . $userEntity->getEmail() . ') because: ' . $error->getMessage());
+            }
+
+            return null;
+        }
+
+        return $mapping;
+    }
+
+    /**
+     * @deprecated
+     * @param array $criteria
+     * @param string $method
+     * @return AuthenticationMappingEntity
+     */
+    public function createMappingFromUserCriteria(array $criteria, $method = ZAuthConstant::AUTHENTICATION_METHOD_EITHER)
+    {
+        $userEntity = $this->userRepository->findOneBy($criteria);
+        $mapping = $this->createMappingFromUser($userEntity, $method);
+        if ($mapping) {
+            $this->entityManager->persist($mapping);
+            // remove data from UserEntity
+            $userEntity->setPass('');
+            $userEntity->setAttribute(UsersConstant::AUTHENTICATION_METHOD_ATTRIBUTE_KEY, $mapping->getMethod());
+            $this->entityManager->flush();
+        }
+
+        return $mapping;
+    }
+}
