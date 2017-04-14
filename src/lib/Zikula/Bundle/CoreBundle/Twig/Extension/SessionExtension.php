a6a53357902574e381e0cede2e646d963ad71fd2
Craig Heydenburg <craigh@mac.com>
2017-04-14 06:19:25 -0400

    Twig extensions improvement (#3547)
    * simplify twig extensions. move some function to better locations. reduce dependency injection where possible.
    
    * ci
    
    * better formatting. always use zikula's translator.
    
    * ci

---
+++ src/lib/Zikula/Bundle/CoreBundle/Twig/Extension/SessionExtension.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\Bundle\CoreBundle\Twig\Extension;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Zikula\Bundle\CoreBundle\Twig;
+
+class SessionExtension extends \Twig_Extension
+{
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /**
+     * CoreExtension constructor.
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('showflashes', [$this, 'showFlashes'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * Display flash messages in twig template. Defaults to bootstrap alert classes.
+     *
+     * <pre>
+     *  {{ showflashes() }}
+     *  {{ showflashes({'class': 'custom-class', 'tag': 'span'}) }}
+     * </pre>
+     *
+     * @param array $params
+     * @return string
+     */
+    public function showFlashes(array $params = [])
+    {
+        $result = '';
+        $total_messages = [];
+        $messageTypeMap = [
+            'error' => 'danger',
+            'warning' => 'warning',
+            'status' => 'success',
+            'danger' => 'danger',
+            'success' => 'success',
+            'info' => 'info'
+        ];
+
+        foreach ($messageTypeMap as $messageType => $bootstrapClass) {
+            $messages = $this->session->getFlashBag()->get($messageType);
+            if (count($messages) > 0) {
+                // Set class for the messages.
+                $class = (!empty($params['class'])) ? $params['class'] : "alert alert-$bootstrapClass";
+                $total_messages = $total_messages + $messages;
+                // Build output of the messages.
+                if (empty($params['tag']) || ($params['tag'] != 'span')) {
+                    $params['tag'] = 'div';
+                }
+                $result .= '<' . $params['tag'] . ' class="' . $class . '"';
+                if (!empty($params['style'])) {
+                    $result .= ' style="' . $params['style'] . '"';
+                }
+                $result .= '>';
+                $result .= implode('<hr />', $messages);
+                $result .= '</' . $params['tag'] . '>';
+            }
+        }
+
+        if (empty($total_messages)) {
+            return "";
+        }
+
+        return $result;
+    }
+}
