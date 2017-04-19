773e70bb54bdc3fa9295679933fcf0854ed9bf54
Guite CI <support@guite.de>
2017-04-19 10:55:28 +0200

    Regenerated using latest generator

--- src/system/RoutesModule/Controller/Base/AbstractAjaxController.php
+++ src/system/RoutesModule/Controller/Base/AbstractAjaxController.php
@@ -14,13 +14,7 @@ namespace Zikula\RoutesModule\Controller\Base;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use RuntimeException;
 use Zikula\Core\Controller\AbstractController;
-use Zikula\Core\Response\Ajax\AjaxResponse;
-use Zikula\Core\Response\Ajax\BadDataResponse;
-use Zikula\Core\Response\Ajax\FatalResponse;
-use Zikula\Core\Response\Ajax\NotFoundResponse;
 
 /**
  * Ajax controller base class.
