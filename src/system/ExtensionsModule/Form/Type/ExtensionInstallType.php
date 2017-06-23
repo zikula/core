47f0d9b30b7feaf717e1abec836476c77849d448
Craig Heydenburg <craigh@mac.com>
2017-06-23 11:39:36 -0400

    correct missing import

--- src/system/ExtensionsModule/Form/Type/ExtensionInstallType.php
+++ src/system/ExtensionsModule/Form/Type/ExtensionInstallType.php
@@ -14,6 +14,7 @@ namespace Zikula\ExtensionsModule\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Zikula\Common\Translator\IdentityTranslator;
