c3a0247fc579e0a65ad59e7c140c4560d80267b7
Craig Heydenburg <craigh@mac.com>
2017-06-23 11:50:34 -0400

    correct missing import

--- src/system/ZAuthModule/Form/Type/RegistrationType.php
+++ src/system/ZAuthModule/Form/Type/RegistrationType.php
@@ -16,6 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
