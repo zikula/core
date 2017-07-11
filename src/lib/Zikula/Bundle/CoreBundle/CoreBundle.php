ddd1dd815a99405cdf99604e123617882a400d2d
Craig Heydenburg <craigh@mac.com>
2017-07-11 07:09:23 -0400

    remove RegisterCoreListenersPass closes # 3631

--- src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
+++ src/lib/Zikula/Bundle/CoreBundle/CoreBundle.php
@@ -17,7 +17,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\DoctrinePass;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\OverrideBlameableListenerPass;
-use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\RegisterCoreListenersPass;
 use Zikula\Bundle\CoreBundle\DependencyInjection\Compiler\LinkContainerPass;
 
 class CoreBundle extends Bundle
@@ -27,8 +26,6 @@ class CoreBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new DoctrinePass(), PassConfig::TYPE_OPTIMIZE);
-
-        $container->addCompilerPass(new RegisterCoreListenersPass(), PassConfig::TYPE_AFTER_REMOVING);
 
         $container->addCompilerPass(new LinkContainerPass());
 
