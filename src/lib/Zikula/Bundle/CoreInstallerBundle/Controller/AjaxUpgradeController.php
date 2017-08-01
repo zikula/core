07cd89e49e4b840e047c288f358906818c128760
craigh <craigh@mac.com>
2017-08-01 12:57:42 -0400

    fix upgrade for forgetful admin

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/AjaxUpgradeController.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Controller/AjaxUpgradeController.php
@@ -272,6 +272,13 @@ class AjaxUpgradeController extends AbstractController
         // set the 'start' page information to empty to avoid missing module errors.
         $variableApi->set(VariableApi::CONFIG, 'startController', '');
         $variableApi->set(VariableApi::CONFIG, 'startargs', '');
+        // on upgrade, if a user doesn't add their custom theme back to the /theme dir, it should be reset to a core theme, if available.
+        $defaultTheme = $variableApi->getSystemVar('Default_Theme');
+        if (!$this->container->get('kernel')->isBundle($defaultTheme)
+            && $this->container->get('kernel')->isBundle('ZikulaBootstrapTheme')) {
+                $variableApi->set(VariableApi::CONFIG,'Default_Theme', 'ZikulaBootstrapTheme');
+        }
+
 
         return true;
     }
