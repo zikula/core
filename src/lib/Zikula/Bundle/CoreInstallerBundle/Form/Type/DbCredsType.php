21798c6243714fb6ed09b6b1dfdfe6b4399c5c63
Guite <info@guite.de>
2017-09-24 13:22:27 +0200

    use sqlsrv value again (becomes pdo_sqlsrv)

--- src/lib/Zikula/Bundle/CoreInstallerBundle/Form/Type/DbCredsType.php
+++ src/lib/Zikula/Bundle/CoreInstallerBundle/Form/Type/DbCredsType.php
@@ -107,7 +107,7 @@ class DbCredsType extends AbstractType
             $types['MySQL'] = 'mysql';
         }
         if (in_array('sqlsrv', $availableDrivers)) {
-            $types['MSSQL (alpha)'] = 'mssql';
+            $types['MSSQL (alpha)'] = 'sqlsrv';
         }
         if (in_array('oci8', $availableDrivers)) {
             $types['Oracle (alpha) via OCI8 driver'] = 'oci8';
