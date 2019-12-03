54f5991a3a3125d1ab4b2cd5012df363e581cf73
Craig Heydenburg <craigh@mac.com>
2019-12-02 19:24:18 -0500

    Revert "await filewrite"
    This reverts commit 7d06f825bc1f3550872cf8a756378a4dacd9813f.

--- src/lib/Zikula/Bundle/CoreBundle/YamlDumper.php
+++ src/lib/Zikula/Bundle/CoreBundle/YamlDumper.php
@@ -194,12 +194,6 @@ class YamlDumper
     {
         $yaml = Yaml::dump($configuration);
         $this->fs->dumpFile($this->fullPath, $yaml);
-        do {
-            // await completion of file write
-            if (file_exists($this->fullPath)) {
-                break;
-            }
-        } while(!file_exists($this->fullPath));
     }
 
     /**
