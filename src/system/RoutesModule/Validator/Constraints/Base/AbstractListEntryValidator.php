7b1c38a70d3c565a5da8df3c699ab655fe71416a
craigh <craigh@mac.com>
2019-12-23 07:13:39 -0500

    do not use deprecated method. @Guite this will need changing in the generator please

--- src/system/RoutesModule/Validator/Constraints/Base/AbstractListEntryValidator.php
+++ src/system/RoutesModule/Validator/Constraints/Base/AbstractListEntryValidator.php
@@ -93,10 +93,10 @@ abstract class AbstractListEntryValidator extends ConstraintValidator
 
         if (null !== $constraint->min && $count < $constraint->min) {
             $this->context->buildViolation(
-                $this->translator->transChoice(
+                $this->translator->trans(
                     'You must select at least "%limit%" choice.|You must select at least "%limit%" choices.',
-                    $count,
                     [
+                        '%count%' => $count,
                         '%limit%' => $constraint->min
                     ]
                 )
@@ -104,10 +104,10 @@ abstract class AbstractListEntryValidator extends ConstraintValidator
         }
         if (null !== $constraint->max && $count > $constraint->max) {
             $this->context->buildViolation(
-                $this->translator->transChoice(
+                $this->translator->trans(
                     'You must select at most "%limit%" choice.|You must select at most "%limit%" choices.',
-                    $count,
                     [
+                        '%count%' => $count,
                         '%limit%' => $constraint->max
                     ]
                 )
