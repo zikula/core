fa54add31c187611337f37f27bb4db5d2b34abae
Axel Guckelsberger <info@guite.de>
2020-01-01 11:15:57 +0100

    use Symfony's HttpClient for update check (#4035)

--- src/system/AdminModule/Helper/UpdateCheckHelper.php
+++ src/system/AdminModule/Helper/UpdateCheckHelper.php
@@ -13,17 +13,21 @@ declare(strict_types=1);
 
 namespace Zikula\AdminModule\Helper;
 
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Zikula\Bundle\CoreBundle\HttpKernel\ZikulaKernel;
 use Zikula\ExtensionsModule\Api\ApiInterface\VariableApiInterface;
 use Zikula\ExtensionsModule\Api\VariableApi;
 
 class UpdateCheckHelper
 {
+    private const URL_RELEASES = 'https://api.github.com/repos/zikula/core/releases';
+
     /**
      * @var VariableApiInterface
      */
-    protected $variableApi;
+    private $variableApi;
 
     /**
      * @var RequestStack
@@ -130,8 +134,9 @@ class UpdateCheckHelper
         if (false === $this->force && (($now - $this->lastChecked) < ($this->checkInterval * 86400))) {
             // dont get an update because TTL not expired yet
         } else {
-            $newVersionInfo = json_decode(trim($this->zcurl('https://api.github.com/repos/zikula/core/releases')), true);
-            // Will be set if rate limits encountered
+            $newVersionInfo = $this->fetchReleases();
+
+            // will be set if rate limits encountered
             if (!is_array($newVersionInfo) || isset($newVersionInfo['message'])) {
                 $this->releases = false;
             } else {
@@ -154,75 +159,18 @@ class UpdateCheckHelper
         }
     }
 
-    /**
-     * Zikula curl
-     *
-     * This function is internal for the time being and may be extended to be a proper library
-     * or find an alternative solution later.
-     *
-     * @return string|bool false if no url handling functions are present or url string
-     */
-    private function zcurl(string $url, int $timeout = 5)
+    private function fetchReleases(): array
     {
-        $urlArray = parse_url($url);
-        $data = '';
-        $userAgent = 'Zikula/' . $this->currentVersion;
-        $request = $this->requestStack->getMasterRequest();
-        $ref = null !== $request ? $request->getBaseURL() : null;
-        $port = ('https' === $urlArray['scheme']) ? 443 : 80;
+        $result = [];
+        try {
+            $client = HttpClient::create();
+            $response = $client->request('GET', self::URL_RELEASES);
 
-        if (function_exists('curl_init')) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url . '?');
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-            curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-            if (null !== $ref) {
-                curl_setopt($ch, CURLOPT_REFERER, $ref);
-            }
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            if (!ini_get('open_basedir')) {
-                // This option does not work with open_basedir set in php.ini
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            }
-            curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-            $data = curl_exec($ch);
-            if (!$data && 443 === $port) {
-                // retry non ssl
-                $url = str_replace('https://', 'http://', $url);
-                curl_setopt($ch, CURLOPT_URL, "${url}?");
-                $data = curl_exec($ch);
-            }
-            curl_close($ch);
-
-            return $data;
+            $result = $response->toArray();
+        } catch (TransportExceptionInterface $exception) {
+            // nothing
         }
 
-        if (ini_get('allow_url_fopen')) {
-            // handle SSL connections
-            $path_query = (isset($urlArray['query']) ? $urlArray['path'] . $urlArray['query'] : $urlArray['path']);
-            $host = (443 === $port ? 'ssl://' . $urlArray['host'] : $urlArray['host']);
-            $fp = fsockopen($host, $port, $errno, $errstr, $timeout);
-            if (!$fp) {
-                return false;
-            }
-
-            $out = 'GET ' . $path_query . "? HTTP/1.1\r\n";
-            $out .= 'User-Agent: ' . $userAgent . "\r\n";
-            if (null !== $ref) {
-                $out .= 'Referer: ' . $ref . "\r\n";
-            }
-            $out .= 'Host: ' . $urlArray['host'] . "\r\n";
-            $out .= 'Connection: Close' . "\r\n\r\n";
-            fwrite($fp, $out);
-            while (!feof($fp)) {
-                $data .= fgets($fp, 1024);
-            }
-            fclose($fp);
-            $dataArray = explode("\r\n\r\n", $data);
-
-            return $dataArray[1];
-        }
-
-        return false;
+        return $result;
     }
 }
