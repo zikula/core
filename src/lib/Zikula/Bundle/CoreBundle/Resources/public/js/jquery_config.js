cfd07574fdee627b981b3b7b86af67e59a62f79b
Guite <info@guite.de>
2020-01-02 14:18:56 +0100

    removed old prototype conflict fix

--- src/lib/Zikula/Bundle/CoreBundle/Resources/public/js/jquery_config.js
+++ src/lib/Zikula/Bundle/CoreBundle/Resources/public/js/jquery_config.js
@@ -1,25 +1,6 @@
 // Copyright Zikula Foundation, licensed MIT.
 
 jQuery.noConflict();
-/*
- Fix provided by http://softec.lu/site/DevelopersCorner/BootstrapPrototypeConflict
- */
-if ('undefined' !== typeof Prototype && Prototype.BrowserFeatures.ElementExtensions) {
-    var disablePrototypeJS = function (method, pluginsToDisable) {
-            var handler = function (event) {
-                event.target[method] = undefined;
-                setTimeout(function () {
-                    delete event.target[method];
-                }, 0);
-            };
-            pluginsToDisable.each(function (plugin) {
-                jQuery(window).on(method + '.bs.' + plugin, handler);
-            });
-        },
-        pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
-    disablePrototypeJS('show', pluginsToDisable);
-    disablePrototypeJS('hide', pluginsToDisable);
-}
 
 // setup ajax for jQuery
 (function($) {
