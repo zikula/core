01989d5e50f4d7d5f0eedebf8757deb48291c9b9
Guite <info@guite.de>
2020-01-10 12:55:33 +0100

    fixed wrong namespace

--- src/system/ThemeModule/EventListener/Core3UpgradeListener.php
+++ src/system/ThemeModule/EventListener/Core3UpgradeListener.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Zikula\PermissionsModule\Listener;
+namespace Zikula\ThemeModule\EventListener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Zikula\Common\ColumnExistsTrait;
