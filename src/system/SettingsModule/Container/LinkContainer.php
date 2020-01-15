e60342a172f4e618d571bae7adca361d02f3444a
Guite <info@guite.de>
2020-01-15 08:58:28 +0100

    wording

--- src/system/SettingsModule/Container/LinkContainer.php
+++ src/system/SettingsModule/Container/LinkContainer.php
@@ -117,7 +117,7 @@ class LinkContainer implements LinkContainerInterface
                 }
                 $i10nLinks[] = [
                     'url' => $this->router->generate('translation_index'),
-                    'text' => $this->translator->trans('Translation WebUI') . ' (experimental)',
+                    'text' => $this->translator->trans('Translation UI') . ' (experimental)',
                     'title' => $this->translator->trans('Web interface to add, edit and remove translations'),
                     'icon' => 'language'
                 ];
