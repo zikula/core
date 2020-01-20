4f4d3e41b3bb37d4aa1585a18389384d5836377c
Guite <info@guite.de>
2020-01-20 09:45:48 +0100

    regenerated Routes

--- src/system/RoutesModule/Menu/ExtensionMenu.php
+++ src/system/RoutesModule/Menu/ExtensionMenu.php
@@ -15,73 +15,54 @@ namespace Zikula\RoutesModule\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
-use Zikula\MenuModule\ExtensionMenu\ExtensionMenuInterface;
 use Zikula\PermissionsModule\Api\ApiInterface\PermissionApiInterface;
+use Zikula\RoutesModule\Menu\Base\AbstractExtensionMenu;
 
-class ExtensionMenu implements ExtensionMenuInterface
+class ExtensionMenu extends AbstractExtensionMenu
 {
-    /**
-     * @var FactoryInterface
-     */
-    private $factory;
-
-    /**
-     * @var PermissionApiInterface
-     */
-    private $permissionApi;
-
-    public function __construct(
-        FactoryInterface $factory,
-        PermissionApiInterface $permissionApi
-    ) {
-        $this->factory = $factory;
-        $this->permissionApi = $permissionApi;
-    }
-
     public function get(string $type = self::TYPE_ADMIN): ?ItemInterface
     {
-        if (self::TYPE_ADMIN === $type) {
-            return $this->getAdmin();
+        if (self::TYPE_ADMIN !== $type) {
+            return parent::get($type);
         }
 
-        return null;
-    }
-
-    private function getAdmin(): ?ItemInterface
-    {
-        if (!$this->permissionApi->hasPermission($this->getBundleName() . ':route:', '::', ACCESS_ADMIN)) {
+        if (!$this->permissionHelper->hasPermission(ACCESS_ADMIN)) {
             return null;
         }
-        $menu = $this->factory->createItem('routesAdminMenu');
+
+        $menu = $this->factory->createItem('zikularoutesmodule' . ucfirst($type) . 'Menu');
         $menu->addChild('Routes', [
             'route' => 'zikularoutesmodule_route_adminview',
-        ])->setAttribute('icon', 'fas fa-list')
-            ->setLinkAttribute('title', 'Route list');
+        ])
+            ->setAttribute('icon', 'fas fa-list')
+            ->setLinkAttribute('title', 'Route list')
+        ;
         $menu->addChild('Reload routes', [
             'route' => 'zikularoutesmodule_update_reload',
-        ])->setAttribute('icon', 'fas fa-sync-alt')
-            ->setLinkAttribute('title', 'Reload routes');
+        ])
+            ->setAttribute('icon', 'fas fa-sync-alt')
+            ->setLinkAttribute('title', 'Reload routes')
+        ;
         $menu->addChild('Reload multilingual routing settings', [
             'route' => 'zikularoutesmodule_update_renew',
-        ])->setAttribute('icon', 'fas fa-sync-alt')
-            ->setLinkAttribute('title', 'Reload multilingual routing settings');
+        ])
+            ->setAttribute('icon', 'fas fa-sync-alt')
+            ->setLinkAttribute('title', 'Reload multilingual routing settings')
+        ;
         $menu->addChild('Dump exposed js routes to file', [
             'route' => 'zikularoutesmodule_update_dumpjsroutes',
-        ])->setAttribute('icon', 'fas fa-file')
-            ->setLinkAttribute('title', 'Dump exposed js routes to file');
+        ])
+            ->setAttribute('icon', 'fas fa-file')
+            ->setLinkAttribute('title', 'Dump exposed js routes to file')
+        ;
 
-        if ($this->permissionApi->hasPermission($this->getBundleName() . '::', '::', ACCESS_ADMIN)) {
-            $menu->addChild('Configuration', [
-                'route' => 'zikularoutesmodule_config_config',
-            ])->setAttribute('icon', 'fas fa-wrench')
-                ->setLinkAttribute('title', 'Manage settings for this application');
-        }
+        $menu->addChild('Configuration', [
+            'route' => 'zikularoutesmodule_config_config',
+        ])
+            ->setAttribute('icon', 'fas fa-wrench')
+            ->setLinkAttribute('title', 'Manage settings for this application')
+        ;
 
-        return $menu->count() === 0 ? null : $menu;
-    }
-
-    public function getBundleName(): string
-    {
-        return 'ZikulaRoutesModule';
+        return 0 === $menu->count() ? null : $menu;
     }
 }
