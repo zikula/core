401c464571a6e9daf0ba5674d7595a6797e64395
Craig Heydenburg <craigh@mac.com>
2020-01-23 19:49:21 -0500

    move bootstrap.php file refs #4052

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 use Symfony\Component\HttpFoundation\Request;
 
-require 'lib/bootstrap.php';
+require 'app/config/bootstrap.php';
 
 $request = Request::createFromGlobals();
 
