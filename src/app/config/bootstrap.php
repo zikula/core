e3b9d426b5cc1ae102b4f392c3e0cdb1c120d1d4
Guite <info@guite.de>
2020-01-24 08:19:30 +0100

    fixed wrong pathes

--- src/app/config/bootstrap.php
+++ src/app/config/bootstrap.php
@@ -15,11 +15,11 @@ use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\Yaml\Yaml;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../lib/RequirementChecker.php';
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../lib/RequirementChecker.php';
 
-$kernelConfig = Yaml::parse(file_get_contents(realpath(__DIR__ . '/../app/config/parameters.yml')));
-if (is_readable($file = __DIR__ . '/../app/config/custom_parameters.yml')) {
+$kernelConfig = Yaml::parse(file_get_contents(realpath(__DIR__ . '/parameters.yml')));
+if (is_readable($file = __DIR__ . '/custom_parameters.yml')) {
     $kernelConfig = array_merge($kernelConfig, Yaml::parse(file_get_contents($file)));
 }
 $parameters = $kernelConfig['parameters'];
