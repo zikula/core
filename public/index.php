9457d4f1bf8138277260f489a01175c061cbffdf
Craig Heydenburg <craigh@mac.com>
2020-01-26 21:02:06 -0500

    add index.php to public dir refs #4052

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__) . '/config/bootstrap.php';
+
+$request = Request::createFromGlobals();
+
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
