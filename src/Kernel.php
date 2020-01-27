5dace91a45ade15a41ceabc17777feb9118df06f
Guite <info@guite.de>
2020-01-27 12:48:20 +0100

    directory layout + configuration: next steps

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,13 +11,21 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+//use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+//use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 use Zikula\Bundle\CoreBundle\DynamicConfigDumper;
 use Zikula\Bundle\CoreBundle\Helper\PersistedBundleHelper;
 use Zikula\Bundle\CoreBundle\HttpKernel\ZikulaKernel;
 
 class Kernel extends ZikulaKernel
 {
+    use MicroKernelTrait;
+
     public function registerBundles(): iterable
     {
         $bundleHelper = new PersistedBundleHelper();
@@ -30,12 +38,32 @@ class Kernel extends ZikulaKernel
         }
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    // could be remove when upgrading Symfony to 5.1.0 (wanted?)
+    public function getProjectDir(): string
     {
-        $configDir = $this->getProjectDir() . '/config/';
-        $loader->load($configDir . 'config_' . $this->getEnvironment() . '.yaml');
+        return dirname(__DIR__);
+    }
 
-        $loader->load($configDir . 'services.yaml');
+    // use new signature when upgrading Symfony to 5.1.0
+    //protected function configureContainer(ContainerConfigurator $container): void
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        /** use with Symfony 5.1.0
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
+        $container->import('../config/{services}.yaml');
+        $container->import('../config/{services}_' . $this->environment . '.yaml');
+        */
+        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', $this->debug);
+        $container->setParameter('container.dumper.inline_factories', true);
+        $configDir = $this->getProjectDir() . '/config/';
+
+        $loader->load($configDir . '{packages}/*.yaml', 'glob');
+        $loader->load($configDir . '{packages}/' . $this->environment . '/*.yaml', 'glob');
+        $loader->load($configDir . '{services}.yaml', 'glob');
+        $loader->load($configDir . '{services}_' . $this->environment . '.yaml', 'glob');
+
         if (is_readable($configDir . 'services_custom.yaml')) {
             $loader->load($configDir . 'services_custom.yaml');
         }
@@ -49,8 +77,20 @@ class Kernel extends ZikulaKernel
         }
     }
 
-    public function getProjectDir()
+    // use new signature when upgrading Symfony to 5.1.0
+    //protected function configureRoutes(RoutingConfigurator $routes): void
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        return dirname(__DIR__);
+        $configDir = $this->getProjectDir() . '/config/';
+
+        /** use with Symfony 5.1.0
+        $routes->import($configDir . '{routes}/' . $this->environment . '/*.yaml');
+        $routes->import($configDir . '{routes}/*.yaml');
+        $routes->import($configDir . '{routes}.yaml');
+        */
+
+        $routes->import($configDir . '{routes}/' . $this->environment . '/*.yaml', '/', 'glob');
+        $routes->import($configDir . '{routes}/*.yaml', '/', 'glob');
+        $routes->import($configDir . '{routes}.yaml', '/', 'glob');
     }
 }
