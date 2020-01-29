e80f37d149de501a2a90cd393e91aba685294e0f
Craig Heydenburg <craigh@mac.com>
2020-01-28 20:14:53 -0500

    rename modules table to extensions

--- src/system/ExtensionsModule/Listener/Core3UpgradeListener.php
+++ src/system/ExtensionsModule/Listener/Core3UpgradeListener.php
@@ -34,13 +34,16 @@ class Core3UpgradeListener implements EventSubscriberInterface
         if (!version_compare($event->getArgument('currentVersion'), '3.0.0', '<')) {
             return;
         }
-        if ($this->columnExists('modules', 'coreCompatibility')) {
-            return;
+        $sqls = [];
+        if (!$this->columnExists('modules', 'coreCompatibility')) {
+            $sqls[] = 'ALTER TABLE `modules` CHANGE `core_min` `coreCompatibility` VARCHAR(64) NOT NULL';
+            $sqls[] = 'ALTER TABLE `modules` DROP COLUMN `core_max`';
         }
-        $commands = [];
-        $commands[] = 'ALTER TABLE `modules` CHANGE `core_min` `coreCompatibility` VARCHAR(64) NOT NULL';
-        $commands[] = 'ALTER TABLE `modules` DROP COLUMN `core_max`';
-        foreach ($commands as $sql) {
+        $sm = $this->conn->getSchemaManager();
+        if ($sm->tablesExist(['modules'])) {
+            $sqls[] = 'ALTER TABLE `modules` RENAME TO `extensions`';
+        }
+        foreach ($sqls as $sql) {
             $this->conn->executeQuery($sql);
         }
     }
