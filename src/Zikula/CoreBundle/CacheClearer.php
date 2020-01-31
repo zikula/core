da5b803ef776878648044a8579c3cb3832180d76
Guite <info@guite.de>
2020-01-31 10:23:31 +0100

    CLI installer fixes

--- src/Zikula/CoreBundle/CacheClearer.php
+++ src/Zikula/CoreBundle/CacheClearer.php
@@ -15,44 +15,97 @@ namespace Zikula\Bundle\CoreBundle;
 
 use FilesystemIterator;
 use FOS\JsRoutingBundle\Extractor\ExposedRoutesExtractorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 class CacheClearer
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
     /**
      * @var string
      */
     private $cacheDir;
 
     /**
+     * @var string
+     */
+    private $kernelContainerClass;
+
+    /**
+     * @var bool
+     */
+    private $installed;
+
+    /**
      * @var array
      */
-    private $cacheTypes;
+    private $routingLocales = [];
+
+    /**
+     * @var array
+     */
+    private $cacheTypes = [];
 
     /**
      * @var Filesystem
      */
-    private $fs;
+    private $fileSystem;
 
     public function __construct(
+        ContainerInterface $container,
         string $cacheDir,
         string $kernelContainerClass,
-        ExposedRoutesExtractorInterface $fosJsRoutesExtractor,
+        bool $installed,
         array $routingLocales = []
     ) {
+        $this->container = $container;
         $this->cacheDir = $cacheDir;
-        $this->fs = new Filesystem();
-
-        $fosJsRoutingFiles = [];
-        foreach ($routingLocales as $locale) {
-            $fosJsRoutingFiles[] = $fosJsRoutesExtractor->getCachePath($locale);
-        }
-
-        $this->initialiseCacheTypeMap($kernelContainerClass, $fosJsRoutingFiles);
+        $this->kernelContainerClass = $kernelContainerClass;
+        $this->installed = $installed;
+        $this->routingLocales = $routingLocales;
+        $this->fileSystem = new Filesystem();
     }
 
-    private function initialiseCacheTypeMap(string $kernelContainerClass, array $fosJsRoutingFiles = [])
+    public function clear(string $type): void
     {
+        if (!count($this->cacheTypes)) {
+            $this->initialiseCacheTypeMap();
+        }
+
+        foreach ($this->cacheTypes as $cacheType => $files) {
+            if (0 !== mb_strpos($cacheType, $type)) {
+                continue;
+            }
+            foreach ($files as $file) {
+                if (is_dir($file)) {
+                    // Do not delete the folder itself, but all files in it.
+                    // Otherwise Symfony somehow can't create the folder anymore.
+                    $file = new FilesystemIterator($file);
+                }
+                // This silently ignores non existing files.
+                $this->fileSystem->remove($file);
+            }
+        }
+    }
+
+    private function initialiseCacheTypeMap()
+    {
+        $fosJsRoutingFiles = [];
+        if ($this->installed) {
+            // avoid accessing FOS extractor before/during installation
+            // because this requires request context
+
+            /** @var ExposedRoutesExtractorInterface */
+            $fosJsRoutesExtractor = $this->container->get('fos_js_routing.extractor');
+            foreach ($this->routingLocales as $locale) {
+                $fosJsRoutingFiles[] = $fosJsRoutesExtractor->getCachePath($locale);
+            }
+        }
+
         $cacheFolder = $this->cacheDir . DIRECTORY_SEPARATOR;
 
         $this->cacheTypes = [
@@ -69,10 +122,10 @@ class CacheClearer
             ],
             'symfony.routing.fosjs' => $fosJsRoutingFiles,
             'symfony.config' => [
-                $cacheFolder . $kernelContainerClass . '.php',
-                $cacheFolder . $kernelContainerClass . '.php.meta',
-                $cacheFolder . $kernelContainerClass . '.xml',
-                $cacheFolder . $kernelContainerClass . 'Compiler.log',
+                $cacheFolder . $this->kernelContainerClass . '.php',
+                $cacheFolder . $this->kernelContainerClass . '.php.meta',
+                $cacheFolder . $this->kernelContainerClass . '.xml',
+                $cacheFolder . $this->kernelContainerClass . 'Compiler.log',
                 $cacheFolder . 'classes.map'
             ],
             'symfony.translations' => [
@@ -88,23 +141,5 @@ class CacheClearer
                 $cacheFolder . 'assets'
             ]
         ];
-    }
-
-    public function clear(string $type): void
-    {
-        foreach ($this->cacheTypes as $cacheType => $files) {
-            if (0 !== mb_strpos($cacheType, $type)) {
-                continue;
-            }
-            foreach ($files as $file) {
-                if (is_dir($file)) {
-                    // Do not delete the folder itself, but all files in it.
-                    // Otherwise Symfony somehow can't create the folder anymore.
-                    $file = new FilesystemIterator($file);
-                }
-                // This silently ignores non existing files.
-                $this->fs->remove($file);
-            }
-        }
     }
 }
