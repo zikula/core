b23bb3673db4b583a12b1fe144b2cc9dc0ef4df4
Craig Heydenburg <craigh@mac.com>
2020-02-01 17:03:57 -0500

    add missing import

--- src/themes/BootstrapTheme/Controller/AjaxController.php
+++ src/themes/BootstrapTheme/Controller/AjaxController.php
@@ -15,6 +15,7 @@ namespace Zikula\BootstrapTheme\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Yaml;
 use Zikula\Bundle\CoreBundle\Controller\AbstractController;
