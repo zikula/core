c6744eb53b9191178c7dd7e8f7b98767df1cb315
Craig Heydenburg <craigh@mac.com>
2020-02-02 08:23:43 -0500

    ci

--- src/system/ExtensionsModule/Controller/ExtensionController.php
+++ src/system/ExtensionsModule/Controller/ExtensionController.php
@@ -19,7 +19,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
