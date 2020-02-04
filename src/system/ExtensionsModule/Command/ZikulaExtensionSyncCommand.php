98d2f761588a07c3412fd3110765d02fa3655159
Guite <info@guite.de>
2020-02-04 10:09:13 +0100

    cs

--- src/system/ExtensionsModule/Command/ZikulaExtensionSyncCommand.php
+++ src/system/ExtensionsModule/Command/ZikulaExtensionSyncCommand.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Zikula\ExtensionsModule\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
