bbe7e003af0d96c353f852e8100f96c13e1e3f9c
Craig Heydenburg <craigh@mac.com>
2020-02-09 17:11:56 -0500

    remove todos from test fixture

--- src/system/UsersModule/Tests/Api/Fixtures/MockUserRepository.php
+++ src/system/UsersModule/Tests/Api/Fixtures/MockUserRepository.php
@@ -56,53 +56,43 @@ class MockUserRepository implements UserRepositoryInterface
 
     public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
     {
-        // TODO: Implement findBy() method.
     }
 
     public function findOneBy(array $criteria)
     {
-        // TODO: Implement findOneBy() method.
     }
 
     public function getClassName()
     {
-        // TODO: Implement getClassName() method.
     }
 
     public function matching(Criteria $criteria)
     {
-        // TODO: Implement matching() method.
     }
 
     public function findByUids(array $userIds = []): array
     {
-        // TODO: Implement findByUids() method.
         return [];
     }
 
     public function persistAndFlush(UserEntity $user): void
     {
-        // TODO: Implement persistAndFlush() method.
     }
 
     public function removeAndFlush(UserEntity $user): void
     {
-        // TODO: Implement removeAndFlush() method.
     }
 
     public function setApproved(UserEntity $user, DateTime $approvedOn, int $approvedBy = null): void
     {
-        // TODO: Implement setApproved() method.
     }
 
     public function queryBySearchForm(array $formData = [])
     {
-        // TODO: Implement queryBySearchForm() method.
     }
 
     public function getSearchResults(array $words = [])
     {
-        // TODO: Implement getSearchResults() method.
     }
 
     public function query(
@@ -112,22 +102,18 @@ class MockUserRepository implements UserRepositoryInterface
         int $offset = 0,
         string $exprType = 'and'
     ) {
-        // TODO: Implement query() method.
     }
 
     public function count(array $filter = [], string $exprType = 'and'): int
     {
-        // TODO: Implement count() method.
         return 123;
     }
 
     public function findAllAsIterable(): IterableResult
     {
-        // TODO: Implement findAllAsIterable() method.
     }
 
     public function searchActiveUser(array $unameFilter = [], int $limit = 50)
     {
-        // TODO: Implement searchActiveUser() method.
     }
 }
