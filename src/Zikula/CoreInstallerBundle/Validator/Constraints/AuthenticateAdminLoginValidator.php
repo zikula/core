4bc14bda0bc3abed34bf34aa67b8c9ca8396bf6b
Craig Heydenburg <craigh@mac.com>
2020-02-17 12:56:56 -0500

    have to use old and new password checks on core upgrade admin login

--- src/Zikula/CoreInstallerBundle/Validator/Constraints/AuthenticateAdminLoginValidator.php
+++ src/Zikula/CoreInstallerBundle/Validator/Constraints/AuthenticateAdminLoginValidator.php
@@ -21,6 +21,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Zikula\Bundle\CoreBundle\Translation\TranslatorTrait;
 use Zikula\PermissionsModule\Api\ApiInterface\PermissionApiInterface;
+use Zikula\ZAuthModule\Api\ApiInterface\PasswordApiInterface;
 use Zikula\ZAuthModule\Entity\AuthenticationMappingEntity;
 
 class AuthenticateAdminLoginValidator extends ConstraintValidator
@@ -42,16 +43,23 @@ class AuthenticateAdminLoginValidator extends ConstraintValidator
      */
     private $encoderFactory;
 
+    /**
+     * @var PasswordApiInterface
+     */
+    private $passwordApi;
+
     public function __construct(
         PermissionApiInterface $permissionApi,
         Connection $connection,
         TranslatorInterface $translator,
-        EncoderFactoryInterface $encoderFactory
+        EncoderFactoryInterface $encoderFactory,
+        PasswordApiInterface $passwordApi
     ) {
         $this->permissionApi = $permissionApi;
         $this->databaseConnection = $connection;
         $this->setTranslator($translator);
         $this->encoderFactory = $encoderFactory;
+        $this->passwordApi = $passwordApi;
     }
 
     public function validate($object, Constraint $constraint)
@@ -64,24 +72,52 @@ class AuthenticateAdminLoginValidator extends ConstraintValidator
             ', [$object['username']]);
         } catch (Exception $exception) {
             $this->context->buildViolation($this->trans('Error! There was a problem with the database connection.'))
-                ->addViolation()
-            ;
+                ->addViolation();
         }
 
         $passwordEncoder = $this->encoderFactory->getEncoder(AuthenticationMappingEntity::class);
-        if (empty($user) || $user['uid'] <= 1 || !$passwordEncoder->isPasswordValid($user['pass'], $object['password'], null)) {
+
+        if (empty($user) || $user['uid'] <= 1) { // || !$passwordEncoder->isPasswordValid($user['pass'], $object['password'], null)) {
             $this->context
-                ->buildViolation($this->trans('Error! Could not login with provided credentials. Please try again.'))
-                ->addViolation()
-            ;
+                ->buildViolation($this->trans('Error! Could not login because the user could not be found. Please try again.'))
+                ->addViolation();
         } else {
+            $validPassword = false;
+            // old way - remove in Core-4.0.0
+            if ($this->passwordApi->passwordsMatch($object['password'], $user['pass'])) {
+                $validPassword = true;
+                // convert old encoding to new
+                $this->setPassword($user['uid'], $object['password']);
+            }
+            // new way
+            if ($passwordEncoder->isPasswordValid($user['pass'], $object['password'], NULL)) {
+                $validPassword = true;
+                if ($passwordEncoder->needsRehash($user['pass'])) { // check to update hash to newer algo
+                    $this->setPassword($user['uid'], $object['password']);
+                }
+            }
+            if (!$validPassword) {
+                $this->context
+                    ->buildViolation($this->trans('Error! Could not login with the provided credentials. Please try again.'))
+                    ->addViolation();
+            }
+
             $granted = $this->permissionApi->hasPermission('.*', '.*', ACCESS_ADMIN, (int) $user['uid']);
             if (!$granted) {
                 $this->context
                     ->buildViolation($this->trans('Error! You logged in to an account without Admin permissions'))
-                    ->addViolation()
-                ;
+                    ->addViolation();
             }
         }
+    }
+
+    private function setPassword(int $uid, string $unHashedPassword)
+    {
+        $passwordEncoder = $this->encoderFactory->getEncoder(AuthenticationMappingEntity::class);
+        $hashedPassword = $passwordEncoder->encodePassword($unHashedPassword, null);
+        $this->databaseConnection->executeUpdate('UPDATE zauth_authentication_mapping SET pass = ? WHERE uid = ?', [
+            $hashedPassword,
+            $uid
+        ]);
     }
 }
