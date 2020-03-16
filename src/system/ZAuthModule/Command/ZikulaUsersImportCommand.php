6ad8ce996c22bd9e660edf6957a35083223a71e9
Craig Heydenburg <craigh@mac.com>
2020-03-16 14:14:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/system/ZAuthModule/Command/ZikulaUsersImportCommand.php
+++ src/system/ZAuthModule/Command/ZikulaUsersImportCommand.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Zikula\ZAuthModule\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -49,10 +60,9 @@ class ZikulaUsersImportCommand extends Command
             $io->success(sprintf('Import successful. %d users imported', count($createdUsers)));
 
             return 0;
-        } else {
-            $io->error($error);
-
-            return 1;
         }
+        $io->error($error);
+
+        return 1;
     }
 }
