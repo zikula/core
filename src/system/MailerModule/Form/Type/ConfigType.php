f84a7a91415eda8cebd3039d6f949ce2f5e14cf2
Axel Guckelsberger <info@guite.de>
2020-03-29 13:32:58 +0200

    add missing mailer config fields (#4190)
    * add missing mailer config fields
    
    * renamed transport options

--- src/system/MailerModule/Form/Type/ConfigType.php
+++ src/system/MailerModule/Form/Type/ConfigType.php
@@ -16,6 +16,7 @@ namespace Zikula\MailerModule\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -32,12 +33,12 @@ class ConfigType extends AbstractType
                 'label' => 'Mailer transport',
                 'choices' => [
                     'Amazon SES' => 'amazon',
-                    'Google gmail' => 'gmail',
+                    'Google Gmail' => 'gmail',
                     'Mailchimp Mandrill' => 'mailchimp',
                     'Mailgun' => 'mailgun',
                     'Postmark' => 'postmark',
                     'Sendgrid' => 'sendgrid',
-                    'SMTP mail transfer protocol' => 'smtp',
+                    'SMTP server' => 'smtp',
                     'Sendmail binary' => 'sendmail',
                     'Development/debug mode (Do not send any email)' => 'test'/*'null'*/
                 ]
@@ -57,6 +58,37 @@ class ConfigType extends AbstractType
                 ],
                 'help' => 'The SECRET_KEY, PASSWORD, ID or KEY for the selected transport.',
                 'required' => false
+            ])
+            ->add('host', TextType::class, [
+                'label' => 'SMTP host server',
+                'attr' => [
+                    'maxlength' => 255
+                ],
+                'required' => false,
+                'help' => "Default: '%value%'",
+                'help_translation_parameters' => [
+                    '%value%' => 'localhost'
+                ]
+            ])
+            ->add('port', IntegerType::class, [
+                'label' => 'SMTP port',
+                'attr' => [
+                    'maxlength' => 5
+                ],
+                'required' => false,
+                'help' => "Default: '%value%'",
+                'help_translation_parameters' => [
+                    '%value%' => '25'
+                ]
+            ])
+            ->add('customParameters', TextType::class, [
+                'label' => 'Custom parameters',
+                'attr' => [
+                    'maxlength' => 255
+                ],
+                'required' => false,
+                'help' => 'Use query parameters syntax, for example: <code>?param1=value1&amp;param2=value2</code>',
+                'help_html' => true
             ])
             ->add('enableLogging', CheckboxType::class, [
                 'label' => 'Enable logging of sent mail',
