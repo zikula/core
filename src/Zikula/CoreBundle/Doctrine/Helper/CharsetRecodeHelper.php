e917624f1a5e7ea804e53217d78d52ada2066e62
Guite <info@guite.de>
2020-04-15 20:20:44 +0200

    use parameter binding

--- src/Zikula/CoreBundle/Doctrine/Helper/CharsetRecodeHelper.php
+++ src/Zikula/CoreBundle/Doctrine/Helper/CharsetRecodeHelper.php
@@ -15,6 +15,7 @@ namespace Zikula\Bundle\CoreBundle\Doctrine\Helper;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\AbstractMySQLDriver;
+use Doctrine\DBAL\ParameterType;
 
 class CharsetRecodeHelper
 {
@@ -45,8 +46,6 @@ class CharsetRecodeHelper
 
         // the following is based on
         // https://dba.stackexchange.com/questions/8239/how-to-easily-convert-utf8-tables-to-utf8mb4-in-mysql-5-5#answer-104866
-        $dbName = $this->conn->getDatabase();
-        $tableFilter = 'table_schema LIKE "' . $dbName . '"';
         $commands = [];
 
         $this->conn->executeQuery('use information_schema;');
@@ -55,7 +54,7 @@ class CharsetRecodeHelper
         $rows = $this->retrieveCommands('
             SELECT CONCAT("ALTER DATABASE `",table_schema,"` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;") AS _sql 
             FROM `TABLES`
-            WHERE ' . $tableFilter . '
+            WHERE table_schema LIKE ?
             GROUP BY table_schema;
         ');
         $commands = array_merge($commands, $rows);
@@ -64,7 +63,7 @@ class CharsetRecodeHelper
         $rows = $this->retrieveCommands('
             SELECT CONCAT("ALTER TABLE `",table_schema,"`.`",table_name,"` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;") AS _sql  
             FROM `TABLES`
-            WHERE ' . $tableFilter . '
+            WHERE table_schema LIKE ?
             GROUP BY table_schema, table_name;
         ');
         $commands = array_merge($commands, $rows);
@@ -73,7 +72,7 @@ class CharsetRecodeHelper
         $rows = $this->retrieveCommands('
             SELECT CONCAT("ALTER TABLE `",table_schema,"`.`",table_name, "` CHANGE `",column_name,"` `",column_name,"` ",data_type,"(",character_maximum_length,") CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",IF(is_nullable="YES"," NULL"," NOT NULL"),";") AS _sql 
             FROM `COLUMNS`
-            WHERE ' . $tableFilter . '
+            WHERE table_schema LIKE ?
             AND data_type IN (\'varchar\', \'char\');
         ');
         $commands = array_merge($commands, $rows);
@@ -81,20 +80,28 @@ class CharsetRecodeHelper
         $rows = $this->retrieveCommands('
             SELECT CONCAT("ALTER TABLE `",table_schema,"`.`",table_name, "` CHANGE `",column_name,"` `",column_name,"` ",data_type," CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci",IF(is_nullable="YES"," NULL"," NOT NULL"),";") AS _sql 
             FROM `COLUMNS`
-            WHERE ' . $tableFilter . '
+            WHERE table_schema LIKE ?
             AND data_type IN (\'text\', \'tinytext\', \'mediumtext\', \'longtext\');
         ');
         $commands = array_merge($commands, $rows);
 
-        $this->conn->executeQuery('use ' . $dbName . ';');
+        $this->conn->executeQuery('use ' . $this->conn->getDatabase() . ';');
 
         return $commands;
     }
 
-    private function retrieveCommands(string $sql): array
+    private function retrieveCommands(string $sql, string $dbName): array
     {
         $result = [];
-        $stmt = $this->conn->executeQuery($sql);
+        $stmt = $this->conn->executeQuery(
+            $sql,
+            [
+                $this->conn->getDatabase()
+            ],
+            [
+                ParameterType::STRING
+            ]
+        );
         while ($row = $stmt->fetch()) {
             $result[] = $row['_sql'];
         }
