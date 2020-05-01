4ef48e9c2c472f84ace800a66e962216a9ec4f17
Craig Heydenburg <craigh@mac.com>
2020-05-01 18:51:20 -0400

    correct translation extractor errors

--- src/system/ZAuthModule/Form/Type/EitherLoginType.php
+++ src/system/ZAuthModule/Form/Type/EitherLoginType.php
@@ -20,6 +20,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Translation\Extractor\Annotation\Ignore;
+use Translation\Extractor\Annotation\Translate;
 
 class EitherLoginType extends AbstractType
 {
@@ -35,16 +37,20 @@ class EitherLoginType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $forgotUsername = /** @Translate */'I forgot my username';
+        $forgotPassword = /** @Translate */'I forgot my password';
         $builder
             ->add('either', TextType::class, [
                 'label' => 'User name or email',
-                'help' => '<a href="' . $this->router->generate('zikulazauthmodule_account_lostusername') . '">I forgot my username</a>',
+                /** @Ignore */
+                'help' => '<a href="' . $this->router->generate('zikulazauthmodule_account_lostusername') . '">' . $forgotUsername . '</a>',
                 'help_html' => true,
                 'input_group' => ['left' => '<i class="fas fa-fw fa-sign-in-alt"></i>']
             ])
             ->add('pass', PasswordType::class, [
                 'label' => 'Password',
-                'help' => '<a href="' . $this->router->generate('zikulazauthmodule_account_lostpassword') . '">I forgot my password</a>',
+                /** @Ignore */
+                'help' => '<a href="' . $this->router->generate('zikulazauthmodule_account_lostpassword') . '">' . $forgotPassword . '</a>',
                 'help_html' => true,
                 'input_group' => ['left' => '<i class="fas fa-fw fa-key"></i>']
             ])
