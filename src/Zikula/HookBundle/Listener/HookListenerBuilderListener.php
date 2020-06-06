d0c7e243e6434ce6a93377d2db5dd311121ede66
Craig Heydenburg <craigh@mac.com>
2020-06-06 12:05:25 -0400

    corrections to add listeners to hooks

--- src/Zikula/HookBundle/Listener/HookListenerBuilderListener.php
+++ src/Zikula/HookBundle/Listener/HookListenerBuilderListener.php
@@ -13,18 +13,30 @@ declare(strict_types=1);
 
 namespace Zikula\Bundle\HookBundle\Listener;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Zikula\Bundle\CoreBundle\HttpKernel\ZikulaHttpKernelInterface;
+use Zikula\Bundle\HookBundle\Collector\HookCollectorInterface;
 use Zikula\Bundle\HookBundle\Repository\RepositoryInterface\HookRuntimeRepositoryInterface;
 
 class HookListenerBuilderListener implements EventSubscriberInterface
 {
     /**
-     * @var ContainerInterface
+     * @var ZikulaHttpKernelInterface
      */
-    private $container;
+    private $kernel;
+
+    /**
+     * @var HookCollectorInterface
+     */
+    private $hookCollector;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
 
     /**
      * @var HookRuntimeRepositoryInterface
@@ -37,11 +49,15 @@ class HookListenerBuilderListener implements EventSubscriberInterface
     private $installed;
 
     public function __construct(
-        ContainerInterface $container,
+        ZikulaHttpKernelInterface $kernel,
+        HookCollectorInterface $hookCollector,
+        EventDispatcherInterface $eventDispatcher,
         HookRuntimeRepositoryInterface $hookRuntimeRepository,
         string $installed
     ) {
-        $this->container = $container;
+        $this->kernel = $kernel;
+        $this->hookCollector = $hookCollector;
+        $this->eventDispatcher = $eventDispatcher;
         $this->hookRuntimeRepository = $hookRuntimeRepository;
         $this->installed = '0.0.0' !== $installed;
     }
@@ -66,17 +82,19 @@ class HookListenerBuilderListener implements EventSubscriberInterface
 
         $handlers = $this->hookRuntimeRepository->findAll();
         foreach ($handlers as $handler) {
-            if (!$this->container->get('kernel')->isBundle($handler['powner'])) {
+            if (!$this->kernel->isBundle($handler['powner'])) {
+                continue;
+            }
+            if (!$this->hookCollector->hasProvider($handler['pareaid'])) {
                 continue;
             }
 
-            $callable = [$handler['classname'], $handler['method']];
+            $callable = [$this->hookCollector->getProvider($handler['pareaid']), $handler['method']];
             if (!is_callable($callable)) {
                 continue;
             }
 
-            $callSubject = $this->container->has($handler['classname']) ? $handler['classname'] : $handler['serviceid'];
-            $this->container->get('event_dispatcher')->addListener($handler['eventname'], [$callSubject, $handler['method']]);
+            $this->eventDispatcher->addListener($handler['eventname'], $callable);
         }
     }
 }
