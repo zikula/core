c2cdbfaa5c17d511b2f39ec40b5a25fbda35ef95
Craig Heydenburg <craigh@mac.com>
2020-08-02 20:12:26 -0400

    start to replace our custom parameters with real symfony config

---
+++ src/system/ThemeModule/DependencyInjection/Configuration.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\ThemeModule\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('zikula_theme');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->scalarNode('script_position')->defaultValue('foot')->end()
+                ->arrayNode('asset_manager')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->booleanNode('combine')->defaultValue(false)->end()
+                        ->scalarNode('lifetime')->defaultValue('1 day')->end()
+                        ->booleanNode('compress')->defaultValue(true)->end()
+                        ->booleanNode('minify')->defaultValue(true)->end()
+                    ->end()
+                ->end() // zikula_asset_manager
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/system/ThemeModule/DependencyInjection/ZikulaThemeExtension.php
+++ src/system/ThemeModule/DependencyInjection/ZikulaThemeExtension.php
@@ -17,13 +17,38 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Zikula\ThemeModule\Engine\Asset\CssResolver;
+use Zikula\ThemeModule\Engine\Asset\JsResolver;
+use Zikula\ThemeModule\Engine\Asset\Merger;
+use Zikula\ThemeModule\Engine\AssetFilter;
+use Zikula\ThemeModule\EventListener\ExtensionInstallationListener;
+use Zikula\ThemeModule\EventListener\HookChangeListener;
 
 class ZikulaThemeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
-
         $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->getDefinition(AssetFilter::class)
+            ->setArgument('$scriptPosition', $config['script_position']);
+
+        $container->getDefinition(ExtensionInstallationListener::class)
+            ->setArgument('$mergerActive', $config['asset_manager']['combine']);
+        $container->getDefinition(HookChangeListener::class)
+            ->setArgument('$mergerActive', $config['asset_manager']['combine']);
+        $container->getDefinition(JsResolver::class)
+            ->setArgument('$combine', $config['asset_manager']['combine']);
+        $container->getDefinition(CssResolver::class)
+            ->setArgument('$combine', $config['asset_manager']['combine']);
+
+        $container->getDefinition(Merger::class)
+            ->setArgument('$lifetime', $config['asset_manager']['lifetime'])
+            ->setArgument('$minify', $config['asset_manager']['minify'])
+            ->setArgument('$compress', $config['asset_manager']['compress']);
     }
 }
