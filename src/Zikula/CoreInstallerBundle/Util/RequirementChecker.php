86311677086c352f4286c0e65fc5d56358e6f7a6
Craig Heydenburg <craigh@mac.com>
2020-08-06 08:46:33 -0400

    Revert "does this work?"
    This reverts commit c5c42ff3e49fc12f498c9a92e9ce07e1923a771b.

--- src/Zikula/CoreInstallerBundle/Util/RequirementChecker.php
+++ src/Zikula/CoreInstallerBundle/Util/RequirementChecker.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace Zikula\Bundle\CoreInstallerBundle\Util;
 
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Yaml\Yaml;
 use Zikula\Bundle\CoreBundle\HttpKernel\ZikulaKernel;
 
@@ -29,9 +28,8 @@ class RequirementChecker
      */
     private $installedVersion;
 
-    public function __construct(ParameterBagInterface $parameterBag, string $installed)
+    public function __construct(string $installed)
     {
-        $this->parameters = $parameterBag->all();
         $this->installedVersion = $installed;
     }
 
@@ -43,7 +41,7 @@ class RequirementChecker
     {
         // on install or upgrade, check if system requirements are met.
         if (version_compare($this->installedVersion, ZikulaKernel::VERSION, '<')) {
-//            $this->loadParametersFromFile();
+            $this->loadParametersFromFile();
             $versionChecker = new ZikulaRequirements();
             $versionChecker->runSymfonyChecks($this->parameters);
             if (empty($versionChecker->requirementsErrors)) {
