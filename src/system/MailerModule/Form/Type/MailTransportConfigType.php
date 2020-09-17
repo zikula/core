d957b9173846b1f373418823edeb28b4c972ce66
Guite <info@guite.de>
2020-09-17 20:57:49 +0200

    wording improvement

--- src/system/MailerModule/Form/Type/MailTransportConfigType.php
+++ src/system/MailerModule/Form/Type/MailTransportConfigType.php
@@ -42,7 +42,7 @@ class MailTransportConfigType extends AbstractType
                     'Sendmail binary' => 'sendmail',
                     'Development/debug mode (Do not send any email)' => 'test'/*'null'*/
                 ],
-                'help' => 'How mails are sent and delivered. For example, to use a mail account, choose "SMTP server". Alternatively, instead of using your own SMTP server, you can send emails via a 3rd party provider. For further information about the mailer transport setup please refer to the Symfony docs at %link%. Please do not install any mailer transport using composer, as they are already included in Zikula.',
+                'help' => 'How mails are sent and delivered. For example, to use a mail account, choose "SMTP server". Alternatively, instead of using your own SMTP server, you can send emails via a 3rd party provider. For further information about the mailer transport setup please refer to the Symfony docs %link%. Please do not install any mailer transport using composer, as they are already included in Zikula.',
                 'help_translation_parameters' => [
                     '%link%' => '<a href="https://symfony.com/doc/current/mailer.html#transport-setup" target="_blank">here</a>',
                 ],
