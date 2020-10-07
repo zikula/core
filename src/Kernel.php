264e51dbeaf353547e53a44a736c9d9e463a5d41
Guite <info@guite.de>
2020-10-07 13:55:40 +0200

    add String functions to autoloader

--- src/Kernel.php
+++ src/Kernel.php
@@ -32,9 +32,6 @@ class Kernel extends ZikulaKernel
         parent::__construct($environment, $debug);
 
         $this->databaseUrl = $databaseUrl;
-
-        // remove again after #4352 has been done
-        require_once $this->getProjectDir() . '/vendor/symfony/symfony/src/Symfony/Component/String/Resources/functions.php';
     }
 
     public function registerBundles(): iterable
