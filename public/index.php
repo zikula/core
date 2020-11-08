e3bbfd6d22b4a5ad8e4437c3af7da2c31894104b
Guite <info@guite.de>
2020-11-08 17:43:05 +0100

    remove trusted proxies/hosts from index, following recipe

--- public/index.php
+++ public/index.php
@@ -22,14 +22,6 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG'], $_SERVER['DATABASE_URL']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
