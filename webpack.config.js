a1655a45796a83f6e8834eebc43a9b77053e0086
Craig Heydenburg <craigh@mac.com>
2020-12-26 14:08:47 -0500

    correct webpack.config.js publicPath to default

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/core.git/public/build')
+    .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
