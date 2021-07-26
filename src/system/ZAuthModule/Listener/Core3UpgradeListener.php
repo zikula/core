3ea87f6fcc14ae26bb4a27786a17102858473cef
Axel Guckelsberger <info@guite.de>
2021-07-26 07:50:00 +0200

    fix cp error

--- src/system/ZAuthModule/Listener/Core3UpgradeListener.php
+++ src/system/ZAuthModule/Listener/Core3UpgradeListener.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Zikula\ZAuthModule\EventListener;
+namespace Zikula\ZAuthModule\Listener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Zikula\Bundle\CoreInstallerBundle\Event\CoreUpgradePreExtensionUpgrade;
