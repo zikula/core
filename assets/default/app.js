c0dd1e5df7082066dc2ed8d40939dcf3ec2a54e8
Kaik <kapitanogor@googlemail.com>
2021-12-14 22:59:17 +0100

    Webpack add legacy packages and default configuration

---
+++ assets/default/app.js
@@ -0,0 +1,19 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+// bootstrap5
+// require('bootstrap5');
+// bootswatch5
+// FA 5
+// require('@fortawesome/fontawesome-free/css/all.min.css');
+// require('@fortawesome/fontawesome-free/js/all.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -6,11 +7,90 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+/**
+ * Legacy Zikula packages - @deprecated remove with 4x
+ */
+const copyPaths = [
+    // legacy jquery
+    {source: './node_modules/jquery/dist', destination: './public/jquery'},
+    {source: './node_modules/jquery/dist/jquery.js', destination: './public/jquery/jquery-built.js'},
+    // legacy jquery-ui
+    {source: './node_modules/components-jqueryui/themes', destination: './public/jqueryui/themes'},
+    {source: './node_modules/components-jqueryui/ui', destination: './public/jqueryui/ui'},
+    {source: './node_modules/components-jqueryui/jquery-ui.js', destination: './public/jqueryui/jquery-ui-built.js'},
+    // legacy bootstrap 4
+    {source: './node_modules/bootstrap4/dist', destination: './public/bootstrap'},
+    {source: './node_modules/bootstrap4/dist/js/bootstrap.js', destination: './public/bootstrap/bootstrap-built.js'},
+    // bootswatch 4
+    {source: './node_modules/bootswatch4', destination: './public/bootswatch'},        
+    // jstree
+    {source: './node_modules/jstree/dist', destination: './public/jstree/dist'},
+    {source: './node_modules/jstree/dist/jstree.js', destination: './public/jstree/jstree-built.js'},
+    {source: './node_modules/jstree/dist/themes/default/style.css', destination: './public/jstree/jstree-built.css'},
+    // FA
+    {source: './node_modules/@fortawesome/fontawesome-free/css', destination: './public/font-awesome/css'},
+    {source: './node_modules/@fortawesome/fontawesome-free/webfonts', destination: './public/font-awesome/webfonts'},
+    {source: './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css', destination: './public/font-awesome/font-awesome-built.css'},
+    // MMenu
+    {source: './node_modules/mmenu-js/dist/mmenu.js', destination: './public/mmenu/js/mmenu.js'},
+    {source: './node_modules/mmenu-js/dist/mmenu.css', destination: './public/mmenu/css/mmenu.css'},
+    // iconpicker
+    {source: './node_modules/fontawesome-iconpicker/dist/css/fontawesome-iconpicker.css', destination: './public/fontawesome-iconpicker/fontawesome-iconpicker.css'},
+    {source: './node_modules/fontawesome-iconpicker/dist/css/fontawesome-iconpicker.min.css', destination: './public/fontawesome-iconpicker/fontawesome-iconpicker.min.css'},
+    {source: './node_modules/fontawesome-iconpicker/dist/js/fontawesome-iconpicker.js', destination: './public/fontawesome-iconpicker/fontawesome-iconpicker.js'},
+    {source: './node_modules/fontawesome-iconpicker/dist/js/fontawesome-iconpicker.min.js', destination: './public/fontawesome-iconpicker/fontawesome-iconpicker.min.js'},
+    // magnific-popup
+    {source: './node_modules/magnific-popup/dist', destination: './public/magnific-popup'},
+];
+
+/**
+ * Currently default config - @deprecated remove with 4x
+ */
 Encore
-    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .enableStimulusBridge('./assets/controllers.json')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .addPlugin(new FileManagerPlugin({
+        events: {
+            onEnd: {
+                copy: copyPaths,
+            }
+        }
+    }))
+;
+
+// build the first configuration
+// https://symfony.com/doc/5.4/frontend/encore/advanced-config.html#defining-multiple-webpack-configurations
+const legacyConfig = Encore.getWebpackConfig();
+// Set a unique name for the config (needed later!)
+legacyConfig.name = 'legacyConfig';
+
+// reset Encore to build the second config
+Encore.reset();
+
+/**
+ *  Core 4 default config
+ */
+ Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/default')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build/default')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -20,10 +100,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/default/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    .enableStimulusBridge('./assets/default/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -56,7 +136,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    // .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -70,6 +150,10 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
 ;
 
-module.exports = Encore.getWebpackConfig();
+const defaultConfig = Encore.getWebpackConfig();
+defaultConfig.name = 'defaultConfig';
+
+module.exports = [legacyConfig, defaultConfig];
