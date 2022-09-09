855ffd13036f2c84e3abfc735e2a40a812d940e2
Axel Guckelsberger <info@guite.de>
2022-09-09 22:05:01 +0200

    cs

--- src/system/UsersBundle/Controller/UserCrudController.php
+++ src/system/UsersBundle/Controller/UserCrudController.php
@@ -1,10 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Zikula\UsersBundle\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use Zikula\UsersBundle\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Zikula\UsersBundle\Entity\User;
 
 class UserCrudController extends AbstractCrudController
 {
