58321be7ccea7267ba71087e69358f006b7181c5
Axel Guckelsberger <info@guite.de>
2025-05-14 21:02:40 +0200

    cs

--- php_cs_fixer.dist.php
+++ php_cs_fixer.dist.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 if (!file_exists(__DIR__)) {
--- src/system/ThemeBundle/config/definition.php
+++ src/system/ThemeBundle/config/definition.php
@@ -13,8 +13,7 @@ declare(strict_types=1);
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 
-return static function (DefinitionConfigurator $definition): void
-{
+return static function (DefinitionConfigurator $definition): void {
     $definition->rootNode()
         ->children()
             ->arrayNode('user_dashboard')
