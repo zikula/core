6053e1d53c614e0df900d5b4e957a056da42e37b
Axel Guckelsberger <info@guite.de>
2025-08-28 07:04:34 +0200

    fix test

--- src/system/CoreBundle/tests/Api/LocaleApiTest.php
+++ src/system/CoreBundle/tests/Api/LocaleApiTest.php
@@ -16,7 +16,7 @@ namespace Zikula\CoreBundle\Tests\Api;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Zikula\CoreBundle\Api\LocaleApi;
-use Zikula\CoreBundle\ApiInterface\LocaleApiInterface;
+use Zikula\CoreBundle\Api\ApiInterface\LocaleApiInterface;
 
 class LocaleApiTest extends TestCase
 {
