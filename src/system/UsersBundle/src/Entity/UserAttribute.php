67c1e37c89a9a57f16de95e8c3893039ee553930
Axel Guckelsberger <info@guite.de>
2025-12-01 09:05:12 +0100

    declare Stringable implementation

--- src/system/UsersBundle/src/Entity/UserAttribute.php
+++ src/system/UsersBundle/src/Entity/UserAttribute.php
@@ -22,7 +22,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'nucleos_user__user_attribute')]
-class UserAttribute
+class UserAttribute implements \Stringable
 {
     #[ORM\Id]
     #[ORM\ManyToOne(inversedBy: 'attributes')]
